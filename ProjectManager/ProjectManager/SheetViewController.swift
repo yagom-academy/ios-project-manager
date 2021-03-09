a95af4966c95c3c0f4d42368b5d8be319219f6a0
innieminnie <rkddlsl1001@gmail.com>
2021-03-09 13:48:48 +0900

    [#3] chores: SheetViewController.swift로 파일 분리

---
+++ ProjectManager/ProjectManager/SheetViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SheetViewController.swift
+//  ProjectManager
+//
+//  Created by 강인희 on 2021/03/09.
+//
+
+import UIKit
+
+class SheetViewController: UIViewController {
+    @IBOutlet weak var modeButtonItem: UIBarButtonItem!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var deadlineDatePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    static let identifier = "SheetViewController"
+    
+    private enum Mode {
+        static let cancel = "Cancel"
+        static let edit = "Edit"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.modeButtonItem.title = Mode.cancel
+    }
+    
+    @IBAction func tappedDoneButton(_ sender: Any) {
+        // 새로운 글 등록 or 기존의 글 수정 -> 내용 저장하기
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tappedModeButton(_ sender: UIBarButtonItem) {
+        if sender.title == Mode.cancel {
+            self.dismiss(animated: true, completion: nil)
+        } else {
+            sender.title = Mode.cancel
+            //기존의 글 수정 및 Done -> update 내용 저장하기
+        }
+    }
+
+}
--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -88,36 +88,3 @@ extension CollectionViewCell: UITableViewDataSource {
 class Board: UITableView {
 
 }
-
-class SheetViewController: UIViewController {
-    @IBOutlet weak var modeButtonItem: UIBarButtonItem!
-    @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var deadlineDatePicker: UIDatePicker!
-    @IBOutlet weak var descriptionTextView: UITextView!
-    
-    static let identifier = "SheetViewController"
-    
-    private enum Mode {
-        static let cancel = "Cancel"
-        static let edit = "Edit"
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.modeButtonItem.title = Mode.cancel
-    }
-    
-    @IBAction func tappedDoneButton(_ sender: Any) {
-        // 새로운 글 등록 or 기존의 글 수정 -> 내용 저장하기
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func tappedModeButton(_ sender: UIBarButtonItem) {
-        if sender.title == Mode.cancel {
-            self.dismiss(animated: true, completion: nil)
-        } else {
-            sender.title = Mode.cancel
-            //기존의 글 수정 및 Done -> update 내용 저장하기
-        }
-    }
-}
