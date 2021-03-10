c9dbf139b6db8ee8bffd0a5195f60bacd8e79057
lina0322 <llim@student.42seoul.kr>
2021-03-10 23:37:01 +0900

    Feat #3 - label 생성하는 함수 구현 및 적용

--- ProjectManager/ProjectManager/ThingTableViewCell.swift
+++ ProjectManager/ProjectManager/ThingTableViewCell.swift
@@ -14,4 +14,21 @@ class ThingTableViewCell: UITableViewCell {
     static var identifier: String {
         return "\(self)"
     }
+    var isDone: Bool = false
+
+    // MARK: - Outlet
+    
+    private let titleLabel: UILabel = makeLabel()
+    private let bodyLabel: UILabel = makeLabel()
+    private let dateLabel: UILabel = makeLabel()
+    
+    // MARK: - UI
+    
+    static private func makeLabel(textColor: UIColor = .black, textSize: UIFont.TextStyle = .body) -> UILabel {
+        let label = UILabel()
+        label.textColor = textColor
+        label.font = .preferredFont(forTextStyle: textSize)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
 }
