f0b2667c9abb6b6122b987bfdd39fe64743713b6
lina0322 <llim@student.42seoul.kr>
2021-03-10 23:25:14 +0900

    Feat #2 - 사이 간격에 색 들어가게 stackView에 넣음

--- ProjectManager/ProjectManager/MainViewController.swift
+++ ProjectManager/ProjectManager/MainViewController.swift
@@ -8,17 +8,44 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - Outlet
+    
     lazy var todoTableView = makeTableView()
     lazy var doingTableView = makeTableView()
     lazy var doneTableView = makeTableView()
 
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureConstratins()
     }
 
+    // MARK: - UI
+    
     private func makeTableView() -> UITableView {
         let tableView = UITableView()
         return tableView
+    }
+    
+    private func configureConstratins() {
+        let safeArea = view.safeAreaLayoutGuide
+        let stackView = UIStackView(arrangedSubviews: [todoTableView, doingTableView, doneTableView])
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.backgroundColor = .systemGray3
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.backgroundColor = .white
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
     }
 }
 
