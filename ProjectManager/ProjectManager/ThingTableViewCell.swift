e1e41bdb29f3131e6433e7cba894a0eb4dfdf33b
lina0322 <llim@student.42seoul.kr>
2021-03-10 23:31:48 +0900

    Add #3 cell 생성 및 indentifier 설정

---
+++ ProjectManager/ProjectManager/ThingTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ThingTableViewCell.swift
+//  ProjectManager
+//
+//  Created by 리나 on 2021/03/10.
+//
+
+import UIKit
+
+class ThingTableViewCell: UITableViewCell {
+
+    // MARK: - Property
+    
+    static var identifier: String {
+        return "\(self)"
+    }
+}
