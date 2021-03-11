b009b3b58cb4f678e4dc42aa32b893f36ead583e
lina0322 <llim@student.42seoul.kr>
2021-03-11 18:01:41 +0900

    Style - headerView 코드 정리

--- ProjectManager/ProjectManager/Controller/ThingTableHeaderView.swift
+++ ProjectManager/ProjectManager/Controller/ThingTableHeaderView.swift
@@ -18,15 +18,18 @@ class ThingTableHeaderView: UIView {
     
     init(height: Int, tableViewType: TableViewType) {
         super.init(frame: CGRect(x: 0, y: 0, width: 0, height: height))
-        
+        titleLabel.text = tableViewType.rawValue
         configureConstraints()
         setStyle()
-        titleLabel.text = tableViewType.rawValue
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        configureConstraints()
+        setStyle()
     }
+    
+    // MARK: - UI
     
     private func configureConstraints() {
         addSubview(titleLabel)
@@ -34,24 +37,27 @@ class ThingTableHeaderView: UIView {
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         countLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
-        countLabel.widthAnchor.constraint(equalToConstant: 20).isActive = true
-        countLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        countLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        countLabel.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 5).isActive = true
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            
+            countLabel.widthAnchor.constraint(equalToConstant: 20),
+            countLabel.heightAnchor.constraint(equalToConstant: 20),
+            countLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            countLabel.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 5)
+        ])
     }
     
     private func setStyle() {
-        titleLabel.font = UIFont.preferredFont(forTextStyle: .title2)
-        countLabel.font = UIFont.systemFont(ofSize: 15)
+        backgroundColor = .systemGray5
+        titleLabel.font = .preferredFont(forTextStyle: .title2)
+        countLabel.font = .systemFont(ofSize: 15)
         countLabel.textColor = .white
         countLabel.textAlignment = .center
         countLabel.backgroundColor = .black
-        // TODO: 자리수가 넘어가도 원형이 잘 유지되는지?
         countLabel.layer.cornerRadius = 10
         countLabel.layer.masksToBounds = true
-        backgroundColor = .systemGray5
     }
     
     func setCount(_ count: Int) {
