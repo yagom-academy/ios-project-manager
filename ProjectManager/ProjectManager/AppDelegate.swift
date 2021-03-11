ad237fa09020410383c23ff3066c1e881acbb2ea
SM <hcooch2ch3@gmail.com>
2021-03-11 15:26:40 +0900

    Remove - storyboard 삭제

--- ProjectManager/ProjectManager/AppDelegate.swift
+++ ProjectManager/ProjectManager/AppDelegate.swift
@@ -9,10 +9,12 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window?.makeKeyAndVisible()
         return true
     }
 
