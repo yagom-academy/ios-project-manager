fbe201b6182735d444614723ea16d3bda629ac33
SM <hcooch2ch3@gmail.com>
2021-03-12 17:39:43 +0900

    Add - TableView group 및 ThingTableView.swift 파일 추가

---
+++ ProjectManager/ProjectManager/Controller/TableView/ThingTableView.swift
@@ -0,0 +1,20 @@
+//
+//  ThingTableView.swift
+//  ProjectManager
+//
+//  Created by 임성민 on 2021/03/12.
+//
+
+import UIKit
+
+class ThingTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
