6acef8a77029d1e8a6f65dff1ee5f62ff781c20c
SM <hcooch2ch3@gmail.com>
2021-03-12 23:59:58 +0900

    Feat - ThingTableView 구현

--- ProjectManager/ProjectManager/Controller/TableView/ThingTableView.swift
+++ ProjectManager/ProjectManager/Controller/TableView/ThingTableView.swift
@@ -7,14 +7,27 @@
 
 import UIKit
 
-class ThingTableView: UITableView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class ThingTableView: UITableView, Draggable, Droppable {
+    var tableViewType: TableViewType
+    
+    init<T: UIViewController>(tableViewType: TableViewType, mainViewController: T) where T: UITableViewDataSource & UITableViewDelegate & UITableViewDragDelegate & UITableViewDropDelegate {
+        self.tableViewType = tableViewType
+        super.init(frame: .zero, style: .grouped)
+        tableHeaderView = ThingTableHeaderView(height: 50, tableViewType: tableViewType)
+        dataSource = mainViewController
+        delegate = mainViewController
+        dragDelegate = mainViewController
+        dropDelegate = mainViewController
+        register(cellType: ThingTableViewCell.self)
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setCount(_ count: Int) {
+        if let tableHeaderView = self.tableHeaderView as? ThingTableHeaderView {
+            tableHeaderView.setCount(count)
+        }
+    }
 }
