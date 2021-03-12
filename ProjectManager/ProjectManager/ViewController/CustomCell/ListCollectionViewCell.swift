9f4c12b81b2b3dbf22a764154af12fc14e6521ee
odong-Tree <ss12357@naver.com>
2021-03-12 13:30:22 +0900

    Feat: 스와이프로 셀 삭제하는 기능 구현 (#9)

--- ProjectManager/ProjectManager/ViewController/CustomCell/ListCollectionViewCell.swift
+++ ProjectManager/ProjectManager/ViewController/CustomCell/ListCollectionViewCell.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ListCollectionViewCell: UICollectionViewCell {
+    var itemList = [Todo]()
+    var todo1 = Todo(title: "무야호", description: "그만큼 기분이 좋으시다는거지.", deadLine: Date())
+    var todo2 = Todo(title: "UI 만들기", description: "잘 하고 있어....!!!잘 하고 있어....!!!잘 하고 있어....!!!잘 하고 있어....!!!잘 하고 있어....!!!잘 하고 있어....!!!", deadLine: Date())
     static var identifier: String {
         return "\(self)"
     }
@@ -26,6 +29,8 @@ class ListCollectionViewCell: UICollectionViewCell {
         delegateDelegate()
         setUpContentView()
         configureAutoLayout()
+        
+        itemList.append(contentsOf: [todo1, todo2])
     }
     
     required init?(coder: NSCoder) {
@@ -60,15 +65,22 @@ class ListCollectionViewCell: UICollectionViewCell {
 // MARK: - TableView DataSource
 extension ListCollectionViewCell: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return itemList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ListItemTableViewCell.identifier, for: indexPath) as? ListItemTableViewCell else {
             return UITableViewCell()
         }
-        let todo = Todo(title: "지금 시간", description: "우리 커밋하고 있어여", deadLine: Date())
+        let todo = itemList[indexPath.row]
         cell.fillLabelsText(item: todo)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+                self.itemList.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
     }
 }
