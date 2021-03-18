1e6370277b1e5601a389913553da44aaa103b908
SM <hcooch2ch3@gmail.com>
2021-03-18 19:18:41 +0900

    Feat - CoreDataStack 구현

--- ProjectManager/ProjectManager/Model/CoreDataStack.swift
+++ ProjectManager/ProjectManager/Model/CoreDataStack.swift
@@ -5,4 +5,31 @@
 //  Created by 임성민 on 2021/03/18.
 //
 
+import CoreData
 import Foundation
+
+final class CoreDataStack {
+    static let shared: CoreDataStack = CoreDataStack()
+    
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
+        let container = NSPersistentCloudKitContainer(name: "Thing")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
