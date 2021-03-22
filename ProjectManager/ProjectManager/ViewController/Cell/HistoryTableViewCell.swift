388f91e420e3771ac9b56aca3de83c9032b1b6bd
GREEN <humains29@gmail.com>
2021-03-22 11:57:00 +0900

    [#15] feat: HistoryTableViewCell 생성 및 구현

---
+++ ProjectManager/ProjectManager/ViewController/Cell/HistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+    static let identifier = "HistoryCell"
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    
+    func configureLabel(with index: Int) {
+        self.title.text = historyManager.historyContainer[index].0
+        self.subTitle.text = convertDateToString(date: historyManager.historyContainer[index].1)
+    }
+    
+    func convertDateToString(date: Date) -> String {
+        let convertedDate: String
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        convertedDate = dateFormatter.string(from: date)
+        
+        return convertedDate
+    }
+}
