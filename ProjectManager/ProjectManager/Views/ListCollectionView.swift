019e412fd521f5896777b9f88724eac5c8d450ef
Joons <elddy0948@gmail.com>
2021-03-30 01:07:27 +0900

    Feat: Add ListCollectionView.swift File

---
+++ ProjectManager/ProjectManager/Views/ListCollectionView.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ListCollectionView: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
