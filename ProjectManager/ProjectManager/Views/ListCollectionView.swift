4be525b90a4523699801617746e7687e68aca644
Joons <elddy0948@gmail.com>
2021-03-30 01:21:40 +0900

    Feat: configureLayout() 메서드 구현

--- ProjectManager/ProjectManager/Views/ListCollectionView.swift
+++ ProjectManager/ProjectManager/Views/ListCollectionView.swift
@@ -4,10 +4,27 @@ class ListCollectionView: UICollectionView {
     
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
-        
+        configureLayout()
+        configureDataSource()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureLayout() {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        self.collectionViewLayout = layout
+    }
+    
+    func configureDataSource() {
+        
     }
 }
