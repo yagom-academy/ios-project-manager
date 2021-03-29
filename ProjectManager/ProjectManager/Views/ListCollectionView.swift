df521a6c77ff80f1830e512f29f9190f72dc69b4
Joons <elddy0948@gmail.com>
2021-03-30 02:12:44 +0900

    Feat: CollectionView에 Header 등록 및 생성

--- ProjectManager/ProjectManager/Views/ListCollectionView.swift
+++ ProjectManager/ProjectManager/Views/ListCollectionView.swift
@@ -19,6 +19,7 @@ class ListCollectionView: UICollectionView {
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
         register(ItemCell.self, forCellWithReuseIdentifier: ItemCell.reuseIdentifier)
+        register(HeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderView.reuseIdentifier)
         backgroundColor = .systemBackground
         configureLayout()
         configureDataSource()
@@ -32,10 +33,16 @@ class ListCollectionView: UICollectionView {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0)
+        
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(36))
         let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
         
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(36))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        
         let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [sectionHeader]
+        
         let layout = UICollectionViewCompositionalLayout(section: section)
         self.collectionViewLayout = layout
     }
@@ -44,6 +51,14 @@ class ListCollectionView: UICollectionView {
         var initialSnapShot = NSDiffableDataSourceSnapshot<Section, Int>()
         initialSnapShot.appendSections([.main])
         initialSnapShot.appendItems(Array(1...7))
+        
+        diffableDataSource.supplementaryViewProvider = { (collectionView: UICollectionView, kind: String, indexPath: IndexPath) -> UICollectionReusableView? in
+            guard let supplementaryView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: HeaderView.reuseIdentifier, for: indexPath) as? HeaderView else {
+                return nil
+            }
+            supplementaryView.configure(headerName: "Header")
+            return supplementaryView
+        }
 
         diffableDataSource.apply(initialSnapShot, animatingDifferences: false)
     }
