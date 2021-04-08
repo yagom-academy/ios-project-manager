1c4ff50047ace7dceab9c07cdc2a04dc61c2228c
taetae <taetae.dev@gmail.com>
2021-04-08 21:47:21 +0900

    refactor: 잘못된 커밋으로 인한 재 커밋

--- ProjectManager/ProjectManager/Model/Todo.swift
+++ ProjectManager/ProjectManager/Model/Todo.swift
@@ -2,7 +2,19 @@
 //  Todo.swift
 //  ProjectManager
 //
-//  Created by 김태형 on 2021/04/08.
+//  Created by 김태형 on 2021/03/29.
 //
-
 import Foundation
+
+struct Todo: Codable {
+    let title: String
+    let description: String
+    let deadline: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case title, description, deadline
+    }
+    var convertedDate: String {
+        return DateFormatter().convertToLocaleDate(deadline)
+    }
+}
