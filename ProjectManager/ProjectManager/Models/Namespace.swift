e69633ed709f666caed200577b8ac236d74606bf
Glenn <be9.smile@gmail.com>
2021-04-14 17:09:22 +0900

    Feat #5 - Namespace 생성

---
+++ ProjectManager/ProjectManager/Models/Namespace.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum PopOverNavigationItems {
+    static let navigationTitle = "TODO"
+    static let doneButton = "Done"
+    static let cancelButton = "Cancel"
+    static let editButton = "Edit"
+}
