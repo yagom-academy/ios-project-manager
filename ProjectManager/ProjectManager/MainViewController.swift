651871345ad71d4f172f7a3cf8670cd584ba8f68
zdodev <iver0945@gmail.com>
2021-04-16 11:15:25 +0900

    feat: 컬렉션 뷰 생성 및 헤더 아이템 생성.

--- ProjectManager/ProjectManager/MainViewController.swift
+++ ProjectManager/ProjectManager/MainViewController.swift
@@ -1,12 +1,45 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    private var todoCollectionView: UICollectionView!
+    private var doingCollectionView: UICollectionView!
+    private var doneCollectionView: UICollectionView!
+    
+    private var todoDataSource: UICollectionViewDiffableDataSource<HeaderItem, Item>!
+    private var doingDataSource: UICollectionViewDiffableDataSource<HeaderItem, Item>!
+    private var doneDataSource: UICollectionViewDiffableDataSource<HeaderItem, Item>!
+    
+    private var todoSnapshot = NSDiffableDataSourceSnapshot<HeaderItem, Item>()
+    
+    private var todoHeaderItem = [
+        HeaderItem(title: "todo", items: [
+            Item(title: "나는 최고다.", description: "정말 최고다.", date: "2021-01-01"),
+            Item(title: "너는 최고다.", description: "너무 최고다.", date: "2021-01-01"),
+            Item(title: "우리는 최고다.", description: "진짜 최고다.", date: "2021-01-01"),
+        ])
+    ]
+    
+    private var doingHeaderItem = [
+        HeaderItem(title: "doing", items: [
+            Item(title: "나는 최고다.", description: "정말 최고다.", date: "2021-01-01"),
+            Item(title: "너는 최고다.", description: "너무 최고다.", date: "2021-01-01"),
+            Item(title: "우리는 최고다.", description: "진짜 최고다.", date: "2021-01-01"),
+            Item(title: "전설의 시작.", description: "zdo", date: "2021-01-01"),
+        ])
+    ]
+
+    private var doneHeaderItem = [
+        HeaderItem(title: "done", items: [
+            Item(title: "나는 최고다.", description: "정말 최고다.", date: "2021-01-01"),
+            Item(title: "너는 최고다.", description: "너무 최고다.", date: "2021-01-01"),
+            Item(title: "우리는 최고다.", description: "진짜 최고다.", date: "2021-01-01"),
+            Item(title: "Let's get it!.", description: "zdo", date: "2021-01-01"),
+            Item(title: "Lin step1이 많이 늦었습니다.😅", description: "😎", date: "2021-01-01"),
+        ])
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
