aa5ae2074936fc14481f953cd7e1876532de3a76
dacodaco <imochi@naver.com>
2021-06-28 17:29:22 +0900

    [#1]docs: Practice 프로젝트 추가

---
+++ Practice/AlanPractice/Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by 김찬우 on 2021/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
