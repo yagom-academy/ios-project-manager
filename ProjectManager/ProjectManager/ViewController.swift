323b728cbf55768c624715d3825eac35e77ef022
DragonTigerXYZ <yb1229@kpu.ac.kr>
2021-06-29 15:28:24 +0900

    [#2]feat: rootView backgroundColor 변경(White)

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -12,19 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let testLabel = UILabel()
-        testLabel.text = "프로젝트 관리앱 코드로 빈화면 만들기"
-        
-        view.addSubview(testLabel)
-        
-        let safeArea = view.safeAreaLayoutGuide
-        
-        testLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        testLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
-
+        view.backgroundColor = .white
     }
-
-
 }
 
