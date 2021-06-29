3af2199ec7f491bb16058f7f88bf37abffa260a6
DragonTigerXYZ <yb1229@kpu.ac.kr>
2021-06-29 15:25:04 +0900

    [#2]feat: SceneDelegate를 통한 rootView 지정.

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -4,13 +4,25 @@
 //  Copyright © yagom. All rights reserved.
 // 
 
+import SwiftUI
 import UIKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let testLabel = UILabel()
+        testLabel.text = "프로젝트 관리앱 코드로 빈화면 만들기"
+        
+        view.addSubview(testLabel)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        testLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+
     }
 
 
