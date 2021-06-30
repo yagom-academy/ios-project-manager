f22345db549c1eb2ead332b8f2f65967fdee947a
Ian <isnn0103@gmail.com>
2021-06-30 17:45:33 +0900

    [#1]feat : DoingTableViewCell 구현

---
+++ ProjectManager/ProjectManager/DoingTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DoingTableViewCell.swift
+//  ProjectManager
+//
+//  Created by 이성노 on 2021/06/29.
+//
+
+import UIKit
+
+class DoingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
