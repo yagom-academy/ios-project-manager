f067ee27d0f42e546d09d54216fa1b2913b70e60
DragonTigerXYZ <yb1229@kpu.ac.kr>
2021-06-30 11:08:27 +0900

    [#5]feat: 상품등록화면 레이아웃 구성

--- ProjectManager/ProjectManager/ViewControllers/RegisterViewController.swift
+++ ProjectManager/ProjectManager/ViewControllers/RegisterViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    let stackView: UIStackView = {
+        let myStackView = UIStackView()
+        myStackView.axis = .vertical
+        myStackView.alignment = .fill
+        
+        return myStackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -21,6 +29,38 @@ class RegisterViewController: UIViewController {
         navigationItem.leftBarButtonItem = leftButton
         navigationItem.rightBarButtonItem = rightButton
         
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            stackView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 1),
+            stackView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 1)
+        ])
+        
+//        stackView.backgroundColor = .green
+        
+        let title = UILabel()
+        let datePicker = UIDatePicker()
+        let textField = UITextField()
+        
+        title.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        title.backgroundColor = .yellow
+        textField.backgroundColor = .green
+
+        stackView.addArrangedSubview(title)
+        stackView.addArrangedSubview(datePicker)
+        stackView.addArrangedSubview(textField)
+        
+        NSLayoutConstraint.activate([
+            title.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.1),
+            title.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1)
+        ])
+
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
     }
 }
