6bf333dfe66c47555de5393d51fcecd332a5e222
dacodaco <imochi@naver.com>
2021-06-30 20:35:28 +0900

    [#9]feat: TODOModel 타입 구현

---
+++ ProjectManager/ProjectManager/Models/TODOModel.swift
@@ -0,0 +1,14 @@
+//
+//  TODOModel.swift
+//  ProjectManager
+//
+//  Created by 김찬우 on 2021/06/30.
+//
+
+import Foundation
+
+struct TODOModel {
+    let title: String
+    let deadline: Date
+    let description: String
+}
