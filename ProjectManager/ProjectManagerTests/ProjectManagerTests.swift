c51fa266f67677a4b94879106aff0908d5ed9cc0
dacodaco <imochi@naver.com>
2021-06-30 20:43:54 +0900

    [#10]docs: 테스트 파일 생성

---
+++ ProjectManager/ProjectManagerTests/ProjectManagerTests.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectManagerTests.swift
+//  ProjectManagerTests
+//
+//  Created by 김찬우 on 2021/06/30.
+//
+
+import XCTest
+@testable import ProjectManager
+
+class ProjectManagerTests: XCTestCase {
+    
+}
