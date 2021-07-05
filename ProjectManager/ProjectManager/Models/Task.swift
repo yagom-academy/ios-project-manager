232ee7fb254bbcf17d8e3f7b37a8520b49618719
dacodaco <imochi@naver.com>
2021-07-05 21:06:10 +0900

    [#7]object 전달을 위한 프로토콜 채택

--- ProjectManager/ProjectManager/Models/Task.swift
+++ ProjectManager/ProjectManager/Models/Task.swift
@@ -6,11 +6,58 @@
 //
 
 import Foundation
+import MobileCoreServices
 
-struct Task: Codable {
+final class Task: NSObject, Codable {
     var title: String
     var date: Double
-    var description: String
+    var myDescription: String
     var status: String
     let identifier: String
+    
+    init(title: String, date: Double, myDescription: String, status: String, identifier: String){
+        self.title = title
+        self.date = date
+        self.myDescription = myDescription
+        self.status = status
+        self.identifier = identifier
+    }
+}
+
+extension Task: NSItemProviderWriting {
+    static var writableTypeIdentifiersForItemProvider: [String] {
+        return [(kUTTypeData) as String]
+    }
+    
+    func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping (Data?, Error?) -> Void) -> Progress? {
+        
+        let progress = Progress(totalUnitCount: 100)
+        
+            do {
+              let data = try JSONEncoder().encode(self)
+                progress.completedUnitCount = 100
+                completionHandler(data, nil)
+            } catch {
+              completionHandler(nil, error)
+            }
+        
+          return progress
+    }
+}
+
+extension Task: NSItemProviderReading {
+    static var readableTypeIdentifiersForItemProvider: [String] {
+        return [(kUTTypeData) as String]
+    }
+    
+    static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Task {
+        let decoder = JSONDecoder()
+        
+        do {
+            let task = try decoder.decode(Task.self, from: data)
+            return task
+        } catch {
+            fatalError("Err")
+        }
+    }
 }
