331bd3481eaf6db7dede5cc77331c4fad566dd50
LeeSungNo-ian <isnn0103@gmail.com>
2021-07-14 17:09:28 +0900

    [#1]feat: protocol TableViewReloadable 생성

---
+++ ProjectManager/ProjectManager/Protocols/TableViewReloadable.swift
@@ -0,0 +1,12 @@
+//
+//  ViewReloadable.swift
+//  ProjectManager
+//
+//  Created by 이성노 on 2021/07/14.
+//
+
+import Foundation
+
+protocol TableViewReloadable {
+    func reloadToDoTableView()
+}
