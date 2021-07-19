b2662fba86bc37b52c5c0a99cf02beab6bb5c8f1
Neph <tony3322@naver.com>
2021-07-19 17:26:51 +0900

    [Feat] Entry Point 관련 설정

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -8,4 +8,12 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController = UINavigationController(rootViewController: KanBanBoardViewController())
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+    }
 }
