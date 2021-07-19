ffb4529ef43e760dfd4fd5123e5a03ea79680232
Neph <tony3322@naver.com>
2021-07-19 14:58:54 +0900

    [Chore] SwiftLint, Alamofire 설정

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -5,14 +5,11 @@
 // 
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
