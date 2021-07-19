0eed8c0978fca8ce6e88c160f44e8685919710a3
Neph <tony3322@naver.com>
2021-07-19 17:44:06 +0900

    [Fix] 스토리보드 삭제 시 설정해주지 않았던 부분 수정

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -10,10 +10,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
-        
-        let rootViewController = UINavigationController(rootViewController: KanBanBoardViewController())
-        window?.rootViewController = rootViewController
-        window?.makeKeyAndVisible()
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let rootViewController = UINavigationController(rootViewController: KanBanBoardViewController(nibName: nil, bundle: nil))
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 }
