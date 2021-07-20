695808848b342c37a1e4320cbd08bb5414079f3a
duckbok <duckbok22@gmail.com>
2021-07-20 22:15:31 +0900

    feat: TaskViewModel state별 task 불러오기
    Fixes: #3

---
+++ ProjectManager/ProjectManager/Sources/ViewModels/TaskViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  TaskViewModel.swift
+//  ProjectManager
+//
+//  Created by duckbok, Ryan-Son on 2021/07/20.
+//
+
+import Foundation
+
+struct TaskViewModel {
+
+    private let repository = TaskRepository()
+    private var taskOrder: [Task.State: [UUID]] = [:]
+    private var tasks: [Task] = []
+
+    mutating func fetchTasks() {
+        repository.fetchTasks { result in
+            switch result {
+            case .success(let tasks):
+                self.tasks = tasks
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    func task(from state: Task.State, at index: Int) -> Task? {
+        guard let stateOrder: [UUID] = taskOrder[state],
+              index < stateOrder.count else { return nil }
+        let taskID = stateOrder[index]
+
+        return task(by: taskID)
+    }
+
+    private func task(by id: UUID) -> Task? {
+        return tasks.filter { $0.id == id }.first
+    }
+}
