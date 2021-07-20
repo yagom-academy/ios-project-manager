96fa205ecb375dde9d772b7e462d9ecd350603ff
Neph <tony3322@naver.com>
2021-07-20 09:34:50 +0900

    [Feat] 섹션 헤더 및 swipe action 구현

--- ProjectManager/ProjectManager/KanBanBoardViewController.swift
+++ ProjectManager/ProjectManager/KanBanBoardViewController.swift
@@ -23,26 +23,44 @@ class KanBanBoardViewController: UIViewController {
     let toDoTableView: UITableView = {
         let tableView = UITableView()
         tableView.register(KanBanBoardCell.self, forCellReuseIdentifier: KanBanBoardCell.reuseIdentifier)
-       return tableView
+        return tableView
     }()
 
     let doingTableView: UITableView = {
         let tableView = UITableView()
         tableView.register(KanBanBoardCell.self, forCellReuseIdentifier: KanBanBoardCell.reuseIdentifier)
-       return tableView
+        return tableView
     }()
 
     let doneTableView: UITableView = {
         let tableView = UITableView()
         tableView.register(KanBanBoardCell.self, forCellReuseIdentifier: KanBanBoardCell.reuseIdentifier)
-       return tableView
+        return tableView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Project Manager"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+
+        toDoTableView.delegate = self
+        doingTableView.delegate = self
+        doneTableView.delegate = self
+
         toDoTableView.dataSource = self
         doingTableView.dataSource = self
         doneTableView.dataSource = self
+
+        toDoTableView.tableHeaderView = {
+            let view = UIView()
+            let label = UILabel()
+            view.addSubview(label)
+            label.snp.makeConstraints { label in
+                label.edges.equalTo(view)
+            }
+            label.text = "TODO"
+            return view
+        }()
 
         view.addSubview(outerStackView)
 
@@ -57,20 +75,18 @@ class KanBanBoardViewController: UIViewController {
     }
 }
 
+// MARK: - DataSource
+
 extension KanBanBoardViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 각각의 table마다 분기처리를 통해 data를 넣어주는 방식
         switch tableView {
-
         case self.toDoTableView:
             return toDoArray.count
-
         case self.doingTableView:
             return doingArray.count
-
         case self.doneTableView:
             return doneArray.count
-
         default:
             return 0
         }
@@ -97,5 +113,53 @@ extension KanBanBoardViewController: UITableViewDataSource {
         }
 
         return cell
+    }
+}
+
+// MARK: - Delegate
+
+extension KanBanBoardViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        100
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: .zero)
+
+        let statusLabel: UILabel = {
+            let label = UILabel()
+            label.text = "TEST"
+            return label
+        }()
+
+        let countLabel: UILabel = {
+            let label = UILabel()
+            label.text = "13"
+            return label
+        }()
+
+        view.addSubview(statusLabel)
+        view.addSubview(countLabel)
+
+        statusLabel.snp.makeConstraints { label in
+            label.leading.equalTo(view).inset(10)
+            label.top.equalTo(view).inset(10)
+        }
+
+        countLabel.snp.makeConstraints { label in
+            label.leading.equalTo(statusLabel.snp.trailing).offset(10)
+            label.top.equalTo(view).inset(10)
+        }
+
+        return view
+    }
+
+    func tableView(_ tableView: UITableView,
+                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        UISwipeActionsConfiguration(actions: [
+            UIContextualAction(style: .destructive,
+                               title: "delete",
+                               handler: { _, _, _ in })
+        ])
     }
 }
