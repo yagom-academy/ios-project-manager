64137a387b5cc58bea15bf75020b80a083ad49e4
kane-young <lyw2100@naver.com>
2021-07-21 13:42:47 +0900

    feat: 스토리보드 삭제

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -10,6 +10,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
         // Do any additional setup after loading the view.
     }
 
