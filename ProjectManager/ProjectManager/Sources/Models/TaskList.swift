e0840f9bee14abc0f347b8ee07c2451d1e8e0ed3
duckbok <duckbok22@gmail.com>
2021-07-21 10:58:21 +0900

    feat: Decoding을 위한 TaskList 타입 정의
    - 이를 위한 sample JSON 추가
    Fixes: #2

---
+++ ProjectManager/ProjectManager/Sources/Models/TaskList.swift
@@ -0,0 +1,12 @@
+//
+//  TaskList.swift
+//  ProjectManager
+//
+//  Created by duckbok, Ryan-Son on 2021/07/21.
+//
+
+struct TaskList: Codable {
+
+    var taskOrder: TaskOrder
+    var tasks: [Task]
+}
