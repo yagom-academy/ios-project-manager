9a3fb876a8f006cdc116f937a8342fcfac4b85c5
kane-young <lyw2100@naver.com>
2021-07-21 14:12:37 +0900

    feat: 메인화면 todo, doing, done tableView 추가

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -8,12 +8,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var todoTableView: UITableView = {
+        let todoTableView: UITableView = UITableView()
+        todoTableView.translatesAutoresizingMaskIntoConstraints = false
+        return todoTableView
+    }()
+    
+    lazy var doingTableView: UITableView = {
+        let doingTableView: UITableView = UITableView()
+        doingTableView.translatesAutoresizingMaskIntoConstraints = false
+        return doingTableView
+    }()
+    
+    lazy var doneTableView: UITableView = {
+        let doneTableView: UITableView = UITableView()
+        doneTableView.translatesAutoresizingMaskIntoConstraints = false
+        return doneTableView
+    }()
+    
+    lazy var tablesStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView(arrangedSubviews: [todoTableView, doingTableView, doneTableView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private func addSubViews() {
+        self.view.addSubview(tablesStackView)
+    }
+    
+    private func configureConstraints() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tablesStackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tablesStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tablesStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tablesStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemRed
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        addSubViews()
+        configureConstraints()
     }
-
-
 }
-
