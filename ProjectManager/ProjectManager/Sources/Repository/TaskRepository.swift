90aa42fff0069043a222963204d24e46a59d4056
duckbok <duckbok22@gmail.com>
2021-07-21 11:04:13 +0900

    feat: TaskList를 사용하여 decode
    Fixes: #2

--- ProjectManager/ProjectManager/Sources/Repository/TaskRepository.swift
+++ ProjectManager/ProjectManager/Sources/Repository/TaskRepository.swift
@@ -2,7 +2,7 @@
 //  TaskRepository.swift
 //  ProjectManager
 //
-//  Created by Ryan-Son on 2021/07/20.
+//  Created by Ryan-Son, duckbok on 2021/07/20.
 //
 
 import UIKit
@@ -11,17 +11,17 @@ struct TaskRepository {
 
     private let decoder = JSONDecoder()
 
-    func fetchTasks(completion: (Result<[Task], PMError>) -> Void) {
-        guard let asset = NSDataAsset(name: "tasks") else {
+    func fetchTasks(completion: (Result<TaskList, PMError>) -> Void) {
+        guard let asset = NSDataAsset(name: "task_list") else {
             completion(.failure(.invalidAsset))
             return
         }
 
-        guard let tasks = try? decoder.decode([Task].self, from: asset.data) else {
+        guard let taskList = try? decoder.decode(TaskList.self, from: asset.data) else {
             completion(.failure(.decodingFailed))
             return
         }
 
-        completion(.success(tasks))
+        completion(.success(taskList))
     }
 }
