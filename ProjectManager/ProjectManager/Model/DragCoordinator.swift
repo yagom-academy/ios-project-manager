7c1adc9eea669c6580d3a476d022b1ae1d520e15
kane-young <lyw2100@naver.com>
2021-07-22 20:17:31 +0900

    feat: DragCoordinator 생성 및 dragSessionDidEnd 메서드 구현

---
+++ ProjectManager/ProjectManager/Model/DragCoordinator.swift
@@ -0,0 +1,16 @@
+//
+//  CacheDragCoordinator.swift
+//  ProjectManager
+//
+//  Created by 이영우 on 2021/07/22.
+//
+
+import Foundation
+
+class DragCoordinator {
+    let indexPath: IndexPath
+    
+    init(indexPath: IndexPath) {
+        self.indexPath = indexPath
+    }
+}
--- ProjectManager/ProjectManager/ViewController/ViewController+DragDelegate.swift
+++ ProjectManager/ProjectManager/ViewController/ViewController+DragDelegate.swift
@@ -21,7 +21,22 @@ extension ViewController: UITableViewDragDelegate {
             task = doneTasks[indexPath.row]
         }
         
+        session.localContext = DragCoordinator(indexPath: indexPath)
         let itemProvider = NSItemProvider(object: task)
         return [UIDragItem(itemProvider: itemProvider)]
+    }
+    
+    func tableView(_ tableView: UITableView, dragSessionDidEnd session: UIDragSession) {
+        guard let dragCoordinator = session.localContext as? DragCoordinator else { return }
+        if tableView == todoTableView {
+            todoTasks.remove(at: dragCoordinator.indexPath.row)
+        } else if tableView == doingTableView {
+            doingTasks.remove(at: dragCoordinator.indexPath.row)
+        } else {
+            doneTasks.remove(at: dragCoordinator.indexPath.row)
+        }
+        tableView.performBatchUpdates{
+            tableView.deleteRows(at: [dragCoordinator.indexPath], with: .automatic)
+        }
     }
 }
