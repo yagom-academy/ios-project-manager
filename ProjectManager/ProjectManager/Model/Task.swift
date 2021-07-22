ab0ca8dc7c5228c73c0ffae501ef86edf293f997
kane-young <lyw2100@naver.com>
2021-07-22 19:33:45 +0900

    feat: Task 모델 drag and drop을 위한 프로토콜 채택 - NSItemProviderWriting - NSItemProviderReading - Codable

--- ProjectManager/ProjectManager/Model/Task.swift
+++ ProjectManager/ProjectManager/Model/Task.swift
@@ -6,15 +6,52 @@
 //
 
 import Foundation
+import MobileCoreServices
 
-final class Task {
+final class Task: NSObject {
     var title: String
     var content: String
     var deadLine: String
+    var state: String
     
-    init(title: String, content: String, deadLine: String) {
+    init(title: String, content: String, deadLine: String, state: String) {
         self.title = title
         self.content = content
         self.deadLine = deadLine
+        self.state = state
+    }
+}
+
+extension Task: NSItemProviderWriting {
+    static var writableTypeIdentifiersForItemProvider: [String] {
+        return [String(kUTTypeData)]
+    }
+    
+    func loadData(withTypeIdentifier typeIdentifier: String,
+                  forItemProviderCompletionHandler completionHandler: @escaping (Data?, Error?) -> Void) -> Progress? {
+        let progress = Progress(totalUnitCount: 100)
+        do {
+            let data = try JSONEncoder().encode(self)
+            progress.completedUnitCount = 100
+            completionHandler(data, nil)
+        } catch {
+            completionHandler(nil, error)
+        }
+        return progress
+    }
+}
+
+extension Task: Codable, NSItemProviderReading {
+    static var readableTypeIdentifiersForItemProvider: [String] {
+        return [String(kUTTypeData)]
+    }
+    
+    static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Task {
+        do {
+            let task = try JSONDecoder().decode(Task.self, from: data)
+            return task
+        } catch {
+            fatalError()
+        }
     }
 }
