4845b66dbd584c6fc97cc8af532f2bd6b219ae6f
eunseo5355 <eunseo5355@naver.com>
2021-07-22 17:32:10 +0900

    ✨ [Feat] 마감일 text color 설정
    - cell 설정하는 configure() 메서드 구현
    - 마감일이 지난 날짜는 text color를 red로 바뀌게함

--- ProjectManager/ProjectManager/TableViewCell.swift
+++ ProjectManager/ProjectManager/TableViewCell.swift
@@ -10,20 +10,48 @@ import UIKit
 class TableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var dueDateLabel: UILabel!
     
     static let identifier = "TableViewCell"
     
+    var formattedDeadLineDate: DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.locale = Locale(identifier: "ko_kr")
+        return dateFormatter
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+    override func prepareForReuse() {
+        self.dueDateLabel.textColor = .black
+    }
+    
+    func configure(_ task: Task) {
+        self.titleLabel.text = task.title
+        self.contentLabel.text = task.content
+        self.dueDateLabel.text = task.formattedDeadLineDate
+        
+        if isOutDated(task.deadLineDate) {
+            self.dueDateLabel.textColor = .red
+        } else {
+            self.dueDateLabel.textColor = .black
+        }
+    }
+    
+    private func isOutDated(_ date: Date) -> Bool {
+        let currentDate = Date()
+
+        let interval = date.timeIntervalSince(currentDate)
+        let days = Int(interval / 86400)
+
+        return days < 0
+    }
 }
