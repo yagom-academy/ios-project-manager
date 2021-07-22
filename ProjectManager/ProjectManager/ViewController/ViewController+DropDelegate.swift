cc7dfbc5ade1496975966479c0eddb5cfacf1a28
kane-young <lyw2100@naver.com>
2021-07-22 20:24:26 +0900

    feat: DropDelegate dropSessionDidUpdate 메서드 구현

--- ProjectManager/ProjectManager/ViewController/ViewController+DropDelegate.swift
+++ ProjectManager/ProjectManager/ViewController/ViewController+DropDelegate.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 extension ViewController: UITableViewDropDelegate {
+//    func tableView(_ tableView: UITableView, canHandle session: UIDropSession) -> Bool {
+//        return session.canLoadObjects(ofClass: Task.self)
+//    }
+
+    func tableView(_ tableView: UITableView,
+                   dropSessionDidUpdate session: UIDropSession,
+                   withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
+        guard session.localDragSession != nil else {
+            return UITableViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+        }
+        guard session.items.count == 1 else { return UITableViewDropProposal(operation: .cancel) }
+        
+        return UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+    }
+    
     func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
         return
     }
