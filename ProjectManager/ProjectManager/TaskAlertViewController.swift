8e071a7352d9a163685e5024a2c1bc3482a66f29
Sookim18 <scstnghks@naver.com>
2021-07-22 14:02:10 +0900

    ✨ [Feat] Task추가기능 구현

--- ProjectManager/ProjectManager/TaskAlertViewController.swift
+++ ProjectManager/ProjectManager/TaskAlertViewController.swift
@@ -9,12 +9,39 @@ import UIKit
 
 class TaskAlertViewController: UIViewController {
 
+    @IBOutlet weak var taskTextView: UITextView!
+    @IBOutlet weak var taskTextField: UITextField!
+    
+    var tempTitle: String?
+    var tempBody: String?
+    var tempDeadLine: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.layer.cornerRadius = 16
         self.view.layer.borderWidth = 2
         self.view.layer.borderColor = UIColor.white.cgColor
+        taskTextView.delegate = self
+        taskTextField.addTarget(self, action: #selector(changedTextField), for: .editingChanged)
     }
+    
+    @objc func changedTextField() {
+        tempTitle = taskTextField.text
+    }
+    
+    @IBAction func getDeadLine(_ sender: UIDatePicker) {
+        let datePicker = sender
+    }
+    
+    @IBAction func finishEditTask(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+}
 
+extension TaskAlertViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        tempBody = self.taskTextView.text
+    }
 }
