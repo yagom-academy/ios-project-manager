8da486b803da96fa7f76e32fbdc40189e6af3f56
ryan-son <ryan.son1002@gmail.com>
2021-07-23 14:41:51 +0900

    fix: ViewModel remove, insert, update 기능 수정
    - update 시 taskOrder 삭제하지 않도록 수정
    - insert 시 id를 통해 반복적으로 task 찾지 않도록 수정
    - remove 시 tasks 배열 내 index를 찾아 삭제하는 방식으로 수정

--- ProjectManager/ProjectManager/Sources/ViewModels/TaskViewModel.swift
+++ ProjectManager/ProjectManager/Sources/ViewModels/TaskViewModel.swift
@@ -51,8 +51,10 @@ struct TaskViewModel {
     }
 
     mutating func remove(_ task: Task) {
-        guard let index = taskOrder[task.state].firstIndex(of: task.id) else { return }
-        tasks.remove(at: index)
+        guard let index = taskOrder[task.state].firstIndex(of: task.id),
+              let indexInTasks = tasks.firstIndex(where: { $0.id == task.id }) else { return }
+
+        tasks.remove(at: indexInTasks)
 
         switch task.state {
         case .todo:
@@ -78,21 +80,17 @@ struct TaskViewModel {
 
         switch newTask.state {
         case .todo:
-            taskOrder.todo.remove(at: index)
             updated?(.todo, index)
         case .doing:
-            taskOrder.doing.remove(at: index)
             updated?(.doing, index)
         case .done:
-            taskOrder.done.remove(at: index)
             updated?(.done, index)
         }
     }
 
     private mutating func insert(_ task: Task, to state: Task.State, at destinationIndex: Int) {
         let stateOrder: [UUID] = taskOrder[state]
-        guard destinationIndex <= stateOrder.count,
-              let task = self.task(by: task.id) else { return }
+        guard destinationIndex <= stateOrder.count else { return }
 
         tasks.append(task)
 
@@ -107,5 +105,7 @@ struct TaskViewModel {
             taskOrder.done.insert(task.id, at: destinationIndex)
             inserted?(.done, destinationIndex)
         }
+
+        tasks[destinationIndex].state = state
     }
 }
