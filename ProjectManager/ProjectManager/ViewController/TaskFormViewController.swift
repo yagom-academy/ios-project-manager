566e09405b3f7327874c4877d817517e2c5a995f
stevenkim18 <11882@naver.com>
2021-07-23 14:14:29 +0900

    feat: TaskFormViewController 뷰들 배치

--- ProjectManager/ProjectManager/ViewController/TaskFormViewController.swift
+++ ProjectManager/ProjectManager/ViewController/TaskFormViewController.swift
@@ -25,6 +25,37 @@ class TaskFormViewController: UIViewController {
         super.init(coder: coder)
     }
     
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Title"
+        textField.font = UIFont.preferredFont(forTextStyle: .title1)
+        return textField
+    }()
+    
+    private lazy var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        return datePicker
+    }()
+    
+    private lazy var contentTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleTextField, datePicker, contentTextView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,6 +67,20 @@ class TaskFormViewController: UIViewController {
         self.navigationItem.title = "TODO"
         
         self.view.backgroundColor = .systemBackground
+        view.addSubview(stackView)
+        configureConstraints()
     }
+    
+    private func configureConstraints() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 8),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -8),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10)
+        ])
+    }
+    
+    
     
 }
