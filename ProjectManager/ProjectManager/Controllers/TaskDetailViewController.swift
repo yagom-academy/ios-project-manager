8e158b7ea35de13dc52d1f1fe11084495cb951d4
fezravine <yjaewoong@naver.com>
2021-07-26 01:41:21 +0900

    ♻️ [Refactor] : Status 열거형 새로운 파일 생성

--- ProjectManager/ProjectManager/Controllers/TaskDetailViewController.swift
+++ ProjectManager/ProjectManager/Controllers/TaskDetailViewController.swift
@@ -7,14 +7,7 @@
 
 import UIKit
 
-enum State {
-    case todo
-    case doing
-    case done
-}
-
-final class AddTaskViewController: UIViewController {
-    
+final class TaskDetailViewController: UIViewController {
     enum Mode {
         case add
         case edit
