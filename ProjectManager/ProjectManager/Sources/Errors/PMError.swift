73a804c62b7d2ba97491b043348281c98df4fc06
ryan-son <ryan.son1002@gmail.com>
2021-07-27 22:18:44 +0900

    feat: PMError 에러 문구 추가

--- ProjectManager/ProjectManager/Sources/Errors/PMError.swift
+++ ProjectManager/ProjectManager/Sources/Errors/PMError.swift
@@ -14,3 +14,18 @@ enum PMError: Error {
     case invalidTypeIdentifier
     case cannotEncodeToJSON
 }
+
+extension PMError: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .invalidAsset:
+            return "유효하지 않은 파일 이름입니다."
+        case .decodingFailed:
+            return "디코딩 작업에 실패하였습니다."
+        case .invalidTypeIdentifier:
+            return "유효하지 않은 Type Identifier입니다."
+        case .cannotEncodeToJSON:
+            return "JSON으로의 인코딩 작업에 실패하였습니다."
+        }
+    }
+}
