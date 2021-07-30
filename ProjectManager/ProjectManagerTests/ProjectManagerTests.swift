b55c478984ea9b2a057d1189ea1f9c42abe90831
kane-young <lyw2100@naver.com>
2021-07-30 21:56:41 +0900

    test: NetworkManager post 메서드 테스트

--- ProjectManager/ProjectManagerTests/ProjectManagerTests.swift
+++ ProjectManager/ProjectManagerTests/ProjectManagerTests.swift
@@ -24,4 +24,20 @@ class ProjectManagerTests: XCTestCase {
         }
         wait(for: [expectation], timeout: 5)
     }
+    
+    func testPostServerAPI() {
+        let networkManager: NetworkManager = NetworkManager()
+        let expectation = expectation(description: "PostTest")
+        let item = RequestTask(title: "진짜 마지막 시도", content: "입니다", deadLine: 111.3334, type: .done)
+        networkManager.post(url: ServerAPI.create.url!, item) { result in
+            switch result {
+            case .success(let task):
+                XCTAssertEqual(item.title, task.title)
+            case .failure(let error):
+                XCTFail()
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 7)
+    }
 }
