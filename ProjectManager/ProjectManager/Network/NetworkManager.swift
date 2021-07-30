9ad4d34c715ba73bbe0bf5e12734d908f90b4537
Tak <oit5538@gmail.com>
2021-07-30 14:43:12 +0900

    feat: ServerAPI 타입 구현

--- ProjectManager/ProjectManager/Network/NetworkManager.swift
+++ ProjectManager/ProjectManager/Network/NetworkManager.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
+enum ServerAPI {
+    static let baseURL = "https://projectmanager-great.herokuapp.com/tasks"
+    
+    case create
+    case read
+    case update(id: String)
+    case delete(id: String)
+
+    var url: URL? {
+        switch self {
+        case .create:
+            return URL(string: Self.baseURL)
+        case .read:
+            return URL(string: Self.baseURL)
+        case .update(let id):
+            return URL(string: Self.baseURL + "/" + id)
+        case .delete(let id):
+            return URL(string: Self.baseURL + "/" + id)
+        }
+    }
+}
+
 class NetworkManager {
     let session: URLSession
     
