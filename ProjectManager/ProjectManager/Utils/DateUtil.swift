06b2b1ad2fe39b6f3c8d71cdc981894f3cb9a1b9
Tak <oit5538@gmail.com>
2021-07-30 16:31:18 +0900

    refactor: fomatter를 지역변수 -> 전역변수로 변경

--- ProjectManager/ProjectManager/Utils/DateUtil.swift
+++ ProjectManager/ProjectManager/Utils/DateUtil.swift
@@ -8,15 +8,14 @@
 import Foundation
 
 class DateUtil {
+    static let formatter = DateFormatter()
     static func parseDate(_ dateString: String) -> Date {
-        let formatter = DateFormatter()
         formatter.dateFormat = "yyyy.MM.dd"
         formatter.locale = Locale.current
         return formatter.date(from: dateString) ?? Date()
     }
 
     static func formatDate(_ date: Date) -> String {
-        let formatter = DateFormatter()
         formatter.dateFormat = "yyyy.MM.dd"
         return formatter.string(from: date)
     }
