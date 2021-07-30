9467135923a055e9608d7c180177c9311580b7da
duckbok <duckbok22@gmail.com>
2021-07-30 15:34:23 +0900

    feat: History 모델과 정보 가공을 위한 ViewModel 구현
    Fixes: #10

---
+++ ProjectManager/ProjectManager/Sources/ViewModels/HistoryViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  HistoryViewModel.swift
+//  ProjectManager
+//
+//  Created by duckbok, Ryan-Son on 2021/07/30.
+//
+
+struct HistoryViewModel {
+
+    private var histories: [History] = []
+
+    mutating func create(history: History) {
+        histories.append(history)
+    }
+
+    func history(at index: Int) -> (title: String, subtitle: String)? {
+        guard index < histories.count else { return nil }
+
+        let title: String = describe(histories[index].method)
+        let subtitle: String = histories[index].date.historyFormat
+        return (title, subtitle)
+    }
+
+    private func describe(_ method: History.Method) -> String {
+        switch method {
+        case let .added(title):
+            return "Added '\(title)'"
+        case let .removed(title, sourceState):
+            let formattedSourceState = sourceState.rawValue.uppercased()
+            return "Removed '\(title) from \(formattedSourceState)"
+        case let .moved(title, sourceState, destinationState):
+            let formattedSourceState = sourceState.rawValue.uppercased()
+            let formattedDestinationState = destinationState.rawValue.uppercased()
+            return "Moved '\(title)' from \(formattedSourceState) to \(formattedDestinationState)"
+        }
+    }
+}
