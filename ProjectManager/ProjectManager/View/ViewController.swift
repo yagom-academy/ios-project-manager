b5c45567f8486de268ac946315ef71d72736e5e6
Wody95 <kwu0626@gmail.com>
2021-07-31 15:55:23 +0900

    fix : info.plist 경로 수정

--- ProjectManager/ProjectManager/View/ViewController.swift
+++ ProjectManager/ProjectManager/View/ViewController.swift
@@ -10,9 +10,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.view.backgroundColor = .systemGreen
     }
-
-
 }
-
