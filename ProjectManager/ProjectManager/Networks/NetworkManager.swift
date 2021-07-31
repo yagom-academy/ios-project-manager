ee2fc13f92bfde585dcc9a7d8e36caa5ee136b09
cjm <stepsw112@gmail.com>
2021-07-31 20:49:21 +0900

    ✨ [Feat] : NetworkManager 구현함

--- ProjectManager/ProjectManager/Networks/NetworkManager.swift
+++ ProjectManager/ProjectManager/Networks/NetworkManager.swift
@@ -8,21 +8,109 @@
 import Foundation
 
 struct NetworkManager {
-    private let baseURL = "".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+    private let baseURL = "https://vaporpms.herokuapp.com"
+    
+    private func checkValidation(data: Data?, response: URLResponse?, error: Error?) {
+        if let error = error {
+            fatalError("\(error)")
+        }
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            print("Invalid Response")
+            ProjectManagerViewController.networkStatus = .disconnection
+            return
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            print("Status Code: \(httpResponse.statusCode)")
+            ProjectManagerViewController.networkStatus = .disconnection
+            return
+        }
+        
+        guard let _ = data else {
+            print("Invalid Data")
+            ProjectManagerViewController.networkStatus = .disconnection
+            return
+        }
+        ProjectManagerViewController.networkStatus = .connection
+    }
+    
+    private func encodedData<T>(data: T) -> Data? where T: Encodable{
+        let encoder = JSONEncoder()
+        return try? encoder.encode(data)
+    }
     
     func get(completion: @escaping ([Task]) -> ()) {
-        completion([])
+        let urlString = baseURL + "/tasks"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let indicatorView = IndicatorView()
+        indicatorView.showIndicator()
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            self.checkValidation(data: data, response: response, error: error)
+            guard let data = data else { return }
+            guard let tasks = try? JSONDecoder().decode([Task].self, from: data) else { return }
+            indicatorView.dismiss()
+            completion(tasks)
+        }.resume()
     }
     
-    func post(completion: @escaping (Task) -> ()) {
-//        completion()
+    func post(task: Task, completion: @escaping (Task) -> ()) {
+        let urlString = baseURL + "/tasks"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        var requset = URLRequest(url: url)
+        
+        requset.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        requset.httpMethod = "POST"
+        requset.httpBody = encodedData(data: task)
+        
+        URLSession.shared.dataTask(with: requset) { data, response, error in
+            self.checkValidation(data: data, response: response, error: error)
+            guard let data = data else { return }
+            guard let task = try? JSONDecoder().decode(Task.self, from: data) else { return }
+            completion(task)
+        }.resume()
     }
     
-    func patch(completion: @escaping () -> ()) {
-        completion()
+    func patch(task: Task, completion: @escaping (Task) -> ()) {
+        let urlString = baseURL + "/tasks" + "/\(task.id)"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        var requset = URLRequest(url: url)
+        
+        requset.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        requset.httpMethod = "PATCH"
+        requset.httpBody = encodedData(data: task)
+        
+        URLSession.shared.dataTask(with: requset) { data, response, error in
+            self.checkValidation(data: data, response: response, error: error)
+            guard let data = data else { return }
+            guard let task = try? JSONDecoder().decode(Task.self, from: data) else { return }
+            completion(task)
+        }.resume()
     }
     
-    func delete(completion: @escaping () -> ()) {
-        completion()
+    func delete(id: String, completion: @escaping () -> ()) {
+        let urlString = baseURL + "/tasks" + "/\(id)"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        var requset = URLRequest(url: url)
+        
+        requset.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        requset.httpMethod = "DELETE"
+                
+        URLSession.shared.dataTask(with: requset) { data, response, error in
+            self.checkValidation(data: data, response: response, error: error)
+        }.resume()
     }
+    
+    
 }
