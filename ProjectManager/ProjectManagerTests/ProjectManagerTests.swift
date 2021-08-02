89ad7ab1345f8957fba990a9e3e484f9df6fcbf6
stevenkim18 <11882@naver.com>
2021-08-02 16:38:36 +0900

    test: NetworkManager put 함수 테스트

--- ProjectManager/ProjectManagerTests/ProjectManagerTests.swift
+++ ProjectManager/ProjectManagerTests/ProjectManagerTests.swift
@@ -12,7 +12,7 @@ class ProjectManagerTests: XCTestCase {
 
     func test() {
         let networkManager: NetworkManager = NetworkManager()
-        let expectation = expectation(description: "GetTest")
+        let expectation1 = expectation(description: "GetTest")
         networkManager.get(url: ServerAPI.read.url!) { result in
             switch result {
             case .success(let tasks):
@@ -20,14 +20,14 @@ class ProjectManagerTests: XCTestCase {
             case .failure(let error):
                 XCTFail()
             }
-            expectation.fulfill()
+            expectation1.fulfill()
         }
-        wait(for: [expectation], timeout: 5)
+        wait(for: [expectation1], timeout: 5)
     }
     
     func testPostServerAPI() {
         let networkManager: NetworkManager = NetworkManager()
-        let expectation = expectation(description: "PostTest")
+        let expectation2 = expectation(description: "PostTest")
         let item = RequestTask(title: "진짜 마지막 시도", content: "입니다", deadLine: 111.3334, type: .done)
         networkManager.post(url: ServerAPI.create.url!, item) { result in
             switch result {
@@ -36,8 +36,25 @@ class ProjectManagerTests: XCTestCase {
             case .failure(let error):
                 XCTFail()
             }
-            expectation.fulfill()
+            expectation2.fulfill()
         }
-        wait(for: [expectation], timeout: 7)
+        wait(for: [expectation2], timeout: 7)
+    }
+    
+    func testPutServerAPI() {
+        let networkManager: NetworkManager = NetworkManager()
+        let id = "38A26C85-A2EB-471C-BF12-D455AF1FA508"
+        let expectation3 = expectation(description: "PostTest")
+        let item = RequestTask(title: "수정1", content: "수정111", deadLine: 12345532.233334, type: .todo)
+        networkManager.put(url: ServerAPI.update(id: id).url!, item) { result in
+            switch result {
+            case .success(let task):
+                XCTAssertEqual(item.title, task.title)
+            case .failure(let error):
+                XCTFail()
+            }
+            expectation3.fulfill()
+        }
+        wait(for: [expectation3], timeout: 7)
     }
 }
