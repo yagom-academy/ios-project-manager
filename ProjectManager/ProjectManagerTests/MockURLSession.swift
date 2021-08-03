114623adc6f9611029f29023892568c515703735
kane-young <lyw2100@naver.com>
2021-08-03 16:06:45 +0900

    test: MockURLSession, FakeDataTask 타입 생성

---
+++ ProjectManager/ProjectManagerTests/MockURLSession.swift
@@ -0,0 +1,45 @@
+//
+//  MockURLSession.swift
+//  ProjectManagerTests
+//
+//  Created by 이영우 on 2021/08/02.
+//
+
+import Foundation
+import XCTest
+@testable import ProjectManager
+
+class MockURLSession: URLSessionProtocol {
+    var url: URL!
+    var data: Data?
+    var response: HTTPURLResponse
+    
+    init(response: HTTPURLResponse, data: Data?) {
+        self.response = response
+        self.data = data
+    }
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        self.url = request.url
+        
+        return FakeDataTask {
+            completionHandler(nil, self.response, nil)
+        }
+    }
+    
+    func verifyDataTask(url: URL) {
+        XCTAssertEqual(self.url, url)
+    }
+}
+
+final class FakeDataTask: URLSessionDataTask {
+    private let handler: () -> Void
+    
+    init(handler: @escaping () -> Void) {
+        self.handler = handler
+    }
+    
+    override func resume() {
+        handler()
+    }
+}
