fc16d004d173a8c2a47f6dd1a8e2dfd13543aad3
stevenkim18 <11882@naver.com>
2021-08-04 22:09:29 +0900

    refactor: Task 모델 타입 커스텀 decode encode 함수 구현

--- ProjectManager/ProjectManager/Model/Task.swift
+++ ProjectManager/ProjectManager/Model/Task.swift
@@ -8,18 +8,36 @@
 import Foundation
 
 final class Task: Codable {
-    var id: String
+    var id: String?
     var title: String
     var content: String
     var deadLine: TimeInterval
     var type: TaskType
     
-    init(id: String, title: String, content: String, deadLine: TimeInterval, type: TaskType) {
+    init(id: String? = nil, title: String, content: String, deadLine: TimeInterval, type: TaskType) {
         self.id = id
         self.title = title
         self.content = content
         self.deadLine = deadLine
         self.type = type
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try? container.decodeIfPresent(String.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.content = try container.decode(String.self, forKey: .content)
+        self.deadLine = try container.decode(Double.self, forKey: .deadLine)
+        self.type = try container.decode(TaskType.self, forKey: .type)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(self.id, forKey: .id)
+        try container.encode(self.title, forKey: .title)
+        try container.encode(self.content, forKey: .content)
+        try container.encode(self.deadLine, forKey: .deadLine)
+        try container.encode(self.type, forKey: .type)
     }
     
     enum CodingKeys: String, CodingKey {
