403889f95f91e8619e69bbf42ae74830eb4a2c22
duckbok <duckbok22@gmail.com>
2021-08-05 16:39:02 +0900

    test: Date에 대한 Unit tests 추가
    Fixes: #13

--- ProjectManager/ProjectManagerTests/Sources/Tests/UtilTests.swift
+++ ProjectManager/ProjectManagerTests/Sources/Tests/UtilTests.swift
@@ -8,9 +8,35 @@
 import XCTest
 @testable import ProjectManager
 
-private final class UtilTests: XCTestCase {
+final class UtilTests: XCTestCase {
 
-    override func setUpWithError() throws { }
+    var sutDate: Date!
 
-    override func tearDownWithError() throws { }
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sutDate = Date(timeIntervalSince1970: 0)
+    }
+
+    override func tearDownWithError() throws {
+        sutDate = nil
+        try super.tearDownWithError()
+    }
+
+    func test_date인스턴스를_taskFormat으로연산하면_현재국가기준으로짧은문자열로반환한다() {
+        let expected: String = "Jan 1, 1970"
+
+        XCTAssertEqual(sutDate.taskFormat, expected)
+    }
+
+    func test_date인스턴스를_historyFormat으로연산하면_현재국가기준으로긴문자열로반환한다() {
+        let expected: String = "January 1, 1970 at 9:00:00 AM"
+
+        XCTAssertEqual(sutDate.historyFormat, expected)
+    }
+
+    func test_date인스턴스를_date으로연산하면_현재국가기준으로시간값을빼서반환한다() {
+        let expected: String = "1969-12-31 15:00:00 +0000"
+
+        XCTAssertEqual(sutDate.date!.description, expected)
+    }
 }
