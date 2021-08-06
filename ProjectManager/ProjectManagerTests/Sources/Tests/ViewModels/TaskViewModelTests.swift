f3b4efc64b7ab666a1bf377af4cd2d1da35bd631
ryan-son <ryan.son1002@gmail.com>
2021-08-06 17:32:02 +0900

    test: TaskViewModel add, remove 유닛 테스트 추가
    Fixes: #13

--- ProjectManager/ProjectManagerTests/Sources/Tests/ViewModels/TaskViewModelTests.swift
+++ ProjectManager/ProjectManagerTests/Sources/Tests/ViewModels/TaskViewModelTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import ProjectManager
 
-private final class TaskViewModelTests: XCTestCase {
+final class TaskViewModelTests: XCTestCase {
 
     var sutTaskViewModel: TaskViewModel!
     var mockCoreDataStack: CoreDataStackProtocol!
@@ -54,12 +54,69 @@ private final class TaskViewModelTests: XCTestCase {
     }
 
     func test_task를_todo에_추가한다() throws {
+        let expectedResponseTask = TestAsset.dummyTodoResponseTask
+        stubNetworkRepository.expectedResponseTask = expectedResponseTask
+        let expectedTask = Task(context: mockCoreDataStack.context, responseTask: expectedResponseTask)
+
+        sutTaskViewModel.add(expectedTask)
+
+        let taskFromTaskList = sutTaskViewModel.taskList[.todo].first
+        XCTAssertEqual(taskFromTaskList, expectedTask)
+        XCTAssertEqual(taskFromTaskList?.id, expectedTask.id)
+        XCTAssertEqual(taskFromTaskList?.title, expectedTask.title)
+        XCTAssertEqual(taskFromTaskList?.body, expectedTask.body)
+        XCTAssertEqual(taskFromTaskList?.dueDate, expectedTask.dueDate)
+        XCTAssertEqual(taskFromTaskList?.state, expectedTask.state)
+        XCTAssertEqual(addedIndex, .zero)
+        XCTAssertTrue(isChanged)
+        XCTAssertTrue(stubNetworkRepository.isPostCalled)
     }
-
-    func test_state와index를통해_해당task를반환한다() {
-
-//        sutTaskViewModel.task(from: ., at: <#T##Int#>)
-    }
-
     
+    func test_네트워크가연결되지않았을때_remove에삭제할task의state와index를전달하면_task를softDelete할수있다() {
+        let expectedResponseTask = TestAsset.dummyTodoResponseTask
+        let expectedResponseStatusCode = 400
+        let expectedPMError: PMError = .failureResponse(expectedResponseStatusCode)
+        stubNetworkRepository.expectedResponseTask = expectedResponseTask
+        stubNetworkRepository.expectedResponseStatusCode = expectedResponseStatusCode
+        stubNetworkRepository.expectedPMError = expectedPMError
+        
+        let expectedTask = Task(context: mockCoreDataStack.context, responseTask: expectedResponseTask)
+        sutTaskViewModel.add(expectedTask)
+
+        let removedTitle = sutTaskViewModel.remove(state: .todo, at: .zero)
+
+        let removed = mockCoreDataStack.context.object(with: expectedTask.objectID) as? Task
+        XCTAssertEqual(removedTitle, expectedTask.title)
+        XCTAssertTrue(sutTaskViewModel.taskList[.todo].isEmpty)
+        XCTAssertEqual(removed, expectedTask)
+        XCTAssertEqual(removed?.id, expectedTask.id)
+        XCTAssertEqual(removed?.title, expectedTask.title)
+        XCTAssertEqual(removed?.body, expectedTask.body)
+        XCTAssertEqual(removed?.dueDate, expectedTask.dueDate)
+        XCTAssertEqual(removed?.state, expectedTask.state)
+        XCTAssertTrue(removed!.isRemoved)
+        XCTAssertEqual(removedStateAndIndex.state, .todo)
+        XCTAssertEqual(removedStateAndIndex.index, .zero)
+        XCTAssertTrue(stubNetworkRepository.isDeleteCalled)
+    }
+
+    func test_네트워크가연결되었을때_remove에삭제할task의state와index를전달하면_task를delete할수있다() {
+        let expectedResponseTask = TestAsset.dummyTodoResponseTask
+        let expectedResponseStatusCode = 204
+        stubNetworkRepository.expectedResponseTask = expectedResponseTask
+        stubNetworkRepository.expectedResponseStatusCode = expectedResponseStatusCode
+        
+        let expectedTask = Task(context: mockCoreDataStack.context, responseTask: expectedResponseTask)
+        sutTaskViewModel.add(expectedTask)
+
+        let removedTitle = sutTaskViewModel.remove(state: .todo, at: .zero)
+
+        let removed = mockCoreDataStack.context.object(with: expectedTask.objectID)
+        XCTAssertEqual(removedTitle, TestAsset.dummyTodoResponseTask.title)
+        XCTAssertTrue(sutTaskViewModel.taskList[.todo].isEmpty)
+        XCTAssertTrue(removed.isFault)
+        XCTAssertEqual(removedStateAndIndex.state, .todo)
+        XCTAssertEqual(removedStateAndIndex.index, .zero)
+        XCTAssertTrue(stubNetworkRepository.isDeleteCalled)
+    }
 }
