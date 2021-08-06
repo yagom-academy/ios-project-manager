0968d917b59a09b898705293d36d9304dc4561f2
duckbok <duckbok22@gmail.com>
2021-08-06 16:50:39 +0900

    test: TestAsset 내용 수정
    Fixes: #13

--- ProjectManager/ProjectManagerTests/Resource/TestAsset.swift
+++ ProjectManager/ProjectManagerTests/Resource/TestAsset.swift
@@ -10,22 +10,25 @@ import Foundation
 
 enum TestAsset {
 
-    static let dummyTodoResponseTask = ResponseTask(id: UUID(),
-                                         title: "나는투두테스트",
-                                         body: "성공한다",
-                                         dueDate: 164835232,
-                                         state: .todo)
+    static let dummyTodoResponseTask: ResponseTask = ResponseTask(id: UUID(), title: "나는투두테스트", body: "성공한다", dueDate: 164835232, state: .todo)
 
-    static let dummyDoingResponseTask = ResponseTask(id: UUID(),
-                                         title: "나는두잉테스트",
-                                         body: "성공한다",
-                                         dueDate: 264835132,
-                                         state: .todo)
+    static let dummyThreeTodoResponseTasks: [ResponseTask] = [
+        ResponseTask(id: UUID(), title: "나는투두테스트1", body: "성공한다", dueDate: 164835232, state: .todo),
+        ResponseTask(id: UUID(), title: "나는투두테스트2", body: "성공한다22", dueDate: 164835233, state: .todo),
+        ResponseTask(id: UUID(), title: "나는투두테스트3", dueDate: 164835234, state: .todo)
+    ]
 
-    static let dummyDoneResponseTask = ResponseTask(id: UUID(),
-                                         title: "나는돈테스트",
-                                         dueDate: 364835132,
-                                         state: .todo)
+    static let dummyThreeDoingResponseTasks: [ResponseTask] = [
+        ResponseTask(id: UUID(), title: "나는두잉테스트1", body: "성공한다", dueDate: 264835132, state: .doing),
+        ResponseTask(id: UUID(), title: "나는두잉테스트2", dueDate: 264835133, state: .doing),
+        ResponseTask(id: UUID(), title: "나는두잉테스트3", body: "성공한다333", dueDate: 264835134, state: .doing)
+    ]
+
+    static let dummyThreeDoneResponseTasks: [ResponseTask] = [
+        ResponseTask(id: UUID(), title: "나는돈테스트1", dueDate: 364835132, state: .done),
+        ResponseTask(id: UUID(), title: "나는돈테스트2", dueDate: 364835133, state: .done),
+        ResponseTask(id: UUID(), title: "나는돈테스트3", dueDate: 364835134, state: .done)
+    ]
 
     static let dummyAddedHistory = History(method: .added(title: "난 더해져요"))
 
