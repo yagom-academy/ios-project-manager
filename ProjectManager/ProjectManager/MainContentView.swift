6d9ccfddc17659830d06560915f90646e5efa0df
YongwooKim <keepingitflow@gmail.com>
2021-10-26 23:50:20 +0900

    refactor: RootViewController의 view를 SwiftUI View로 변경
    기존 ViewController.swift 파일 삭제, MainContentView.swift 파일 생성

---
+++ ProjectManager/ProjectManager/MainContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectManager
+//
+//  Created by Yongwoo Marco on 2021/10/26.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .font(.largeTitle)
+    }
+}
+
+struct MainContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainContentView()
+    }
+}
