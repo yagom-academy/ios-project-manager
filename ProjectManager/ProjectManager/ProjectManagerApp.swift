cc5757c96f3416a951ecb6ce2bea577ac4dc66c1
soll4u <dasollpark@gmail.com>
2021-10-26 23:08:48 +0900

    feat: SPM을 이용해 Firebase 추가

--- ProjectManager/ProjectManager/ProjectManagerApp.swift
+++ ProjectManager/ProjectManager/ProjectManagerApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TestApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
