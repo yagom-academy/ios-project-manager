ade5e411f8981a17bb81de9940e511ea28f7fe34
soll4u <dasollpark@gmail.com>
2021-10-26 15:50:33 +0900

    chore: SwiftUI 프로젝트 세팅으로 변경

---
+++ ProjectManager/ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by Dasoll Park on 2021/10/26.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
