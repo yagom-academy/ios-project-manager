dd7d9ee3c4caabee32f55fff8d66542a70a86ccd
soll4u <dasollpark@gmail.com>
2021-10-27 15:41:38 +0900

    feat: Model 타입인 Task 생성

---
+++ ProjectManager/ProjectManager/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  ProjectManager
+//
+//  Created by Dasoll Park on 2021/10/27.
+//
+
+import Foundation
+
+struct Task {
+    
+    let title: String
+    let description: String
+    let date: Date
+    let id: UUID
+}
