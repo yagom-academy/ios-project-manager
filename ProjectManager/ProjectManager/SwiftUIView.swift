9cd5b8d396090826650fef312ceaee1558fb1e8e
ellenJ <jcrescent61@gmail.com>
2021-10-27 18:16:07 +0900

    feat: SwfitUIView 생성

---
+++ ProjectManager/ProjectManager/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  ProjectManager
+//
+//  Created by Ellen on 2021/10/27.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, Hyunable!")
+            .foregroundColor(.pink)
+            .font(.title)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
