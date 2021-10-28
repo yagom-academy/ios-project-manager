b6f728c1238ca8de776a8a856aeb3958c1ec7ad4
ictechgy <ictechgy@gmail.com>
2021-10-28 17:36:23 +0900

    feat: 할일에 대한 상세 화면인 ToDoDetail 뷰 구현

---
+++ ProjectManager/ProjectManager/Views/ToDoDetail.swift
@@ -0,0 +1,49 @@
+//
+//  ToDoDetail.swift
+//  ProjectManager
+//
+//  Created by JINHONG AN on 2021/10/28.
+//
+
+import SwiftUI
+
+struct ToDoDetail: View {
+    @State private var title = ""
+    @State private var date = Date()
+    @State private var description = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("Edit")
+                }
+                Spacer()
+                Text("TODO")
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Done")
+                }
+            }
+            TextField("Title", text: $title)
+                .padding()
+                .background(Color.white.shadow(color: .gray, radius: 3, x: 1, y: 4))
+            DatePicker(selection: $date, label: {})
+                .datePickerStyle(.wheel)
+                .labelsHidden()
+            TextEditor(text: $description)
+                .background(Color.white.shadow(color: .gray, radius: 3, x: 1, y: 4))
+        }
+        .padding()
+    }
+}
+
+struct ToDoDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoDetail()
+    }
+}
