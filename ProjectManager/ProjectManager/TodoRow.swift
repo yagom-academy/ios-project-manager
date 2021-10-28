d2ef1ab402bb217e9f1c01837d7b9cb28c4ec482
YongwooKim <keepingitflow@gmail.com>
2021-10-28 17:16:44 +0900

    feat: 요구조건에 맞게 제목과 본문 Text에 설정추가
    제목 : 한줄, 길면 잘라서 마지막 부분 ...
    본문 : 최대 3줄, 길면 잘라서 마지막 부분 ..., 글 크기에 맞게 높이가 맞춰짐,

--- ProjectManager/ProjectManager/TodoRow.swift
+++ ProjectManager/ProjectManager/TodoRow.swift
@@ -12,13 +12,16 @@ struct TodoRow: View {
         VStack(alignment: .leading) {
             Text("제목")
                 .font(.title3)
+                .lineLimit(1)
             Text("본문을 이렇게 많이 써볼려고 함")
                 .font(.body)
                 .foregroundColor(.gray)
                 .padding(.bottom, 1.0)
+                .lineLimit(3)
             Text("2021. 10. 28.")
                 .font(.footnote)
         }
+        .truncationMode(/*@START_MENU_TOKEN@*/.tail/*@END_MENU_TOKEN@*/)
     }
 }
 
