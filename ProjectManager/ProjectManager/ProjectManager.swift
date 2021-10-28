8379f37b140922f271986dc2df1a858586c3dd45
joey-ful <isjoeyful@gmail.com>
2021-10-28 14:40:39 +0900

    chore: SwiftUI를 하기 위한 세팅

---
+++ ProjectManager/ProjectManager/ProjectManager.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectManager.swift
+//  ProjectManager
+//
+//  Created by 홍정아 on 2021/10/28.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManager: App {
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+    
+        }
+    }
+}
