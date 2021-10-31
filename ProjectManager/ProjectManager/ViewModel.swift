d8ebdc66f09ebc1401901948b67c09ef1551c107
Ldoy <leedoy7989@gmail.com>
2021-10-31 13:33:50 +0900

    feat: Implement ListsManager Type for Model object

--- ProjectManager/ProjectManager/ViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 protocol ViewModelAble {
     associatedtype Input
@@ -17,3 +18,31 @@ protocol ViewModelAble {
     var dependency: Dependency { get }
 }
 
+//MARK: -Model
+struct ListsManager {
+    private(set) var lists: [List]
+    
+    mutating func append(list: List) {
+        self.lists.append(list)
+    }
+    
+    init() {
+        self.lists = [List(title: "제목을 써주세요",
+                           description: "해야하는 목록을 작성 해 볼까요?",
+                           date: Date(),
+                           state: .ToDo)]
+    }
+    
+    struct List {
+        var title: String
+        var description: String
+        var date: Date
+        var state: ListState
+        
+        enum ListState {
+            case ToDo
+            case Doing
+            case Done
+        }
+    }
+}
