88af084acdf56c22c620461accbe0fa613d4d7b2
Ldoy <leedoy7989@gmail.com>
2021-10-31 21:41:57 +0900

    feat: Add Navigation and ListView to MainView
    - Add DetailEventView for onTabGesture method

---
+++ ProjectManager/ProjectManager/View/DetailEventView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailEventView.swift
+//  ProjectManager
+//
+//  Created by Do Yi Lee on 2021/10/31.
+//
+
+import SwiftUI
+
+struct DetailEventView: View {
+    @State private var title: String = ""
+    var body: some View {
+
+        Text("dddd")
+    }
+}
+
+struct DetailEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEventView()
+    }
+}
--- ProjectManager/ProjectManager/View/MainView.swift
+++ ProjectManager/ProjectManager/View/MainView.swift
@@ -9,25 +9,36 @@ import SwiftUI
 
 struct MainView: View {
     @ObservedObject var viewModel = ProjectLists()
+    
     var body: some View {
-        HStack {
-            List(self.viewModel.jobs) {
-                if $0.state == .ToDo {
-                    EventView(title: $0.title, description: $0.description, date: "\($0.date)")
+        NavigationView {
+            HStack {
+                Section {
+                    List(viewModel.jobs) {
+                        if $0.state == .ToDo {
+                            EventView(title: $0.title, description: $0.description, date: "\($0.date)")
+                        }
+                    }
+                }
+                Section {
+                      List(self.viewModel.jobs) {
+                          if $0.state == .Doing {
+                              EventView(title: $0.title, description: $0.description, date: "\($0.date)")
+                          }
+                      }
+                }
+                
+                Section {
+                    List(self.viewModel.jobs) {
+                        if $0.state == .Done {
+                            EventView(title: $0.title, description: $0.description, date: "\($0.date)")
+                        }
+                    }
                 }
             }
-            List(self.viewModel.jobs) {
-                if $0.state == .Doing {
-                    EventView(title: $0.title, description: $0.description, date: "\($0.date)")
-                }
-            }
-            
-            List(self.viewModel.jobs) {
-                if $0.state == .Done {
-                    EventView(title: $0.title, description: $0.description, date: "\($0.date)")
-                }
-            }
-        }
+            .navigationBarTitle(Text("프로젝트 관리").font(.title))
+            .navigationBarTitleDisplayMode(.inline)
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
