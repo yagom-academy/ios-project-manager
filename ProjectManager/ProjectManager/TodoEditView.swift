cc800ae7f49b0e4f826dbb4999716c588552d39b
ohsg0226 <ohsg0226@naver.com>
2021-11-01 12:41:36 +0900

    feat: edtiText 추가
    - Title 추가
    - DatePicker 추가
    - description 추가

--- ProjectManager/ProjectManager/TodoEditView.swift
+++ ProjectManager/ProjectManager/TodoEditView.swift
@@ -11,6 +11,7 @@ struct TodoEditView: View {
     var body: some View {
         VStack {
             ToDoEditBar()
+            ToDoEditText()
         }
     }
 }
@@ -34,6 +35,25 @@ struct ToDoEditBar: View {
         }
         .padding()
         .background(Color.gray)
+    }
+}
+
+struct ToDoEditText: View {
+    @State private var title = ""
+    @State private var date = Date()
+    @State private var description = ""
+    var body: some View {
+        VStack {
+            TextField("Title", text: $title)
+                .padding()
+                .background(Color.white.shadow(color: .gray, radius: 3, x: 1, y: 4))
+            DatePicker(selection: $date, label: {})
+                .datePickerStyle(.wheel)
+                .labelsHidden()
+            TextEditor(text: $description)
+                .background(Color.white)
+                .shadow(color: .gray, radius: 5)
+        }
     }
 }
 
