a2ac1a1895ee0a63db439bb25c29a8725f5a8e08
hrjy6278 <hrjy6278@naver.com>
2021-11-01 21:43:42 +0900

    chore: SwiftUI 통합 및 NonFirebase Target 추가

---
+++ ProjectManager/ProjectManager/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ProjectManager
+//
+//  Created by KimJaeYoun on 2021/11/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
