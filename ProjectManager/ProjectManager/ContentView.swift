c749baf5d5ed4a01ab25bb5ed371c5c854251a65
soll4u <dasollpark@gmail.com>
2021-11-01 22:04:06 +0900

    feat: ContentView에 viewModel과 필터링된 연산프로퍼티 추가
    * todo, doing, done status에 따라 filter된 [Task] 형태로 연산프로퍼티내에 저장

--- ProjectManager/ProjectManager/ContentView.swift
+++ ProjectManager/ProjectManager/ContentView.swift
@@ -8,6 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject var taskViewModel: TaskViewModel
+    
+    var todoTask: [Task] {
+        taskViewModel.tasks.filter { task in
+            task.state == .todo
+        }
+    }
+    var doingTask: [Task] {
+        taskViewModel.tasks.filter { task in
+            task.state == .doing
+        }
+    }
+    var doneTask: [Task] {
+        taskViewModel.tasks.filter { task in
+            task.state == .done
+        }
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
