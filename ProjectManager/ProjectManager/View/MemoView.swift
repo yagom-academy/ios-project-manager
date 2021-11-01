809aecddb12512a983406e82c08e7ea16b523c2d
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-11-01 21:39:24 +0900

    feat: Added MemoView to add new memo

---
+++ ProjectManager/ProjectManager/View/MemoView.swift
@@ -0,0 +1,52 @@
+//
+//  MemoView.swift
+//  ProjectManager
+//
+//  Created by kjs on 2021/11/01.
+//
+
+import SwiftUI
+
+struct MemoView: View {
+    @State private var date = Date(timeIntervalSinceNow: 0)
+    @State private var title = ""
+    @State private var text = ""
+
+    var body: some View {
+        UINavigationBar.appearance().backgroundColor = UIColor(cgColor: Color.basic.cgColor!)
+
+        return NavigationView {
+            VStack(alignment: .center, spacing: UIStyle.minInsetAmount
+            ) {
+                TextField(
+                    "Title",
+                    text: $title
+                )
+                    .padding()
+                    .border(.red)
+
+                DatePicker(
+                    "Start date",
+                    selection: $date,
+                    displayedComponents: .date
+                )
+                    .labelsHidden()
+                    .datePickerStyle(.wheel)
+
+                TextEditor(text: $text)
+                    .padding()
+                    .border(.red)
+            }
+            .padding()
+            .navigationTitle("Edit")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct MemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoView()
+    }
+}
