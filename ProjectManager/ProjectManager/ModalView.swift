9cfe443b3e4da27073b2bfc505addcaf99a2ce18
soll4u <dasollpark@gmail.com>
2021-11-02 17:43:30 +0900

    feat: ModalView 추가

---
+++ ProjectManager/ProjectManager/ModalView.swift
@@ -0,0 +1,21 @@
+//
+//  ModalView.swift
+//  ProjectManager
+//
+//  Created by Dasoll Park on 2021/11/02.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
