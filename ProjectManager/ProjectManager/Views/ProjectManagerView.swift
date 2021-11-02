76c6575dc461314e03b357f5051f2bc85b56a9fc
YongHoonJJo <starctak@gmail.com>
2021-11-03 00:25:30 +0900

    feat: ProjectManagerView UI 구현
    - NavigationView : StackNavigationViewStyle 로 적용

---
+++ ProjectManager/ProjectManager/Views/ProjectManagerView.swift
@@ -0,0 +1,52 @@
+//
+//  ProjectManagerView.swift
+//  ProjectManager
+//
+//  Created by YongHoon JJo on 2021/11/02.
+//
+
+import SwiftUI
+
+struct ProjectManagerView: View {
+    
+    @ObservedObject private var todoListVM = TodoListViewModel()
+    
+    init() {
+        todoListVM.load()
+    }
+    
+    var body: some View {
+        NavigationView {
+            HStack {
+                TodoListView(todoStatus: .todo, todoList: todoListVM.todos)
+                
+                TodoListView(todoStatus: .doing, todoList: todoListVM.todos)
+                
+                TodoListView(todoStatus: .done, todoList: todoListVM.todos)
+            }
+            .background(Color.init(UIColor(red: 210/256,
+                                           green: 210/256,
+                                           blue: 210/256,
+                                           alpha: 1)))
+            .navigationBarTitle(Text("Project Manager"))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        print("Show Modal")
+                      }) {
+                        Image(systemName: "plus")
+                      }
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+
+    }
+}
+
+struct ProjectManagerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectManagerView()
+    }
+}
