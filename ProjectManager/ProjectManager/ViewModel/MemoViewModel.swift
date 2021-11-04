aa033082e8db94a8852e1a3be495ea8c591ba4a1
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-04 18:46:47 +0900

    feat: MemoViewModel 전체적인 틀 구현

---
+++ ProjectManager/ProjectManager/ViewModel/MemoViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  MemoViewModel.swift
+//  ProjectManager
+//
+//  Created by Kim Do hyung on 2021/11/04.
+//
+
+import Foundation
+
+protocol MemoViewModelInput {
+    func add()
+    func delete()
+    func moveColumn()
+    func modify()
+}
+
+protocol MemoViewModelOutput {
+    var memos: [[Memo]] { get }
+}
+
+final class MemoViewModel: ObservableObject {
+    
+}
