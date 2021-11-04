45ad6597aa4bcb34cbc2984c148e827521608182
blanche37 <wkddbsghk16@gmail.com>
2021-11-04 17:23:04 +0900

    refactor: SceneDelegate, AppDelegate삭제
    ProjectManagerApp 생성

---
+++ ProjectManager/ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,18 @@
+//
+//  step2App.swift
+//  step2
+//
+//  Created by yun on 2021/11/03.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+//    var object = TodoListViewModel()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()//.environmentObject(object)
+        }
+    }
+}
