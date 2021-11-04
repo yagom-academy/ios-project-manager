d4b7257aa120af70ffe41d746e1e137efe4cef07
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-11-05 04:30:02 +0900

    refactor: Added access-controls in PersistenceController
    * Deleted unnecessary parentheses

--- ProjectManager/ProjectManager/CoreData/PersistenceController.swift
+++ ProjectManager/ProjectManager/CoreData/PersistenceController.swift
@@ -9,10 +9,11 @@ import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController()
-    let container = NSPersistentContainer(name: "ProjectManager")
-
-    init() {
-        container.loadPersistentStores { (_, error) in
+    
+    private let container = NSPersistentContainer(name: "ProjectManager")
+    
+    private init() {
+        container.loadPersistentStores { _, error in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
