8569f65587ae13b08b1a5893dbdd3442d2f62865
blanche37 <wkddbsghk16@gmail.com>
2021-11-04 17:43:28 +0900

    feat: ContentView 구현

--- ProjectManager/ProjectManager/View/ContentView.swift
+++ ProjectManager/ProjectManager/View/ContentView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var viewModel: TodoListViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(alignment: .center) {
+                Spacer()
+                Text("ProjectManager")
+                    .padding(.leading, 30)
+                Spacer()
+                Button {
+                    viewModel.isPresented = true
+                } label: {
+                    TodoListImageButton(imageName: "plus")
+                        .padding(.trailing)
+                }
+            }
+            
+            HStack {
+                TodoList()
+                DoingList()
+                DoneList()
+            }
+        }
+        .sheet(isPresented: $viewModel.isPresented) {
+            AddItemView()
+        }
     }
 }
 
