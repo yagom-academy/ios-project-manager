85bf0649511c976c5393a6caf69df2b87b517b54
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-04 19:15:25 +0900

    feat: MemoViewModel 프로토콜 준수
    - MemoviewModel이 MemoViewModelInput 채택
    - MemoViewModel이 MemoViewModelOutput 채택
    - add 메서드 구현

--- ProjectManager/ProjectManager/ViewModel/MemoViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel/MemoViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol MemoViewModelInput {
-    func add()
+    func add(_ memo: Memo)
     func delete()
     func moveColumn()
     func modify()
@@ -18,7 +18,31 @@ protocol MemoViewModelOutput {
     var memos: [[Memo]] { get }
 }
 
-final class MemoViewModel: ObservableObject {
+final class MemoViewModel: ObservableObject, MemoViewModelOutput {
     @Published
     private(set) var model = MemoModel()
+    
+    var memos: [[Memo]] {
+        return model.memos
+    }
+}
+
+extension MemoViewModel: MemoViewModelInput {
+    func add(_ memo: Memo) {
+        model.add(memo)
+    }
+    
+    func delete() {
+        
+    }
+    
+    func moveColumn() {
+        
+    }
+    
+    func modify() {
+        
+    }
+    
+    
 }
