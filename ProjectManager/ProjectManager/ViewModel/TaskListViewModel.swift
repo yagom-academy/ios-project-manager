7ab2d7592b0d46e83ad14597881d4b9f0399374c
jgkim1008 <rlawnsrjs100@naver.com>
2021-11-05 19:21:27 +0900

    feat: 상태에 따른 List 별도 구현
    * fetchTask() 동작시 리스트들도 자동으로 갱신되게 fetchTask()에 filterStatus(of) 메서드 추가

--- ProjectManager/ProjectManager/ViewModel/TaskListViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel/TaskListViewModel.swift
@@ -10,22 +10,40 @@ import SwiftUI
 protocol TaskListViewModelProtocol {
     var tasks: [TLTask] { get set }
     func fetchTasks()
-
+    func filterStatus(of task: [TLTask])
 }
 
 final class TaskListViewModel: ObservableObject {
     @Published var tasks: [TLTask] = []
+    private var todoTasks: [TLTask] = []
+    private var doingTasks: [TLTask] = []
+    private var completeTasks: [TLTask] = []
+    
     private var dataManager: DataManagerProtocol
     
     init(dataManager: DataManagerProtocol = DataManager.shared) {
         self.dataManager = dataManager
         fetchTasks()
+        filterStatus(of: tasks)
     }
 }
 
 extension TaskListViewModel: TaskListViewModelProtocol {
     func fetchTasks() {
         tasks = dataManager.fetchTask
+    }
+    
+    func filterStatus(of task: [TLTask]) {
+        Status.allCases.forEach { status in
+            switch status {
+            case .TODO:
+                todoTasks = tasks.filter { $0.status == .TODO }.sorted{ $0.date < $1.date}
+            case .DOING:
+                doingTasks = tasks.filter { $0.status == .DOING }.sorted{ $0.date < $1.date}
+            case .DONE:
+                completeTasks = tasks.filter { $0.status == .DONE }.sorted{ $0.date < $1.date}
+            }
+        }
     }
 }
 
