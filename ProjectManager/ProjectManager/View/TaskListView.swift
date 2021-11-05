df4efdf6ff62c1b1af644eb3d60678bffaf6b069
jgkim1008 <rlawnsrjs100@naver.com>
2021-11-05 19:52:49 +0900

    feat: RowView에게 데이터 전달 및 RowView를 생성하는 TaskListView구현
    * TaskListViewModel에서 status에 맞는 리스트를 ForEach문을 통해 RowView를 생성

---
+++ ProjectManager/ProjectManager/View/TaskListView.swift
@@ -0,0 +1,23 @@
+//
+//  TaskListView.swift
+//  ProjectManager
+//
+//  Created by 김준건 on 2021/11/05.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    @State var memoStatus : Status
+    @ObservedObject var viewModel: TaskListViewModel
+    
+    var body: some View {
+        List{
+            Section(header: Text(memoStatus.rawValue).font(.title3).foregroundColor(.black)) {
+                ForEach(viewModel.selectTaskList(through: memoStatus), id: \.self) { task in
+                    RowView(viewModel: viewModel, task: task)
+                }
+            }
+        }
+    }
+}
