0b244de9d0f7273daaa6a932810d531417b00e1d
jgkim1008 <rlawnsrjs100@naver.com>
2021-11-05 20:37:22 +0900

    feat: Row에 TapGesture 구현
    * Row Tap시 status를 변형해줄수 있는 Button 구현
    * 기존 Tap시 Text영역만 터치가 되는것을 Space 및 contentShape(Rectangle())로 Row 전체를 Tap 되도록 변경

--- ProjectManager/ProjectManager/View/RowView.swift
+++ ProjectManager/ProjectManager/View/RowView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct RowView: View {
     @ObservedObject var viewModel: TaskListViewModel
     @State var task: TLTask
+    @State var clicked: Bool = false
+    @State var longPressed: Bool = false
     
     var body: some View {
         HStack {
@@ -19,6 +21,28 @@ struct RowView: View {
                 Text(DateFormatter.format(date: task.date)).foregroundColor(viewModel.deadlineOver(date: task.date) ? Color.red : Color.black)
             }
             Spacer()
-        }}
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            clicked.toggle()
+        }.sheet(isPresented: $clicked) {
+            ManageTaskView(viewModel: viewModel, title: task.title, date: task.date, message: task.message, task: task)
+        }
+        .onLongPressGesture {
+            longPressed.toggle()
+
+        }.popover(isPresented: $longPressed, attachmentAnchor: .point(.center), arrowEdge: .leading, content: {
+            Button {
+                viewModel.updateTaskList(task: task, status: .DOING, title: task.title, message: task.message, date: task.date)
+            } label: {
+                Text("Move to Doing").padding()
+            }
+            Button {
+                viewModel.updateTaskList(task: task, status: .DONE, title: task.title, message: task.message, date: task.date)
+            } label: {
+                Text("Move to Done").padding()
+            }
+        })
+    }
 }
 
