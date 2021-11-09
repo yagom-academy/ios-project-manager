2348b32b30414f6eb89f46bc8d24ad2c332cf009
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-10 02:09:11 +0900

    feat: MemoStroge에 find 메서드 구현

--- ProjectManager/ProjectManager/PersistentStorage/MemoStorage.swift
+++ ProjectManager/ProjectManager/PersistentStorage/MemoStorage.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+enum MemoStorageError: Error, LocalizedError {
+    case notFound
+    case multipleDuplicateResultsFound
+    
+    var errorDescription: String? {
+        switch self {
+        case .notFound:
+            return "일치하는 메모가 없습니다."
+        case .multipleDuplicateResultsFound:
+            return "다수의 중복된 메모가 발견되었습니다."
+        }
+    }
+}
+
 final class MemoStorage {
     private let coreDataStorage = CoreDataStorage.shared
     
@@ -35,5 +49,27 @@ final class MemoStorage {
                 completion(.failure(error))
             }
         }
+    }
+    
+    private func find(memo: Memo, completion: @escaping (Result<Memo, Error>) -> Void) {
+        coreDataStorage.performBackgroundTask { context in
+            let predicate = NSPredicate(format: "id == %@", memo.id as CVarArg)
+            let fetchRequest = MemoEntity.fetchRequest()
+            fetchRequest.predicate = predicate
+            
+            do {
+                let fetchResult = try context.fetch(fetchRequest)
+                guard fetchResult.count < 2 else {
+                    throw MemoStorageError.multipleDuplicateResultsFound
+                }
+                guard let memoEntity = fetchResult.first else {
+                    throw MemoStorageError.notFound
+                }
+                completion(.success(memoEntity.toDomain()))
+            } catch  {
+                completion(.failure(error))
+            }
+        }
+        
     }
 }
