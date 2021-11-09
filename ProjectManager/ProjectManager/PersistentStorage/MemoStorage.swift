791a61e19e453e0dcb7b97b665734d73fdcf7090
ictechgy <ictechgy@gmail.com>
2021-11-10 01:45:17 +0900

    feat: MemoStorage에서 fetch메서드 구현

--- ProjectManager/ProjectManager/PersistentStorage/MemoStorage.swift
+++ ProjectManager/ProjectManager/PersistentStorage/MemoStorage.swift
@@ -22,4 +22,18 @@ final class MemoStorage {
             }
         }
     }
+    
+    func fetch(completion: @escaping (Result<[Memo], Error>) -> Void) {
+        coreDataStorage.performBackgroundTask { context in
+            let fetchRequest = MemoEntity.fetchRequest()
+            
+            do {
+                let fetchResult = try context.fetch(fetchRequest)
+                let memoList = fetchResult.map { $0.toDomain() }
+                completion(.success(memoList))
+            } catch  {
+                completion(.failure(error))
+            }
+        }
+    }
 }
