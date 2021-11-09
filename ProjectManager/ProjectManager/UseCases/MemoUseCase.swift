b98e6a995093beecc959f206a2620faaa8fddd4e
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-09 21:28:08 +0900

    feat: UseCase 생성 및 기본 형태 구현

---
+++ ProjectManager/ProjectManager/UseCases/MemoUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  MemoUseCase.swift
+//  ProjectManager
+//
+//  Created by Kim Do hyung on 2021/11/09.
+//
+
+import Foundation
+
+protocol UseCase {
+    typealias Completion = (Result<Memo, Error>) -> Void
+    func add(_ memo: Memo, completion: @escaping Completion)
+    func modify(_ memo: Memo, completion: @escaping Completion)
+    func delete(_ memo: Memo, completion: @escaping Completion)
+    func move(_ memo: Memo, completion: @escaping Completion)
+}
+
+struct MemoUseCase: UseCase {
+
+    func add(_ memo: Memo, completion: @escaping Completion) {
+    }
+    
+    func modify(_ memo: Memo, completion: @escaping Completion) {
+    }
+    
+    func delete(_ memo: Memo, completion: @escaping Completion) {
+    }
+    
+    func move(_ memo: Memo, completion: @escaping Completion) {
+    }
+}
