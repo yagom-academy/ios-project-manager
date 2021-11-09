c9c4f87672683ce2ace134da94e29c6e90423699
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-10 00:44:27 +0900

    feat: MemoStorage 의 create 메서드 구현

--- ProjectManager/ProjectManager/PersistentStorage/MemoStorage.swift
+++ ProjectManager/ProjectManager/PersistentStorage/MemoStorage.swift
@@ -10,7 +10,16 @@ import Foundation
 final class MemoStorage {
     private let coreDataStorage = CoreDataStorage.shared
     
-    func add() {
-        //Memo Model -> Data Entity
+    func create(memo: Memo, completion: @escaping (Result<Memo, Error>) -> Void) {
+        coreDataStorage.performBackgroundTask { context in
+            let memoEntity = MemoEntity(memo: memo, insertInto: context)
+            
+            do {
+                try context.save()
+                completion(.success(memoEntity.toDomain()))
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
 }
