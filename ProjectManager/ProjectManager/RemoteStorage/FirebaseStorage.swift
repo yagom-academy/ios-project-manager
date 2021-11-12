bf4fb00cefb4c36dcf8b537496da24aac75b2f0e
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-12 22:16:15 +0900

    feat: FirebaseStorage fetch 메서드 구현
    - uid를 얻어 오기전에 앱이 실행되고 fetch를 제대로 해오지 못하는 문제가 있어 Fir
    evaseStorage의 CRUD 메서드를 signInAnonymously를 항상 거쳐서 실행하도록 변경

--- ProjectManager/ProjectManager/RemoteStorage/FirebaseStorage.swift
+++ ProjectManager/ProjectManager/RemoteStorage/FirebaseStorage.swift
@@ -15,49 +15,75 @@ enum FirebaseError: Error {
 }
 
 final class FirebaseStorage {
-    private var userId: String?
     private let db: Firestore = Firestore.firestore()
     private let dbCollectionRef: CollectionReference
     
     init() {
         dbCollectionRef = db.collection("Memos")
-        Auth.auth().signInAnonymously { result, _ in
-            self.userId = result?.user.uid
-        }
     }
     
     func create(_ memo: Memo, completion: @escaping (Result<Memo, Error>) -> Void) {
-        guard let uid = userId else {
-            return completion(.failure(FirebaseError.signingFailed))
-        }
-        
-        do {
-            try dbCollectionRef
-                .document(uid)
-                .collection("UserMemos")
-                .document(memo.id.uuidString)
-                .setData(from: memo)
-            completion(.success(memo))
-        } catch {
-            completion(.failure(error))
+        Auth.auth().signInAnonymously { [self] result, _ in
+            guard let uid = result?.user.uid else {
+                return completion(.failure(FirebaseError.signingFailed))
+            }
+            
+            do {
+                try dbCollectionRef
+                    .document(uid)
+                    .collection("UserMemos")
+                    .document(memo.id.uuidString)
+                    .setData(from: memo)
+                completion(.success(memo))
+            } catch {
+                completion(.failure(error))
+            }
         }
     }
     
     func delete(_ memo: Memo, completion: @escaping (Result<Memo, Error>) -> Void) {
-        guard let uid = userId else {
-            return completion(.failure(FirebaseError.signingFailed))
-        }
-        
-        dbCollectionRef
-            .document(uid)
-            .collection("UserMemos")
-            .document(memo.id.uuidString)
-            .delete { error in
-                if let error = error {
-                    completion(.failure(error))
-                } else {
-                    completion(.success(memo))
-                }
+        Auth.auth().signInAnonymously { [self] result, _ in
+            guard let uid = result?.user.uid else {
+                return completion(.failure(FirebaseError.signingFailed))
             }
+            
+            dbCollectionRef
+                .document(uid)
+                .collection("UserMemos")
+                .document(memo.id.uuidString)
+                .delete { error in
+                    if let error = error {
+                        completion(.failure(error))
+                    } else {
+                        completion(.success(memo))
+                    }
+                }
+        }
+    }
+    
+    func fetch(completion: @escaping (Result<[Memo], Error>) -> Void) {
+        Auth.auth().signInAnonymously { [self] result, _ in
+            guard let uid = result?.user.uid else {
+                return completion(.failure(FirebaseError.signingFailed))
+            }
+            
+            dbCollectionRef
+                .document(uid)
+                .collection("UserMemos")
+                .getDocuments { querySnapshot, error in
+                    if let error = error {
+                        completion(.failure(error))
+                    }
+                    guard let querySnapshot = querySnapshot else {
+                        return completion(.success([]))
+                    }
+                    let memos = querySnapshot
+                        .documents
+                        .compactMap { document in
+                            try? document.data(as: Memo.self)
+                        }
+                    completion(.success(memos))
+                }
+        }
     }
 }
