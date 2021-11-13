d7d9d5683347c508a3b4e92656ac856f721d4f75
soll4u <dasollpark@gmail.com>
2021-11-13 17:45:54 +0900

    refactor: ModalView에 나타나는 데이터를 State 프로퍼티로 변경
    * State 초기화 시 task의 값을 복사해서 넣어줌

--- ProjectManager/ProjectManager/ModalView.swift
+++ ProjectManager/ProjectManager/ModalView.swift
@@ -17,6 +17,10 @@ struct ModalView: View {
 
     @EnvironmentObject var taskViewModel: TaskViewModel
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State private var title: String
+    @State private var date: Date
+    @State private var description: String
+    @State private var state: TaskState
     
     var task: Task
     var taskIndex: Int {
@@ -24,12 +28,22 @@ struct ModalView: View {
     }
     @State var modalState: ModalState
     
+    init(modalState: ModalState, task: Task) {
+        _modalState = State(initialValue: modalState)
+        self.task = task
+        
+        _title = State(initialValue: task.title)
+        _date = State(initialValue: task.date)
+        _description = State(initialValue: task.description)
+        _state = State(initialValue: task.state)
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
                 TextField(
                     "Title",
-                    text: $taskViewModel.tasks[taskIndex].title
+                    text: $title
                 )
                 .frame(height: 44)
                 .background(Color.white)
@@ -38,7 +52,7 @@ struct ModalView: View {
                 
                 DatePicker(
                     "",
-                    selection: $taskViewModel.tasks[taskIndex].date,
+                    selection: $date,
                     displayedComponents: [.date]
                 )
                 .datePickerStyle(WheelDatePickerStyle())
@@ -46,7 +60,7 @@ struct ModalView: View {
                 
                 TextField(
                     "Description",
-                    text: $taskViewModel.tasks[taskIndex].description
+                    text: $description
                 )
                 .frame(height: 300, alignment: .top)
                 .background(Color.white)
