5588d171b783a054690b06c91c95e15a09bcd410
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-14 22:35:02 +0900

    feat: HistoryRepository 타입 기본 구현
    - HistoryUsecase와 HistoryRepositoryable 연결

---
+++ ProjectManager/ProjectManager/Repositories/HistoryRepository.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryRepository.swift
+//  ProjectManager
+//
+//  Created by Kim Do hyung on 2021/11/14.
+//
+
+import Foundation
+
+final class HistoryRepository: HistoryRepositoryable {
+    func fetch(completion: @escaping (Result<[History], Error>) -> Void) {
+        
+    }
+}
--- ProjectManager/ProjectManager/UseCases/HistoryUseCase.swift
+++ ProjectManager/ProjectManager/UseCases/HistoryUseCase.swift
@@ -12,7 +12,20 @@ protocol HistoryUseCaseable {
 }
 
 struct HistoryUseCase: HistoryUseCaseable {
+    private let repository: HistoryRepositoryable
+    
+    init(repository: HistoryRepositoryable = HistoryRepository()) {
+        self.repository = repository
+    }
+    
     func fetch(completion: @escaping (Result<[History], Error>) -> Void) {
-        
+        repository.fetch { result in
+            switch result {
+            case .success(let histories):
+                completion(.success(histories))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
