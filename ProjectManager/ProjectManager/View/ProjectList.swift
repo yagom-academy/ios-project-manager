5459c399582f03eea1013da545e6d7e5c0347a6f
Joey Hong <isjoeyful@gmail.com>
2021-11-16 12:00:10 +0900

    feat: ProjectList를 위한 ListViewModel 구현
    - ProjectList가 데이터를 지니는 대신 viewModel만 지니도록 수정
    - ListViewModel이 ProjectList를 구현하는데 필요한 데이터와 가공 로직을 지니도록 수정

--- ProjectManager/ProjectManager/View/ProjectList.swift
+++ ProjectManager/ProjectManager/View/ProjectList.swift
@@ -8,10 +8,8 @@
 import SwiftUI
 
 struct ProjectList: View {
-    @EnvironmentObject var viewModel: Project
-    var projects: [ProjectModel.Project]
-    var status: ProjectModel.Status
-    
+    @ObservedObject var viewModel: ListViewModel
+
     var body: some View {
         List {
             Section(header: header) {
@@ -19,59 +17,30 @@ struct ProjectList: View {
                     ProjectRow(project: project)
                 }
                 .onDelete(perform: { indexSet in
-                    viewModel.delete(status, at: indexSet.first)
+                    viewModel.delete(at: indexSet.first)
                 })
             }
         }
         .listStyle(GroupedListStyle())
         .background(Color(.systemGray5))
+        
     }
     
     var header: some View {
         HStack {
-            Text(status.header)
+            Text(viewModel.header)
                         .foregroundColor(.primary)
                         .font(.title)
-            Image(systemName: "\(projects.count).circle.fill")
+            Image(systemName: "\(viewModel.count).circle.fill")
                 .font(.title2)
                 .foregroundColor(.black)
         }
     }
 }
 
-struct ProjectList_Previews: PreviewProvider {
-    static var previews: some View {
-        ProjectList(projects: [
-            ProjectModel.Project(id: UUID(),
-                                 title: "책상정리",
-                                 content: "집중이 안될땐 역시나 책상정리",
-                                 dueDate: Date(),
-                                 created: Date(timeIntervalSince1970: 1649651333),
-                                 status: .todo),
-            ProjectModel.Project(id: UUID(),
-                                 title: "라자냐 재료사러 가기",
-                                 content: "프로젝트 회고를 작성하면 내가 이번 프로젝트에서 무엇을 놓쳤는지 명확히 알 수 있어요.",
-                                 dueDate: Date(timeIntervalSince1970: 1564223600),
-                                 created: Date(timeIntervalSince1970: 1538651333),
-                                 status: .todo),
-            ProjectModel.Project(id: UUID(),
-                                 title: "일기쓰기",
-                                 content: "난...ㄱㅏ끔...일ㄱㅣ를 쓴ㄷㅏ...",
-                                 dueDate: Date(timeIntervalSince1970: 1608651333),
-                                 created: Date(timeIntervalSince1970: 1588651333),
-                                 status: .todo),
-            ProjectModel.Project(id: UUID(),
-                                 title: "설거지하기",
-                                 content: "밥을 먹었으면 응당 해야할 일",
-                                 dueDate: Date(timeIntervalSince1970: 1624223600),
-                                 created: Date(timeIntervalSince1970: 1574223600),
-                                 status: .todo),
-            ProjectModel.Project(id: UUID(),
-                                 title: "빨래하기",
-                                 content: "그만 쌓아두고...\n근데...\n여전히 하기 싫다",
-                                 dueDate: Date(timeIntervalSince1970: 1694223600),
-                                 created: Date(timeIntervalSince1970: 1554223600),
-                                 status: .todo)
-        ], status: .todo)
-    }
-}
+//struct ProjectList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ProjectList(viewModel: ProjectManagerViewModel(), status: .todo)
+//            .previewLayout(.fixed(width: 1136/3, height: 820))
+//    }
+//}
---
+++ ProjectManager/ProjectManager/ViewModel/ListViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  ProjectListViewModel.swift
+//  ProjectManager
+//
+//  Created by 홍정아 on 2021/11/12.
+//
+
+import Foundation
+
+class ListViewModel: ObservableObject {
+    private weak var managerViewModel: ManagerViewModel?
+    private(set) var projects: [Project]
+    private(set) var status: Project.Status
+    
+    init(managerViewModel: ManagerViewModel,projects: [Project], status: Project.Status) {
+        self.managerViewModel = managerViewModel
+        self.projects = projects
+        self.status = status
+    }
+    
+    func delete(at index: Int?) {
+        managerViewModel?.delete(status, at: index)
+    }
+    
+    var header: String {
+        return status.header
+    }
+    
+    var count: Int {
+        return projects.count
+    }
+    
+    func update(_ newStatus: Project.Status, of projectID: UUID) {
+        managerViewModel?.update(newStatus, of: projectID)
+    }
+    
+    func update(_ project: Project, of projectID: UUID) {
+        managerViewModel?.update(project, with: projectID)
+    }
+}
