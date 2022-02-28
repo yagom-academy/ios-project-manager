22eea0236f56063434b302bbda98c08e4e081b27
yim2627 <yim2627@gmail.com>
2022-02-28 17:12:26 +0900

    feat: Storyboard 제거 및 rootVC 설정 #2
    - VC bgcolor 설정

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -1,18 +1,13 @@
-//
-//  ProjectManager - ViewController.swift
-//  Created by yagom. 
-//  Copyright © yagom. All rights reserved.
-// 
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configUI()
     }
-
-
+    
+    func configUI() {
+        self.view.backgroundColor = .white
+    }
 }
 
