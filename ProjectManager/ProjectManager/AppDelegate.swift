efd2595e63f7dba963bc8c645ec5acfa3de17e04
hoBahk <qudgh849@gmail.com>
2022-03-01 01:18:05 +0900

    feat: Firebase 초기화

--- ProjectManager/ProjectManager/AppDelegate.swift
+++ ProjectManager/ProjectManager/AppDelegate.swift
@@ -1,18 +1,11 @@
-//
-//  ProjectManager - AppDelegate.swift
-//  Created by yagom. 
-//  Copyright © yagom. All rights reserved.
-// 
-
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -1,9 +1,3 @@
-//
-//  ProjectManager - SceneDelegate.swift
-//  Created by yagom. 
-//  Copyright © yagom. All rights reserved.
-// 
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
