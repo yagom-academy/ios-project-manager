0694aa46e3ee5e41cf9a156e84815b2f7b1d4f03
yanghojoon <yanghojoon8487@gmail.com>
2022-03-01 12:47:47 +0900

    chore: SwiftLint 규칙 추가
    - Pods 파일 제외
    - unused_optional_binding 제외

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -1,18 +1,8 @@
-//
-//  ProjectManager - ViewController.swift
-//  Created by yagom. 
-//  Copyright © yagom. All rights reserved.
-// 
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
