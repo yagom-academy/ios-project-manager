4db23c98f1658b323e14175f60dff903e21551b5
Jager-yoo <y73447jh@gmail.com>
2022-03-01 13:31:13 +0900

    fix: Firebase Realtime DB 위치 재설정하고 연동 확인

--- ProjectManager/ProjectManager/ContentView.swift
+++ ProjectManager/ProjectManager/ContentView.swift
@@ -12,6 +12,12 @@ struct ContentView: View {
     var body: some View {
         Text("프로젝트 관리 앱")
             .font(.largeTitle)
+            .onAppear {
+                print("💚 화면 두두등장!")
+                let rootRef = Database.database().reference()
+                let itemRef = rootRef.child("list")
+                itemRef.setValue("되나?")
+            }
     }
 }
 
