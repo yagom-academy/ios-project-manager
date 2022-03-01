d847fcec672050d48b4aee604eb10edd93cada1c
ChaminLee <trueman9512@naver.com>
2022-03-01 18:26:37 +0900

    feat: Work 타입 구현

---
+++ ProjectManager/ProjectManager/Model/Work.swift
@@ -0,0 +1,14 @@
+//
+//  Work.swift
+//  ProjectManager
+//
+//  Created by 이차민 on 2022/03/01.
+//
+
+import Foundation
+
+struct Work {
+    let title: String
+    let description: String
+    let deadline: TimeInterval
+}
