6c4d24f943be8419a11de789a76e17304777cd9f
yanghojoon <yanghojoon8487@gmail.com>
2022-03-01 13:34:38 +0900

    test: 데이터베이스에 잘 연결되어 있는지 확인하는 테스트 진행

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -1,8 +1,13 @@
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    lazy var rootReference = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let itemReference = rootReference.child("test")
+        itemReference.setValue("데이터베이스 연동 테스트")
     }
 }
