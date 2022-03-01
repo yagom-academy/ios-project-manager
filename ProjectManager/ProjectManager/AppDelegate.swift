ba0ad3246b63a8ec32821dfd2c49dbdba33755e0
yanghojoon <yanghojoon8487@gmail.com>
2022-03-01 13:18:34 +0900

    chore: Firebase 설치 및 초기 설정

--- ProjectManager/ProjectManager/AppDelegate.swift
+++ ProjectManager/ProjectManager/AppDelegate.swift
@@ -1,11 +1,15 @@
 import UIKit
+import Firebase
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
