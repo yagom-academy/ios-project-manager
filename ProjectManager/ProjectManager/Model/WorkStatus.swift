3f152338b20a03a5f4a4f08d9a74076e96e67a0e
JeongTaek Han <smart8612@gmail.com>
2022-03-01 21:16:56 +0900

    feat: Task, WorkStatus Entity 구현 #2

---
+++ ProjectManager/ProjectManager/Model/WorkStatus.swift
@@ -0,0 +1,16 @@
+//
+//  WorkStatus.swift
+//  ProjectManager
+//
+//  Created by JeongTaek Han on 2022/03/01.
+//
+
+import Foundation
+
+enum WorkStatus {
+    
+    case todo
+    case doing
+    case done
+    
+}
