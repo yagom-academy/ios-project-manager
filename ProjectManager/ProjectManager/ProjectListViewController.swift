e7545424d4f5ee5bab040f15a0e10a9a10f31d39
zoe <kny0651@naver.com>
2022-03-02 23:32:16 +0900

    feat: ProjectListViewController 구현 #1

---
+++ ProjectManager/ProjectManager/ProjectListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ProjectListViewController.swift
+//  ProjectManager
+//
+//  Created by 권나영 on 2022/03/02.
+//
+
+import UIKit
+
+class ProjectListViewController: UIViewController {
+    
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+        configureTableViewLayout()
+    }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    func configureTableViewLayout() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+//MARK: - UITableViewDataSource
+
+extension ProjectListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
