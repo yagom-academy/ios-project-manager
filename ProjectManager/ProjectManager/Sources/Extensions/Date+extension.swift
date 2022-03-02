9e92cf79550831e9a0c8f799d72b71c0b2d838cf
Ari <lee_ari95@icloud.com>
2022-03-02 21:47:06 +0900

    feat: Date 타입 extension 추가 #3
    - 기한이 지났는지 판단하는 프로퍼티 추가
    - Date 타입을 String으로 반환해주는 프로퍼티 추가

---
+++ ProjectManager/ProjectManager/Sources/Extensions/Date+extension.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+extension Date {
+    var formattedString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        let localeID = Locale.preferredLanguages.first
+        let deviceLocale = Locale(identifier: localeID ?? "ko-kr").languageCode
+        dateFormatter.locale = Locale(identifier: deviceLocale ?? "ko-kr")
+        dateFormatter.timeZone = TimeZone.current
+        return dateFormatter.string(from: self)
+    }
+    
+    var isPastDeadline: Bool {
+        let today = Date()
+        let calendar = Calendar.current
+        return calendar.compare(today, to: self, toGranularity: .day) == .orderedDescending
+    }
+}
