d44ad561d36b0bf699e4e04d51e5a0b9ef4ba69d
GOSARI <goeunrhdms@gmail.com>
2022-03-02 19:50:21 +0900

    chore: Storyboard 삭제

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -15,6 +15,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        // ViewController를 default interface로 지정
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = SplitViewController()
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- ProjectManager/ProjectManager/SplitViewController.swift
+++ ProjectManager/ProjectManager/SplitViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplitViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
