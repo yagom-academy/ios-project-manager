7dc763638d9e53625efb91b27803733028996e62
Ari <lee_ari95@icloud.com>
2022-03-02 22:16:23 +0900

    feat: MemoryRepository에 ProjectService를 채택하여 기능 구현 #2

--- ProjectManager/ProjectManager/Sources/Models/MemoryRepository.swift
+++ ProjectManager/ProjectManager/Sources/Models/MemoryRepository.swift
@@ -7,3 +7,27 @@ final class MemoryRepository {
         self.projects = projects
     }
 }
+
+extension MemoryRepository: ProjectService {
+    func create(_ item: Project, completion: ((Project?) -> Void)? = nil) {
+        projects.append(item)
+        completion?(item)
+    }
+    
+    func update(with item: Project, completion: ((Project?) -> Void)? = nil) {
+        _ = projects.indices
+            .filter { projects[$0].id == item.id }.first
+            .flatMap {
+                projects[$0] = item
+                completion?(projects[$0])
+            }
+    }
+    
+    func delete(_ item: Project, completion: ((Project?) -> Void)? = nil) {
+        _ = projects.indices
+            .filter { projects[$0].id == item.id }.first
+            .flatMap {
+                completion?(projects.remove(at: $0))
+            }
+    }
+}
