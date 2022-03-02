c066c82ff34784cf051fb52a64027bb32ea018b7
yanghojoon <yanghojoon8487@gmail.com>
2022-03-02 15:48:50 +0900

    test: MemoryDataManager의 각 메서드 테스트 #1 - create - delete - update - 올바른 리스트에 들어가는지도 확인

---
+++ ProjectManager/ProjectManagerTests/ProjectManagerModelTests.swift
@@ -0,0 +1,87 @@
+import XCTest
+
+class ProjectManagerModelTests: XCTestCase {
+    var sut: MemoryDataManager?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = MemoryDataManager()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    // MARK: - create 메서드 검증
+    func test_create_will_make_appropriate_Work_instance_in_todo_storage() {
+        let firstWork = Work()
+        firstWork.title = "a"
+        firstWork.body = "a"
+        firstWork.date = Date()
+        sut?.create(firstWork)
+        
+        XCTAssertFalse(sut?.todoList == nil)
+        XCTAssertEqual(firstWork.title, sut?.todoList[0].title)
+    }
+    
+    func test_create_will_not_make_Work_instance_in_doing_storage() {
+        let firstWork = Work()
+        firstWork.title = "a"
+        firstWork.body = "a"
+        firstWork.date = Date()
+        sut?.create(firstWork)
+        guard let sut = sut else { return }
+
+        XCTAssertTrue(sut.doingList.isEmpty)
+    }
+    
+    func test_create_two_element_which_have_different_id() {
+        let firstWork = Work()
+        let secondWork = Work()
+
+        XCTAssertNotEqual(firstWork.id, secondWork.id)
+    }
+    
+    // MARK: - delete 메서드 검증
+    func test_delete_will_remove_right_data() {
+        let firstWork = Work()
+        let secondWork = Work()
+        sut?.create(firstWork)
+        sut?.create(secondWork)
+        try? sut?.delete(secondWork)
+        guard let sut = sut else { return }
+        
+        XCTAssertEqual(sut.todoList[0].id, firstWork.id)
+        XCTAssertEqual(sut.todoList.count, 1)
+    }
+    
+    func test_delete_will_remove_right_data_in_array_with_four_elements() {
+        let firstWork = Work()
+        let secondWork = Work()
+        let thirdWork = Work()
+        let fourthWork = Work()
+        sut?.create(firstWork)
+        sut?.create(secondWork)
+        sut?.create(thirdWork)
+        sut?.create(fourthWork)
+        try? sut?.delete(thirdWork)
+        guard let sut = sut else { return }
+        
+        XCTAssertEqual(sut.todoList[0].id, firstWork.id)
+        XCTAssertEqual(sut.todoList[1].id, secondWork.id)
+        XCTAssertEqual(sut.todoList[2].id, fourthWork.id)
+    }
+    
+    // MARK: - update 메서드 검증
+    func test_update_will_change_right_property() {
+        let firstWork = Work()
+        firstWork.title = "a"
+        firstWork.body = "가나다"
+        firstWork.date = Date()
+        sut?.create(firstWork)
+        sut?.update(firstWork, title: "b", body: "가나다", date: Date())
+        
+        XCTAssertEqual(sut?.todoList[0].title, "b")
+    }
+}
