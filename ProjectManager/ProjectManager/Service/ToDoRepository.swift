59fdc1704fb4e61242d0041b0701b6359fc977a6
chacha <45652743+ChaminLee@users.noreply.github.com>
2022-03-02 17:17:55 +0900

    feat: ToDoRepository 구현 #1

---
+++ ProjectManager/ProjectManager/Service/ToDoRepository.swift
@@ -0,0 +1,28 @@
+//
+//  ToDoRepository.swift
+//  ProjectManager
+//
+//  Created by 이차민 on 2022/03/02.
+//
+
+import Foundation
+
+class ToDoRepository: Repository {
+    private var todos = [UUID: ToDo]()
+    
+    func create(with todo: ToDo) {
+        todos[todo.id] = todo
+    }
+    
+    func fetch() -> [ToDo] {
+        return todos.map { $0.value }
+    }
+    
+    func update(with todo: ToDo) {
+        todos.updateValue(todo, forKey: todo.id)
+    }
+    
+    func delete(with todo: ToDo) {
+        todos.removeValue(forKey: todo.id)
+    }
+}
