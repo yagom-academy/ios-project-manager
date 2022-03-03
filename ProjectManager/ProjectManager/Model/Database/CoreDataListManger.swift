f5e4ad74f5cf93273e6c3a8f2dd2e352c271f8a5
July911 <min95911@nate.com>
2022-03-03 23:55:23 +0900

    chore: 파일 정리 및 컨벤션 수정 #3

---
+++ ProjectManager/ProjectManager/Model/Database/CoreDataListManger.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+final class CoredataListManger: ListManager {
+    var list: [Listable] = []
+    
+    func creatProject(attributes: [String : [Any]]) -> Listable {
+        <#code#>
+    }
+    
+    func readProject(index: Int) -> Listable {
+        <#code#>
+    }
+    
+    func updateProject(to subject: Listable, how attributes: [String : [Any]]) -> Listable {
+        <#code#>
+    }
+    
+    func deleteProject(index: Int) {
+        <#code#>
+    }
+    
+    func save() {
+        <#code#>
+    }
+    
+    
+}
---
+++ ProjectManager/ProjectManager/Model/Database/FireBaseListManager.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+final class FireBaseListManger: ListManager {
+    var list: [Listable] = [] 
+    
+    func creatProject(attributes: [String : [Any]]) -> Listable {
+        <#code#>
+    }
+    
+    func readProject(index: Int) -> Listable {
+        <#code#>
+    }
+    
+    func updateProject(to subject: Listable, how attributes: [String : [Any]]) -> Listable {
+        <#code#>
+    }
+    
+    func deleteProject(index: Int) {
+        <#code#>
+    }
+    
+    func save() {
+        <#code#>
+    }
+}
