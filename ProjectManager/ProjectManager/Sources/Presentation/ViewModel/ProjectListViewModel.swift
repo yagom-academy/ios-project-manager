70ebe47067947ed3b4d1670b7d3e1c0c1c8b4936
Ari <lee_ari95@icloud.com>
2022-03-04 15:28:09 +0900

    refactor: 직접 구현했던 Observable 제거 및 ViewModel 리팩토링 #3
    - Observable을 제거하고 PublishSubject로 타입 변경
    - 타입 수정에 따른 메소드 리팩토링

--- ProjectManager/ProjectManager/Sources/Presentation/ViewModel/ProjectListViewModel.swift
+++ ProjectManager/ProjectManager/Sources/Presentation/ViewModel/ProjectListViewModel.swift
@@ -1,4 +1,5 @@
 import Foundation
+import RxSwift
 
 final class ProjectListViewModel {
     private(set) var projects = [Project]()
@@ -9,43 +10,43 @@ final class ProjectListViewModel {
     }
 
     // MARK: - Output
-    var inserted: Observable<Bool> = Observable(false)
-    var updated: Observable<IndexPath> = Observable(IndexPath(row: .zero, section: .zero))
-    var deleted: Observable<IndexPath> = Observable(IndexPath(row: .zero, section: .zero))
-    var errorMessage: Observable<String> = Observable("")
+    var inserted = PublishSubject<Bool>()
+    var updated = PublishSubject<IndexPath>()
+    var deleted = PublishSubject<IndexPath>()
+    var errorMessage = PublishSubject<String>()
 
     // MARK: - input
     func add(_ project: Project) {
         useCase.create(project) { item in
             guard item != nil else {
-                self.errorMessage.value = "알 수 없는 에러가 발생했습니다."
+                self.errorMessage.onNext("알 수 없는 에러가 발생했습니다.")
                 return
             }
             self.projects = self.useCase.fetch()
-            self.inserted.value = !self.inserted.value
+            self.inserted.onNext(true)
         }
     }
     
     func update(_ project: Project, indexPath: IndexPath) {
         useCase.update(project) { item in
             guard item != nil else {
-                self.errorMessage.value = "존재하지 않는 Project 입니다."
+                self.errorMessage.onNext("존재하지 않는 Project 입니다.")
                 return
             }
             self.projects = self.useCase.fetch()
-            self.updated.value = indexPath
+            self.updated.onNext(indexPath)
         }
     }
     
     func delete(_ indexPath: IndexPath, completion: ((Project?) -> Void)?) {
         useCase.delete(projects[safe: indexPath.row]) { item in
             guard let item = item else {
-                self.errorMessage.value = "삭제를 실패했습니다."
+                self.errorMessage.onNext("삭제를 실패했습니다.")
                 completion?(nil)
                 return
             }
-            self.deleted.value = indexPath
             self.projects = self.useCase.fetch()
+            self.deleted.onNext(indexPath)
             completion?(item)
         }
     }
