1f46ac555e8e8811cad839d93d3009d93b835c4a
Yeonhee Ha <dev.yeha@gmail.com>
2022-03-04 18:33:42 +0900

    build: 입출력 관련 protocol 및 TaskViewModel 구현 #5

--- ProjectManager/ProjectManager/Presentation/TaskViewModel.swift
+++ ProjectManager/ProjectManager/Presentation/TaskViewModel.swift
@@ -1,3 +1,22 @@
 import Foundation
 
-final class TaskViewModel {}
+protocol TaskViewModelInput {
+    func executeFetch()
+}
+
+protocol TaskViewModelOutput {
+    var tasks: [TaskEntity] { get set }
+}
+
+final class TaskViewModel: TaskViewModelInput, TaskViewModelOutput {
+    private var useCase: TaskUseCase
+    var tasks: [TaskEntity] = []
+
+    init(useCase: TaskUseCase) {
+        self.useCase = useCase
+    }
+    
+    func executeFetch() {
+        tasks = useCase.execute()
+    }
+}
