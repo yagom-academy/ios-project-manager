225602989a9fefa9063822b25a053c452d082e8d
Yeonhee Ha <dev.yeha@gmail.com>
2022-03-04 18:29:50 +0900

    build: ViewController 네이밍 변경 및 폴더 생성 #5

---
+++ ProjectManager/ProjectManager/Presentation/TaskTablesViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class TaskTablesViewController: UIViewController {
+    var viewModel: TaskViewModel
+    
+    init(viewModel: TaskViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
