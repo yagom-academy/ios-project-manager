6d6d0b8f6f33e7f3a30b3743bc733b2c9677974b
hoBahk <qudgh849@gmail.com>
2022-03-04 19:44:23 +0900

    feat: Task 모델 구현

---
+++ ProjectManager/ProjectManager/Task.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+enum ProgressStatus {
+    case todo
+    case doing
+    case done
+}
+
+class Task: ObservableObject, Identifiable {
+    var id: UUID
+    var title: String
+    var deadline: TimeInterval
+    var description: String
+    var progressStatus: ProgressStatus
+    
+    init(title: String, description: String, deadline: Date) {
+        self.id = UUID()
+        self.title = title
+        self.description = description
+        self.deadline = deadline.timeIntervalSince1970
+        self.progressStatus = .todo
+    }
+}
