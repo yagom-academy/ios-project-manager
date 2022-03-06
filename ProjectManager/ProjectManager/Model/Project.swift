2987a41b4efffba9cb5bc024af88c8ab6e78e1c9
yeahg-dev <yeahg.dev@gmail.com>
2022-03-06 18:01:45 +0900

    feat: Project 타입 구현 #2

--- ProjectManager/ProjectManager/Model/Project.swift
+++ ProjectManager/ProjectManager/Model/Project.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+struct Project {
+    // MARK: - Property
+    private var title: String?
+    private var deadline: Date?
+    private var description: String?
+    private var status: Status
+    
+    var isExpired: Bool {
+        let currentDate = Date()
+        guard let deadline = self.deadline else {
+           return false
+        }
+        return deadline < currentDate
+    }
+    
+    // MARK: - Method
+    func readTitle() -> String? {
+        return self.title
+    }
+    
+    func readDeadline() -> Date? {
+        return self.deadline
+    }
+    
+    func readDescription() -> String? {
+        return self.description
+    }
+    
+    func readStatus() -> Status {
+        return self.status
+    }
+    
+    mutating func updateContent(with input: [String: Any]) {
+        self.title = input["title"] as? String
+        self.deadline = input["deadline"] as? Date
+        self.description = input["description"] as? String
+    }
+    
+    mutating func updateStatus(with status: Status) {
+        self.status = status
+    }
+}
