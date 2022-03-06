f81d11871c0896b74ea9398bea0374e0627ecd41
JeongTaek Han <smart8612@gmail.com>
2022-03-06 17:06:02 +0900

    feat: ProjectManager UI 레이아웃 구성 #3

--- ProjectManager/ProjectManager/View/ContentView.swift
+++ ProjectManager/ProjectManager/View/ContentView.swift
@@ -15,12 +15,63 @@ struct ContentView: View {
         
         NavigationView {
             HStack {
-                Text("Hello World")
+                todoList
+                doingList
+                doneList
             }
             .navigationTitle("Project Manager")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { print("add Button Clicked") }) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
         .navigationViewStyle(.stack)
+        
+    }
+    
+    var todoList: some View {
+        List {
+            ForEach(viewModel.todoTasks) { task in
+                TaskRowView(task: task)
+            }
+        }
+    }
+    
+    var doingList: some View {
+        List {
+            ForEach(viewModel.doingTasks) { task in
+                TaskRowView(task: task)
+            }
+        }
+    }
+    
+    var doneList: some View {
+        List {
+            ForEach(viewModel.doneTasks) { task in
+                TaskRowView(task: task)
+            }
+        }
+    }
+    
+}
+
+struct TaskRowView: View {
+    
+    let task: Task
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text(task.title)
+                .font(.headline)
+            Text(task.description)
+                .font(.subheadline)
+            Text(task.dueDate.description)
+        }
         
     }
     
