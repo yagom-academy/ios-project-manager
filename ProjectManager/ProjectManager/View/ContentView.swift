6a39f27c45616e8c12ad9c904c3da51a592c9add
JeongTaek Han <smart8612@gmail.com>
2022-03-06 15:40:30 +0900

    feat: SwiftUI View Navigation Style 구현 #3

--- ProjectManager/ProjectManager/View/ContentView.swift
+++ ProjectManager/ProjectManager/View/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            HStack {
+                Text("Hello World")
+            }
+            .navigationTitle("Project Manager")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .navigationViewStyle(.stack)
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
