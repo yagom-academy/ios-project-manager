b5c16990eec150e07f3837a2ce2f6a1670bbb6a3
yanghojoon <yanghojoon8487@gmail.com>
2022-03-06 21:33:17 +0900

    feat: ActionView의 텍스트 뷰와 필드에 그림자 구현 #2

--- ProjectManager/ProjectManager/View/ActionViewController.swift
+++ ProjectManager/ProjectManager/View/ActionViewController.swift
@@ -13,11 +13,33 @@ class ActionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        titleTextField.layer.borderColor = UIColor.systemGray6.cgColor
-        titleTextField.layer.borderWidth = 1
-        bodyTextView.layer.borderColor = UIColor.systemGray6.cgColor
-        bodyTextView.layer.borderWidth = 1
+        setupTextField()
+        setupTextView()
+    }
+    
+    private func setupTextField() {
+        configureTextFieldShadow()
+    }
+    
+    private func setupTextView() {
+        configureTextViewShadow()
+    }
+    
+    private func configureTextFieldShadow() {
+        titleTextField.layer.backgroundColor = UIColor.white.cgColor
+        titleTextField.layer.shadowColor = UIColor.black.cgColor
+        titleTextField.layer.shadowOffset = CGSize(width: 0, height: 5)
+        titleTextField.layer.shadowOpacity = 0.3
+        titleTextField.layer.shadowRadius = 5.0
+    }
+    
+    private func configureTextViewShadow() {
+        bodyTextView.layer.masksToBounds = false
+        bodyTextView.layer.backgroundColor = UIColor.white.cgColor
+        bodyTextView.layer.shadowColor = UIColor.black.cgColor
+        bodyTextView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        bodyTextView.layer.shadowOpacity = 0.3
+        bodyTextView.layer.shadowRadius = 5.0
     }
     
     @IBAction func touchUpDoneButton(_ sender: UIBarButtonItem) {
@@ -27,6 +49,7 @@ class ActionViewController: UIViewController {
         
         dismiss(animated: true, completion: nil)
     }
+    
     @IBAction func touchUpCancelButton(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
