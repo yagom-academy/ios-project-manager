ecaf9112ae15fd81d625a63b8344548f675966a8
JeongTaek Han <smart8612@gmail.com>
2022-03-06 15:11:17 +0900

    feat: ViewModel 타입 구현 #3
    - ProjectManagerViewModel.swift 파일 생성

---
+++ ProjectManager/ProjectManager/ViewModel/ProjectManagerViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  ProjectManagerViewModel.swift
+//  ProjectManager
+//
+//  Created by JeongTaek Han on 2022/03/06.
+//
+
+import SwiftUI
+
+class ProjectManagerViewModel: ObservableObject {
+    
+    @Published var model = TaskMemoryManager(tasks: projectTasks)
+    
+    var todoTasks: [Task] {
+        model.todoTasks
+    }
+    
+    var doingTasks: [Task] {
+        model.doingTasks
+    }
+    
+    var doneTasks: [Task] {
+        model.doneTasks
+    }
+    
+    func create(title: String, description: String, dueDate: Date) {
+        let newTask = Task(
+            id: UUID(),
+            title: title,
+            description: description,
+            dueDate: dueDate,
+            status: .todo
+        )
+        try? model.create(newTask)
+    }
+    
+    func remove(_ task: Task) {
+        try? model.delete(task)
+    }
+    
+    func update(_ task: Task,
+                title: String? = nil, description: String? = nil, dueDate: Date? = nil,
+                taskStatus: TaskStatus? = nil) {
+        let newTask = Task(
+            id: task.id,
+            title: title ?? task.title,
+            description: description ?? task.description,
+            dueDate: dueDate ?? task.dueDate,
+            status: taskStatus ?? task.status
+        )
+        try? model.update(task, to: newTask)
+    }
+}
+
+extension ProjectManagerViewModel {
+    
+    static let projectTasks = [
+        Task(id: UUID(), title: "Hello", description: "World", dueDate: Date(), status: .todo),
+        Task(id: UUID(), title: "My Name Is", description: "New Task", dueDate: Date(), status: .doing),
+        Task(id: UUID(), title: "Nice to", description: "Meet You", dueDate: Date(), status: .done),
+        Task(id: UUID(), title: "I Love", description: "Camel Case", dueDate: Date(), status: .todo)
+    ]
+    
+}
