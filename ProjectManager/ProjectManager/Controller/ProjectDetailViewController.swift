f0e9c1c251dbb1de5bc3c5bf3e3350c5f90fe3b5
yeahg-dev <yeahg.dev@gmail.com>
2022-03-07 17:00:49 +0900

    feat: ProjectDetailVC의 입력 UI 구현 #5

--- ProjectManager/ProjectManager/Controller/ProjectDetailViewController.swift
+++ ProjectManager/ProjectManager/Controller/ProjectDetailViewController.swift
@@ -16,6 +16,41 @@ class ProjectDetailViewController: UIViewController {
         return navigationBar
     }()
     
+    private var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.font = .preferredFont(forTextStyle: .body, compatibleWith: nil)
+        textField.placeholder = "Title"
+        return textField
+    }()
+    
+    private var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        return datePicker
+    }()
+    
+    private var descriptionTextView: UITextView = {
+        let textView = UITextView(frame: .zero)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .systemGray6
+        textView.font = .preferredFont(forTextStyle: .body)
+        return textView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleTextField, datePicker, descriptionTextView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
     override func loadView() {
         self.view = .init()
         self.view.backgroundColor = .white
@@ -24,14 +59,15 @@ class ProjectDetailViewController: UIViewController {
     override func viewDidLoad() {
         self.configureNavigationItem()
         self.configureNavigationBarLayout()
+        self.configureStackViewLayout()
     }
     
     // MARK: - Configure View
     private func configureNavigationItem() {
         let navigationItem = UINavigationItem()
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done,
-                                        target: self,
-                                        action: nil)
+                                         target: self,
+                                         action: nil)
         let editButton = UIBarButtonItem(barButtonSystemItem: .edit,
                                          target: self,
                                          action: nil)
@@ -40,12 +76,27 @@ class ProjectDetailViewController: UIViewController {
         
         navigationBar.items = [navigationItem]
     }
-   
+    
     private func configureNavigationBarLayout() {
         self.view.addSubview(navigationBar)
         let safeArea = self.view.safeAreaLayoutGuide
         navigationBar.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
         navigationBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
         navigationBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+    }
+    
+    private func configureStackViewLayout() {
+        self.view.addSubview(stackView)
+        let safeArea = self.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            descriptionTextView.topAnchor.constraint(equalTo: datePicker.bottomAnchor),
+            descriptionTextView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 15),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -15),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 15),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -15)
+        ])
     }
 }
