744602358ee571bbd21648b5c259becf7caea9fa
Jager-yoo <y73447jh@gmail.com>
2022-03-07 17:39:19 +0900

    feat: 할일 추가 버튼을 오른쪽에 배치

--- ProjectManager/ProjectManager/View/ContentView.swift
+++ ProjectManager/ProjectManager/View/ContentView.swift
@@ -21,16 +21,23 @@ struct ContentView: View {
         VStack {
             Section {
                 HStack {
+                    Image(systemName: "plus")
+                        .font(.title)
+                        .padding(.leading, 30)
+                        .hidden()
+                    Spacer()
                     Text("Project Manager")
                         .font(.title3)
                         .bold()
+                    Spacer()
                     Button {
                         print("할일 추가 버튼 눌림!")
                     } label: {
                         Image(systemName: "plus")
-                            .font(.title2)
+                            .font(.title)
                             .foregroundColor(.blue)
                     }
+                    .padding(.trailing, 30)
                 }
             }
             HStack {
