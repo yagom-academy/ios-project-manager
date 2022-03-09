7ebc907ebcfc653095284f32435ea38015216a34
hoBahk <qudgh849@gmail.com>
2022-03-09 17:55:06 +0900

    feat: 메인화면 title 및 추가버튼 설정

--- ProjectManager/ProjectManager/View/ContentView.swift
+++ ProjectManager/ProjectManager/View/ContentView.swift
@@ -1,15 +1,32 @@
-//
-//  ContentView.swift
-//  ProjectManager
-//
-//  Created by 박병호 on 2022/03/01.
-//
-
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @EnvironmentObject var viewModel: TaskListViewModel
+    @State var isPopoverPresentedForCreateTask = false
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    var title: some View {
+        HStack {
+            Spacer()
+            
+            Text("Project Manager")
+                .padding(.leading)
+            
+            Spacer()
+            
+            Button(action: {
+                isPopoverPresentedForCreateTask = true
+            }, label: {
+                Image(systemName: "plus")
+                .sheet(isPresented: $isPopoverPresentedForCreateTask) {
+                }
+            })
+        }
+        .padding()
     }
 }
 
