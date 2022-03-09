d5f789345e1820afa34564d34ed6e6982b61a68c
forestjae <forestsj30@gmail.com>
2022-03-09 17:38:19 +0900

    feat: ScheduleDetailVC 구현

--- ProjectManager/ProjectManager/Presentation/ScheduleDetail/ScheduleDetailViewController.swift
+++ ProjectManager/ProjectManager/Presentation/ScheduleDetail/ScheduleDetailViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class ScheduleDetailViewController: UIViewController {
 
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        return stackView
+    }()
+
+    private let titleTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+
+    private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        return datePicker
+    }()
+
+    private let bodyTextView: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +43,25 @@ class ScheduleDetailViewController: UIViewController {
 private extension ScheduleDetailViewController {
     func configure() {
 
+    }
+
+    func configureHierarchy() {
+        self.view.addSubview(stackView)
+        self.stackView.addArrangedSubview(titleTextField)
+        self.stackView.addArrangedSubview(datePicker)
+        self.stackView.addArrangedSubview(bodyTextView)
+    }
+
+    func configureConstraint() {
+        let safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            self.stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            self.stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            self.stackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            self.stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            self.bodyTextView.heightAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.5
+            )
+        ])
     }
 }
