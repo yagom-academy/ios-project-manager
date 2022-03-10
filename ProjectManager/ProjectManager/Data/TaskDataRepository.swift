c08fc7c2b16fe96154629e0479c5560efdd6cf91
Yeonhee Ha <dev.yeha@gmail.com>
2022-03-11 07:33:22 +0900

    refactor: TaskDataSource 삭제, TaskRepository protocol CRUD 메서드 수정 #5

--- ProjectManager/ProjectManager/Data/TaskDataRepository.swift
+++ ProjectManager/ProjectManager/Data/TaskDataRepository.swift
@@ -1,32 +1,18 @@
 import Foundation
 
 protocol TaskRepository {
-    func create(task: TaskEntity)
-    func readAll() -> [TaskEntity]
-    func update(task: TaskEntity)
-    func delete(task: TaskEntity)
+    func create(taskList: TaskListEntity, completed: @escaping (Bool) -> Void)
+    func read(completed: @escaping ([TaskListEntity]) -> Void)
+    func update(taskList: TaskListEntity, completed: @escaping (Bool) -> Void)
+    func delete(by id: UUID, completed: @escaping (Bool) -> Void)
 }
 
 class TaskDataRepository: TaskRepository {
-    private var dataSource: TaskDataSource
-    
-    init(dataSource: TaskDataSource) {
-        self.dataSource = dataSource
-    }
-    
-    func create(task: TaskEntity) {
-        dataSource.create(data: task)
-    }
-    
-    func readAll() -> [TaskEntity] {
-        return dataSource.readAll()
-    }
-    
-    func update(task: TaskEntity) {
-        dataSource.update(data: task)
-    }
-    
-    func delete(task: TaskEntity) {
-        dataSource.delete(data: task)
-    }
+    func create(taskList: TaskListEntity, completed: @escaping (Bool) -> Void) { }
+
+    func read(completed: @escaping ([TaskListEntity]) -> Void) { }
+
+    func update(taskList: TaskListEntity, completed: @escaping (Bool) -> Void) { }
+
+    func delete(by id: UUID, completed: @escaping (Bool) -> Void) { }
 }
