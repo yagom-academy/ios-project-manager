4e9d79def1361c7a73a5eecee3755d013fbb461c
yanghojoon <yanghojoon8487@gmail.com>
2022-03-11 00:16:27 +0900

    refactor: Storyboard Reference를 사용해 반복되는 UI 통합 #4

--- ProjectManager/ProjectManager/View/ProjectViewController.swift
+++ ProjectManager/ProjectManager/View/ProjectViewController.swift
@@ -3,8 +3,14 @@ import RxSwift
 import RxCocoa
 
 
+private enum UIName {
+    static let workFormViewStoryboard = "WorkFormView"
+    static let todoSegue = "todoSegue"
+    static let doingSegue = "doingSegue"
+    static let doneSegue = "doneSegue"
+}
+
 private enum Content {
-    static let workFormViewStoryboardName = "WorkFormView"
     static let todoTitle = "TODO"
     static let doingTitle = "DOING"
     static let doneTitle = "DONE"
@@ -19,22 +25,32 @@ final class ProjectViewController: UIViewController {
     
     private let viewModel = ProjectViewModel()
     private var disposeBag = DisposeBag()
-    private let formSheetViewStorboardName = Content.workFormViewStoryboardName
-    
-    @IBOutlet weak private var todoTableView: UITableView!
-    @IBOutlet weak private var doingTableView: UITableView!
-    @IBOutlet weak private var doneTableView: UITableView!
-    @IBOutlet weak private var todoTitleLabel: UILabel!
-    @IBOutlet weak private var todoCountLabel: ProjectHeaderCircleLabel!
-    @IBOutlet weak private var doingTitleLabel: UILabel!
-    @IBOutlet weak private var doingCountLabel: ProjectHeaderCircleLabel!
-    @IBOutlet weak private var doneTitleLabel: UILabel!
-    @IBOutlet weak private var doneCountLabel: ProjectHeaderCircleLabel!
+    private let formSheetViewStorboardName = UIName.workFormViewStoryboard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        setupTableViews()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let test = segue.destination as? ProjectTableViewController else { return }
+
+        switch segue.identifier {
+        case UIName.todoSegue:
+            test.titleText = Content.todoTitle
+            test.count = viewModel.todoCount
+            test.list = viewModel.todoList
+        case UIName.doingSegue:
+            test.titleText = Content.doingTitle
+            test.count = viewModel.doingCount
+            test.list = viewModel.doingList
+        case UIName.doneSegue:
+            test.titleText = Content.doneTitle
+            test.count = viewModel.doneCount
+            test.list = viewModel.doneList
+        default:
+            break
+        }
     }
     
     @IBAction private func addNewWork(_ sender: UIBarButtonItem) {
@@ -51,80 +67,6 @@ final class ProjectViewController: UIViewController {
     
     private func setupView() {
         view.backgroundColor = Design.viewBackgroundColor
-    }
-    
-    private func setupTableViews() {
-        configureTableViewCells()
-        configureTableViews()
-        configureHeaders()
-    }
-    
-    private func configureTableViewCells() {
-        let nibName = UINib(nibName: String(describing: ProjectTableViewCell.self), bundle: nil)
-        
-        todoTableView.register(nibName, cellClass: ProjectTableViewCell.self)
-        doingTableView.register(nibName, cellClass: ProjectTableViewCell.self)
-        doneTableView.register(nibName, cellClass: ProjectTableViewCell.self)
-    }
-    
-    private func configureHeaders() {
-        todoTitleLabel.text = Content.todoTitle
-        doingTitleLabel.text = Content.doingTitle
-        doneTitleLabel.text = Content.doneTitle
-        
-        _ = viewModel.todoCount
-            .subscribe(onNext: { [weak self] in
-                self?.todoCountLabel.text = $0.description
-            })
-            .disposed(by: disposeBag)
-        
-        _ = viewModel.doingCount
-            .subscribe(onNext: { [weak self] in
-                self?.doingCountLabel.text = $0.description
-            })
-            .disposed(by: disposeBag)
-        
-        _ = viewModel.doneCount
-            .subscribe(onNext: { [weak self] in
-                self?.doneCountLabel.text = $0.description
-            })
-            .disposed(by: disposeBag)
-    }
-    
-    private func configureTableViews() {
-        viewModel.todoList
-            .observe(on: MainScheduler.instance)
-            .bind(to: todoTableView.rx.items(
-                cellIdentifier: String(describing: ProjectTableViewCell.self),
-                cellType: ProjectTableViewCell.self
-            )) { _, item, cell in
-                cell.configureCellContent(for: item)
-            }
-            .disposed(by: disposeBag)
-        
-        viewModel.doingList
-            .observe(on: MainScheduler.instance)
-            .bind(to: doingTableView.rx.items(
-                cellIdentifier: String(describing: ProjectTableViewCell.self),
-                cellType: ProjectTableViewCell.self
-            )) { _, item, cell in
-                cell.configureCellContent(for: item)
-            }
-            .disposed(by: disposeBag)
-        
-        viewModel.doneList
-            .observe(on: MainScheduler.instance)
-            .bind(to: doneTableView.rx.items(
-                cellIdentifier: String(describing: ProjectTableViewCell.self),
-                cellType: ProjectTableViewCell.self
-            )) { _, item, cell in
-                cell.configureCellContent(for: item)
-            }
-            .disposed(by: disposeBag)
-        
-        todoTableView.backgroundColor = Design.tableViewBackgroundColor
-        doingTableView.backgroundColor = Design.tableViewBackgroundColor
-        doneTableView.backgroundColor = Design.tableViewBackgroundColor
     }
     
 }
