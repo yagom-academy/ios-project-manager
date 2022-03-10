9855bf303ad3793627d375041247e6bb27e2323b
JeongTaek Han <smart8612@gmail.com>
2022-03-10 16:48:22 +0900

    feat: TaskListView 타입 구현 #3
    - 실제로 Task 목록을 표현하는 List View에 대한 모듈화 진행

--- ProjectManager/ProjectManager/View/TaskListViewModule/TaskListContainerView.swift
+++ ProjectManager/ProjectManager/View/TaskListViewModule/TaskListContainerView.swift
@@ -17,47 +17,12 @@ struct TaskListContainerView: View {
     var body: some View {
         VStack(alignment: .leading) {
             TaskListTitleView(taskType: taskType)
-            taskList
+            TaskListView(taskType: taskType, isShowSheet: $isShowSheet)
         }
-    }
-    
-    private var taskList: some View {
-        List {
-            ForEach(tasks) { task in
-                Button(action: {
-                    self.isShowSheet.toggle()
-                }) {
-                    TaskRowView(task: task)
-                        .contextMenu {
-                            contextMenuView(task, taskType)
-                        }
-                        .sheet(isPresented: $isShowSheet, onDismiss: nil) {
-                            TaskDetailView(
-                                task: task,
-                                isShowSheet: $isShowSheet
-                            )
-                        }
-                }
-            }
-            .onDelete { indexSet in
-                viewModel.remove(tasks[indexSet])
-            }
-        }
-        
     }
     
     private var tasks: [Task] {
         viewModel.findTasks(of: taskType)
-    }
-    
-    private func contextMenuView(_ task: Task, _ taskType: TaskStatus) -> some View {
-        ForEach(TaskStatus.allCases) { status in
-            if status != taskType {
-                Button(action: { viewModel.update(task, taskStatus: status) }) {
-                    Text("Move to \(status.description)")
-                }
-            }
-        }
     }
     
 }
---
+++ ProjectManager/ProjectManager/View/TaskListViewModule/TaskListView.swift
@@ -0,0 +1,55 @@
+//
+//  TaskListView.swift
+//  ProjectManager
+//
+//  Created by JeongTaek Han on 2022/03/10.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    
+    let taskType: TaskStatus
+    
+    @EnvironmentObject private var viewModel: ProjectManagerViewModel
+    @Binding var isShowSheet: Bool
+    
+    var body: some View {
+        List {
+            ForEach(tasks) { task in
+                Button(action: {
+                    self.isShowSheet.toggle()
+                }) {
+                    TaskRowView(task: task)
+                        .contextMenu {
+                            contextMenuView(task, taskType)
+                        }
+                        .sheet(isPresented: $isShowSheet, onDismiss: nil) {
+                            TaskDetailView(
+                                task: task,
+                                isShowSheet: $isShowSheet
+                            )
+                        }
+                }
+            }
+            .onDelete { indexSet in
+                viewModel.remove(tasks[indexSet])
+            }
+        }
+    }
+    
+    private var tasks: [Task] {
+        viewModel.findTasks(of: taskType)
+    }
+    
+    private func contextMenuView(_ task: Task, _ taskType: TaskStatus) -> some View {
+        ForEach(TaskStatus.allCases) { status in
+            if status != taskType {
+                Button(action: { viewModel.update(task, taskStatus: status) }) {
+                    Text("Move to \(status.description)")
+                }
+            }
+        }
+    }
+    
+}
