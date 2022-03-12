37a2c8b5574fb1efa384fcd1ca40d81e6cdf688d
Yeonhee Ha <dev.yeha@gmail.com>
2022-03-12 16:22:19 +0900

    fix: fetchTaskRequest 메서드에 sortDescriptors 추가 #5

--- ProjectManager/ProjectManager/Data/TaskDataModel+CoreDataProperties.swift
+++ ProjectManager/ProjectManager/Data/TaskDataModel+CoreDataProperties.swift
@@ -4,18 +4,14 @@ import CoreData
 
 extension TaskDataModel {
     @nonobjc public class func fetchTaskRequest() -> NSFetchRequest<TaskDataModel> {
-        return NSFetchRequest<TaskDataModel>(entityName: "TaskDataModel")
+        let fetchRequest = NSFetchRequest<TaskDataModel>(entityName: "TaskDataModel")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: false)]
+        return fetchRequest
     }
 
     @nonobjc public class func fetchTaskRequest(with id: UUID) -> NSFetchRequest<TaskDataModel> {
         let fetchRequest = TaskDataModel.fetchTaskRequest()
         fetchRequest.predicate = NSPredicate(format: "id == %@", id.uuidString)
-        return fetchRequest
-    }
-
-    @nonobjc public class func fetchSortedTaskRequest() -> NSFetchRequest<TaskDataModel> {
-        let fetchRequest = TaskDataModel.fetchTaskRequest()
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: false)]
         return fetchRequest
     }
 
