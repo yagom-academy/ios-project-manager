19c8cf48a97996f0627712610888f9bd2f015ab5
Yeonhee Ha <dev.yeha@gmail.com>
2022-03-12 16:14:32 +0900

    build: TaskDataModel 메서드 추가 구현 #5

--- ProjectManager/ProjectManager/Data/TaskDataModel+CoreDataProperties.swift
+++ ProjectManager/ProjectManager/Data/TaskDataModel+CoreDataProperties.swift
@@ -7,7 +7,13 @@ extension TaskDataModel {
         return NSFetchRequest<TaskDataModel>(entityName: "TaskDataModel")
     }
 
-    @nonobjc public class func fetchSortedNoteRequest() -> NSFetchRequest<TaskDataModel> {
+    @nonobjc public class func fetchTaskRequest(with id: UUID) -> NSFetchRequest<TaskDataModel> {
+        let fetchRequest = TaskDataModel.fetchTaskRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", id.uuidString)
+        return fetchRequest
+    }
+
+    @nonobjc public class func fetchSortedTaskRequest() -> NSFetchRequest<TaskDataModel> {
         let fetchRequest = TaskDataModel.fetchTaskRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: false)]
         return fetchRequest
--- ProjectManager/ProjectManager/Data/TaskDataRepository.swift
+++ ProjectManager/ProjectManager/Data/TaskDataRepository.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 protocol TaskRepository {
+    func saveContext()
+
     func create(taskList: TaskListEntity, completed: @escaping (Bool) -> Void)
     func read(completed: @escaping ([TaskListEntity]) -> Void)
     func update(taskList: TaskListEntity, completed: @escaping (Bool) -> Void)
@@ -8,6 +10,10 @@ protocol TaskRepository {
 }
 
 class TaskDataRepository: TaskRepository {
+    func saveContext() {
+        <#code#>
+    }
+
     func create(taskList: TaskListEntity, completed: @escaping (Bool) -> Void) { }
 
     func read(completed: @escaping ([TaskListEntity]) -> Void) { }
