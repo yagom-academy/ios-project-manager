4128871a396b044b800713c74ad5b79efc3388f8
Ari <lee_ari95@icloud.com>
2022-03-16 15:59:52 +0900

    test: ViewModel 테스트 코드 기본 세팅 #3

--- ProjectManager/ProjectManagerTests/ProjectListViewModelTests.swift
+++ ProjectManager/ProjectManagerTests/ProjectListViewModelTests.swift
@@ -1,15 +1,26 @@
 import XCTest
 import RxBlocking
+import RxSwift
+import RxTest
 
 class ProjectListViewModelTests: XCTestCase {
 
-    var sut: ProjectListViewModel?
+    var viewModel: ProjectListViewModel?
+    var disposeBag: DisposeBag!
+    var scheduler: TestScheduler!
     
     override func setUpWithError() throws {
-        
+        self.viewModel = ProjectListViewModel(coordinator: MainCoordinator())
+        self.disposeBag = DisposeBag()
+        self.scheduler = TestScheduler(initialClock: 0)
     }
     
     override func tearDownWithError() throws {
-        sut = nil
+        self.viewModel = nil
+        self.disposeBag = nil
+    }
+    
+    func test_() {
+        
     }
 }
