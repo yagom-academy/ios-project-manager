fbd3caf44a241d501ea39e5d5420dce9e87193b4
namu <simjh27@gmail.com>
2022-03-18 14:23:46 +0900

    feat: NetworkCheck 연결상태 모니터링 기능 구현

---
+++ ProjectManager/ProjectManager/Utility/NetworkCheck.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  ProjectManager
+//
+//  Created by Lee Seung-Jae on 2022/03/16.
+//
+
+import Foundation
+import RxSwift
+import Network
+
+final class NetworkCheck {
+    static let shared = NetworkCheck()
+    private let queue = DispatchQueue.global()
+    private let monitor: NWPathMonitor
+
+    private(set) var isConnected: Observable<Bool>
+
+    private init() {
+        self.monitor = NWPathMonitor()
+        self.isConnected = Observable.just(false)
+    }
+
+    func startMonitoring() {
+        let connectingObservable = BehaviorSubject<Bool>(value: false)
+        self.isConnected = connectingObservable
+
+        self.monitor.start(queue: queue)
+        self.monitor.pathUpdateHandler = { path in
+            connectingObservable.onNext(path.status == .satisfied)
+        }
+    }
+
+    func stopMonitoring() {
+        self.monitor.cancel()
+    }
+}
