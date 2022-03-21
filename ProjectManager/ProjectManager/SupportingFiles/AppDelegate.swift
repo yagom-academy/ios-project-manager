d69dfeaf397fa95107a841bdd10702c7129d672f
JeongTaek Han <smart8612@gmail.com>
2022-03-21 11:08:58 +0900

    feat: app delegate 로 모델 객체 이동 #6

--- ProjectManager/ProjectManager/SupportingFiles/AppDelegate.swift
+++ ProjectManager/ProjectManager/SupportingFiles/AppDelegate.swift
@@ -10,7 +10,7 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let model = TaskRepositoryManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,6 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        try? model.sync()
     }
 
 
