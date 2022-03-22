e21598922fdf777cd961df872e49214047b4f26d
JeongTaek Han <smart8612@gmail.com>
2022-03-23 08:57:32 +0900

    feat: NetworkMonitor 타입 구현 #6

---
+++ ProjectManager/ProjectManager/SupportingFiles/NetworkMonitor.swift
@@ -0,0 +1,56 @@
+//
+//  NetworkMonitor.swift
+//  ProjectManager
+//
+//  Created by JeongTaek Han on 2022/03/23.
+//
+
+import Foundation
+import Network
+
+final class NetworkMonitor {
+    
+    static let shared = NetworkMonitor()
+    
+    private let queue = DispatchQueue.global()
+    private let monitor: NWPathMonitor
+    
+    private(set) var isConnected = false
+    private(set) var connectionType: ConnectionType = .unknown
+    
+    private init() {
+        monitor = NWPathMonitor()
+    }
+    
+    enum ConnectionType {
+        case wifi
+        case cellular
+        case ethernet
+        case unknown
+    }
+    
+    func startMonitoring() {
+        monitor.start(queue: queue)
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.isConnected = path.status == .satisfied
+            self?.getConnectionType(path)
+        }
+    }
+    
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+    
+    func getConnectionType(_ path: NWPath) {
+        if path.usesInterfaceType(.wifi) {
+            connectionType = .wifi
+        } else if path.usesInterfaceType(.cellular) {
+            connectionType = .cellular
+        } else if path.usesInterfaceType(.wiredEthernet) {
+            connectionType = .ethernet
+        } else {
+            connectionType = .unknown
+        }
+    }
+    
+}
