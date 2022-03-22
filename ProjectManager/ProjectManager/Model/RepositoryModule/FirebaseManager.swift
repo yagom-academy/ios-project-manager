6bb0556c3e9c8ade1b6671666c9b5897ddd28067
JeongTaek Han <smart8612@gmail.com>
2022-03-22 11:31:32 +0900

    feat: FirebaseManager 구현 #6

--- ProjectManager/ProjectManager/Model/RepositoryModule/FirebaseManager.swift
+++ ProjectManager/ProjectManager/Model/RepositoryModule/FirebaseManager.swift
@@ -8,35 +8,55 @@
 import Foundation
 import Firebase
 
-final class FirebaseManager<Element: Codable>: RemoteRepositoryManager {
+final class FirebaseManager: RemoteRepositoryManager {
    
     private let database = Firestore.firestore()
     
     private let collectionLink = "tasks"
-    private let documentLink = "task"
     
-    func create(_ object: Element, completionHandler: (Result<Data, Error>) -> Void) {
-        
+    func create(_ object: FirebaseTask) async throws {
+        let reference = database.collection(collectionLink).document(object.id.uuidString)
+        try await reference.setData([
+            "id": object.id.uuidString,
+            "title": object.title,
+            "description": object.description,
+            "dueDate": object.dueDate,
+            "status": object.status
+        ])
     }
     
-    func fetch(completionHandler: (Result<[Element], Error>) -> Void) {
-        
+    func fetch() async throws -> [FirebaseTask] {
+        let reference = database.collection(collectionLink)
+        let taskDocuments = try await reference.getDocuments()
+        let result = taskDocuments.documents.map { snapshot -> FirebaseTask in
+            let data = snapshot.data()
+            return FirebaseTask(
+                id: UUID(uuidString: (data["id"] as? String) ?? "") ?? UUID(),
+                title: (data["title"] as? String) ?? "",
+                description: (data["description"] as? String) ?? "",
+                dueDate: (data["dueDate"] as? Date) ?? Date(),
+                status: (data["status"] as? Int) ?? Int()
+            )
+        }
+        return result
     }
     
-    func fetch(queryHandler: (Element) -> Bool, completionHandler: (Result<[Element], Error>) -> Void) {
-        
+    func update(_ object: FirebaseTask) async throws {
+        try await remove(object)
+        try await create(object)
     }
     
-    func update(_ object: Element, completionHandler: (Result<Data, Error>) -> Void) {
-        
+    func remove(_ object: FirebaseTask) async throws {
+        let reference = database.collection(collectionLink).document(object.id.uuidString)
+        try await reference.delete()
     }
     
-    func remove(_ object: Element, completionHandler: (Result<Data, Error>) -> Void) {
-        
-    }
-    
-    func removeAll(completionHandler: (Result<Data, Error>) -> Void) {
-        
+    func removeAll() async throws {
+        let reference = database.collection(collectionLink)
+        let documents = try await reference.getDocuments()
+        documents.documents.forEach { snapshot in
+            snapshot.reference.delete()
+        }
     }
     
 }
--- ProjectManager/ProjectManager/Model/RepositoryModule/RemoteRepositoryManager.swift
+++ ProjectManager/ProjectManager/Model/RepositoryModule/RemoteRepositoryManager.swift
@@ -9,16 +9,10 @@ import Foundation
 
 protocol RemoteRepositoryManager {
     
-    associatedtype Element
-    
-    func create(_ object: Element, completionHandler: (Result<Data, Error>) -> Void)
-    
-    func fetch(completionHandler: (Result<[Element], Error>) -> Void)
-    func fetch(queryHandler: (Element) -> Bool, completionHandler: (Result<[Element], Error>) -> Void)
-    
-    func update(_ object: Element, completionHandler: (Result<Data, Error>) -> Void)
-    
-    func remove(_ object: Element, completionHandler: (Result<Data, Error>) -> Void)
-    func removeAll(completionHandler: (Result<Data, Error>) -> Void)
+    func create(_ object: FirebaseTask) async throws
+    func fetch() async throws -> [FirebaseTask]
+    func update(_ object: FirebaseTask) async throws
+    func remove(_ object: FirebaseTask) async throws
+    func removeAll() async throws
     
 }
