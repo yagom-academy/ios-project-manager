7f1ee75b2aaa0f163b57f3a0a37e65772dcbadb1
JeongTaek Han <smart8612@gmail.com>
2022-03-23 09:08:19 +0900

    feat: 네트워크 감지 설정 추가 #6

--- ProjectManager/ProjectManager/SupportingFiles/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SupportingFiles/SceneDelegate.swift
@@ -57,8 +57,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        guard let model = appDelegate?.model else { return }
-        try? model.sync()
+        if NetworkMonitor.shared.connectionType != .unknown {
+            guard let model = appDelegate?.model else { return }
+            try? model.sync()
+        }
     }
 
 
