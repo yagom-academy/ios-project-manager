6480ec715e3ffd3b061adc800de68a4ae0410783
Yeonhee Ha <dev.yeha@gmail.com>
2022-03-27 02:05:01 +0900

    refactor: TaskTableView custom class 생성 및 updateUI메서드 구현 #8
    - TaskTableView custom class 생성
    - TaskTableViewCell 내 updateUI메서드 구현
    - 네이밍 수정

--- ProjectManager/ProjectManager/Presentation/View/TaskTableView.swift
+++ ProjectManager/ProjectManager/Presentation/View/TaskTableView.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+final class TaskTableView: UITableView {}
+
 final class TaskTableViewCell: UITableViewCell, Reusable {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var bodyLabel: UILabel!
@@ -12,5 +14,11 @@ final class TaskTableViewCell: UITableViewCell, Reusable {
         bodyLabel.text = nil
         dateLabel = nil
         isSelected = false
+    }
+
+    func updateUI(title: String, body: String, dueDate: String) {
+        self.titleLabel?.text = title
+        self.bodyLabel?.text = body
+        self.dateLabel?.text = dueDate
     }
 }
