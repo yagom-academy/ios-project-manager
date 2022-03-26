234bc71413702c5f0e15c753da542fb5c56dc0f1
Yeonhee Ha <dev.yeha@gmail.com>
2022-03-27 02:02:28 +0900

    refactor: TaskViewModel, TaskCelViewModel protocol 생성 및 구현 #5
    - Collection View Cell에서 사용될 TaskCelViewModel protocol 생성 및 구현
    - Collection View에서 사용될 TaskCelViewModel을 채택한 TaskViewModel 생성 및 구현

--- ProjectManager/ProjectManager/Presentation/ViewModel/TaskViewModel.swift
+++ ProjectManager/ProjectManager/Presentation/ViewModel/TaskViewModel.swift
@@ -1,21 +1,83 @@
-
 import Foundation
 
-protocol TaskViewModel {
-    var onUpdated: () -> Void { get set }
+protocol TaskViewModel: TaskCellViewModel {
+    func countOfTaskList() -> Int
+    func titleOfTaskList(by index: Int) -> String
 
-    var taskLists: [TaskList] { get set }
-    var countOfTaskList: Int { get }
+    func loadTaskList()
+    func didChangeData(updatedData: [TaskListModel])
 
-    func fetchTaskList(at index: Int) -> TaskList?
-    func fetchTaskList(by listName: String) -> TaskList?
-    func fetchTask(at index: Int, in listTitle: String) -> Task?
-
-    func reloadTaskList()
     func addNewTaskList(with title: String)
     func updateTaskList(_ taskList: TaskList)
     func deleteTaskList(by id: String)
 
-    func createTask(_ task: Task, in taskList: TaskList)
+    func addNewTask(title: String, dueDate: String, body: String, in taskListIndex: Int)
+    func updateTask(_ task: Task, in taskListIndex: Int)
+    func deleteTask(taskID: String, in taskListIndex: Int)
+}
 
+extension TaskViewModel {
+    func countOfTaskList() -> Int { taskLists.count }
+
+    func titleOfTaskList(by index: Int) -> String { taskLists[safe: index]?.title ?? "" }
+
+    func loadTaskList() {
+        repository.read { [weak self] allTaskList in
+            guard let self = self else { return }
+            let convertedTaskList = self.convertToTaskList(from: allTaskList)
+            self.taskLists = convertedTaskList
+        }
+    }
+
+    func didChangeData(updatedData: [TaskListModel]) {
+        self.taskLists = convertToTaskList(from: updatedData)
+        self.observer?.updated()
+    }
+
+    func addNewTaskList(with title: String) {
+        let newTaskListModel = TaskListModel(title: title, creationDate: Date())
+        repository.create(taskListModel: newTaskListModel) { [weak self] success in
+            guard success, let self = self else { return }
+            self.loadTaskList()
+        }
+    }
+
+    func updateTaskList(_ taskList: TaskList) {
+        let convertedTaskListModel = self.convertToTaskListModel(from: taskList)
+        repository.update(taskListModel: convertedTaskListModel) { [weak self] success in
+            guard success, let self = self else { return }
+            self.loadTaskList()
+        }
+    }
+
+    func deleteTaskList(by id: String) {
+        guard let id = UUID(uuidString: id) else { return }
+        repository.delete(by: id) { [weak self] success in
+            guard success, let self = self else { return }
+            self.loadTaskList()
+        }
+    }
+
+    func addNewTask(title: String, dueDate: String, body: String, in taskListIndex: Int) {
+        guard var taskList = fetchTaskList(by: searchTaskListID(by: taskListIndex)) else { return }
+        let task = Task(title: title, body: body, dueDate: dueDate)
+        taskList.items.append(task)
+        updateTaskList(taskList)
+    }
+
+    func updateTask(_ task: Task, in taskListIndex: Int) {
+        guard var taskList = fetchTaskList(by: searchTaskListID(by: taskListIndex)) else { return }
+        if let index = taskList.items.firstIndex(where: { $0.id == task.id }) {
+            taskList.items[index] = task
+        }
+        updateTaskList(taskList)
+    }
+
+    func deleteTask(taskID: String, in taskListIndex: Int) {
+        guard var taskList = fetchTaskList(by: searchTaskListID(by: taskListIndex)) else { return }
+        if let index = taskList.items.firstIndex(where: { $0.id == taskID }) {
+            taskList.items.remove(at: index)
+        }
+        updateTaskList(taskList)
+    }
 }
