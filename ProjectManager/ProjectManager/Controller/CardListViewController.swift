0448c86d8b56b511fd7becce67303a266f6dd829
Lingo <llingo@kakao.com>
2022-07-05 01:04:42 +0900

    feat: [메인 화면] CardListViewController 생성 #3
    close #3

--- ProjectManager/ProjectManager/Controller/CardListViewController.swift
+++ ProjectManager/ProjectManager/Controller/CardListViewController.swift
@@ -6,9 +6,44 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class CardListViewController: UIViewController {
+  private let todoTableView = UITableView()
+  private let doingTableView = UITableView()
+  private let doneTableView = UITableView()
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  private func configureUI() {
+    view.backgroundColor = .systemGray4
+    let tableViews = [todoTableView, doingTableView, doneTableView]
+    tableViews.forEach { $0.backgroundColor = .systemGray5 }
+    
+    let containerStackView = UIStackView(arrangedSubviews: tableViews)
+    containerStackView.axis = .horizontal
+    containerStackView.spacing = 20.0
+    containerStackView.distribution = .fillEqually
+    containerStackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    view.addSubview(containerStackView)
+    NSLayoutConstraint.activate([
+      containerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      containerStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      containerStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      containerStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+    ])
   }
 }
