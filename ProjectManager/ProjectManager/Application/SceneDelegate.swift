67037914a0b00afae95f813e3c62bd50fc1775d2
Quokkaa <rktrkdml@gmail.com>
2022-07-05 08:33:25 +0900

    chore: 스토리보드 제거 및 iOS target 설정

---
+++ ProjectManager/ProjectManager/Application/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  ProjectManager - SceneDelegate.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = UINavigationController.init(rootViewController: ViewController())
+    window?.makeKeyAndVisible()
+  }
+}
+
---
+++ ProjectManager/ProjectManager/Scene/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectManager - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
