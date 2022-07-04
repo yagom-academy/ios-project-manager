409b521d785159b5b07ddef2d8c7fd82cf9a362f
Lingo <llingo@kakao.com>
2022-07-04 14:04:52 +0900

    chore: 프로젝트 환경 설정 및 컨벤션 적용 #1
    close #1

---
+++ ProjectManager/ProjectManager/Controller/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  ProjectManager
+//
+//  Created by Lingo on 2022/07/04.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
