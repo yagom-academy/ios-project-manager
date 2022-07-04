4518d524d8e0f19fa5d50f33c2337ccdc3a3cbb2
Taeangel <wngks2050@gmail.com>
2022-07-04 22:56:44 +0900

    chore: SwiftUI인 ContentView로 rootView 변경 및 스토리보드 제거

---
+++ ProjectManager/ProjectManager/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ProjectManager
+//
+//  Created by Red and Taeangel on 2022/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -5,6 +5,7 @@
 // 
 
 import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,6 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+      
+      if let windowSecene = (scene as? UIWindowScene) {
+                  let contentView = ContentView()
+                  window = UIWindow(windowScene: windowSecene)
+                  window?.rootViewController = UIHostingController(rootView: contentView)
+                  window?.makeKeyAndVisible()
+              }
+      
+      
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
