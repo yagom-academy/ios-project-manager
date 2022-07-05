6bb98960111bae3337c7ea5b2716aa402411af5b
doogie97 <chris_97@naver.com>
2022-07-06 00:26:50 +0900

    ⚰️del : 쓸모없는 코드 삭제

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -18,20 +18,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
 }
 
