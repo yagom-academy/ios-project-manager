f35d7ca166888f296242ac606072a5bd37c34fe6
Lingo <llingo@kakao.com>
2022-07-05 11:14:08 +0900

    feat: DEBUG 모드일때만 사용될 Card 샘플 데이터 추가

--- ProjectManager/ProjectManager/Model/Card.swift
+++ ProjectManager/ProjectManager/Model/Card.swift
@@ -20,3 +20,26 @@ struct Card {
   var deadlineDate: Date
   var cardType: CardType = .todo
 }
+
+#if DEBUG
+extension Card {
+  static let day: TimeInterval = 60 * 60 * 24
+  static let sample = [
+    Card(title: "할일1", description: "할일 설명", deadlineDate: Date().advanced(by: day), cardType: .todo),
+    Card(title: "할일2", description: "할일 설명", deadlineDate: Date().advanced(by: -day), cardType: .done),
+    Card(title: "할일3", description: "할일 설명", deadlineDate: Date().advanced(by: day), cardType: .done),
+    Card(title: "할일4", description: "할일 설명", deadlineDate: Date(), cardType: .todo),
+    Card(title: "할일5", description: "할일 설명", deadlineDate: Date().advanced(by: -day), cardType: .doing),
+    Card(title: "할일6", description: "할일 설명", deadlineDate: Date(), cardType: .todo),
+    Card(title: "할일7", description: "할일 설명", deadlineDate: Date().advanced(by: -day), cardType: .doing),
+    Card(title: "할일8", description: "할일 설명", deadlineDate: Date().advanced(by: day), cardType: .done),
+    Card(title: "할일9", description: "할일 설명", deadlineDate: Date().advanced(by: -day), cardType: .todo),
+  ]
+}
+#endif
+
+
+
+
+
+
