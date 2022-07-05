2339617e9a3e6b185c2b5be2b286a940e8bb92f2
Eddy <kimkyunghun3@naver.com>
2022-07-06 01:46:53 +0900

    feat: TodoListCell 내부 요소 구현 #3

--- ProjectManager/ProjectManager/TodoList/TodoListCell.swift
+++ ProjectManager/ProjectManager/TodoList/TodoListCell.swift
@@ -12,6 +12,41 @@ final class TodoListCell: UITableViewCell {
         return String(describing: Self.self)
     }
     
+    private let contentsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .preferredFont(forTextStyle: .title3)
+        
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .preferredFont(forTextStyle: .title3)
+        
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.setUpCell()
@@ -22,27 +57,31 @@ final class TodoListCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        
-        label.font = .preferredFont(forTextStyle: .title3)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
     private func setUpCell() {
-        self.addSubview(titleLabel)
+        self.contentView.addSubview(self.contentsStackView)
+        self.contentView.addSubview(self.dateLabel)
     }
     
     private func setUpLayout() {
+        self.contentsStackView.addArrangedSubviews(with: [titleLabel, descriptionLabel])
+        
         NSLayoutConstraint.activate([
-            self.titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            self.titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            self.contentsStackView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.contentsStackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.contentsStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            self.contentsStackView.bottomAnchor.constraint(equalTo: self.dateLabel.topAnchor, constant: -5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            self.dateLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.dateLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            self.dateLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
         ])
     }
     
     func configure(_ todo: Todo) {
         self.titleLabel.text = todo.title
+        self.descriptionLabel.text = todo.description
+        self.dateLabel.text = todo.date
     }
 }
