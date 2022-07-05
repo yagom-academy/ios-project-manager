32a3d67060b8061ef7fd5182b2fe6262a5550f4e
JoSH0318 <jsh9870@naver.com>
2022-07-05 22:27:32 +0900

    feat: MainVC mainView 프로퍼티, setUpNavigationItem메서드 구현

--- ProjectManager/ProjectManager/Presentation/Main/View/MainViweController.swift
+++ ProjectManager/ProjectManager/Presentation/Main/View/MainViweController.swift
@@ -7,4 +7,31 @@
 
 import UIKit
 
-final class MainViweController { }
+final class MainViweController: UIViewController {
+    private let mainView = MainView(frame: .zero)
+    
+    override func loadView() {
+        super.loadView()
+        
+        view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpNavigationItem()
+    }
+    
+    private func setUpNavigationItem() {
+        navigationItem.title = "Project Manager"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(presentDetailView)
+        )
+    }
+    
+    @objc func presentDetailView() {
+        
+    }
+}
