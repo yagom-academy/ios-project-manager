d10c715606c5db2ad1cece63f762d25180836c20
Eddy <kimkyunghun3@naver.com>
2022-07-05 12:01:38 +0900

    feat: Todo 모델 구현

---
+++ ProjectManager/ProjectManager/Model/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  ProjectManager
+//
+//  Created by Eddy on 2022/07/05.
+//
+
+import Foundation
+
+struct Todo {
+    let title: String
+    let body: String
+    let date: Date
+}
