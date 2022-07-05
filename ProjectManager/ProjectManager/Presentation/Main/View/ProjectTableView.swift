2fa987d5d90e6b412cc58c319ad33bd7058ce98c
JoSH0318 <jsh9870@naver.com>
2022-07-05 20:37:46 +0900

    refactor: 불필요한 setUpAttribute메서드 삭제

--- ProjectManager/ProjectManager/Presentation/Main/View/ProjectTableView.swift
+++ ProjectManager/ProjectManager/Presentation/Main/View/ProjectTableView.swift
@@ -11,14 +11,12 @@ final class ProjectTableView: UITableView {
     init() {
         super.init(frame: .zero, style: .grouped)
         
-        setUpAttribute()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setUpAttribute() {
         
     }
 }
