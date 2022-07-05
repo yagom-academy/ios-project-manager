5d5a90101d60109ee9a4ca28da686dc48778842f
Eddy <kimkyunghun3@naver.com>
2022-07-06 01:48:15 +0900

    feat: TodoListViewController에서 StackView 안에 TableView들 구현 #3

--- ProjectManager/ProjectManager/TodoList/TodoListViewController.swift
+++ ProjectManager/ProjectManager/TodoList/TodoListViewController.swift
@@ -11,11 +11,27 @@ import RxSwift
 
 final class TodoListViewController: UIViewController {
     private var todoView: UITableView
+    private var doingView: UITableView
+    private var doneView: UITableView
     private let viewModel: TodoListViewModel
     private let disposeBag = DisposeBag()
     
+    private let todoListStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+        stackView.backgroundColor = .lightGray
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
     init() {
         self.todoView = UITableView()
+        self.doingView = UITableView()
+        self.doneView = UITableView()
         self.viewModel = TodoListViewModel()
         super.init(nibName: nil, bundle: nil)
     }
@@ -32,15 +48,18 @@ final class TodoListViewController: UIViewController {
     }
     
     private func setUpView() {
-        self.view.addSubview(self.todoView)
         self.todoView.register(TodoListCell.self, forCellReuseIdentifier: TodoListCell.identifier)
-        self.todoView.translatesAutoresizingMaskIntoConstraints = false
+        self.doingView.register(TodoListCell.self, forCellReuseIdentifier: TodoListCell.identifier)
+        self.doneView.register(TodoListCell.self, forCellReuseIdentifier: TodoListCell.identifier)
+        
+        self.view.addSubview(self.todoListStackView)
+        self.todoListStackView.addArrangedSubviews(with: [self.todoView, self.doingView, self.doneView])
         
         NSLayoutConstraint.activate([
-            self.todoView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            self.todoView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            self.todoView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            self.todoView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+            self.todoListStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.todoListStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.todoListStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.todoListStackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
@@ -57,6 +76,34 @@ final class TodoListViewController: UIViewController {
     private func bind() {
         self.viewModel.tableViewData?
             .bind(to: self.todoView.rx.items) { tabelView, row, element in
+                guard let cell = tabelView.dequeueReusableCell(
+                    withIdentifier: TodoListCell.identifier,
+                    for: IndexPath(row: row, section: .zero)) as? TodoListCell
+                else {
+                    return UITableViewCell()
+                }
+                cell.configure(element)
+                
+                return cell
+            }
+            .disposed(by: disposeBag)
+        
+        self.viewModel.tableViewData?
+            .bind(to: self.doingView.rx.items) { tabelView, row, element in
+                guard let cell = tabelView.dequeueReusableCell(
+                    withIdentifier: TodoListCell.identifier,
+                    for: IndexPath(row: row, section: .zero)) as? TodoListCell
+                else {
+                    return UITableViewCell()
+                }
+                cell.configure(element)
+                
+                return cell
+            }
+            .disposed(by: disposeBag)
+        
+        self.viewModel.tableViewData?
+            .bind(to: self.doneView.rx.items) { tabelView, row, element in
                 guard let cell = tabelView.dequeueReusableCell(
                     withIdentifier: TodoListCell.identifier,
                     for: IndexPath(row: row, section: .zero)) as? TodoListCell
