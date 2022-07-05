a715fdd8a7fb13c1dfc0f81a40384320ec1ee9be
marisol <marisol.develop@gmail.com>
2022-07-05 11:49:36 +0900

    ✨Feat : SwiftUI로 변경

---
+++ ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by marisol on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
