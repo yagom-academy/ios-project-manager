ed7245828e0a4e05936786f6799b24bbd23d5c9e
Red <seryprk@gmail.com>
2022-07-05 10:40:26 +0900

    feat: firebase API 추가

--- ProjectManager/ProjectManager/AppDelegate.swift
+++ ProjectManager/ProjectManager/AppDelegate.swift
@@ -5,12 +5,14 @@
 // 
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
     return true
   }
   
