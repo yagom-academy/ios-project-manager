5ea1ae4ecee0d25a4a446451cb5c3640a71501ef
dudu <whqtkf12@naver.com>
2022-07-06 23:21:59 +0900

    chore: 더미데이터 수정

--- ProjectManager/ProjectManager/Domain/Entities/TodoListModel.swift
+++ ProjectManager/ProjectManager/Domain/Entities/TodoListModel.swift
@@ -18,19 +18,14 @@ struct TodoListModel: Hashable {
 extension TodoListModel {
     static func dummyData() -> [TodoListModel] {
         [
-            TodoListModel(title: "책상정리1", content: "더미데이터더미데이터\n더미데이터더미데이터\n더미데이터더미데이터\n더미데이터더미데이터", deadLine: Date(), processType: .doing),
-            TodoListModel(title: "라자냐 재료사러 가기2", content: "TIL을 작성하면\n오늘의 상큼한 마무리도 되고\n 나중에 포트폴리오 용으로도 좋죠!", deadLine: Date(), processType: .done),
-            TodoListModel(title: "일기쓰기3", content: "더미데이터더미데이터", deadLine: Date(), processType: .todo),
-            TodoListModel(title: "설거지하기4", content: "더미데이터더미데이터", deadLine: Date(), processType: .doing),
-            TodoListModel(title: "빨래하기5", content: "더미데이터더미데이터", deadLine: Date(), processType: .doing),
-            TodoListModel(title: "더미데이터입니다6", content: "더미데이터더미데이터", deadLine: Date(), processType: .doing),
-            TodoListModel(title: "더미데이터입니다7", content: "더미데이터더미데이터", deadLine: Date(), processType: .done),
-            TodoListModel(title: "더미데이터입니다8", content: "더미데이터더미데이터", deadLine: Date(), processType: .todo),
-            TodoListModel(title: "더미데이터입니다9", content: "더미데이터더미데이터", deadLine: Date(), processType: .doing),
-            TodoListModel(title: "더미데이터입니다10", content: "더미데이터더미데이터", deadLine: Date(), processType: .todo),
-            TodoListModel(title: "더미데이터입니다11", content: "더미데이터더미데이터", deadLine: Date(), processType: .doing),
-            TodoListModel(title: "더미데이터입니다12", content: "더미데이터더미데이터", deadLine: Date(), processType: .done),
-            TodoListModel(title: "더미데이터입니다13", content: "더미데이터더미데이터", deadLine: Date(), processType: .doing),
+            TodoListModel(title: "책상정리", content: "집중이 안될땐 역시나 책상정리", deadLine: Date(), processType: .todo),
+            TodoListModel(title: "라자냐 재료사러 가기", content: "프로젝트 회고를 작성하면 내가 이번 프로젝트에서\n무엇을 놓쳤는지 명확히 알 수 있어요", deadLine: Date(), processType: .done),
+            TodoListModel(title: "일기쓰기", content: "난.. ㄱ ㅏ끔...읽ㄱㅣ를 쓴ㄷㅏ...", deadLine: Date(), processType: .todo),
+            TodoListModel(title: "설거지하기", content: "밥을 먹었으면 응당 해야할 일", deadLine: Date(), processType: .doing),
+            TodoListModel(title: "빨래하기", content: "그만 쌓아두고...\n근데...\n여전히 하기 싫다", deadLine: Date(), processType: .doing),
+            TodoListModel(title: "TIL 작성하기", content: "TIL을 작성하면\n오늘의 상큼한 마무리도 되고\n나중에 포트폴리오 용으로도 좋죠!", deadLine: Date(), processType: .doing),
+            TodoListModel(title: "프로젝트 회고 작성", content: "프로젝트 회고를 작성하면 내가 이번 프로젝트에서\n무엇을 놓쳤는지 명확히 알 수 있어요", deadLine: Date(), processType: .done),
+            TodoListModel(title: "마이노는 맨날 리액션만 하고 있어요. 혼내주세요 진짜로 마이노는 맨날 리액션만 하고 있어요. 혼내주세요 진짜로", content: "굿!", deadLine: Date(), processType: .doing),
         ]
     }
 }
