0fdbe75a9b135b927b67cfe6cb7d8f0d7afe0b14
marisol <marisol.develop@gmail.com>
2022-07-06 18:16:45 +0900

    ✨Feat : RegisterView와 RegisterElementsView 생성

---
+++ ProjectManager/View/RegisterElementsView.swift
@@ -0,0 +1,56 @@
+//
+//  RegisterElementsView.swift
+//  ProjectManager
+//
+//  Created by OneTool, marisol on 2022/07/06.
+//
+
+import SwiftUI
+
+struct RegisterElementsView: View {
+    @State private var title = ""
+    @State private var date = Date()
+    @State private var text: String = "Enter Text"
+    @State private var font: UIFont = UIFont()
+    
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: date)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: date)!
+        return min...max
+    }
+    
+    var body: some View {
+        VStack {
+            TextField("Title", text: $title)
+                .foregroundColor(Color.gray)
+                .padding(.all)
+                .border(Color(UIColor.separator))
+                .padding(.leading)
+                .padding(.trailing)
+                .font(.title2)
+                
+            DatePicker("",
+                       selection: $date,
+                       in: dateRange,
+                       displayedComponents: [.date])
+                .datePickerStyle(.wheel)
+                .labelsHidden()
+            
+            TextEditor(text: $text)
+                .foregroundColor(Color.gray)
+                .lineSpacing(5)
+                .frame(minWidth: 0,
+                       maxWidth: .infinity,
+                       minHeight: 0,
+                       maxHeight: 200)
+                .border(Color.yellow, width: 1)
+        }
+    }
+}
+
+struct RegisterElementsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterElementsView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
---
+++ ProjectManager/View/RegisterView.swift
@@ -0,0 +1,46 @@
+//
+//  RegisterView.swift
+//  ProjectManager
+//
+//  Created by OneTool, marisol on 2022/07/06.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    
+    var body: some View {
+        NavigationView {
+            RegisterElementsView()
+            .navigationTitle("TODO")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarColor(.systemGray5)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        print("Cancelbutton")
+                    }) {
+                        Text("Cancel")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        print("Donebutton")
+                    }) {
+                        Text("Done")
+                    }
+                }
+            }
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
