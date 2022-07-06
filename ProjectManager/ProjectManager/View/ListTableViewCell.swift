4823b3c5b72f295e8e860e45578c0fed7bd154d1
doogie97 <chris_97@naver.com>
2022-07-06 15:04:35 +0900

    💎feat : ListTableViewCell 생성

---
+++ ProjectManager/ProjectManager/View/ListTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  ListTableViewCell.swift
+//  ProjectManager
+//
+//  Created by 두기 on 2022/07/06.
+//
+
+import UIKit
+
+final class ListTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setInitailView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [topStackView,
+                                                       deadlineLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 5
+
+        return stackView
+    }()
+    
+    private lazy var topStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel,
+                                                      bodyLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 3
+        
+        return stackView
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title2)
+        
+        return label
+    }()
+    
+    lazy var bodyLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        label.numberOfLines = 3
+        
+        return label
+    }()
+    
+    lazy var deadlineLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private func setInitailView() {
+        self.addSubview(mainStackView)
+        mainStackView.snp.makeConstraints{
+            $0.edges.equalToSuperview().inset(15)
+        }
+    }
+}
