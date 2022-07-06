fcd4ded07e2cb3d5a77ce93638db3442c70ceee1
Donnie <seo_17@naver.com>
2022-07-06 21:40:12 +0900

    chore: MainView 코드 컨벤션 준수

--- ProjectManager/ProjectManager/Scene/View/MainView.swift
+++ ProjectManager/ProjectManager/Scene/View/MainView.swift
@@ -8,45 +8,49 @@
 import UIKit
 
 final class MainView: UIView {
-    let todoHeaderView = WorkHeaderView(workType: .todo)
-    let doingHeaderView = WorkHeaderView(workType: .doing)
-    let doneHeaderView = WorkHeaderView(workType: .done)
-    private lazy var baseStackView = UIStackView(
-        arrangedSubviews: [
-            todoStackView,
-            doingStackView,
-            doneStackView
-        ]
-    ).then {
-        $0.axis = .horizontal
-        $0.spacing = 15
-        $0.distribution = .fillEqually
-    }
-    private lazy var todoStackView = UIStackView(arrangedSubviews: [
-        todoHeaderView,
-        todoTableView
-    ]).then {
-        $0.axis = .vertical
-    }
-    
-    private lazy var doingStackView = UIStackView(arrangedSubviews: [
-        doingHeaderView,
-        doingTableView
-    ]).then {
-        $0.axis = .vertical
-    }
-    
-    private lazy var doneStackView = UIStackView(arrangedSubviews: [
-        doneHeaderView,
-        doneTableView
-    ]).then {
-        $0.axis = .vertical
-    }
+    private let todoHeaderView = WorkHeaderView(workType: .todo)
+    private let doingHeaderView = WorkHeaderView(workType: .doing)
+    private let doneHeaderView = WorkHeaderView(workType: .done)
     
     lazy var todoTableView = UITableView()
     lazy var doingTableView = UITableView()
     lazy var doneTableView = UITableView()
     
+    private lazy var baseStackView = UIStackView(
+        arrangedSubviews: [
+            todoStackView,
+            doingStackView,
+            doneStackView
+        ]).then {
+            $0.axis = .horizontal
+            $0.spacing = 15
+            $0.distribution = .fillEqually
+        }
+    
+    private lazy var todoStackView = UIStackView(
+        arrangedSubviews: [
+            todoHeaderView,
+            todoTableView
+        ]).then {
+            $0.axis = .vertical
+        }
+    
+    private lazy var doingStackView = UIStackView(
+        arrangedSubviews: [
+            doingHeaderView,
+            doingTableView
+        ]).then {
+            $0.axis = .vertical
+        }
+    
+    private lazy var doneStackView = UIStackView(
+        arrangedSubviews: [
+            doneHeaderView,
+            doneTableView
+        ]).then {
+            $0.axis = .vertical
+        }
+
     func setupSubViews() {
         addSubview(baseStackView)
         backgroundColor = .systemGray4
@@ -57,15 +61,16 @@ final class MainView: UIView {
             $0.top.equalTo(safeAreaLayoutGuide)
             $0.leading.trailing.bottom.equalToSuperview()
         }
+        
+        let baseStackViewHeight = baseStackView.snp.height
         todoHeaderView.snp.makeConstraints {
-            $0.height.equalTo(80)
+            $0.height.equalTo(baseStackViewHeight).multipliedBy(0.1)
         }
         doingHeaderView.snp.makeConstraints {
-            $0.height.equalTo(80)
+            $0.height.equalTo(baseStackViewHeight).multipliedBy(0.1)
         }
         doneHeaderView.snp.makeConstraints {
-            $0.height.equalTo(80)
+            $0.height.equalTo(baseStackViewHeight).multipliedBy(0.1)
         }
-        
     }
 }
