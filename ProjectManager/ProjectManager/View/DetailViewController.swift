a6e53e5796ad245ade8127c0b1c140fa9ad209da
doogie97 <chris_97@naver.com>
2022-07-06 17:03:57 +0900

    💎feat : DetailViewController 생성

---
+++ ProjectManager/ProjectManager/View/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  ProjectManager
+//
+//  Created by 두기 on 2022/07/06.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+    }
+}
