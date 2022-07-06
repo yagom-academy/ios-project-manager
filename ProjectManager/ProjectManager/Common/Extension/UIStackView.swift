df8bd9a20c9423ad676d68642ec0b7ab657b7160
dudu <whqtkf12@naver.com>
2022-07-06 17:09:09 +0900

    fix: 잘못된 매서드 호출 수정

--- ProjectManager/ProjectManager/Common/Extension/UIStackView.swift
+++ ProjectManager/ProjectManager/Common/Extension/UIStackView.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIStackView {
     func addArrangeSubviews(_ views: UIView...) {
         views.forEach {
-            addArrangeSubviews($0)
+            addArrangedSubview($0)
         }
     }
 }
