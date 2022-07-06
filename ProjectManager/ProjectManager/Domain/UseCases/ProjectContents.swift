e67d4c18fbce4cfb25789f28b8ab518dc3ce811e
Tiana <97kth01@naver.com>
2022-07-06 11:35:16 +0900

    feat: ProjectContents 구현

---
+++ ProjectManager/ProjectManager/Domain/UseCases/ProjectContents.swift
@@ -0,0 +1,46 @@
+//
+//  ProjectContents.swift
+//  ProjectManager
+//
+//  Created by 김태현 on 2022/07/06.
+//
+
+import Foundation
+
+struct ProjectContents {
+    var title: String
+    var deadline: String
+    var description: String
+    
+    init(_ item: ProjectItem) {
+        self.title = item.title
+        self.deadline = DateFormatter().formatted(date: item.deadline)
+        self.description = item.description
+    }
+    
+    func asProjectItem() -> ProjectItem? {
+        guard let deadline = DateFormatter().formatted(string: deadline) else {
+            return nil
+        }
+        return ProjectItem(title: title, deadline: deadline, description: description)
+    }
+}
+
+fileprivate extension DateFormatter {
+    private var dateFormatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .long
+        dateFormatter.locale = .autoupdatingCurrent
+        
+        return dateFormatter
+    }
+    
+    func formatted(string: String) -> Date? {
+        return dateFormatter.date(from: string)
+    }
+    
+    func formatted(date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+}
