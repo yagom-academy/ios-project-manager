4fbe2b503915ef39b5b4a0facf6864bf20a16733
Lingo <llingo@kakao.com>
2022-07-06 11:15:36 +0900

    feat: [메인 화면] TableView와 HeaderView를 합친 SectionView 구현 #20

--- ProjectManager/ProjectManager/Scene/MainScene/View/CardSectionView.swift
+++ ProjectManager/ProjectManager/Scene/MainScene/View/CardSectionView.swift
@@ -5,4 +5,35 @@
 //  Created by Lingo on 2022/07/06.
 //
 
-import Foundation
+import UIKit
+
+import Then
+
+final class CardSectionView: UIStackView {
+  let headerView: CardListHeaderView
+  let tableView = UITableView().then {
+    $0.backgroundColor = .systemGray6
+  }
+  
+  init(sectionType: CardType) {
+    headerView = CardListHeaderView(cardType: sectionType)
+    super.init(frame: .zero)
+  }
+  
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - UI Configuration
+
+extension CardSectionView {
+  private func configureSubViews() {
+    addArrangedSubview(headerView)
+    addArrangedSubview(tableView)
+  }
+  
+  private func configureLayouts() {
+    axis = .vertical
+  }
+}
