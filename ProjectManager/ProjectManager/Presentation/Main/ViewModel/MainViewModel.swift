d15e6177ca6c0cba7eeb7443fdb3c764d358f43c
JoSH0318 <jsh9870@naver.com>
2022-07-06 15:47:51 +0900

    refactor: totalCount 프로퍼티, transform 메서드 구현

--- ProjectManager/ProjectManager/Presentation/Main/ViewModel/MainViewModel.swift
+++ ProjectManager/ProjectManager/Presentation/Main/ViewModel/MainViewModel.swift
@@ -9,16 +9,42 @@ import RxSwift
 import RxRelay
 
 class MainViewModel {
+    let toDoTableProjects = BehaviorRelay<[ProjectContents]>(value: [])
+    let totalCount: Observable<String>
+    let disposeBag = DisposeBag()
+    
+    init() {
+        self.totalCount = toDoTableProjects
+            .map { projectContents in
+                projectContents.count
+            }
+            .map { String($0) }
+    }
+    
     struct Input {
-        let saveButtonTapEvent: Observable<Void>
         let cellTapEvent: Observable<IndexPath>
     }
     
     struct Output {
+        let content: Observable<ProjectContents?>
     }
     
     func transform(input: Input) -> Output {
+        let content = input.cellTapEvent
+            .map { [weak self] indexPath -> ProjectContents? in
+                guard let content = self?.toDoTableProjects.value[safe: indexPath.row] else {
+                    return nil
+                }
+                
+                return content
+            }
         
-        return Output()
+        return Output(content: content)
+    }
+}
+
+private extension Collection {
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
