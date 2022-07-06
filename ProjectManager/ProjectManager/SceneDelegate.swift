1962e71a841bc1ab99c160bc4075da4341763458
saafaaari <111dltldnjs@naver.com>
2022-07-07 00:59:04 +0900

    feat: Medel 타입 구현

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -10,10 +10,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
-   
-
-
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
@@ -48,7 +44,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
---
+++ ProjectManager/ProjectManager/TodoModel.swift
@@ -0,0 +1,14 @@
+//
+//  TodoModel.swift
+//  ProjectManager
+//
+//  Created by 이시원 on 2022/07/07.
+//
+
+import Foundation
+
+struct TodoMedel {
+    let title: String
+    let body: String
+    let createAt: Date
+}
