ef5d882af83ca74224006cb0897397a7738f95e6
Tiana <97kth01@naver.com>
2022-07-06 11:33:26 +0900

    feat: ProjectItem 구현

---
+++ ProjectManager/ProjectManager/Domain/Entity/ProjectItem.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectItem.swift
+//  ProjectManager
+//
+//  Created by Tiana, mmim on 2022/07/06.
+//
+
+import Foundation
+
+struct ProjectItem {
+    var title: String
+    var deadline: Date
+    var description: String
+}
