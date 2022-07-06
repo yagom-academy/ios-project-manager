d62b1190735f0199a3a8f027c8bf91d643c2a8e0
Taeangel <wngks2050@gmail.com>
2022-07-07 01:05:44 +0900

    refactor: ProjectManagerView의 NavigationBar 수정
    navigationViewStyle(.stack)을 추가하여 문제 해결

--- ProjectManager/ProjectManager/View/CreateView.swift
+++ ProjectManager/ProjectManager/View/CreateView.swift
@@ -11,51 +11,27 @@ struct CreateView: View {
   @State var title: String
   @State var date: Date
   @State var content: String
-  @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-  
-    var body: some View {
+  var body: some View {
+    NavigationView {
       VStack {
-        HStack {
-          Button(
-            action: {
-              self.presentationMode.wrappedValue.dismiss()
-          }, label: {
-            Text("Cancel")
-          })
-          .padding()
-          Spacer()
-          Text("TODO")
-            .font(.body)
-            .fontWeight(.bold)
-          Spacer()
-          Button(
-            action: {
-              self.presentationMode.wrappedValue.dismiss()
-          }, label: {
-            Text("Done")
-          })
-          .padding()
-        }
-        .background(Color(UIColor.systemGray5))
-        
-        VStack {
-          ZStack {
-            Rectangle()
-              .fill(.white)
-              .shadow(color: .gray, radius: 3, x: 0, y: 1)
-              .frame(height: 30)
-            TextField("title", text: $title)
-              .padding(.leading)
-          }
-          
-          DatePicker("", selection: $date)
-            .datePickerStyle(.wheel)
-            .labelsHidden()
-          TextEditor(text: $content)
+        ZStack {
+          Rectangle()
+            .fill(.white)
             .shadow(color: .gray, radius: 3, x: 0, y: 1)
+            .frame(height: 30)
+          TextField("title", text: $title)
+            .padding(.leading)
         }
-        .padding()
+        
+        DatePicker("", selection: $date)
+          .datePickerStyle(.wheel)
+          .labelsHidden()
+        TextEditor(text: $content)
+          .shadow(color: .gray, radius: 3, x: 0, y: 1)
       }
-      
+      .padding()
+      .navigationTitle("TODO")
+      .font(.body)
     }
+  }
 }
--- ProjectManager/ProjectManager/View/ProjectManagerView.swift
+++ ProjectManager/ProjectManager/View/ProjectManagerView.swift
@@ -8,20 +8,34 @@
 import SwiftUI
 
 struct ProjectManagerView: View {
+  @State private var showCreatView = false
+  
   var body: some View {
-    VStack(spacing: 10) {
-      TodoNavigationView()
+    NavigationView {
       HStack(spacing: 10) {
-          TodoListView(status: .todo)
-            .environmentObject(TodoViewModel())
-          
-          TodoListView(status: .doing)
-            .environmentObject(TodoViewModel())
-          
-          TodoListView(status: .done)
-            .environmentObject(TodoViewModel())
-        }
+        TodoListView(status: .todo)
+          .environmentObject(TodoViewModel())
+        
+        TodoListView(status: .doing)
+          .environmentObject(TodoViewModel())
+        
+        TodoListView(status: .done)
+          .environmentObject(TodoViewModel())
+      }
       .background(Color(UIColor.systemGray4))
+      .navigationTitle("Project Manager")
+      .font(.title)
+      .toolbar {
+        Button(action: {
+          showCreatView.toggle()
+        }, label: {
+          Image(systemName: "plus")
+        })
+      }
+      .sheet(isPresented: $showCreatView) {
+        CreateView(title: "", date: Date(), content: "")
+      }
     }
+    .navigationViewStyle(.stack)
   }
 }
