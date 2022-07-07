876aef971b48fbebb0bb8c1b45eaff649ad063c3
JoSH0318 <jsh9870@naver.com>
2022-07-07 11:47:47 +0900

    feat: class DetailViewModel 구현

---
+++ ProjectManager/ProjectManager/Presentation/Detail/View/DetailViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewModel.swift
+//  ProjectManager
+//
+//  Created by Tiana, mmim on 2022/07/07.
+//
+
+import RxSwift
+import RxRelay
+
+class DetailViewModel {
+    let content: Observable<ProjectContent>
+    let disposeBag = DisposeBag()
+    
+    init(content: Observable<ProjectContent>) {
+        self.content = content
+    }
+}
