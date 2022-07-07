004a4c2590cedd0a64adf78fd79c7c2050e40176
saafaaari <111dltldnjs@naver.com>
2022-07-07 21:20:58 +0900

    feat: ViewModel Output 구현

--- ProjectManager/ProjectManager/Presentation/TodoListViewModel.swift
+++ ProjectManager/ProjectManager/Presentation/TodoListViewModel.swift
@@ -13,7 +13,9 @@ protocol TodoListViewModelInput {
 }
 
 protocol TodoListViewModelOutput {
-    
+    var todoList: Observable<[TodoMedel]> { get }
+    var doingList: Observable<[TodoMedel]> { get }
+    var doneList: Observable<[TodoMedel]> { get }
 }
 
 protocol TodoListViewModel: TodoListViewModelInput, TodoListViewModelOutput {}
@@ -21,5 +23,18 @@ protocol TodoListViewModel: TodoListViewModelInput, TodoListViewModelOutput {}
 final class DefaultTodoListViewModel: TodoListViewModel {
     let listItems = BehaviorSubject<[TodoMedel]>(value: TodoMedel.makeDummy())
     
+    private(set) lazy var todoList = listItems
+        .map { items in
+            items.filter { $0.state == .todo }
+        }
     
+    private(set) lazy var doingList = listItems
+        .map { items in
+            items.filter { $0.state == .doing }
+        }
+    
+    private(set) lazy var doneList = listItems
+        .map { items in
+            items.filter { $0.state == .done }
+        }
 }
