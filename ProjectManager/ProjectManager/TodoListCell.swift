a1c01845832e18a563f8da7e40cd2107897a301c
saafaaari <111dltldnjs@naver.com>
2022-07-07 12:51:01 +0900

    feat: TableViewCell 구현

---
+++ ProjectManager/ProjectManager/TodoListCell.swift
@@ -0,0 +1,55 @@
+//
+//  TodoListCell.swift
+//  ProjectManager
+//
+//  Created by 이시원 on 2022/07/07.
+//
+
+import UIKit
+import SnapKit
+
+final class TodoListCell: UITableViewCell {
+    private lazy var labelStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, bodyLabel, deadlineLabel])
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title2)
+        
+        return label
+    }()
+    
+    let bodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.numberOfLines = 3
+        
+        return label
+    }()
+    
+    let deadlineLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureLayout() {
+        self.addSubview(labelStackView)
+        labelStackView.snp.makeConstraints { make in
+            make.edges.equalTo(self.safeAreaLayoutGuide)
+        }
+    }}
