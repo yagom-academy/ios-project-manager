ced3de957a3ed7675d90c806e6e8a0f434e926f6
Tiana <97kth01@naver.com>
2022-07-07 11:09:17 +0900

    feat: ProjectTable 타입 구현

---
+++ ProjectManager/ProjectManager/Presentation/Main/View/ProjectTable.swift
@@ -0,0 +1,104 @@
+//
+//  ProjectTable.swift
+//  ProjectManager
+//
+//  Created by Tiana, mmim on 2022/07/07.
+//
+
+import UIKit
+
+final class ProjectTable: UIStackView {
+    let headerView: HeaderView
+    let tableView = UITableView()
+    
+    init(title: String) {
+        self.headerView = HeaderView(title: title)
+        super.init(frame: .zero)
+        
+        axis = .vertical
+        addArrangedSubview(headerView)
+        addArrangedSubview(tableView)
+        
+        registerCell()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func registerCell() {
+        tableView.register(ProjectCell.self, forCellReuseIdentifier: "\(ProjectCell.self)")
+    }
+}
+
+final class HeaderView: UIView {
+    private let listTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .largeTitle)
+        label.sizeToFit()
+        return label
+    }()
+    
+    lazy var countLabel: UILabel = {
+        let label = UILabel()
+        
+        func round() {
+            label.layer.masksToBounds = true
+            label.layer.cornerRadius = 20
+        }
+        
+        func text() {
+            label.textColor = .white
+            label.text = "0"
+            label.textAlignment = .center
+            label.font = .preferredFont(forTextStyle: .body)
+        }
+        
+        round()
+        text()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .black
+        
+        return label
+    }()
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        setUpLayout()
+        setUpTitle(title: title)
+        backgroundColor = .systemGray5
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpLayout() {
+        addSubview(listTitleLabel)
+        addSubview(countLabel)
+        
+        NSLayoutConstraint.activate([
+            listTitleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            listTitleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            listTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10)
+        ])
+        
+        NSLayoutConstraint.activate([
+            countLabel.topAnchor.constraint(equalTo: listTitleLabel.topAnchor, constant: 2),
+            countLabel.bottomAnchor.constraint(equalTo: listTitleLabel.bottomAnchor, constant: -2),
+            countLabel.leadingAnchor.constraint(equalTo: listTitleLabel.trailingAnchor, constant: 10),
+            countLabel.widthAnchor.constraint(equalTo: countLabel.heightAnchor, constant: 2)
+        ])
+    }
+    
+    private func setUpTitle(title: String) {
+        listTitleLabel.text = title
+    }
+    
+    func setUpCountLabel(count: Int) {
+        countLabel.text = String(count)
+    }
+}
