9c24f80491f69f06f9ade38e2d3efe8690931608
Tiana <97kth01@naver.com>
2022-07-08 00:15:48 +0900

    feat: RegisterModel 구현

---
+++ ProjectManager/ProjectManager/Presentation/Detail/View/RegistrationViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  RegistrationViewModel.swift
+//  ProjectManager
+//
+//  Created by Tiana, mmim on 2022/07/07.
+//
+
+import Foundation
+
+class RegistrationViewModel {
+    func registerate(title: String, date: Date, description: String) {
+        MockStorageManager.shared.creat(
+            projectContent: ProjectContent(
+                ProjectItem(id: UUID(),
+                            status: ProjectStatus.todo.string,
+                            title: title,
+                            deadline: date,
+                            description: description
+                           )
+            )
+        )
+    }
+}
