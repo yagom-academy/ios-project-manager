83077577fce2d87af50a1675bde01463f3b7143c
malrang <ruehd7924@gmail.com>
2022-07-08 17:43:21 +0900

    feat: TodoListView 화면 전환 기능 구현 #4

---
+++ ProjectManager/ProjectManager/TodoList/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  ProjectManager
+//
+//  Created by 김동욱 on 2022/07/08.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+    }
+}
