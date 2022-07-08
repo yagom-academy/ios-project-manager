fdcbb7db59354010c2cbff64fa23928902c5facd
doogie97 <chris_97@naver.com>
2022-07-08 15:04:50 +0900

    ✏️chore : dummy data 수정

--- ProjectManager/ProjectManager/ViewModel/MainViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel/MainViewModel.swift
@@ -18,21 +18,22 @@ final class MainViewModel {
     // MARK: - for test
     private func addDummyData() {
         let todoDummy: [List] = [
-            List(title: "안녕이요", body: "네 저는 안녕이에요?", deadline: Date(timeIntervalSince1970: 7531235), type: .todo),
+            List(title: "안녕이요", body: "네 저는 안녕이에요?", deadline: Date(timeIntervalSince1970: 16537259728), type: .todo),
             List(title: "안녕", body: "네 저는 안니근데 이렇게 죽ㅈ길게 써볼라는데 너는 뭐야!라이에요?", deadline: Date(timeIntervalSince1970: 751231235), type: .todo),
-            List(title: "그래맞아", body: "네 \n저는 \n그래그래에요? \n그러면 말입니다", deadline: Date(), type: .todo)
+            List(title: "그래맞아", body: "네 \n저는 \n그래그래에요? \n그러면 말입니다", deadline: Date(timeIntervalSince1970: 1677269760), type: .todo)
         ]
         
         let doingDummy: [List] = [
-            List(title: "나는 두잉이야", body: "ㅋㄷㅋㄷ 나 알아? 난 너 몰라", deadline: Date(), type: .doing),
-            List(title: "안녕 나는 세잉이야", body: "모 \n 어쩔건데!! \n ㅋㄷㅋㄷ", deadline: Date(), type: .doing)
+            List(title: "나는 두잉이야", body: "ㅋㄷㅋㄷ 나 알아? 난 너 몰라", deadline: Date(timeIntervalSince1970: 1677269760), type: .doing),
+            List(title: "안녕 나는 세잉이야", body: "모 \n 어쩔건데!! \n ㅋㄷㅋㄷ", deadline: Date(timeIntervalSince1970: 751231235), type: .doing)
         ]
         
         let doneDummy: [List] = [
-            List(title: "나는 던이야", body: "노래는 못불러", deadline: Date(timeIntervalSince1970: 75231235), type: .done),
-            List(title: "안녕 똥이야!", body: "모 \n 어쩔건데!! \n ㅋㄷㅋㄷ", deadline: Date(), type: .done)
+            List(title: "나는 던이야", body: "노래는 못불러", deadline: Date(timeIntervalSince1970: 1677269760), type: .done),
+            List(title: "안녕 똥이야!", body: "모 \n 어쩔건데!! \n ㅋㄷㅋㄷ", deadline: Date(timeIntervalSince1970: 751231235), type: .done)
         ]
         
-        listObservable.accept(todoDummy + doingDummy + doneDummy)
+        listObservable.accept((todoDummy + doingDummy + doneDummy)
+            .sorted(by: { $0.deadline < $1.deadline}))
     }
 }
