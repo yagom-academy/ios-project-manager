dd8e7fa8e7f1fb255c482465c36b83dff56cefd5
saafaaari <111dltldnjs@naver.com>
2022-07-08 17:06:55 +0900

    feat: TodoEditView navigationBar 추가

--- ProjectManager/ProjectManager/Presentation/TodoEditViewController.swift
+++ ProjectManager/ProjectManager/Presentation/TodoEditViewController.swift
@@ -11,8 +11,13 @@ import SnapKit
 class TodoEditViewController: UIViewController {
     private let mainView = TodoEditView()
     
+    private let navigationBar = UINavigationBar()
+    private let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: nil)
+    private let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationBar()
         configureView()
     }
 }
@@ -20,9 +25,22 @@ class TodoEditViewController: UIViewController {
 //MARK: - View Setting
 extension TodoEditViewController {
     private func configureView() {
+        self.view.addSubview(navigationBar)
+        navigationBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+        }
         self.view.addSubview(mainView)
         mainView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.equalTo(navigationBar.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
         }
+    }
+    
+    private func configureNavigationBar() {
+        title = "Todo"
+        navigationItem.leftBarButtonItem = cancelButton
+        navigationItem.rightBarButtonItem = doneButton
+        
+        navigationBar.items = [navigationItem]
     }
 }
