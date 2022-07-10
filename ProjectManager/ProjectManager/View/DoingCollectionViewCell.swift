3af8b423807e76e8d8221e8b59668bb8e41efa27
minseong <msb0987@naver.com>
2022-07-11 02:45:15 +0900

    chore: Cell 파일 생성
    * Todo Cell
    * Doing Cell
    * Done Cell

---
+++ ProjectManager/ProjectManager/View/DoingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  DoingCollectionViewCell.swift
+//  ProjectManager
+//
+//  Created by Minseong on 2022/07/11.
+//
+
+import UIKit
+
+class DoingCollectionViewCell: UICollectionViewCell {
+
+}
---
+++ ProjectManager/ProjectManager/View/DoneCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  DoneCollectionViewCell.swift
+//  ProjectManager
+//
+//  Created by Minseong on 2022/07/11.
+//
+
+import UIKit
+
+class DoneCollectionViewCell: UICollectionViewCell {
+
+}
