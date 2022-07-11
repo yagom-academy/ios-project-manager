ce2e4327949c940f35df34938328c9572c584cce
minseong <msb0987@naver.com>
2022-07-12 02:49:54 +0900

    feat: ProjectList 모델 구현

--- ProjectManager/ProjectManager/Model/ProjectList.swift
+++ ProjectManager/ProjectManager/Model/ProjectList.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+enum projectCategory {
+  case todo
+  case doing
+  case done
+
+  var description: String {
+    switch self {
+    case .todo:
+      return "TODO"
+    case .doing:
+      return "DOING"
+    case .done:
+      return "DONE"
+    }
+  }
+}
+
+struct ProjectList {
+  let title: String
+  let body: String
+  let date: Date
+  let projectCategory: projectCategory
+}
