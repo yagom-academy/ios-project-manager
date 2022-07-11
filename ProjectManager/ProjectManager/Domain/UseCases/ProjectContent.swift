c177147199f92de65cd40075146297ccf4f64fda
Tiana <97kth01@naver.com>
2022-07-12 01:05:52 +0900

    feat: getStatus 메서드 구현

--- ProjectManager/ProjectManager/Domain/UseCases/ProjectContent.swift
+++ ProjectManager/ProjectManager/Domain/UseCases/ProjectContent.swift
@@ -66,24 +66,12 @@ struct ProjectContent {
             self.description = description
         }
     }
-}
-
-fileprivate extension DateFormatter {
-    private var dateFormatter: DateFormatter {
-        let dateFormatter = DateFormatter()
-        
-        dateFormatter.dateStyle = .long
-        dateFormatter.locale = .autoupdatingCurrent
-        
-        return dateFormatter
-    }
     
-    func formatted(string: String) -> Date? {
-        return dateFormatter.date(from: string)
-    }
-    
-    func formatted(date: Date) -> String {
-        return dateFormatter.string(from: date)
+    func getStatus() -> ProjectStatus {
+        let projects = MockStorageManager.shared.read().value
+        let project = projects.filter { $0 == self }
+        
+        return project.first?.status ?? .todo
     }
 }
 
