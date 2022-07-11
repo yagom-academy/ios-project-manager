fc95ed5ff18ce142efdc0d53f952d6870d40b117
malrang <ruehd7924@gmail.com>
2022-07-11 18:07:51 +0900

    refactor: DataBase protocol 구현 및 Fixture 데이터 적용 #3

--- ProjectManager/ProjectManager/TodoList/TodoListViewModel.swift
+++ ProjectManager/ProjectManager/TodoList/TodoListViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import TodoListViewModel_Fixture
 
 import RxCocoa
 import RxRelay
@@ -15,25 +16,53 @@ final class TodoListViewModel {
     let todoViewData: Driver<[Todo]>
     let doingViewData: Driver<[Todo]>
     let doneViewData: Driver<[Todo]>
-    
-    private let mockData = BehaviorRelay<[Todo]>(value: [
-        Todo(status: .todo, title: "책상정리", description: "집중이 안될때 역시나 책상정리", date: CurrentDateFormatter.fetch()),
-        Todo(status: .done, title: "TIL 작성하기", description: "TIL 작성하면\n오늘의 상큼한 마무리도 되고\n나중에 포트폴리오 용으로 좋죠!", date: CurrentDateFormatter.fetch()),
-        Todo(status: .doing, title: "오늘의 할일 찾기", description: "내가 가는 이길이 어디로 가는지 날 데려가는지 그곳은 어딘지 알 수 없지만 알 수 없지만 오늘도 난 걸어가고 있네 사람들은 길이 다 정해져 있지 내가 가는 길이 어디로 가는지 잘 모르겠지만 활 수 있따!", date: CurrentDateFormatter.fetch()),
-        Todo(status: .todo, title: "프로젝트 회고 작성", description: "프로젝트 회고를 작성하면 내가 이번 프로젝트에서\n무엇을 놓쳤는지 명확히 알 수 있어요.", date: CurrentDateFormatter.fetch())
-    ])
+
+    let dataBase: DataBase = MockDataBase()
     
     init() {
-        todoViewData = mockData
+        let data = BehaviorRelay<[Todo]>(value: dataBase.read())
+            
+        todoViewData = data
             .map { $0.filter { $0.status == .todo } }
             .asDriver(onErrorJustReturn: [])
         
-        doingViewData = mockData
+        doingViewData = data
             .map { $0.filter { $0.status == .doing } }
             .asDriver(onErrorJustReturn: [])
         
-        doneViewData = mockData
+        doneViewData = data
             .map { $0.filter { $0.status == .done } }
             .asDriver(onErrorJustReturn: [])
+    }
+}
+
+protocol DataBase {
+    func read() -> [Todo]
+}
+
+fileprivate struct MockDataBase: DataBase {
+    let data1 = MockData().data
+    let data2 = MockData().data2
+    let data3 = MockData().data3
+    
+    func read() -> [Todo] {
+        let status1 = data1["status"]
+        let title1 = data1["title"]
+        let description1 = data1["description"]
+        
+        let status2 = data2["status"]
+        let title2 = data2["title"]
+        let description2 = data2["description"]
+        
+        let status3 = data3["status"]
+        let title3 = data3["title"]
+        let description3 = data3["description"]
+        
+        let data: [Todo] = [
+            Todo(status: Status(rawValue: status1!)!, title: title1!, description: description1!, date: Date()),
+            Todo(status: Status(rawValue: status2!)!, title: title2!, description: description2!, date: Date()),
+            Todo(status: Status(rawValue: status3!)!, title: title3!, description: description3!, date: Date())
+        ]
+        return data
     }
 }
