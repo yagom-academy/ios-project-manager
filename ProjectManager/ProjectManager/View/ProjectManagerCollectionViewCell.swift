2e01718ad4fd68a361217427ca00eb5d248517ab
minseong <msb0987@naver.com>
2022-07-12 03:08:42 +0900

    feat: cell 요소 연결

--- ProjectManager/ProjectManager/View/ProjectManagerCollectionViewCell.swift
+++ ProjectManager/ProjectManager/View/ProjectManagerCollectionViewCell.swift
@@ -8,5 +8,7 @@
 import UIKit
 
 class ProjectManagerCollectionViewCell: UICollectionViewCell {
-
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var bodyLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
 }
