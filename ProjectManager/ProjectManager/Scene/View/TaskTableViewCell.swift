38ee75740894ba9d95273ca1a110d8f51e496ced
Grumpy <sewoong2890@gmail.com>
2022-07-12 17:56:58 +0900

    fix: Done 영역의 cell에서도 기한이 지난 날짜를 빨간색으로 표현하던 현상 수정

--- ProjectManager/ProjectManager/Scene/View/TaskTableViewCell.swift
+++ ProjectManager/ProjectManager/Scene/View/TaskTableViewCell.swift
@@ -67,11 +67,14 @@ final class TaskTableViewCell: UITableViewCell {
         titleLabel.text = task.title
         descriptionLabel.text = task.body
         dateLabel.text = task.date.formattedString
-        let now = Calendar.current.startOfDay(for: Date()).timeIntervalSince1970
-        if now > task.date {
-            dateLabel.textColor = .systemRed
-        } else {
-            dateLabel.textColor = .black
+        guard task.taskType == .done else {
+            let now = Calendar.current.startOfDay(for: Date()).timeIntervalSince1970
+            if now > task.date {
+                dateLabel.textColor = .systemRed
+            } else {
+                dateLabel.textColor = .black
+            }
+            return
         }
     }
 }
