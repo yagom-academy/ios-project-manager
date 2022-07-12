b575e29364d9674862a576d0bc839751d91eb186
Quokkaa <rktrkdml@gmail.com>
2022-07-12 15:13:09 +0900

    feat: ToDo Model 구현

---
+++ ProjectManager/Model/ToDo.swift
@@ -0,0 +1,53 @@
+//
+//  ToDo.swift
+//  ProjectManager
+//
+//  Created by LIMGAUI on 2022/07/07.
+
+import Foundation
+import RealmSwift
+
+enum State {
+  case todo
+  case doing
+  case done
+}
+
+struct Todo: Hashable {
+  var title: String = ""
+  var content: String = ""
+  var date: Date = Date.now
+  var state: State = .done
+  var identifier: String = UUID().uuidString
+  
+  var readList: [Todo] {
+    return [
+      Todo(title: "test1", content: "쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!", date: Date().addingTimeInterval(3600 * -24), state: .todo),
+      Todo(title: "test2", content: "쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!", date: Date(), state: .doing),
+      Todo(title: "test3", content: "쿼카 하이!", date: Date().addingTimeInterval(3600 * -48), state: .done),
+      Todo(title: "test4", content: "쿼카 하이!", date: Date(), state: .todo),
+      Todo(title: "test5", content: "쿼카 하이!", date: Date().addingTimeInterval(3600 * 72), state: .done),
+      Todo(title: "test6", content: "쿼카 하이!", date: Date(), state: .doing),
+      Todo(title: "test7", content: "쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!", date: Date(), state: .todo),
+      Todo(title: "test8", content: "쿼카 하이!", date: Date().addingTimeInterval(3600 * -96), state: .todo),
+      Todo(title: "test9", content: "쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!쿼카 하이!", date: Date(), state: .todo),
+      Todo(title: "test10", content: "쿼카 하이!쿼카 하이!쿼카 하이!", date: Date().addingTimeInterval(3600 * 96), state: .todo),
+      Todo(title: "test11", content: "쿼카 하이!", date: Date().addingTimeInterval(3600 * 24), state: .todo),
+      Todo(title: "test12", content: "쿼카 하이!", date: Date().addingTimeInterval(3600 * 12), state: .todo),
+      Todo(title: "test13", content: "쿼카 하이!", date: Date().addingTimeInterval(3600 * 12), state: .todo),
+      Todo(title: "test14", content: "쿼카 하이!", date: Date().addingTimeInterval(3600 * 48), state: .todo)
+      ]
+  }
+  
+  var readTodoCount: [Todo] {
+    return readList.filter { $0.state == .todo }
+  }
+  
+  var readDoingCount: [Todo] {
+    return readList.filter { $0.state == .doing }
+  }
+  
+  var readDoneCount: [Todo] {
+    return readList.filter { $0.state == .done }
+  }
+}
