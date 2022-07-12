e45f9a224f13b2ba09d317d3fccb90efe4a1b1ca
minseong <msb0987@naver.com>
2022-07-12 14:39:37 +0900

    feat: listCompositionLayout 메서드 구현 및 적용
    * collectionView dataSouce 할당

--- ProjectManager/ProjectManager/ViewController/ProjectManagerHomeViewController.swift
+++ ProjectManager/ProjectManager/ViewController/ProjectManagerHomeViewController.swift
@@ -13,9 +13,56 @@ final class ProjectManagerHomeViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     self.initializeNavigationBar()
+    self.initializeCollectionView()
   }
 
   private func initializeNavigationBar() {
     self.navigationItem.title = "Project Manager"
+  }
+
+  private func initializeCollectionView() {
+    self.todoCollectionView.dataSource = self
+    self.doingCollectionView.dataSource = self
+    self.doneCollectionView.dataSource = self
+    self.todoCollectionView.collectionViewLayout = listCompositionLayout()
+    self.doingCollectionView.collectionViewLayout = listCompositionLayout()
+    self.doneCollectionView.collectionViewLayout = listCompositionLayout()
+  }
+
+  private func listCompositionLayout() -> UICollectionViewCompositionalLayout {
+    let itemSize = NSCollectionLayoutSize(
+       widthDimension: NSCollectionLayoutDimension.fractionalWidth(1),
+       heightDimension: NSCollectionLayoutDimension.estimated(1)
+    )
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+    let group = NSCollectionLayoutGroup.horizontal(layoutSize: itemSize, subitem: item, count: 1)
+
+    let section = NSCollectionLayoutSection(group: group)
+    section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0)
+    section.interGroupSpacing = 10
+
+    let layout = UICollectionViewCompositionalLayout(section: section)
+
+    return layout
+  }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ProjectManagerHomeViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 1
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(
+      withReuseIdentifier: "projectManagerCell",
+      for: indexPath
+    ) as? ProjectManagerCollectionViewCell else {
+      return UICollectionViewCell()
+    }
+
+    return cell
   }
 }
