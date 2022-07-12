507f9452b73bfea0815e8f4e910b99f32c330d47
minseong <msb0987@naver.com>
2022-07-13 00:32:00 +0900

    feat: Formatter enum 타입 구현
    * Date형식을 바꿔 String으로 반환

--- ProjectManager/ProjectManager/Util/Formatter.swift
+++ ProjectManager/ProjectManager/Util/Formatter.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+enum Formatter {
+  private static let dateFormatter = DateFormatter()
+
+  static func changeToString(from date: Date) -> String {
+    dateFormatter.dateFormat = "yyyy. MM. dd"
+    dateFormatter.locale = Locale.current
+    dateFormatter.timeZone = TimeZone.current
+
+    return dateFormatter.string(from: date)
+  }
+}
