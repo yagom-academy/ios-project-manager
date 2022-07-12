ff0a43d75775888a7e0184f2763b7e05ede17b19
marisol <marisol.develop@gmail.com>
2022-07-12 16:16:22 +0900

    ✨Feat : ContentViewModel 생성

---
+++ ProjectManager/ViewModel/ContentViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  ContentViewModel.swift
+//  ProjectManager
+//
+//  Created by OneTool, marisol on 2022/07/12.
+//
+
+import Foundation
+import SwiftUI
+
+class ContentViewModel: ObservableObject {
+    @ObservedObject var data = TaskViewModel()
+    @Published var todoTasks: [Task] = []
+    @Published var doingTasks: [Task] = []
+    @Published var doneTasks: [Task] = []
+    
+    func appendData() {
+        data.addTask()
+        if let task = data.task {
+            todoTasks.append(task)
+        }
+    }
+    
+    func moveData(_ task: Task, from: TaskType, to: TaskType) {
+        switch from {
+        case .todo:
+            if let taskToDelete = todoTasks.firstIndex(of: task) {
+                todoTasks.remove(at: taskToDelete)
+            }
+        case .doing:
+            if let taskToDelete = doingTasks.firstIndex(of: task) {
+                doingTasks.remove(at: taskToDelete)
+            }
+        case .done:
+            if let taskToDelete = doneTasks.firstIndex(of: task) {
+                doneTasks.remove(at: taskToDelete)
+            }
+        }
+        
+        switch to {
+        case .todo:
+            todoTasks.append(task)
+        case .doing:
+            doingTasks.append(task)
+        case .done:
+            doneTasks.append(task)
+        }
+    }
+}
