e7481ab6e8ce6f2fabee224758378c21d1a8c2a6
doogie97 <chris_97@naver.com>
2022-07-12 20:35:56 +0900

    💎feat : Storege 프로토콜 구현 메서드 구현

--- ProjectManager/ProjectManager/MockStorage.swift
+++ ProjectManager/ProjectManager/MockStorage.swift
@@ -6,9 +6,14 @@
 //
 
 import RxRelay
+protocol Storege {
+    func creatList(listItem: ListItem)
+    func updateList(listItem: ListItem)
+    func deleteList(listItem: ListItem)
+}
 
-struct MockStorage {
-    private let dummyList: [ListItem] = [
+class MockStorage: Storege {
+    private var dummyList: [ListItem] = [
         ListItem(title: "안녕이요", body: "네 저는 안녕이에요?", deadline: Date(timeIntervalSince1970: 16537259728), type: .todo),
         ListItem(title: "안녕", body: "네 저는 안니근데 이렇게 죽ㅈ길게 써볼라는데 너는 뭐야!라이에요?", deadline: Date(timeIntervalSince1970: 751231235), type: .todo),
         ListItem(title: "그래맞아", body: "네 \n저는 \n그래그래에요? \n그러면 말입니다", deadline: Date(timeIntervalSince1970: 1677269760), type: .todo),
@@ -18,5 +23,21 @@ struct MockStorage {
         ListItem(title: "안녕 똥이야!", body: "모 \n 어쩔건데!! \n ㅋㄷㅋㄷ", deadline: Date(timeIntervalSince1970: 751231235), type: .done)
     ].sorted(by: { $0.deadline < $1.deadline})
     
-    lazy var list =  BehaviorRelay<[ListItem]>(value: dummyList)
+    lazy var list = BehaviorRelay<[ListItem]>(value: dummyList)
+    
+    func creatList(listItem: ListItem) {
+        dummyList.append(listItem)
+        list.accept(dummyList)
+    }
+    
+    func updateList(listItem: ListItem) {
+        dummyList = dummyList.filter { listItem.id != $0.id }
+        dummyList.append(listItem)
+        list.accept(dummyList)
+    }
+    
+    func deleteList(listItem: ListItem) {
+        dummyList = dummyList.filter { listItem.id != $0.id }
+        list.accept(dummyList)
+    }
 }
