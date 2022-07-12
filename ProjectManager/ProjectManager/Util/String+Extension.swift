21ba7040f9f5c19f930fa4ed1ee88e01e47f2a07
minseong <msb0987@naver.com>
2022-07-13 01:41:30 +0900

    feat: String Extension을 통한 dateCompare 메서드 구현

--- ProjectManager/ProjectManager/Util/String+Extension.swift
+++ ProjectManager/ProjectManager/Util/String+Extension.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension String {
+  func dateCompare(from date: Date) -> ComparisonResult {
+    let currentDate = Formatter.changeToString(from: date)
+    let result = self.compare(currentDate)
+    
+    return result
+  }
+}
