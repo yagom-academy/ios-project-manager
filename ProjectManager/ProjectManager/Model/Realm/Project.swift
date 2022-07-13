f6ac96f02377c954bd82216014ec760e7a833c4f
minseong <msb0987@naver.com>
2022-07-14 03:03:04 +0900

    feat: Project realm 데이터 모델  생성

--- ProjectManager/ProjectManager/Model/Realm/Project.swift
+++ ProjectManager/ProjectManager/Model/Realm/Project.swift
@@ -5,4 +5,16 @@
 //  Created by Minseong on 2022/07/14.
 //
 
-import Foundation
+import RealmSwift
+
+final class Project: Object {
+  @objc dynamic var uuid: String = ""
+  @objc dynamic var title: String = ""
+  @objc dynamic var body: String?
+  @objc dynamic var date: Date = Date()
+  @objc dynamic var projectCategory: String = ""
+
+  override static func primaryKey() -> String? {
+    return "uuid"
+  }
+}
