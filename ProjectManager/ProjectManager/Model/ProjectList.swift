bcd9978778efb6625696ca5d820378402045d595
minseong <msb0987@naver.com>
2022-07-13 22:52:46 +0900

    refactor: ProjectCategory 분리

--- ProjectManager/ProjectManager/Model/ProjectList.swift
+++ ProjectManager/ProjectManager/Model/ProjectList.swift
@@ -7,23 +7,6 @@
 
 import Foundation
 
-enum ProjectCategory {
-  case todo
-  case doing
-  case done
-
-  var description: String {
-    switch self {
-    case .todo:
-      return "TODO"
-    case .doing:
-      return "DOING"
-    case .done:
-      return "DONE"
-    }
-  }
-}
-
 struct ProjectList {
   let title: String
   let body: String
--- ProjectManager/ProjectManager/Util/ProjectCategory.swift
+++ ProjectManager/ProjectManager/Util/ProjectCategory.swift
@@ -5,4 +5,19 @@
 //  Created by Minseong on 2022/07/13.
 //
 
-import Foundation
+enum ProjectCategory {
+  case todo
+  case doing
+  case done
+
+  var description: String {
+    switch self {
+    case .todo:
+      return "TODO"
+    case .doing:
+      return "DOING"
+    case .done:
+      return "DONE"
+    }
+  }
+}
