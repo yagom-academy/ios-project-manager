5641bf82171ee6b2b0d60d3b04a2b35f11b23a90
minseong <msb0987@naver.com>
2022-07-13 21:41:32 +0900

    feat: initializeUI() 메서드 구현 및 TextField 여백추가

--- ProjectManager/ProjectManager/ViewController/ProjectAddViewController.swift
+++ ProjectManager/ProjectManager/ViewController/ProjectAddViewController.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class ProjectAddViewController: UIViewController {
+  @IBOutlet weak var addProjectTitleTextField: UITextField!
+  @IBOutlet weak var datePicker: UIDatePicker!
+  @IBOutlet weak var bodyTextView: UITextView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.initializeUI()
+  }
 
-    }
+  private func initializeUI() {
+    self.addProjectTitleTextField.leftView = UIView(
+      frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0)
+    )
+    self.addProjectTitleTextField.leftViewMode = .always
+  }
 }
