8b15c62746f51d47f6db40641fbae4b0bdfea3bc
Quokkaa <rktrkdml@gmail.com>
2022-07-14 22:39:44 +0900

    feat: CRUD 기능하는 TodoManager 구현 #16

---
+++ ProjectManager/ProjectManager/DB/TodoManager.swift
@@ -0,0 +1,37 @@
+//
+//  TodoManager.swift
+//  ProjectManager
+//
+//  Created by LIMGAUI on 2022/07/14.
+//
+
+import Foundation
+
+final class TodoManager {
+  static let shared = TodoManager(realmManager: RealmService())
+  private let realmManager: RealmService
+  
+  init(realmManager: RealmService) {
+    self.realmManager = realmManager
+  }
+  
+  func create(_ object: TodoModel) {
+    realmManager.create(object)
+  }
+  
+  func readAll() -> [TodoModel] {
+    return realmManager.readAll()
+  }
+  
+  func update(_ object: TodoModel, with data: [String: Any?]) {
+    realmManager.update(object, with: data)
+  }
+  
+  func delete(_ object: TodoModel) {
+    realmManager.delete(object)
+  }
+  
+  func mappingTodoModel(from todo: Todo) -> TodoModel {
+    return TodoModel(title: todo.title, content: todo.content, state: todo.state)
+  }
+}
