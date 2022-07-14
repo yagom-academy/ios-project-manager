62ac5af8f6477aa02a66020247ba9ff658687e5d
Quokkaa <rktrkdml@gmail.com>
2022-07-14 22:39:23 +0900

    refactor: TodoModel 동작 가능하게 리펙토링

---
+++ ProjectManager/ProjectManager/DB/TodoModel.swift
@@ -0,0 +1,25 @@
+//
+//  TodoModel.swift
+//  ProjectManager
+//
+//  Created by LIMGAUI on 2022/07/12.
+//
+
+import RealmSwift
+
+final class TodoModel: Object {
+  @Persisted var title: String
+  @Persisted var content: String
+  @Persisted var date: Date
+  @Persisted var state: State.RawValue
+  @Persisted var identifier: String
+  
+  convenience init(title: String, content: String, state: State) {
+    self.init()
+    self.title = title
+    self.content = content
+    self.date = Date.now
+    self.state = state.rawValue
+    self.identifier = UUID().uuidString
+  }
+}
