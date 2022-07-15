abc5af985668416419d0bace8187c3c449953175
saafaaari <111dltldnjs@naver.com>
2022-07-15 14:00:27 +0900

    refactor: distinctUntilChanged 클로져 삭제

--- ProjectManager/ProjectManager/Presentation/TodoList/ViewModel/TodoListViewModel.swift
+++ ProjectManager/ProjectManager/Presentation/TodoList/ViewModel/TodoListViewModel.swift
@@ -39,7 +39,7 @@ final class DefaultTodoListViewModel: TodoListViewModel {
         todoLists.map { items in
             items.filter { $0.state == .todo }
         }
-        .distinctUntilChanged { $0 == $1 }
+        .distinctUntilChanged()
         .withUnretained(self)
         .map { (self, items) in
             self.toTodoCellContents(todoModels: items)
@@ -50,7 +50,7 @@ final class DefaultTodoListViewModel: TodoListViewModel {
         todoLists.map { items in
             items.filter { $0.state == .doing }
         }
-        .distinctUntilChanged { $0 == $1 }
+        .distinctUntilChanged()
         .withUnretained(self)
         .map { (self, items) in
             self.toTodoCellContents(todoModels: items)
@@ -61,7 +61,7 @@ final class DefaultTodoListViewModel: TodoListViewModel {
         todoLists.map { items in
             items.filter { $0.state == .done }
         }
-        .distinctUntilChanged { $0 == $1 }
+        .distinctUntilChanged()
         .withUnretained(self)
         .map { (self, items) in
             self.toTodoCellContents(todoModels: items)
