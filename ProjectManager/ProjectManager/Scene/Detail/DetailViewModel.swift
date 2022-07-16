c3249652ca47bb2f3048f6339d28eddcd2d1ed77
doogie97 <chris_97@naver.com>
2022-07-16 13:43:34 +0900

    💎feat : DetailViewModel의 in/output 정의

--- ProjectManager/ProjectManager/Scene/Detail/DetailViewModel.swift
+++ ProjectManager/ProjectManager/Scene/Detail/DetailViewModel.swift
@@ -7,13 +7,39 @@
 
 import Foundation
 
-class DetailViewModel {
+protocol DetailViewModelable: DetailViewModelOutput, DetailViewModelInput{}
+
+protocol DetailViewModelOutput {
+    var list: ListItem { get }
+}
+
+protocol DetailViewModelInput {
+    func changeTitle(_ text: String?)
+    func changeDaedLine(_ date: Date?)
+    func changeBody(_ text: String?)
+    func creatList(listItem: ListItem)
+    func updateList(listItem: ListItem)
+}
+
+final class DetailViewModel: DetailViewModelable {
     private let storage: Storegeable
     var list: ListItem
     
     init(storage: Storegeable, list: ListItem?) {
         self.storage = storage
         self.list = list ?? ListItem(title: "", body: "", deadline: Date())
+    }
+    
+    func changeTitle(_ text: String?) {
+        
+    }
+    
+    func changeDaedLine(_ date: Date?) {
+        
+    }
+    
+    func changeBody(_ text: String?) {
+        
     }
     
     func creatList(listItem: ListItem) {
