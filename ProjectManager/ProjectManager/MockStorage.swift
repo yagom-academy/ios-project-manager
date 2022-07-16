11da5773be4b8b4773d79c5c03668eb4dd883521
doogie97 <chris_97@naver.com>
2022-07-16 12:25:01 +0900

    ✏️chore : 접근제어 설정

--- ProjectManager/ProjectManager/MockStorage.swift
+++ ProjectManager/ProjectManager/MockStorage.swift
@@ -6,6 +6,7 @@
 //
 
 import RxRelay
+
 protocol Storegeable {
     var list: BehaviorRelay<[ListItem]> { get }
     func creatList(listItem: ListItem)
@@ -13,7 +14,7 @@ protocol Storegeable {
     func deleteList(listItem: ListItem)
 }
 
-class MockStorage: Storegeable {
+final class MockStorage: Storegeable {
     private var dummyList: [ListItem] = [
         ListItem(title: "안녕이요", body: "네 저는 안녕이에요?", deadline: Date(timeIntervalSince1970: 16537259728), type: .todo),
         ListItem(title: "안녕", body: "네 저는 안니근데 이렇게 죽ㅈ길게 써볼라는데 너는 뭐야!라이에요?", deadline: Date(timeIntervalSince1970: 751231235), type: .todo),
