39a0cc1bb74b4a64db1cf4a816aa3f51a2f64857
Eddy <kimkyunghun3@naver.com>
2022-07-17 23:47:52 +0900

    chore: 적절하지 않는 인덴트 수정

--- ProjectManager/ProjectManager/Utils/Reactive+Sugar.swift
+++ ProjectManager/ProjectManager/Utils/Reactive+Sugar.swift
@@ -11,26 +11,26 @@ import RxCocoa
 import RxSwift
 
 extension Reactive where Base: UITableView {
-  func modelLongPressed<T>(_ modelType: T.Type) -> ControlEvent<(UITableViewCell, T)> {
-      let longPressGesture: UILongPressGestureRecognizer = {
-          let gesture = UILongPressGestureRecognizer(target: nil, action: nil)
-          gesture.minimumPressDuration = 0.5
-          return gesture
-      }()
-      
-    base.addGestureRecognizer(longPressGesture)
-    let source = longPressGesture.rx.event
-      .filter { $0.state == .began }
-      .map { base.indexPathForRow(at: $0.location(in: base)) }
-      .flatMap { [weak tableView = base as UITableView] indexPath -> Observable<(UITableViewCell, T)> in
-        guard let tableView = tableView,
-              let indexPath = indexPath,
-              let cell = tableView.cellForRow(at: indexPath) else { return Observable.empty() }
-        return Observable.zip(
-          Observable.just(cell),
-          Observable.just(try tableView.rx.model(at: indexPath))
-        )
-      }
-    return ControlEvent(events: source)
-  }
+    func modelLongPressed<T>(_ modelType: T.Type) -> ControlEvent<(UITableViewCell, T)> {
+        let longPressGesture: UILongPressGestureRecognizer = {
+            let gesture = UILongPressGestureRecognizer(target: nil, action: nil)
+            gesture.minimumPressDuration = 0.5
+            return gesture
+        }()
+        
+        base.addGestureRecognizer(longPressGesture)
+        let source = longPressGesture.rx.event
+            .filter { $0.state == .began }
+            .map { base.indexPathForRow(at: $0.location(in: base)) }
+            .flatMap { [weak tableView = base as UITableView] indexPath -> Observable<(UITableViewCell, T)> in
+                guard let tableView = tableView,
+                      let indexPath = indexPath,
+                      let cell = tableView.cellForRow(at: indexPath) else { return Observable.empty() }
+                return Observable.zip(
+                    Observable.just(cell),
+                    Observable.just(try tableView.rx.model(at: indexPath))
+                )
+            }
+        return ControlEvent(events: source)
+    }
 }
