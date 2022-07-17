75e330b9007174dae7d2225bbb31516a3ff73996
Quokkaa <rktrkdml@gmail.com>
2022-07-17 11:08:41 +0900

    feat: StorageType 프로토콜 및 타입구현

--- ProjectManager/ProjectManager/DB/Storage.swift
+++ ProjectManager/ProjectManager/DB/Storage.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Combine
+
+protocol StorageType {
+  func create(_ todo: Todo)
+  func read() -> AnyPublisher<[Todo], Never>
+  func update(_ todo: Todo)
+  func delete(_ todo: Todo)
+}
+
+final class MemoryStorage: StorageType {
+  static let shared = MemoryStorage()
+  private init() {}
+  
+  @Published private var items = Todo.dummy
+  
+  func create(_ todo: Todo) {
+    items.append(todo)
+  }
+  
+  func read() -> AnyPublisher<[Todo], Never> {
+    return $items.eraseToAnyPublisher()
+  }
+  
+  func update(_ todo: Todo) {
+    guard let index = items.firstIndex(where: { $0.id == todo.id }) else {
+      return
+    }
+  
+    items[index] = todo
+  }
+  
+  func delete(_ todo: Todo) {
+    items.removeAll(where: { $0.id == todo.id })
+  }
+}
