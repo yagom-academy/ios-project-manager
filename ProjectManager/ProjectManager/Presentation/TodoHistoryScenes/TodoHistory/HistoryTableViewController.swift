e6e73e3ed520efdf76a926176474c1a9bf15a477
dudu <whqtkf12@naver.com>
2022-07-19 13:38:30 +0900

    feat: HistoryTableViewController 생성

---
+++ ProjectManager/ProjectManager/Presentation/TodoHistoryScenes/TodoHistory/HistoryTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryTableViewController.swift
+//  ProjectManager
+//
+//  Created by 김도연 on 2022/07/19.
+//
+
+import UIKit
+
+final class HistoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
