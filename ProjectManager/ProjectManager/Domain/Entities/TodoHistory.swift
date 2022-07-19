d3456815f90cf2e67160f0a6480fa855fa073be2
dudu <whqtkf12@naver.com>
2022-07-19 13:50:21 +0900

    feat: TodoHistory Entity 생성

---
+++ ProjectManager/ProjectManager/Domain/Entities/TodoHistory.swift
@@ -0,0 +1,13 @@
+//
+//  TodoHistory.swift
+//  ProjectManager
+//
+//  Created by 김도연 on 2022/07/19.
+//
+
+import Foundation
+
+struct TodoHistory {
+    let title: String
+    let deadline: Date
+}
