5520e73d1fe93e18ec948ae8666e1be5b4f826dd
Lingo <llingo@kakao.com>
2022-07-19 17:38:17 +0900

    feat: CoreData에 직접 접근하는 CoreDataStorage 구현 #35

--- ProjectManager/ProjectManager/Data/Storages/CoreDataStorage.swift
+++ ProjectManager/ProjectManager/Data/Storages/CoreDataStorage.swift
@@ -5,4 +5,34 @@
 //  Created by Lingo on 2022/07/19.
 //
 
-import Foundation
+import CoreData
+
+final class CoreDataStorage {
+  private enum Settings {
+    static let localDataBaseName = "LocalDB"
+  }
+  
+  static let shared = CoreDataStorage()
+  
+  private lazy var container: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: Settings.localDataBaseName)
+    container.loadPersistentStores { description, error in
+      if let error = error { assertionFailure("CoreDataStorage unresolved Error") }
+    }
+    return container
+  }()
+  
+  private var context: NSManagedObjectContext { container.viewContext }
+  
+  private init() {}
+  
+  func saveContext() {
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        assertionFailure("CoreDataStorage unresolved \(error)")
+      }
+    }
+  }
+}
