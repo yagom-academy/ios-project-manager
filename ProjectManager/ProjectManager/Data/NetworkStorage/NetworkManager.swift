f9ca7d35c13b1f9b8b33016329d5f320e15ece47
Tiana <97kth01@naver.com>
2022-07-20 00:06:36 +0900

    feat: NetworkManager의 CR 구현

---
+++ ProjectManager/ProjectManager/Data/NetworkStorage/NetworkManager.swift
@@ -0,0 +1,75 @@
+//
+//  NetworkManager.swift
+//  ProjectManager
+//
+//  Created by Tiana, mmim on 2022/07/19.
+//
+
+import FirebaseDatabase
+
+final class NetworkManager {
+    private let projectsReference = Database.database().reference(withPath: "projects")
+}
+
+enum NetworkError: Error {
+    case loadFailure
+}
+
+extension NetworkManager {
+    func create(project: ProjectDTO) {
+        let projectItem = projectsReference.child(project.id.uuidString)
+        let values: [String: Any] = [
+            "status": project.status,
+            "title": project.title,
+            "deadline": project.deadline,
+            "body": project.body
+        ]
+        
+        projectItem.setValue(values)
+    }
+    
+    func read(completion: @escaping (Result<[ProjectDTO], NetworkError>) -> Void) {
+        projectsReference.getData(completion: { error, snapshot in
+            guard error == nil else {
+                completion(.failure(.loadFailure))
+                return
+            }
+            
+            guard let projects = snapshot?.value as? [ProjectDTO] else {
+                completion(.failure(.loadFailure))
+                return
+            }
+            
+            completion(.success(projects))
+        })
+    }
+    
+    func read(
+        id: UUID?,
+        completion: @escaping (Result<ProjectDTO, NetworkError>) -> Void
+    ) {
+        guard let id = id?.uuidString else {
+            return
+        }
+        
+        projectsReference.child(id).getData(completion: { error, snapshot in
+            guard error == nil else {
+                completion(.failure(.loadFailure))
+                return
+            }
+            
+            guard let project = snapshot?.value as? ProjectDTO else {
+                completion(.failure(.loadFailure))
+                return
+            }
+            
+            completion(.success(project))
+        })
+    }
+
+    func update(project: ProjectDTO) {
+    }
+
+    func delete(projectContentID: UUID?) {
+    }
+}
