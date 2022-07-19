e1c4ba047e03a088ce41637afb437e7a69f5a6b8
dudu <whqtkf12@naver.com>
2022-07-19 14:06:02 +0900

    feat: TodoHistoryRepositorible 프로토콜 구현

---
+++ ProjectManager/ProjectManager/Domain/RepositoryInterface/TodoHistoryRepositorible.swift
@@ -0,0 +1,16 @@
+//
+//  TodoHistoryRepositorible.swift
+//  ProjectManager
+//
+//  Created by 김도연 on 2022/07/19.
+//
+
+import Foundation
+import Combine
+
+protocol TodoHistoryRepositorible {
+    func create(_ item: TodoHistory) -> AnyPublisher<Void, StorageError>
+    func read() -> CurrentValueSubject<[TodoHistory], Never>
+    func update(_ item: TodoHistory) -> AnyPublisher<Void, StorageError>
+    func delete(item: TodoHistory) -> AnyPublisher<Void, StorageError>
+}
