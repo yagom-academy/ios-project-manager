5c1a839ad1e4e0ee2eebf32e697c77cd654e559d
Lingo <llingo@kakao.com>
2022-07-20 23:22:47 +0900

    feat: RealTimeRepository, DefaultRealTimeRepository 파일 생성

---
+++ ProjectManager/ProjectManager/Data/Repository/DefaultRealTimeRepository.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultRealTimeRepository.swift
+//  ProjectManager
+//
+//  Created by Lingo on 2022/07/20.
+//
+
+import Foundation
---
+++ ProjectManager/ProjectManager/Domain/Interface/RealTimeRepository.swift
@@ -0,0 +1,8 @@
+//
+//  RealTimeRepository.swift
+//  ProjectManager
+//
+//  Created by Lingo on 2022/07/20.
+//
+
+import Foundation
