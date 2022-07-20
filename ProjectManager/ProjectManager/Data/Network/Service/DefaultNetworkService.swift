bdefa2cade083e62a07d3f10e0468d4eb0036a38
Lingo <llingo@kakao.com>
2022-07-20 23:13:04 +0900

    feat: NetworkService 프로토콜과 DefaultNetworkService 타입 구현
    + 에러 처리를 위한 NetworkServiceError 타입 구현

--- ProjectManager/ProjectManager/Data/Network/Service/DefaultNetworkService.swift
+++ ProjectManager/ProjectManager/Data/Network/Service/DefaultNetworkService.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class DefaultNetworkService: NetworkService {
+  private let urlSession: URLSession
+  
+  init(urlSession: URLSession) {
+    self.urlSession = urlSession
+  }
+  
+  func request(endpoint: Endpoint) -> Observable<Data> {
+    return Single<Data>.create { [weak self] observer in
+      guard let urlRequest = try? endpoint.create() else {
+        observer(.failure(NetworkServiceError.createURLRequestFailure))
+        return Disposables.create()
+      }
+      
+      let task = self?.urlSession.dataTask(with: urlRequest) { data, response, error in
+        guard error == nil, let data = data else {
+          observer(.failure(NetworkServiceError.errorIsOccurred(error)))
+          return
+        }
+        guard let response = response as? HTTPURLResponse else {
+          observer(.failure(NetworkServiceError.invalidateResponse))
+          return
+        }
+        
+        switch response.statusCode {
+        case 200..<300: observer(.success(data))
+        case 400: observer(.failure(NetworkServiceError.badRequest))
+        case 401: observer(.failure(NetworkServiceError.unauthorized))
+        case 404: observer(.failure(NetworkServiceError.notFound))
+        case 500: observer(.failure(NetworkServiceError.internalServerError))
+        case 503: observer(.failure(NetworkServiceError.serviceUnavailable))
+        default: observer(.failure(NetworkServiceError.unknown))
+        }
+      }
+      task?.resume()
+      
+      return Disposables.create {
+        task?.suspend()
+        task?.cancel()
+      }
+    }.asObservable()
+  }
+}
--- ProjectManager/ProjectManager/Data/Network/Service/NetworkService.swift
+++ ProjectManager/ProjectManager/Data/Network/Service/NetworkService.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+import RxSwift
+
+protocol NetworkService {
+  func request(endpoint: Endpoint) -> Observable<Data>
+}
