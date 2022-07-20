c4edcc4f7214c710eae0a179e04bcc673e5aa185
Lingo <llingo@kakao.com>
2022-07-20 22:29:53 +0900

    feat: HTTPMethod 열거형 타입 생성

---
+++ ProjectManager/ProjectManager/Data/Network/Infrastructure/HTTPMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethod.swift
+//  ProjectManager
+//
+//  Created by Lingo on 2022/07/20.
+//
+
+enum HTTPMethod: String {
+  case get = "GET"
+  case post = "POST"
+  case put = "PUT"
+  case patch = "PATCH"
+  case delete = "DELETE"
+}
