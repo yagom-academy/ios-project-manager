65b59c233c8d22f7ae70e8a0f0fda40a4ba8e824
saafaaari <111dltldnjs@naver.com>
2022-07-20 20:18:54 +0900

    refactor: 메서드를 활용한 중복코드 분리

--- ProjectManager/ProjectManager/Presentation/TodoMove/ViewModel/TodoMoveViewModel.swift
+++ ProjectManager/ProjectManager/Presentation/TodoMove/ViewModel/TodoMoveViewModel.swift
@@ -47,6 +47,12 @@ final class DefaultTodoMoveViewModel {
             return "Move to DONE"
         }
     }
+    
+    private func changeItemState(to state: State) {
+        var newItem = item
+        newItem.state = state
+        useCase.saveItem(to: newItem)
+    }
 }
 
 extension DefaultTodoMoveViewModel: TodoMoveViewModel {
@@ -62,16 +68,14 @@ extension DefaultTodoMoveViewModel: TodoMoveViewModel {
     
     //MARK: - Input
     func firstButtonDidTap() {
-        var newItem = item
-        newItem.state = useCase.moveState(from: item.state).first
-        useCase.saveItem(to: newItem)
+        let newState = useCase.moveState(from: item.state).first
+        changeItemState(to: newState)
         actions?.dismiss()
     }
     
     func secondButtonDidTap() {
-        var newItem = item
-        newItem.state = useCase.moveState(from: item.state).second
-        useCase.saveItem(to: newItem)
+        let newState = useCase.moveState(from: item.state).second
+        changeItemState(to: newState)
         actions?.dismiss()
     }
 }
