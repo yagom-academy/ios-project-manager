4b3e75ba9b1142feed6fa16951a3806bdcdd8ff1
Lingo <llingo@kakao.com>
2022-07-21 11:47:25 +0900

    feat: History 타입 및 Action 열거형 생성

--- ProjectManager/ProjectManager/Domain/Entity/History.swift
+++ ProjectManager/ProjectManager/Domain/Entity/History.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct History {
+  let actionType: HistoryActionType
+  let actionTime: Date
+}
+
+enum HistoryActionType: CustomStringConvertible {
+  case create(String)
+  case update(String, CardType)
+  case delete(String, CardType)
+  case move(String, CardType, CardType)
+  
+  var description: String {
+    switch self {
+    case let .create(title):
+      return "Created: '\(title)'"
+    case let .update(title, from):
+      return "Updated: '\(title)' from \(from.description)"
+    case let .delete(title, from):
+      return "Deleted: '\(title)' from \(from.description)"
+    case let .move(title, from, to):
+      return "Moved: '\(title)' from \(from.description) to \(to.description)"
+    }
+  }
+}
