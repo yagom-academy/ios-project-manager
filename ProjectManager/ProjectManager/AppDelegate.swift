85a944616e2b0ce0310bc6b3a5aaeeae1bc1290d
dnwhd0112 <dnwhd0112@naver.com>
2022-07-21 19:31:29 +0900

    feat: firebase 초기 세팅

--- ProjectManager/ProjectManager/AppDelegate.swift
+++ ProjectManager/ProjectManager/AppDelegate.swift
@@ -5,10 +5,12 @@
 // 
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
