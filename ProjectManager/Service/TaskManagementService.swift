1a1dc4fc7f5050afc7642dfc68c9bb26d6a5b1f9
marisol <marisol.develop@gmail.com>
2022-07-22 00:38:00 +0900

    🩹Chore : TaskManagementService 파일 이동

---
+++ ProjectManager/Service/TaskManagementService.swift
@@ -0,0 +1,21 @@
+//
+//  SomeViewModel.swift
+//  ProjectManager
+//
+//  Created by OneTool, marisol on 2022/07/12.
+//
+
+import SwiftUI
+
+class TaskManagementService: ObservableObject {
+    
+    @Published var tasks: [Task] = []
+    
+    // CREATE
+    
+    // READ
+    
+    // UPDATE
+    
+    // DELETE
+}
