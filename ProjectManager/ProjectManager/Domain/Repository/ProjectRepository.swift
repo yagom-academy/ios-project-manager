bfe8996605237b19f606bf89bb6370c31bd5c807
Tiana <97kth01@naver.com>
2022-07-21 22:08:21 +0900

    chore: 프로퍼티 접근제어 수정

--- ProjectManager/ProjectManager/Domain/Repository/ProjectRepository.swift
+++ ProjectManager/ProjectManager/Domain/Repository/ProjectRepository.swift
@@ -19,7 +19,7 @@ protocol Storagable {
 }
 
 struct ProjectRepository {
-    let storageManager: Storagable
+    private let storageManager: Storagable
 
     init(storageManager: Storagable) {
         self.storageManager = storageManager
