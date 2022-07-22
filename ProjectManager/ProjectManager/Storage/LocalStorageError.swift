fbae116592ae312b8b7ad79f85851d59c6d86d17
doogie97 <chris_97@naver.com>
2022-07-22 12:11:00 +0900

    💎feat : errorDescription 정의

--- ProjectManager/ProjectManager/Storage/LocalStorageError.swift
+++ ProjectManager/ProjectManager/Storage/LocalStorageError.swift
@@ -9,4 +9,15 @@ enum LocalStorageError: Error {
     case creatError
     case updateError
     case deleteError
+    
+    var errorDescription: String {
+        switch self {
+        case .creatError:
+            return "리스트 생성에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+        case .updateError:
+            return "리스트 수정에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+        case .deleteError:
+            return "리스트 삭제에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+        }
+    }
 }
