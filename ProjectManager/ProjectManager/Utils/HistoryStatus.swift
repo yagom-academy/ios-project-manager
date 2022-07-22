5be0b1d930eabc5ebdb1c7830801f6909aca8227
malrang <ruehd7924@gmail.com>
2022-07-22 23:28:19 +0900

    feat: HistoryStatus 구현

---
+++ ProjectManager/ProjectManager/Utils/HistoryStatus.swift
@@ -0,0 +1,22 @@
+//
+//  HistoryStatus.swift
+//  ProjectManager
+//
+//  Created by 김동욱 on 2022/07/22.
+//
+
+import Foundation
+
+enum HistoryStatus {
+    case from(currentStatus: TodoListItemStatus)
+    case move(lastStatus: TodoListItemStatus, currentStatus: TodoListItemStatus)
+    
+    var value: String {
+        switch self {
+        case .from(let currentStatus):
+            return "from \(currentStatus)"
+        case .move(let lastStatus, let currentStatus):
+            return "from \(lastStatus) to \(currentStatus)"
+        }
+    }
+}
