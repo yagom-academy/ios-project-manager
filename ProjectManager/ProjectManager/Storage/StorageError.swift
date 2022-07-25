aa5e5f70033dabd8717cd2176d02206f1417c6cf
doogie97 <chris_97@naver.com>
2022-07-25 19:58:24 +0900

    💎feat : readError 추가

--- ProjectManager/ProjectManager/Storage/StorageError.swift
+++ ProjectManager/ProjectManager/Storage/StorageError.swift
@@ -9,6 +9,7 @@ enum StorageError: Error {
     case creatError
     case updateError
     case deleteError
+    case readError
     
     var errorDescription: String {
         switch self {
@@ -18,6 +19,8 @@ enum StorageError: Error {
             return "리스트 수정에 실패하였습니다. 잠시 후 다시 시도해 주세요"
         case .deleteError:
             return "리스트 삭제에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+        case .readError:
+            return "리스트 불러오기에 실패하였습니다. 잠시 후 다시 시도해 주세요"
         }
     }
 }
