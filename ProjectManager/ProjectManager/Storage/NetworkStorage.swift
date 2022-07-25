fffac95a68d0993b3d45bf7b0110fb10f85e526d
doogie97 <chris_97@naver.com>
2022-07-25 18:22:15 +0900

    💎feat : NetworkStorage 생성 및 오프라인 저장 기능 활성화

---
+++ ProjectManager/ProjectManager/Storage/NetworkStorage.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkStorage.swift
+//  ProjectManager
+//
+//  Created by 두기 on 2022/07/25.
+//
+
+import FirebaseDatabase
+
+struct NetworkStorage {
+    private let database = Database.database()
+    
+    init() {
+        database.isPersistenceEnabled = true
+    }
+}
