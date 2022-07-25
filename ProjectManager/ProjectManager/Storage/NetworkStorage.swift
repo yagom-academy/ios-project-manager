6ee85fd6b30a1501ddda2426e09b4209d3392331
doogie97 <chris_97@naver.com>
2022-07-25 19:57:02 +0900

    💎feat : NetworkStorageManagerable 생성

--- ProjectManager/ProjectManager/Storage/NetworkStorage.swift
+++ ProjectManager/ProjectManager/Storage/NetworkStorage.swift
@@ -1,5 +1,5 @@
 //
-//  NetworkStorage.swift
+//  NetworkStorageManager.swift
 //  ProjectManager
 //
 //  Created by 두기 on 2022/07/25.
@@ -7,10 +7,32 @@
 
 import FirebaseDatabase
 
-struct NetworkStorageManager {
+protocol NetworkStorageManagerable {
+    func create()
+    func read()
+    func update()
+    func delete()
+}
+
+struct NetworkStorageManager: NetworkStorageManagerable {
     private let database = Database.database()
     
     init() {
         database.isPersistenceEnabled = true
+    }
+    
+    func create() {
+        
+    }
+    
+    func read() {
+    }
+    
+    func update() {
+    
+    }
+    
+    func delete() {
+        
     }
 }
