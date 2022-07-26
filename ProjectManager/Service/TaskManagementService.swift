581a0efb51ce53e8508725bdd2b84f3f5135dee5
marisol <marisol.develop@gmail.com>
2022-07-26 21:11:29 +0900

    ♻️Refactor : TaskManagementService가 채택한 프로토콜 삭제

--- ProjectManager/Service/TaskManagementService.swift
+++ ProjectManager/Service/TaskManagementService.swift
@@ -5,11 +5,6 @@
 //  Created by OneTool, marisol on 2022/07/12.
 //
 
-import SwiftUI
-
-class TaskManagementService: ObservableObject {
-    
-    @Published var tasks: [Task] = []
     
     // CREATE
     
