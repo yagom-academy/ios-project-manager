af94aae45aa3760c8cc28e0df16acd1d26e0c344
marisol <marisol.develop@gmail.com>
2022-07-26 21:12:25 +0900

    ♻️Refactor : AllListView에서 ListView 분리

--- ProjectManager/View/AllListView.swift
+++ ProjectManager/View/AllListView.swift
@@ -13,60 +13,60 @@ struct AllListView: View {
     var body: some View {
         HStack {
             // TODO
-            VStack(alignment: .leading) {
-                List {
-                    Section(header: HeaderView(allListViewModel: allListViewModel, type: .todo)){
-                        ForEach(allListViewModel.service.tasks.filter({ $0.type == .todo })) { task in
-                            if let index = allListViewModel.service.tasks.firstIndex(of: task) {
-                                CellView(cellViewModel: CellViewModel(withService: allListViewModel.service, task: task), cellIndex: index)
-                            }
-                        }
-                        .onDelete { offset in
-                            allListViewModel.service.tasks.remove(atOffsets: offset)
-                        }
-                    }
-                }
-                .listStyle(.grouped)
-            }
+            ListView(listViewModel: ListViewModel(withService: allListViewModel.service),
+                     taskType: TaskType.todo,
+                     numberOfTasks: allListViewModel.showLists().filter({ $0.type == .todo }).count)
             
             // DOING
-            VStack(alignment: .leading) {
-                List {
-                    Section(header: HeaderView(allListViewModel: allListViewModel, type: .doing)){
-                        ForEach(allListViewModel.service.tasks.filter({ $0.type == .doing })) { task in
-                            if let index = allListViewModel.service.tasks.firstIndex(of: task) {
-                                CellView(cellViewModel: CellViewModel(withService: allListViewModel.service, task: task), cellIndex: index)
-                            }
-                        }
-                        .onDelete { offset in
-                            allListViewModel.service.tasks.remove(atOffsets: offset)
-                        }
-                    }
-                }
-                .listStyle(.grouped)
-            }
+            ListView(listViewModel: ListViewModel(withService: allListViewModel.service),
+                     taskType: TaskType.doing,
+                     numberOfTasks: allListViewModel.showLists().filter({ $0.type == .doing }).count)
             
             // DONE
-            VStack(alignment: .leading) {
-                List {
-                    Section(header: HeaderView(allListViewModel: allListViewModel, type: .done)){
-                        ForEach(allListViewModel.service.tasks.filter({ $0.type == .done })) { task in
-                            if let index = allListViewModel.service.tasks.firstIndex(of: task) {
-                                CellView(cellViewModel: CellViewModel(withService: allListViewModel.service, task: task), cellIndex: index)
-                            }
-                        }
-                        .onDelete { offset in
-                            allListViewModel.service.tasks.remove(atOffsets: offset)
-                        }
-                    }
-                }
-                .listStyle(.grouped)
-            }
+            ListView(listViewModel: ListViewModel(withService: allListViewModel.service),
+                     taskType: TaskType.done,
+                     numberOfTasks: allListViewModel.showLists().filter({ $0.type == .done }).count)
         }
     }
 }
 
-
+private struct ListView: View {
+    @ObservedObject var listViewModel: ListViewModel
+    let taskType: TaskType
+    let numberOfTasks: Int
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            List {
+                Section(header: HeaderView(title: taskType.title, numberOfTasks: numberOfTasks)){
+                    ForEach(listViewModel.service.readTasks().filter({ $0.type == taskType })) { task in
+                        ListRowView(task: task)
+                            .onTapGesture {
+                                listViewModel.toggleShowingSheet()
+                            }
+                            .sheet(isPresented: $listViewModel.isShowingSheet) {
+                                EditView(editViewModel: EditViewModel(withService: listViewModel.service),
+                                         task: task)
+                            }
+                            .onLongPressGesture(minimumDuration: 1) {
+                                listViewModel.toggleShowingPopover()
+                            }
+                            .popover(isPresented: $listViewModel.isShowingPopover,
+                                     arrowEdge: .bottom) {
+                                PopoverButton(taskType: taskType) { to in
+                                    listViewModel.moveTask(task, to: to)
+                                }
+                            }
+                    }
+                    .onDelete { offset in
+                        //                        tasks.remove(atOffsets: offset)
+                    }
+                }
+            }
+            .listStyle(.grouped)
+        }
+    }
+}
 
 struct AllListView_Previews: PreviewProvider {
     static var previews: some View {
--- ProjectManager/ViewModel/AllListViewModel.swift
+++ ProjectManager/ViewModel/AllListViewModel.swift
@@ -8,4 +8,24 @@
 import Foundation
 
 class AllListViewModel: ViewModelType {
+    func showLists() -> [Task] {
+        self.service.readTasks()
+    }
+}
+
+class ListViewModel: ViewModelType {
+    @Published var isShowingSheet = false
+    @Published var isShowingPopover = false
+    
+    func toggleShowingSheet() {
+        isShowingSheet.toggle()
+    }
+    
+    func toggleShowingPopover() {
+        isShowingPopover.toggle()
+    }
+    
+    func moveTask(_ task: Task, to: TaskType) {
+        self.service.moveTask(task, to: to)
+    }
 }
