42d1417bd56e74c6c05a750995768ca4ac45eb46
doogie97 <chris_97@naver.com>
2022-07-27 12:59:31 +0900

    💎feat : HistoryView관련 파일 생성

---
+++ ProjectManager/ProjectManager/Scene/History/HistoryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HistoryViewController.swift
+//  ProjectManager
+//
+//  Created by 두기 on 2022/07/27.
+//
+
+import UIKit
+
+final class HistoryViewController: UIViewController {
+    
+}
---
+++ ProjectManager/ProjectManager/Scene/History/HistoryViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  HistoryViewModel.swift
+//  ProjectManager
+//
+//  Created by 두기 on 2022/07/27.
+//
+
+final class HistoryViewModel {
+    
+}
