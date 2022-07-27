ac4e9fcf51430fe1116b427b1ab1b3e4cfd7c51e
doogie97 <chris_97@naver.com>
2022-07-27 12:22:10 +0900

    💎feat : history 생성 실패 에러 케이스 추가

--- ProjectManager/ProjectManager/Storage/StorageError.swift
+++ ProjectManager/ProjectManager/Storage/StorageError.swift
@@ -10,6 +10,7 @@ enum StorageError: Error {
     case updateError
     case deleteError
     case readError
+    case historyError
     
     var errorDescription: String {
         switch self {
@@ -21,6 +22,8 @@ enum StorageError: Error {
             return "리스트 삭제에 실패하였습니다. 잠시 후 다시 시도해 주세요"
         case .readError:
             return "리스트 불러오기에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+        case .historyError:
+            return "히스토리 생성에 실패하였습니다."
         }
     }
 }
