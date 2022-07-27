ae33a1d0b224d08b87818d0895aaae348325f8af
doogie97 <chris_97@naver.com>
2022-07-27 11:30:07 +0900

    ⚰️del : 불필요한 기본값 제거

--- ProjectManager/ProjectManager/Model/ListItemDTO.swift
+++ ProjectManager/ProjectManager/Model/ListItemDTO.swift
@@ -8,9 +8,9 @@
 import RealmSwift
 
 final class ListItemDTO: Object {
-    @Persisted var title: String = ""
-    @Persisted var body: String = ""
-    @Persisted var deadline: Date = Date()
-    @Persisted var type: String = ""
-    @Persisted(primaryKey: true) var id: String = ""
+    @Persisted var title: String
+    @Persisted var body: String
+    @Persisted var deadline: Date
+    @Persisted var type: String
+    @Persisted(primaryKey: true) var id: String
 }
