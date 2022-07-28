837a842d37ad4f175aa1a7e88a10e9345175e2cc
doogie97 <chris_97@naver.com>
2022-07-28 17:51:32 +0900

    ♻️refactor : error description에 줄바꿈 추가

--- ProjectManager/ProjectManager/Storage/StorageError.swift
+++ ProjectManager/ProjectManager/Storage/StorageError.swift
@@ -15,13 +15,13 @@ enum StorageError: Error {
     var errorDescription: String {
         switch self {
         case .creatError:
-            return "리스트 생성에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+            return "리스트 생성에 실패하였습니다.\n잠시 후 다시 시도해 주세요"
         case .updateError:
-            return "리스트 수정에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+            return "리스트 수정에 실패하였습니다.\n잠시 후 다시 시도해 주세요"
         case .deleteError:
-            return "리스트 삭제에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+            return "리스트 삭제에 실패하였습니다.\n잠시 후 다시 시도해 주세요"
         case .readError:
-            return "리스트 불러오기에 실패하였습니다. 잠시 후 다시 시도해 주세요"
+            return "리스트 불러오기에 실패하였습니다.\n잠시 후 다시 시도해 주세요"
         case .historyError:
             return "히스토리 생성에 실패하였습니다."
         }
