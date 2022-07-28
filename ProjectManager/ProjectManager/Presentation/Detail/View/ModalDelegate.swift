f62671fb2cdeffe4e1e1bdecf41b5d97fbb7b575
JoSH0318 <jsh9870@naver.com>
2022-07-28 22:30:24 +0900

    feat: ModalConstant, ModalDelegate 구현

---
+++ ProjectManager/ProjectManager/Presentation/Detail/View/ModalDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ModalDelegate.swift
+//  ProjectManager
+//
+//  Created by Tiana, mmim on 2022/07/28.
+//
+
+import Foundation
+
+protocol ModalDelegate: AnyObject {
+    func changeModalViewTopConstant(to constant: Double)
+}
