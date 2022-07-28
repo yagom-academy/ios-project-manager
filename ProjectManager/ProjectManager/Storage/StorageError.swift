64138531603a8d378102dcc8f60ec56985425764
doogie97 <chris_97@naver.com>
2022-07-28 17:51:50 +0900

    💎feat : error case 추가

--- ProjectManager/ProjectManager/Storage/StorageError.swift
+++ ProjectManager/ProjectManager/Storage/StorageError.swift
@@ -11,6 +11,8 @@ enum StorageError: Error {
     case deleteError
     case readError
     case historyError
+    case networkError
+    case localStorageError
     
     var errorDescription: String {
         switch self {
@@ -24,6 +26,10 @@ enum StorageError: Error {
             return "리스트 불러오기에 실패하였습니다.\n잠시 후 다시 시도해 주세요"
         case .historyError:
             return "히스토리 생성에 실패하였습니다."
+        case .networkError:
+            return "어플 최초 실행시 서버와의 연동을 위해 인터넷 연결이 필요합니다.\n연결 확인 후 어플을 다시 실행해주세요."
+        case .localStorageError:
+            return "저장소를 불러오는 데 실패했습니다.\n잠시 후 다시 시도해 주세요"
         }
     }
 }
