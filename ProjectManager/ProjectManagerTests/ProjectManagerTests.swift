4230fc5a67c9935692b474496ea4d5ddc2a60131
Tiana <97kth01@naver.com>
2022-07-29 10:31:21 +0900

    feat: 테스트 파일 생성 및 Mock 파일 테스트 그룹으로 이동

---
+++ ProjectManager/ProjectManagerTests/ProjectManagerTests.swift
@@ -0,0 +1,21 @@
+//
+//  ProjectManagerTests.swift
+//  ProjectManagerTests
+//
+//  Created by Tiana, mmim on 2022/07/29.
+//
+
+import XCTest
+@testable import ProjectManager
+
+class ProjectManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+    
+    override func tearDownWithError() throws {
+    }
+
+    
+
+}
