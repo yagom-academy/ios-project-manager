58b91f22ec96bddb490a16807576f994c35fac45
Tiana <97kth01@naver.com>
2022-07-29 16:16:38 +0900

    feat: MockNetworkManager 구현

--- ProjectManager/ProjectManagerTests/Data/Mock/MockNetworkManager.swift
+++ ProjectManager/ProjectManagerTests/Data/Mock/MockNetworkManager.swift
@@ -14,25 +14,46 @@ struct SampleData {
     static let data = ProjectDTO(id: UUID().uuidString, status: ProjectStatus.todo.string, title: "title111", deadline: String(deadline.timeIntervalSince1970), body: "bodyttt")
 }
 
-struct MockFirebase {
+final class MockFirebase {
     let error: Error?
-    let defaultData = [SampleData.data]
+    private let defaultData = SampleData.data
+    var database: [String: [String: String]] = [:]
     
-    func getData(completion: @escaping (Error?, [ProjectDTO]?) -> Void) {
-        completion(error, defaultData)
+    init(error: Error?) {
+        self.error = error
+        
+        database[defaultData.id] = ["id": defaultData.id,
+                                    "status": defaultData.status,
+                                    "title": defaultData.title,
+                                    "deadline": defaultData.deadline,
+                                    "body": defaultData.body]
     }
     
-    func setValue(_ dic: [String: [String: String]]) {
-        
+    func getData(completion: @escaping (Error?, [String: [String: String]]?) -> Void) {
+        completion(error, database)
+    }
+    
+    func setValue(_ newData: [String: [String: String]]) {
+        database = newData
     }
 }
 
-final class MockNetworkManager: NetworkManagerProtocol {
+final class MockNetworkManager {
+    let database = MockFirebase(error: nil)
+}
+
+extension MockNetworkManager: NetworkManagerProtocol {
     func read() -> Observable<[ProjectDTO]> {
         
-        return Observable.create { emitter in
-            MockFirebase(error: nil).getData { _, projects in
-                emitter.onNext(projects!)
+        return Observable.create { [weak self] emitter in
+            self?.database.getData { _, snapshot in
+                guard let value = snapshot,
+                      let data = try? JSONSerialization.data(withJSONObject: value.map { $1 }),
+                      let projects = try? JSONDecoder().decode([ProjectDTO].self, from: data) else {
+                    return
+                }
+                
+                emitter.onNext(projects)
             }
             
             return Disposables.create()
@@ -40,12 +61,12 @@ final class MockNetworkManager: NetworkManagerProtocol {
     }
     
     func update(projects: [ProjectDTO]) {
-        var dic: [String: [String: String]] = [:]
+        var newData: [String: [String: String]] = [:]
         
         projects.forEach {
-            dic[$0.id] = ["id": $0.id, "status": $0.status, "title": $0.title, "deadline": $0.deadline, "body": $0.body]
+            newData[$0.id] = ["id": $0.id, "status": $0.status, "title": $0.title, "deadline": $0.deadline, "body": $0.body]
         }
         
-        MockFirebase(error: nil).setValue(dic)
+        database.setValue(newData)
     }
 }
