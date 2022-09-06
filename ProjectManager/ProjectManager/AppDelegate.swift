48d983a30aceea696c3f0fb7a4f624ab2b4441f0
ic_6_user_28 <toskw@naver.com>
2022-09-06 16:52:35 +0900

    feat: Swift Lint 라이브러리 추가

--- ProjectManager/ProjectManager/AppDelegate.swift
+++ ProjectManager/ProjectManager/AppDelegate.swift
@@ -10,7 +10,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -30,6 +29,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
