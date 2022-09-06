09ba63f0c4017f83af87c24240b7330bb8cc45af
derrickkim0109 <derrickkim0109@naver.com>
2022-09-06 11:57:00 +0900

    refactor : MainStroyboard 제거 및 코드 베이스를 위한 초기 설정

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -10,6 +10,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        rootViewController.view.backgroundColor = .systemBackground
+        window.rootViewController = UINavigationController(rootViewController: rootViewController )
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
