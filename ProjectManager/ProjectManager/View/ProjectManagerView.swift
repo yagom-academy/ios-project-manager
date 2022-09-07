c65bdaf535916595e989607afe2cbfc19977df1d
Judy <joo00ee@naver.com>
2022-09-07 17:06:48 +0900

    feat: 메인 화면인 ProjectManagerView 생성

---
+++ ProjectManager/ProjectManager/View/ProjectManagerView.swift
@@ -0,0 +1,119 @@
+//
+//  ProjectManagerView.swift
+//  ProjectManager
+//
+//  Created by Judy on 2022/09/07.
+//
+
+import UIKit
+
+final class ProjectManagerView: UIView {
+    // MARK: - Properties
+    private let toDoTitleView = HeaderView()
+    private let doingTitleView = HeaderView()
+    private let doneTitleView = HeaderView()
+    
+    private let toDoTableVeiw: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private let doingTableVeiw: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private let doneTableVeiw: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private let toDoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+        stackView.alignment = .fill
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private let doingStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+        stackView.alignment = .fill
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private let doneStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+        stackView.alignment = .fill
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private let horizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    // MARK: - Methods
+    private func addSubView() {
+        toDoTitleView.configure(title: "TODO", count: 0)
+        doingTitleView.configure(title: "DOING", count: 0)
+        doneTitleView.configure(title: "DONE", count: 0)
+        
+        toDoStackView.addArrangedSubview(toDoTitleView)
+        toDoStackView.addArrangedSubview(toDoTableVeiw)
+        doingStackView.addArrangedSubview(doingTitleView)
+        doingStackView.addArrangedSubview(doingTableVeiw)
+        doneStackView.addArrangedSubview(doneTitleView)
+        doneStackView.addArrangedSubview(doneTableVeiw)
+        
+        horizontalStackView.addArrangedSubview(toDoStackView)
+        horizontalStackView.addArrangedSubview(doingStackView)
+        horizontalStackView.addArrangedSubview(doneStackView)
+        
+        self.addSubview(horizontalStackView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            horizontalStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            horizontalStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+            horizontalStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            horizontalStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8)
+        ])
+    }
+    
+    private func setupView() {
+        addSubView()
+        setupConstraints()
+        self.backgroundColor = .systemBackground
+    }
+}
