7c8037e070b5b1a69a9b84ff5ff0ad22e55517f2
starting1231 <starting1231@naver.com>
2022-09-07 20:42:58 +0900

    feat: 초기화면 설정

--- ProjectManager/ProjectManager/Resource/SceneDelegate.swift
+++ ProjectManager/ProjectManager/Resource/SceneDelegate.swift
@@ -6,26 +6,35 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        guard (scene as? UIWindowScene) != nil else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let mainViewController = ProjectManagerViewController()
+        mainViewController.view.backgroundColor = .systemBackground
+        
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
     }
 }
---
+++ ProjectManager/ProjectManager/Scene/Controller/ProjectManagerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProjectManagerViewController.swift
+//  ProjectManager
+//
+//  Created by NAMU on 2022/09/07.
+//
+
+import UIKit
+
+class ProjectManagerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
