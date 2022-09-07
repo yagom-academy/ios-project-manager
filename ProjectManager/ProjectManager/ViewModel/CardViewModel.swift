6cb8434af9ddfc031e5ea0bf826e0e545203aeec
derrickkim0109 <derrickkim0109@naver.com>
2022-09-08 00:26:54 +0900

    feat : ViewModel 파일 및 추상화 파일 생성

---
+++ ProjectManager/ProjectManager/ViewModel/CardViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  CardViewModel.swift
+//  ProjectManager
+//
+//  Created by Derrick kim on 9/6/22.
+//
+
+final class CardViewModel: CardViewModelProtocol {
+    
+}
---
+++ ProjectManager/ProjectManager/ViewModel/CardViewModelProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CardViewModelProtocol.swift
+//  ProjectManager
+//
+//  Created by Derrick kim on 9/8/22.
+//
+
+import Foundation
+
+protocol CardViewModelProtocol {
+    
+}
