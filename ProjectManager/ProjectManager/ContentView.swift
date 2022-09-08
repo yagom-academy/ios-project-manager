0ab8e5a99730b833416ddd29e15c4f41e58b1e15
ZZBAE <jebyun2@gmail.com>
2022-09-08 17:08:26 +0900

    feat: swiftLint 적용 및 초기 세팅
    - brew로 swiftLint 적용
    - created by 이름 변경 및 필요없는 주석 제거

--- ProjectManager/ProjectManager/ContentView.swift
+++ ProjectManager/ProjectManager/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  ProjectManager
 //
-//  Created by 변재은 on 2022/09/08.
+//  Created by 재재, 언체인 on 2022/09/08.
 //
 
 import SwiftUI
@@ -50,8 +50,6 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -65,8 +63,6 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
--- ProjectManager/ProjectManager/ProjectManagerApp.swift
+++ ProjectManager/ProjectManager/ProjectManagerApp.swift
@@ -2,7 +2,7 @@
 //  ProjectManagerApp.swift
 //  ProjectManager
 //
-//  Created by 변재은 on 2022/09/08.
+//  Created by 재재, 언체인 on 2022/09/08.
 //
 
 import SwiftUI
