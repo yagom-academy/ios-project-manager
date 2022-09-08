2bd94318b1e69b8690d6225c503c98a95a15c73f
minsson <mskaang@gmail.com>
2022-09-08 14:53:10 +0900

    chore: SwiftUI로 프로젝트 세팅

---
+++ ProjectManager/ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by minsson on 2022/09/08.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
