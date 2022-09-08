9e6497e6b936beeb6c16ba625b5cbc185be01cbf
derrickkim0109 <derrickkim0109@naver.com>
2022-09-08 16:04:12 +0900

    chore : 코드 컨벤션에 따른 개행 처리 및 불필요한 부분 제거

--- ProjectManager/ProjectManager/Application/AppDelegate.swift
+++ ProjectManager/ProjectManager/Application/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name: "Default Configuration",
+                                    sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication,
