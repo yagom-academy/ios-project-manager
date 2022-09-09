da08c5fa957f4b8ebe1b43a98a6787efbe976017
finnn1 <tgss3535@gmail.com>
2022-09-09 18:41:32 +0900

    feat: Todo를 추가할 수 있는 ViewController 정의 및 기본 UI 구현

---
+++ ProjectManager/ProjectManager/View/AddTodoViewController.swift
@@ -0,0 +1,150 @@
+//
+//  AddTodoViewController.swift
+//  ProjectManager
+//
+//  Created by Finnn on 2022/09/09.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    private let titleTextField: UITextField = {
+        let textField = UITextField()
+        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 0))
+        textField.font = .preferredFont(forTextStyle: .title2)
+        textField.placeholder = "Title"
+        textField.textAlignment = .left
+        textField.leftViewMode = .always
+        textField.leftView = leftPaddingView
+
+        return textField
+    }()
+    
+    private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.locale = .autoupdatingCurrent
+        datePicker.timeZone = .autoupdatingCurrent
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        return datePicker
+    }()
+    
+    private let bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.adjustsFontForContentSizeCategory = true
+        textView.font = .preferredFont(forTextStyle: .body)
+        textView.textContainerInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        
+        return textView
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        
+        return stackView
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureBackgroundColor()
+        configureHierarchy()
+        configureLayout()
+        configureNavigationBar()
+        
+        configureShadow(titleTextField)
+        configureShadow(bodyTextView)
+    }
+}
+
+// MARK: - Configure Methods
+
+extension AddTodoViewController {
+    private func configureNavigationBar() {
+        title = "TODO"
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.navigationBar.backgroundColor = .systemGray6
+        
+        let cancelButton = UIBarButtonItem(
+            barButtonSystemItem: .cancel,
+            target: self,
+            action: #selector(cancelButtonTapped)
+        )
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(doneButtonTapped)
+        )
+        
+        navigationItem.setLeftBarButton(cancelButton, animated: true)
+        navigationItem.setRightBarButton(doneButton, animated: true)
+    }
+    
+    private func configureBackgroundColor() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func configureHierarchy() {
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(titleTextField)
+        stackView.addArrangedSubview(datePicker)
+        stackView.addArrangedSubview(bodyTextView)
+    }
+    
+    private func configureLayout() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+        
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleTextField.heightAnchor.constraint(equalTo: datePicker.heightAnchor, multiplier: 0.2)
+        ])
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            datePicker.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3)
+        ])
+    }
+    
+    private func configureShadow(_ view: UIView) {
+        view.backgroundColor = .white
+        view.clipsToBounds = false
+
+        view.layer.borderWidth = 2
+        view.layer.borderColor = UIColor.systemGray6.cgColor
+
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowRadius = 3.0
+        view.layer.shadowOffset = CGSize(width: 3, height: 3)
+        view.layer.shadowColor = UIColor.gray.cgColor
+    }
+}
+
+// MARK: - Objective-C Methods
+
+extension AddTodoViewController {
+    @objc private func cancelButtonTapped() {
+        dismiss(animated: true)
+    }
+    
+    @objc private func doneButtonTapped() {
+        dismiss(animated: true)
+    }
+}
