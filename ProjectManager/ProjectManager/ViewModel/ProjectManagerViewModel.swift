f42ab6c3812331b16c393e6a9f45b2e61a40b04c
finnn1 <tgss3535@gmail.com>
2022-09-09 18:03:12 +0900

    feat: Todo List 데이터를 Status에 따라 분류해서 관리할 수 있도록 정의

--- ProjectManager/ProjectManager/ViewModel/ProjectManagerViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel/ProjectManagerViewModel.swift
@@ -13,6 +13,8 @@ class ProjectManagerViewModel {
     // MARK: - Properties
     
     private var allTodoList = BehaviorSubject<[Todo]>(value: [])
+    var categorizedTodoList: [TodoStatus: Observable<[Todo]>] = [:]
+    
     // MARK: - Life Cycle
     
     init() {
@@ -23,5 +25,43 @@ class ProjectManagerViewModel {
             endData: "2022-09-30"
         )
         allTodoList.onNext(sampleData)
+        
+        TodoStatus.allCases.forEach { status in
+            categorizedTodoList[status] = categorizeTodoList(by: status)
+        }
+    }
+}
+
+// MARK: - Categorizer Methods
+
+extension ProjectManagerViewModel {
+    private func categorizeTodoList(by status: TodoStatus) -> Observable<[Todo]> {
+        let categorizedTodoList = allTodoList
+            .map { todoList -> [Todo] in
+                self.categorizeTodoList(whether: status, todoList: todoList)
+            }
+            .map { todoList -> [Todo] in
+                self.checkOutdated(of: todoList)
+            }
+        
+        return categorizedTodoList
+    }
+    
+    private func categorizeTodoList(whether status: TodoStatus, todoList: [Todo]) -> [Todo] {
+        return todoList.filter { todo in
+            todo.status == status
+        }
+    }
+    
+    private func checkOutdated(of todoList: [Todo]) -> [Todo] {
+        return todoList.map { todo -> Todo in
+            if todo.createdAt < Date() {
+                var outDatedTodo: Todo = todo
+                outDatedTodo.isOutdated = true
+                return outDatedTodo
+            }
+            
+            return todo
+        }
     }
 }
