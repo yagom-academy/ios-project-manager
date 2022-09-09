37cf39c4eb5c7950394bd3e3d7ade389439894f7
finnn1 <tgss3535@gmail.com>
2022-09-09 18:07:30 +0900

    feat: Formatted된 Date를 반환하는 String 타입의 연산 프로퍼티 정의

--- ProjectManager/ProjectManager/Extension/Date+Extension.swift
+++ ProjectManager/ProjectManager/Extension/Date+Extension.swift
@@ -1,3 +1,24 @@
+//
+//  Date+Extension.swift
+//  ProjectManager
+//
+//  Created by Finnn on 2022/09/09.
+//
+
+import Foundation
+
+extension Date {
+    var formattedDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .autoupdatingCurrent
+        dateFormatter.timeZone = .autoupdatingCurrent
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        
+        return dateFormatter.string(from: self)
+    }
+}
+
 // MARK: - Generate Random Date (for Test)
 
 extension Date {
