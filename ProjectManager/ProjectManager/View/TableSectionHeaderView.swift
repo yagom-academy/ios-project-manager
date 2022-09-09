5627f6ee4696cec9453b84ba18fa7598d11fc25b
finnn1 <tgss3535@gmail.com>
2022-09-09 18:13:21 +0900

    feat: TableView의 Section Header로 사용될 View 정의

---
+++ ProjectManager/ProjectManager/View/TableSectionHeaderView.swift
@@ -0,0 +1,110 @@
+//
+//  TableSectionHeaderView.swift
+//  ProjectManager
+//
+//  Created by Finnn on 2022/09/09.
+//
+
+import UIKit
+
+class TableSectionHeaderView: UIView {
+    
+    // MARK: - Properties
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.text = "Default Title"
+        
+        return label
+    }()
+    
+    let countLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.layer.cornerRadius = label.font.pointSize
+        label.backgroundColor = .black
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        label.textColor = .white
+        label.text = "77"
+        
+        return label
+    }()
+    
+    private let blankView: UIView = {
+        let view = UIView()
+
+        return view
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray5
+        
+        return view
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureBackgroundColor()
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Configure Methods
+
+extension TableSectionHeaderView {
+    private func configureBackgroundColor() {
+        backgroundColor = .systemGray6
+    }
+    
+    private func configureHierarchy() {
+        addSubview(stackView)
+        addSubview(separatorView)
+        
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(countLabel)
+        stackView.addArrangedSubview(blankView)
+    }
+    
+    private func configureLayout() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 15),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15)
+        ])
+        
+        countLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            countLabel.widthAnchor.constraint(equalTo: countLabel.heightAnchor)
+        ])
+        
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            separatorView.heightAnchor.constraint(equalToConstant: 3),
+            separatorView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            separatorView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            separatorView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -1)
+        ])
+    }
+}
