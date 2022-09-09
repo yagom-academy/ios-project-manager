11bc1a2a6162d957b191a206d8370c6a96560ee2
finnn1 <tgss3535@gmail.com>
2022-09-09 17:56:04 +0900

    feat: Test를 위한 Sample Data를 생성하는 메서드 정의

---
+++ ProjectManager/ProjectManager/Extension/Date+Extension.swift
@@ -0,0 +1,37 @@
+// MARK: - Generate Random Date (for Test)
+
+extension Date {
+    
+    static func randomBetween(start: String, end: String, format: String = "yyyy-MM-dd") -> Date {
+        let date1 = Date.parse(start, format: format)
+        let date2 = Date.parse(end, format: format)
+        return Date.randomBetween(start: date1, end: date2)
+    }
+
+    static func randomBetween(start: Date, end: Date) -> Date {
+        var date1 = start
+        var date2 = end
+        if date2 < date1 {
+            let temp = date1
+            date1 = date2
+            date2 = temp
+        }
+        let span = TimeInterval.random(in: date1.timeIntervalSinceNow...date2.timeIntervalSinceNow)
+        return Date(timeIntervalSinceNow: span)
+    }
+
+    func dateString(_ format: String = "yyyy-MM-dd") -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
+    }
+
+    static func parse(_ string: String, format: String = "yyyy-MM-dd") -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = NSTimeZone.default
+        dateFormatter.dateFormat = format
+
+        let date = dateFormatter.date(from: string)!
+        return date
+    }
+}
--- ProjectManager/ProjectManager/Model/Todo.swift
+++ ProjectManager/ProjectManager/Model/Todo.swift
@@ -14,3 +14,34 @@ struct Todo {
     var status: TodoStatus
     var isOutdated: Bool
 }
+
+// MARK: - Sample Data (for Test)
+
+extension Todo {
+    static func generateSampleData(count: Int, maxBodyLine: Int, startDate: String, endData: String) -> [Todo] {
+        var initialTodoList: [Todo] = []
+        var randomStringList: [String] = []
+        
+        for index in (0..<maxBodyLine) {
+            guard !randomStringList.isEmpty else {
+                randomStringList.append("\(index)번째 줄")
+                continue
+            }
+            
+            let mergedString = randomStringList[index - 1] + "\n\(index)번째 줄"
+            randomStringList.append(mergedString)
+        }
+        
+        for index in (0..<count) {
+            guard let randomStatus = TodoStatus(rawValue: Int.random(in: 0...2)),
+                  let randomString = randomStringList.randomElement() else { break }
+            let randomDate = Date.randomBetween(start: "2022-09-01", end: "2022-09-30")
+            
+            let todo = Todo(title: "\(index)번째 할 일", body: "\(index)번째 : " + randomString, createdAt: randomDate, status: randomStatus, isOutdated: false)
+            
+            initialTodoList.append(todo)
+        }
+        
+        return initialTodoList
+    }
+}
