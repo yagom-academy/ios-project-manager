d5bd2ca9e8cca43b4c7cbdc7d3876a066aa21a80
derrickkim0109 <derrickkim0109@naver.com>
2022-09-09 16:46:32 +0900

    feat : CardType 생성

---
+++ ProjectManager/ProjectManager/Model/CardType.swift
@@ -0,0 +1,12 @@
+//
+//  CardType.swift
+//  ProjectManager
+//
+//  Created by Derrick kim on 9/8/22.
+//
+
+enum CardType: String {
+    case todo = "TODO"
+    case doing = "DOING"
+    case done = "DONE"
+}
