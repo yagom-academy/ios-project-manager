68869038c33b3c86ede9612d3b67e54886f6575b
finnn1 <tgss3535@gmail.com>
2022-09-09 18:05:43 +0900

    feat: Project Manager 기본 UI 세팅

--- ProjectManager/ProjectManager/View/ProjectManagerViewController.swift
+++ ProjectManager/ProjectManager/View/ProjectManagerViewController.swift
@@ -7,11 +7,145 @@
 import UIKit
 
 class ProjectManagerViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    private let cellIdentifier = "tableViewCell"
+    private var collectionView: UICollectionView?
+    
+    private var viewModel = ProjectManagerViewModel()
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        configureViewBackgroundColor()
+        configureCollectionView()
+        configureNavigationBar()
+        configureHierarchy()
+        configureLayout()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        addCollectionViewBottomLine(borderWidth: 2, color: .systemGray5)
+    }
+}
 
+// MARK: - Configure Methods
+
+extension ProjectManagerViewController {
+    private func configureNavigationBar() {
+        title = "Project Manager"
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(rightBarButtonTapped)
+        )
+    }
+    
+    private func configureViewBackgroundColor() {
+        self.view.backgroundColor = .systemBackground.withAlphaComponent(0.98)
+    }
+    
+    private func configureCollectionView() {
+        let collectionViewLayout = createLayout()
+        let initialCollectionView = UICollectionView(
+            frame: view.bounds,
+            collectionViewLayout: collectionViewLayout
+        )
+        
+        initialCollectionView.backgroundColor = .systemGray5
+        initialCollectionView.register(
+            ProjectManagerCollectionViewCell.self,
+            forCellWithReuseIdentifier: cellIdentifier
+        )
+        initialCollectionView.isScrollEnabled = false
+        initialCollectionView.dataSource = self
+        
+        self.collectionView = initialCollectionView
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitem: item,
+            count: 3
+        )
+        group.interItemSpacing = .fixed(10)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    private func configureHierarchy() {
+        guard let collectionView = self.collectionView else { return }
+        view.addSubview(collectionView)
+    }
+    
+    private func configureLayout() {
+        guard let collectionView = self.collectionView else { return }
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - Other Methods
+
+extension ProjectManagerViewController {
+    private func addCollectionViewBottomLine(borderWidth: CGFloat, color: UIColor) {
+        guard let collectionView = collectionView else { return }
+
+        let downBorder = CALayer()
+        downBorder.frame = CGRect(
+            x: 0, y: collectionView.frame.height - borderWidth,
+            width: collectionView.frame.width, height: borderWidth
+        )
+        downBorder.backgroundColor = color.cgColor
+
+        collectionView.layer.addSublayer(downBorder)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ProjectManagerViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return TodoStatus.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let collectionView = self.collectionView,
+              let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: cellIdentifier,
+                for: indexPath
+              ) as? ProjectManagerCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.statusType = TodoStatus(rawValue: indexPath.row)
+        cell.viewModel = self.viewModel
+        
+        return cell
+    }
 }
