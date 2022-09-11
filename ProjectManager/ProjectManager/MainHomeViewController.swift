6149e590bea64f051982a71309d065e88eff3ebc
seohyeon2 <seo_hyeon418@naver.com>
2022-09-12 08:56:54 +0900

    feat: 가로모드만 지원되도록 구현

---
+++ ProjectManager/ProjectManager/MainHomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManager - MainHomeViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class MainHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+    }
+}
