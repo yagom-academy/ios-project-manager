5ed930633f46a9e4a340727a48f9a9b0c062d94d
MinsuJeon <chso294@naver.com>
2022-09-11 16:10:23 +0900

    feat: Cell과 SectionHeaderView 생성에 따른 테이블뷰 로직 수정

--- ProjectManager/ProjectManager/View/ProjectManagerController.swift
+++ ProjectManager/ProjectManager/View/ProjectManagerController.swift
@@ -6,17 +6,20 @@
 
 import UIKit
 
-class ProjectManagerController: UIViewController {
+final class ProjectManagerController: UIViewController {
     typealias DataSource = UITableViewDiffableDataSource<Schedule, ProjectUnit>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Schedule, ProjectUnit>
     
     private let scheduleStackView = ScheduleStackView()
+    private var toDoViewdataSource: DataSource?
+    private var toDoViewSnapshot: Snapshot?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationItems()
         configureUI()
-        
+        configureToDoViewDataSource()
+        configureToDoViewSnapshot()
     }
     
     private func configureNavigationItems() {
@@ -39,13 +42,55 @@ class ProjectManagerController: UIViewController {
             scheduleStackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
+    private func configureToDoViewDataSource() {
+        let tableView = scheduleStackView.toDoListView
+
+        tableView.register(
+            ProjectManagerListCell.self,
+            forCellReuseIdentifier: ProjectManagerListCell.identifier
+        )
+        
+        tableView.delegate = self
+        
+        toDoViewdataSource = DataSource(
+            tableView: tableView,
+            cellProvider: { tableView, indexPath, _ in
+                guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: ProjectManagerListCell.identifier,
+                    for: indexPath
+                ) as? ProjectManagerListCell else {
+                    return nil
+                }
+                
+                cell.separatorInset = .zero
+                
+                return cell
+            }
+        )
+    }
+    
+    private func configureToDoViewSnapshot() {
+        toDoViewSnapshot = Snapshot()
+
+        let unit = ProjectUnit(id: UUID(), title: "쥬스 메이커", body: "쥬스 메이커 프로젝트입니다", section: "ToDo", deadLine: Date())
+        let unit2 = ProjectUnit(id: UUID(), title: "은행 창구 매니저", body: "은행 창구 매니저 프로젝트입니다", section: "Doing", deadLine: Date())
+
+        toDoViewSnapshot?.appendSections([.todo])
+        toDoViewSnapshot?.appendItems([unit, unit2])
+
+        toDoViewdataSource?.apply(toDoViewSnapshot!)
+    }
 }
 
 extension ProjectManagerController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         switch tableView {
         case scheduleStackView.toDoListView:
-            return UIView()
+            let headerView = SectionHeaderView()
+            headerView.setupLabelText(section: "TODO", number: 5)
+            
+            return headerView
         case scheduleStackView.doingListView:
             return UIView()
         case scheduleStackView.doneListView:
@@ -56,16 +101,8 @@ extension ProjectManagerController: UITableViewDelegate {
     }
 }
 
-struct Schedule: Hashable {
-    enum Todo {
-        case main
-    }
-    
-    enum Doing {
-        case main
-    }
-    
-    enum Done {
-        case main
-    }
+enum Schedule {
+    case todo
+    case doing
+    case done
 }
