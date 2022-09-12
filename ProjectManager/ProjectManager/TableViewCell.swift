4ac4ed2fedacd6de5108a1dd147cafdad5742052
seohyeon2 <seo_hyeon418@naver.com>
2022-09-12 13:45:47 +0900

    create: TableViewCell 파일 생성

---
+++ ProjectManager/ProjectManager/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  ProjectManager
+//
+//  Created by seohyeon park on 2022/09/12.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var deadlineLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
