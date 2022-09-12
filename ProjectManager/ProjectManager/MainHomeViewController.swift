bb09ee13d637c72e9632937816ff37865af06ab6
seohyeon2 <seo_hyeon418@naver.com>
2022-09-12 20:34:48 +0900

    refactor: tableView의 cell을 seedData에서 Database 내용으로 변경
    - todoList, doingLis, doneList 배열 생성
    -  setUpDataList 메서드 구현

--- ProjectManager/ProjectManager/MainHomeViewController.swift
+++ ProjectManager/ProjectManager/MainHomeViewController.swift
@@ -12,11 +12,27 @@ class MainHomeViewController: UIViewController {
     @IBOutlet weak var todoTableView: UITableView!
     @IBOutlet weak var doneTableView: UITableView!
 
+    @IBOutlet weak var todoCount: UIButton!
+    @IBOutlet weak var doingCount: UIButton!
+    @IBOutlet weak var doneCount: UIButton!
+
+    private let databaseManager: DatabaseManager = RealmDatabaseManager()
+    private var todoList = [TaskModel]()
+    private var doingList = [TaskModel]()
+    private var doneList = [TaskModel]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let value = UIInterfaceOrientation.landscapeLeft.rawValue
         UIDevice.current.setValue(value, forKey: "orientation")
+
+        setUpDataList()
+
+        todoCount.setTitle(String(todoList.count), for: .normal)
+        doingCount.setTitle(String(doingList.count), for: .normal)
+        doneCount.setTitle(String(doneList.count), for: .normal)
+
         todoTableView.dataSource = self
         todoTableView.delegate = self
 
@@ -26,16 +42,30 @@ class MainHomeViewController: UIViewController {
         doneTableView.dataSource = self
         doneTableView.delegate = self
     }
+
+    private func setUpDataList() {
+        let allDatabase = databaseManager.readDatabase()
+
+        allDatabase.forEach { task in
+            if task.taskState == TaskState.todo {
+                todoList.append(task)
+            } else if task.taskState == TaskState.doing {
+                doingList.append(task)
+            } else {
+                doneList.append(task)
+            }
+        }
+    }
 }
 
 extension MainHomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if tableView == todoTableView {
-            return 5
+            return databaseManager.getTaskStateCount(state: TaskState.todo)
         } else if tableView == doingTableView {
-            return 3
+            return databaseManager.getTaskStateCount(state: TaskState.doing)
         } else {
-            return 1
+            return databaseManager.getTaskStateCount(state: TaskState.done)
         }
     }
 
@@ -43,21 +73,24 @@ extension MainHomeViewController: UITableViewDelegate, UITableViewDataSource {
 
         if tableView == todoTableView {
             let cell = todoTableView.dequeueReusableCell(withIdentifier: "todoTableViewCell", for: indexPath) as! TableViewCell
-            cell.titleLabel.text = "Todo 제목"
-            cell.descriptionLabel.text = "Todo 설명"
-            cell.deadlineLabel.text = "Todo 기한"
+            let data = todoList[indexPath.row]
+            cell.titleLabel.text = data.taskTitle
+            cell.descriptionLabel.text = data.taskDescription
+            cell.deadlineLabel.text = data.taskDeadline
             return cell
         } else if tableView == doingTableView {
             let cell = todoTableView.dequeueReusableCell(withIdentifier: "todoTableViewCell", for: indexPath) as! TableViewCell
-            cell.titleLabel.text = "Doing 제목"
-            cell.descriptionLabel.text = "Doing 설명"
-            cell.deadlineLabel.text = "Doing 기한"
+            let data = doingList[indexPath.row]
+            cell.titleLabel.text = data.taskTitle
+            cell.descriptionLabel.text = data.taskDescription
+            cell.deadlineLabel.text = data.taskDeadline
             return cell
         } else {
             let cell = todoTableView.dequeueReusableCell(withIdentifier: "todoTableViewCell", for: indexPath) as! TableViewCell
-            cell.titleLabel.text = "Done 제목"
-            cell.descriptionLabel.text = "Done 설명"
-            cell.deadlineLabel.text = "Done 기한"
+            let data = doneList[indexPath.row]
+            cell.titleLabel.text = data.taskTitle
+            cell.descriptionLabel.text = data.taskDescription
+            cell.deadlineLabel.text = data.taskDeadline
             return cell
         }
     }
