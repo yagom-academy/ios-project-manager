ea556f10dd6fdf067a64c2ceba4b208b5884b4be
finnn1 <tgss3535@gmail.com>
2022-09-13 18:10:47 +0900

    feat: Firebase 서버로 save, delete, update 요청 메서드 정의

--- ProjectManager/ProjectManager/ViewModel/ProjectManagerViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel/ProjectManagerViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Finnn on 2022/09/08.
 //
 
-import Foundation
+import FirebaseFirestore
 import RxSwift
 
 class ProjectManagerViewModel {
@@ -15,6 +15,9 @@ class ProjectManagerViewModel {
     private var allTodoList = BehaviorSubject<[Todo]>(value: [])
     var categorizedTodoList: [TodoStatus: Observable<[Todo]>] = [:]
     
+    var saveTodoData: PublishSubject<Todo>?
+    var deleteTodoData: PublishSubject<Todo>?
+    var changeStatusTodoData: PublishSubject<(TodoStatus, Int, TodoStatus)>?
     // MARK: - Life Cycle
     
     init() {
@@ -25,10 +28,43 @@ class ProjectManagerViewModel {
             endData: "2022-09-30"
         )
         allTodoList.onNext(sampleData)
+        configureSaveTodoData()
+        configureDeleteTodoData()
+        configureChangeStatusTodoData()
         
         TodoStatus.allCases.forEach { status in
             categorizedTodoList[status] = categorizeTodoList(by: status)
         }
+    }
+    
+    private func configureSaveTodoData() {
+        saveTodoData = PublishSubject<Todo>()
+        
+        saveTodoData?
+            .subscribe(onNext: { todoData in
+                self.requestSave(using: todoData)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureDeleteTodoData() {
+        deleteTodoData = PublishSubject<Todo>()
+        
+        deleteTodoData?
+            .subscribe(onNext: { todoData in
+                self.requestDelete(using: todoData)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureChangeStatusTodoData() {
+        changeStatusTodoData = PublishSubject<(TodoStatus, Int, TodoStatus)>()
+        
+        changeStatusTodoData?
+            .subscribe(onNext: { (currentStatus, indexPathRow, destinationStatus) in
+                self.requestChangeStatus(currentStatus, indexPathRow, destinationStatus)
+            })
+            .disposed(by: disposeBag)
     }
 }
 
@@ -52,6 +88,59 @@ extension ProjectManagerViewModel {
             todo.status == status
         }
     }
+
+// MARK: - Request Methods
+
+extension ProjectManagerViewModel {
+    private func requestSave(using todoData: Todo) {
+            guard todoData.title.isEmpty == false else {
+                alertError.onNext(TodoError.emptyTitle)
+                return
+            }
+            
+            FirebaseManager.shared.sendData(
+                collection: ProjectManagerViewModel.firestoreCollectionName,
+                document: todoData.todoId.uuidString,
+                data: todoData
+            )
+            .take(1)
+            .subscribe(onError: { error in
+                self.alertError.onNext(error)
+            }, onCompleted: {
+                self.saveTodoData?.onCompleted()
+                self.configureSaveTodoData()
+                self.fetchTodoData()
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    private func requestDelete(using todoData: Todo) {
+        FirebaseManager.shared.deleteTodoData(collection: ProjectManagerViewModel.firestoreCollectionName, document: todoData.todoId.uuidString)
+            .subscribe(onError: { error in
+                self.alertError.onNext(error)
+            }, onCompleted: {
+                self.deleteTodoData?.onCompleted()
+                self.configureDeleteTodoData()
+                self.fetchTodoData()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func requestChangeStatus(_ currentStatus: TodoStatus, _ indexPathRow: Int, _ destinationStatus: TodoStatus) {
+        self.categorizedTodoList[currentStatus]?
+            .take(1)
+            .subscribe(onNext: { todoData in
+                var changedTodo = todoData[indexPathRow]
+                changedTodo.status = destinationStatus
+                self.requestSave(using: changedTodo)
+            }, onCompleted: {
+                self.changeStatusTodoData?.onCompleted()
+                self.configureChangeStatusTodoData()
+                self.fetchTodoData()
+            })
+            .disposed(by: disposeBag)
+    }
+}
     
     private func checkOutdated(of todoList: [Todo]) -> [Todo] {
         return todoList.map { todo -> Todo in
