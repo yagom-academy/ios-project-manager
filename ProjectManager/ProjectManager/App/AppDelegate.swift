7dfa00f8aa3080ef2eece9c5dfdf1810ecbc2a28
finnn1 <tgss3535@gmail.com>
2022-09-13 18:00:27 +0900

    feat: FireBase 라이브러리 추가 및 초기 세팅

--- ProjectManager/ProjectManager/App/AppDelegate.swift
+++ ProjectManager/ProjectManager/App/AppDelegate.swift
@@ -5,11 +5,13 @@
 // 
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
