5483c811c94549775815ca8432312d89593d6e66
minsson <mskaang@gmail.com>
2022-09-15 01:21:46 +0900

    feat: "Task" 모델 타입 구현
    - "Status" Task의 할일, 진행중, 완료 여부를 나타낼 타입 구현

---
+++ ProjectManager/ProjectManager/Sources/Common/Status.swift
@@ -0,0 +1,12 @@
+//
+//  Status.swift
+//  ProjectManager
+//
+//  Created by minsson on 2022/09/15.
+//
+
+enum Status {
+    case todo
+    case doing
+    case done
+}
---
+++ ProjectManager/ProjectManager/Sources/Models/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  ProjectManager
+//
+//  Created by minsson on 2022/09/15.
+//
+
+import Foundation
+
+struct Task {
+    var title, description: String
+    var dueDate: Date
+    var status: Status
+}
