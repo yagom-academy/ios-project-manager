36a69b2adb6ab0b7de89b975614ef37a73d5703f
derrickkim0109 <derrickkim0109@naver.com>
2022-09-15 16:20:36 +0900

    feat : 모달을 위한 CardType 문구 생성

--- ProjectManager/ProjectManager/Model/CardType.swift
+++ ProjectManager/ProjectManager/Model/CardType.swift
@@ -9,4 +9,15 @@ enum CardType: String {
     case todo = "TODO"
     case doing = "DOING"
     case done = "DONE"
+    
+    var moveToAnotherSection: String {
+        switch self {
+        case .todo:
+            return "MOVE TO TODO"
+        case .doing:
+            return "MOVE TO DOING"
+        case .done:
+            return "MOVE TO DONE"
+        }
+    }
 }
