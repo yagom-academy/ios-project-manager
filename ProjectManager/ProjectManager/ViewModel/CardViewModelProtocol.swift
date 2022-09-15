2ec89646e6a31ce744d3fcb3cb76113b83aafec8
derrickkim0109 <derrickkim0109@naver.com>
2022-09-15 16:15:46 +0900

    feat : ViewModel 추상화

--- ProjectManager/ProjectManager/ViewModel/CardViewModelProtocol.swift
+++ ProjectManager/ProjectManager/ViewModel/CardViewModelProtocol.swift
@@ -5,8 +5,20 @@
 //  Created by Derrick kim on 9/8/22.
 //
 
-import Foundation
+import UIKit
 
 protocol CardViewModelProtocol {
-    
+    func convert(from card: CardModel) -> CardEntity
+    func append(_ newCard: CardModel)
+    func update(_ selectedCard: CardModel)
+    func delete(_ cardType: CardType, at indextPath: Int)
+    func move(_ card: CardModel?, to anotherCardSection: CardType)
+
+    var todoList: [CardModel]? { get set }
+    var doingList: [CardModel]? { get set }
+    var doneList: [CardModel]? { get set }
+
+    var reloadTodoListTableViewClosure: (([CardModel]) -> Void)? { get set }
+    var reloadDoingListTableViewClosure: (([CardModel]) -> Void)? { get set }
+    var reloadDoneListTableViewClosure: (([CardModel]) -> Void)? { get set }
 }
