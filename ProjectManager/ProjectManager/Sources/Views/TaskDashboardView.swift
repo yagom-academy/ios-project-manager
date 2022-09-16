4d193f8b4c0a527bd7286d993a1428e08fff8131
minsson <mskaang@gmail.com>
2022-09-16 22:56:25 +0900

    feat: "TaskEditingView" Task 작성, 수정, 저장을 위한 시트뷰 구현
    - 시트뷰의 높이를 조절할 방법을 찾지 못해, 우선 구현 완료 후 추후 높이 조절을 시도해볼 예정

--- ProjectManager/ProjectManager/Sources/Views/TaskDashboardView.swift
+++ ProjectManager/ProjectManager/Sources/Views/TaskDashboardView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct TaskDashboardView: View {
     
     @StateObject var taskDashboardViewModel = TaskDashboardViewModel()
+    @State var isShowingSheet = false
     
     var body: some View {
-        
         ZStack(alignment: .bottomTrailing) {
             HStack {
                 TaskListView(status: .todo, tasks: $taskDashboardViewModel.todoTasks)
@@ -21,11 +21,14 @@ struct TaskDashboardView: View {
             }
             
             Button {
-                
+                isShowingSheet.toggle()
             } label: {
                 AddingTaskButtonView()
                     .frame(width: 70, height: 70, alignment: .bottomTrailing)
                     .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 40))
+            }
+            .sheet(isPresented: $isShowingSheet) {
+                TaskEditingView(isShowingSheet: $isShowingSheet, taskDashboardViewModel: taskDashboardViewModel)
             }
         }
     }
---
+++ ProjectManager/ProjectManager/Sources/Views/TaskEditingView.swift
@@ -0,0 +1,59 @@
+//
+//  TaskEditingView.swift
+//  ProjectManager
+//
+//  Created by minsson on 2022/09/16.
+//
+
+import SwiftUI
+
+struct TaskEditingView: View {
+    
+    @Binding var isShowingSheet: Bool
+    @ObservedObject var taskDashboardViewModel: TaskDashboardViewModel
+    @State private var task = Task(title: "", description: "", dueDate: Date.now, status: .todo)
+    
+    var body: some View {
+        VStack {
+            HStack() {
+                Image(systemName: "calendar")
+                
+                DatePicker("", selection: $task.dueDate, in: Date()..., displayedComponents: .date)
+                    .labelsHidden()
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.horizontal)
+            
+            Group {
+                TextField("할 일의 제목을 입력해주세요", text: $task.title)
+                TextField("필요한 경우 상세설명을 적어주세요", text: $task.description)
+            }
+            .padding(.horizontal)
+            .frame(height: 55)
+            .background(Color(UIColor.secondarySystemBackground))
+            .cornerRadius(10)
+            .padding(.horizontal)
+            
+            HStack {
+                SquareButtonView(label: "수정", color: .gray) {
+                    //TODO: 수정 버튼 기능 구현
+                }
+                
+                SquareButtonView(label: "저장", color: Color.accentColor) {
+                    taskDashboardViewModel.todoTasks.append(task)
+                    isShowingSheet.toggle()
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct TaskEditingView_Previews: PreviewProvider {
+    
+    static var taskDashboardView = TaskDashboardView()
+    
+    static var previews: some View {
+        TaskEditingView(isShowingSheet: taskDashboardView.$isShowingSheet, taskDashboardViewModel: TaskDashboardViewModel())
+    }
+}
