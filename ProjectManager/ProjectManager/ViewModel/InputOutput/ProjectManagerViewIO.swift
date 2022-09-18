4a2f748c509627200c6437c27278b046a66d1a43
finnn1 <tgss3535@gmail.com>
2022-09-18 17:19:27 +0900

    feat: Rx Input Output 으로 사용할 구조체 정의

---
+++ ProjectManager/ProjectManager/ViewModel/InputOutput/ProjectManagerViewIO.swift
@@ -0,0 +1,21 @@
+//
+//  ProjectManagerViewOutput.swift
+//  ProjectManager
+//
+//  Created by Finnn on 2022/09/18.
+//
+
+import RxSwift
+import UIKit
+
+// MARK: - ProjectManager View <-> ProjectManager ViewModel (1 : 1)
+
+struct ProjectManagerViewInput {
+    var doneAction: Observable<Todo?>?
+}
+
+struct ProjectManagerViewOutput {
+    var allTodoList: Observable<[Todo]>?
+    var errorAlertContoller: Observable<UIAlertController>?
+    var viewModels: Observable<[TodoStatus: CellViewModelType]>?
+}
