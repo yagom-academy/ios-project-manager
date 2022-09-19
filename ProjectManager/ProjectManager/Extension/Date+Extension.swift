dbd587b8f6bb04fc7aaaee8b2cb7ad0bcb7f0d00
starting1231 <starting1231@naver.com>
2022-09-19 12:15:12 +0900

    refactor: DateFormat 수정

--- ProjectManager/ProjectManager/Extension/Date+Extension.swift
+++ ProjectManager/ProjectManager/Extension/Date+Extension.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Date {
     func convertLocalization() -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .long
+        dateFormatter.dateFormat = "yyyy. MM. dd."
         dateFormatter.locale = Locale(identifier: Locale.preferredLanguages.first ?? "ko")
     
         return dateFormatter.string(from: self)
