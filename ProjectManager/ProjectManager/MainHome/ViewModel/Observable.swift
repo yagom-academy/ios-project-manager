90047adc9f429ffbef6ee0f2c2ee193ddd9c45cd
현이 <50102522+seohyeon2@users.noreply.github.com>
2022-09-20 14:03:37 +0900

    chore: 코드 정리
    Co-authored-by: Doe-Hyung KIM <kimdo2297@gmail.com>

--- ProjectManager/ProjectManager/MainHome/ViewModel/Observable.swift
+++ ProjectManager/ProjectManager/MainHome/ViewModel/Observable.swift
@@ -8,14 +8,17 @@
 class Observable<T> {
   typealias Listener = (T) -> Void
   var listener: Listener?
+  
   var value: T {
     didSet {
       listener?(value)
     }
   }
+  
   init(_ value: T) {
     self.value = value
   }
+  
   func bind(listener: Listener?) {
     self.listener = listener
     listener?(value)
