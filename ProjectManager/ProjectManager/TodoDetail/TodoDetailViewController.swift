82972fb28f3927a29959ec46e85c7464cb2649de
BaekGom <jihunmm02@gmail.com>
2022-09-21 22:10:19 +0900

    feat: TodoDetailViewController 생성

---
+++ ProjectManager/ProjectManager/TodoDetail/TodoDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TodoDetailViewController.swift
+//  ProjectManager
+//
+//  Created by Baek on 2022/09/21.
+//
+
+import Foundation
+import UIKit
+import RxCocoa
+import RxSwift
+
+class TodoDetailViewController: UIViewController {
+    
+}
