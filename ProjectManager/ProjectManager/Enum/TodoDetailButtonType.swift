19b6781f9487a04c2b456e228f434c5d656c9eb6
BaekGom <jihunmm02@gmail.com>
2022-09-23 20:25:15 +0900

    feat: 하드코딩 제거를 위한 Enum 추가

---
+++ ProjectManager/ProjectManager/Enum/TodoDetailButtonType.swift
@@ -0,0 +1,15 @@
+//
+//  TodoDetailButtonType.swift
+//  ProjectManager
+//
+//  Created by Baek on 2022/09/22.
+//
+
+import Foundation
+
+enum TodoDetailButtonType {
+    static let title = "Title"
+    static let done = "Done"
+    static let edit = "Edit"
+    static let cancel = "Cancel"
+}
