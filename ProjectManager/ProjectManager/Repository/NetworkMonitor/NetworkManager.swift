8f3a9b4383f1a310cd121379188371f8ff1194a1
derrickkim0109 <derrickkim0109@naver.com>
2022-09-27 23:19:31 +0900

    refactor : `NetworkManager` 불필요한 메서드

--- ProjectManager/ProjectManager/Repository/NetworkMonitor/NetworkManager.swift
+++ ProjectManager/ProjectManager/Repository/NetworkMonitor/NetworkManager.swift
@@ -13,17 +13,8 @@ final class NetworkManager {
 
     private let queue = DispatchQueue.global()
     private let monitor: NWPathMonitor
-
     public private(set) var isConnected: Bool = false
-    public private(set) var connectionType: ConnectionType = .unknown
-    
-    enum ConnectionType {
-        case wifi
-        case cellular
-        case ethernet
-        case unknown
-    }
-    
+
     private init() {
         monitor = NWPathMonitor()
     }
@@ -32,26 +23,6 @@ final class NetworkManager {
         monitor.start(queue: queue)
         monitor.pathUpdateHandler = { [weak self] path in
             self?.isConnected = path.status == .satisfied
-            self?.getConenctionType(path)
-        }
-    }
-    
-    public func stopMonitoring() {
-        monitor.cancel()
-    }
-    
-    private func getConenctionType(_ path: NWPath) {
-        if path.usesInterfaceType(.wifi) {
-            connectionType = .wifi
-
-        } else if path.usesInterfaceType(.cellular) {
-            connectionType = .cellular
-
-        } else if path.usesInterfaceType(.wiredEthernet) {
-            connectionType = .ethernet
-
-        } else {
-            connectionType = .unknown
         }
     }
 }
