9a555d4fb15897b3fac9f14fa1dd514636d49dfe
MinsuJeon <chso294@naver.com>
2022-09-28 10:56:06 +0900

    refactor: Recognizer 프로퍼티 변경

--- ProjectManager/ProjectManager/View/MainView/ProjectListViewController.swift
+++ ProjectManager/ProjectManager/View/MainView/ProjectListViewController.swift
@@ -73,13 +73,13 @@ final class ProjectListViewController:
         }
 
         let longPressLocation = recognizer.location(in: self.tableView)
+        let currentCursorLocation = recognizer.location(in: self.view)
 
-        guard let tapIndexPath = self.tableView.indexPathForRow(at: longPressLocation),
-              let tappedCell = self.tableView.cellForRow(at: tapIndexPath) as? ProjectManagerListCell else {
+        guard let tapIndexPath = self.tableView.indexPathForRow(at: longPressLocation) else {
             return
         }
 
-        configurePopoverController(indexPath: tapIndexPath.row, in: tappedCell)
+        configurePopoverController(indexPath: tapIndexPath.row, at: currentCursorLocation)
     }
 
     private func configureUI() {
@@ -188,7 +188,7 @@ final class ProjectListViewController:
         self.present(navigationController, animated: true)
     }
 
-    private func configurePopoverController(indexPath: Int, in cell: UITableViewCell) {
+    private func configurePopoverController(indexPath: Int, at current: CGPoint) {
         let controller = PopoverController()
         controller.viewModel = self.viewModel as? StatusChangable
         controller.indexPath = indexPath
@@ -199,13 +199,12 @@ final class ProjectListViewController:
         guard let popController = controller.popoverPresentationController else {
             return
         }
-        popController.permittedArrowDirections = .up
-
+        popController.permittedArrowDirections = .any
         popController.delegate = self
         popController.sourceView = view
         popController.sourceRect = CGRect(
-            x: cell.frame.midX,
-            y: cell.frame.midY,
+            x: current.x,
+            y: current.y,
             width: 0,
             height: 0
         )
