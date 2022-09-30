39b5e5cd5d3ff3537817248c7347fce88f1967bf
yusw10 <yusw10@naver.com>
2022-09-30 16:21:40 +0900

    feat: Private 과 Shared가 가능하도록 contiainer 수정

--- ProjectManager/ProjectManager/Sources/LocalDB/CoreDataManager.swift
+++ ProjectManager/ProjectManager/Sources/LocalDB/CoreDataManager.swift
@@ -6,19 +6,77 @@
 //
 
 import CoreData
+import CloudKit
 
 class CoreDataManager: CoreDataManagerInterface {
     
     //MARK: - Properties
     
-    private lazy var persistentContainer: NSPersistentCloudKitContainer = {
+    private var _privatePersistentStore: NSPersistentStore?
+    private var _sharedPersistentStore: NSPersistentStore?
+    
+    var privatePersistentStore: NSPersistentStore {
+        guard let privateStore = _privatePersistentStore else {
+            fatalError("Private store is not set")
+        }
+        return privateStore
+    }
+    
+    var sharedPersistentStore: NSPersistentStore {
+        guard let sharedStore = _sharedPersistentStore else {
+            fatalError("Shared store is not set")
+        }
+        return sharedStore
+    }
+    
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
         let container = NSPersistentCloudKitContainer(name: "ProjectTask")
         
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        guard let privateStoreDescription = container.persistentStoreDescriptions.first else {
+            fatalError("Unable to get persistentStoreDescription")
+        }
+        
+        let storesURL = privateStoreDescription.url?.deletingLastPathComponent()
+        privateStoreDescription.url = storesURL?.appendingPathComponent("private.sqlite")
+        let sharedStoreURL = storesURL?.appendingPathComponent("shared.sqlite")
+        guard let sharedStoreDescription = privateStoreDescription.copy() as? NSPersistentStoreDescription else {
+            fatalError("Copying the private store description returned an unexpected value.")
+        }
+        sharedStoreDescription.url = sharedStoreURL
+        
+        guard let containerIdentifier = privateStoreDescription.cloudKitContainerOptions?.containerIdentifier else {
+            fatalError("Unable to get containerIdentifier")
+        }
+        let sharedStoreOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: containerIdentifier)
+        sharedStoreOptions.databaseScope = .shared
+        sharedStoreDescription.cloudKitContainerOptions = sharedStoreOptions
+        container.persistentStoreDescriptions.append(sharedStoreDescription)
+        
+        container.loadPersistentStores { loadedStoreDescription, error in
             if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                fatalError("Failed to load persistent stores: \(error)")
+            } else if let cloudKitContainerOptions = loadedStoreDescription.cloudKitContainerOptions {
+                guard let loadedStoreDescritionURL = loadedStoreDescription.url else {
+                    return
+                }
+                
+                if cloudKitContainerOptions.databaseScope == .private {
+                    let privateStore = container.persistentStoreCoordinator.persistentStore(for: loadedStoreDescritionURL)
+                    self._privatePersistentStore = privateStore
+                } else if cloudKitContainerOptions.databaseScope == .shared {
+                    let sharedStore = container.persistentStoreCoordinator.persistentStore(for: loadedStoreDescritionURL)
+                    self._sharedPersistentStore = sharedStore
+                }
             }
-        })
+        }
+        
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        do {
+            try container.viewContext.setQueryGenerationFrom(.current)
+        } catch {
+            fatalError("Failed to pin viewContext to the current generation: \(error)")
+        }
         
         return container
     }()
