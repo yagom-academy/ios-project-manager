329584cc3cbe3d1161b416eac339b3559f29161c
미니 <cow970814@naver.com>
2023-01-09 19:11:01 +0900

    :tada: swiftUI 설정 변경

---
+++ ProjectManager/ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+        
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
