736f53e16d7b36857de3d4daac1b20d523b734b0
Kyo <hyosing92@gmail.com>
2023-01-11 23:32:38 +0900

    feat : NavigationBar 구현 및 Process 별 StackView 생성

--- ProjectManager/ProjectManager/View/MainViewController.swift
+++ ProjectManager/ProjectManager/View/MainViewController.swift
@@ -7,17 +7,64 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-
+    private enum Constant {
+        case navigationTitle
+        
+        var description: String {
+            switch self {
+            case .navigationTitle:
+                return "Project Manager"
+            }
+        }
+    }
+    
+    private let todoStackView = ProcessStackView(process: .todo)
+    private let doingStackView = ProcessStackView(process: .doing)
+    private let doneStackView = ProcessStackView(process: .done)
+    
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView(
+            arrangedSubviews: [todoStackView, doingStackView, doneStackView]
+        )
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+        setupView()
+    }
+    
+    @objc private func addButtonTapped() {
+        // 추후 구현
     }
 }
 
-
 // MARK: - UI Configuration
-
 extension MainViewController {
+    private func setupNavigationBar() {
+        title = Constant.navigationTitle.description
+        let appearence = UINavigationBarAppearance()
+        
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.backgroundColor = .systemGray6
+        navigationController?.navigationBar.standardAppearance = appearence
+        navigationController?.navigationBar.scrollEdgeAppearance = appearence
+        
+        let addBarButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonTapped)
+        )
+        navigationItem.rightBarButtonItem = addBarButton
+    }
+    
     private func setupView() {
-        title = "Project Manager"
+        view.backgroundColor = .systemGray5
+        view.addSubview(mainStackView)
     }
 }
