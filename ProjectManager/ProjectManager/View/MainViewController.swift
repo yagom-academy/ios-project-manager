a4de3e28a265a051c9c8f0e8e8fe08ace51f1ebe
Kyo <hyosing92@gmail.com>
2023-01-12 23:03:25 +0900

    feat : MainViewController 내부 각 테이블의 DiffableDataSource 생성 메서드 구현

--- ProjectManager/ProjectManager/View/MainViewController.swift
+++ ProjectManager/ProjectManager/View/MainViewController.swift
@@ -7,6 +7,8 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    typealias DataSource = UITableViewDiffableDataSource<Process, Todo>
+    
     private enum Constant {
         static let navigationTitle = "Project Manager"
         static let tableSpacing = 10.0
@@ -16,7 +18,11 @@ final class MainViewController: UIViewController {
     private let todoView = ProcessStackView(process: .todo)
     private let doingView = ProcessStackView(process: .doing)
     private let doneView = ProcessStackView(process: .done)
-        
+    
+    private lazy var todoDataSource = configureTodoDataSource()
+    private lazy var doingDataSource = configureDoingDataSource()
+    private lazy var doneDataSource = configureDoneDataSource()
+    
     private lazy var mainStackView = UIStackView(
         views: [todoView, doingView, doneView],
         axis: .horizontal,
@@ -72,5 +78,74 @@ extension MainViewController {
                 constant: Constant.bottomValue
             )
         ])
+    }
+}
+
+// MARK: - DataSource Configuration
+extension MainViewController {
+    private func configureTodoDataSource() -> DataSource {
+        let dataSource = DataSource(
+            tableView: todoView.tableView
+        ) { tableView, indexPath, todoData in
+
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: ProcessTableViewCell.identifier,
+                for: indexPath
+            ) as? ProcessTableViewCell else {
+                // Error Alert 구현예정
+                let errorCell = UITableViewCell()
+                return errorCell
+            }
+            
+            cell.titleLabel.text = todoData.title
+            cell.descriptionLabel.text = todoData.content
+            cell.dateLabel.text = todoData.convertDeadline
+            return cell
+        }
+        return dataSource
+    }
+
+    private func configureDoingDataSource() -> DataSource {
+        let dataSource = DataSource(
+            tableView: doingView.tableView
+        ) { tableView, indexPath, todoData in
+
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: ProcessTableViewCell.identifier,
+                for: indexPath
+            ) as? ProcessTableViewCell else {
+                // Error Alert 구현예정
+                let errorCell = UITableViewCell()
+                return errorCell
+            }
+            
+            cell.titleLabel.text = todoData.title
+            cell.descriptionLabel.text = todoData.content
+            cell.dateLabel.text = todoData.convertDeadline
+            return cell
+        }
+        return dataSource
+    }
+
+    private func configureDoneDataSource() -> DataSource {
+        let dataSource = DataSource(
+            tableView: doneView.tableView
+        ) { tableView, indexPath, todoData in
+            
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: ProcessTableViewCell.identifier,
+                for: indexPath
+            ) as? ProcessTableViewCell else {
+                // Error Alert 구현예정
+                let errorCell = UITableViewCell()
+                return errorCell
+            }
+            
+            cell.titleLabel.text = todoData.title
+            cell.descriptionLabel.text = todoData.content
+            cell.dateLabel.text = todoData.convertDeadline
+            return cell
+        }
+        return dataSource
     }
 }
