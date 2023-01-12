b5897c4f782c92b8fd65549f0748439c1c32273e
lnhk98 <hozzang666@gmail.com>
2023-01-12 17:29:23 +0900

    feat: ViewController에 totalStackView와 ListStackView프로퍼티 3개 구현 및 레이아웃 구성

--- ProjectManager/ProjectManager/Source/View/ViewController.swift
+++ ProjectManager/ProjectManager/Source/View/ViewController.swift
@@ -7,9 +7,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let totalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.backgroundColor = .systemGray3
+        
+        return stackView
+    }()
+    
+    private let todoListStackView = ListStackView(title: Constant.todo)
+    private let doingListStackView = ListStackView(title: Constant.doing)
+    private let doneListStackView = ListStackView(title: Constant.done)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureLayout()
     }
+    
+    private func configureLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        [todoListStackView, doingListStackView, doneListStackView].forEach {
+            totalStackView.addArrangedSubview($0)
+        }
+        view.addSubview(totalStackView)
+        
+        NSLayoutConstraint.activate([
+            totalStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            totalStackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            totalStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            totalStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+}
+
+private enum Constant {
+    static let todo = "TODO"
+    static let doing = "DOING"
+    static let done = "DONE"
 }
