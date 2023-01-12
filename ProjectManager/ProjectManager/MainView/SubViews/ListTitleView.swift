c9f36bac586ce3083c32992551f37467e1f42a52
미니 <cow970814@naver.com>
2023-01-12 17:45:47 +0900

    :recycle: Project 모델 타입 상태에 대한 값 추가

---
+++ ProjectManager/ProjectManager/MainView/SubViews/ListTitleView.swift
@@ -0,0 +1,8 @@
+//
+//  ListTitleView.swift
+//  ProjectManager
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+        
+
+import Foundation
--- ProjectManager/ProjectManager/Model/Project.swift
+++ ProjectManager/ProjectManager/Model/Project.swift
@@ -8,4 +8,18 @@ struct Project: Decodable {
   let title: String
   let date: Int
   let description: String
+  var state: ProjectState = .todo
+  
+  static let mock: [Project] = [
+    .init(title: "책상 정리", date: 1000000, description: "집중이 안될때는 역시나 책상 정리"),
+    .init(title: "책상 정리2", date: 1000000, description: "집중이 안될때는 역시나 책상 정리2"),
+    .init(title: "책상 정리3", date: 1000000, description: "집중이 안될때는 역시나 책상 정리3"),
+    .init(title: "책상 정리4", date: 1000000, description: "집중이 안될때는 역시나 책상 정리4"),
+  ]
+}
+
+enum ProjectState: Int, CaseIterable, Decodable {
+  case todo = 1
+  case doing = 2
+  case done = 3
 }
