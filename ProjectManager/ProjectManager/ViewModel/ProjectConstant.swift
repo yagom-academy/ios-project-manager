40f5bbfe5215d41314b24f1556a9f959b6f8c696
Gundy <korea1880@naver.com>
2023-01-13 00:22:52 +0900

    feat: 상수 값을 관리할 ProjectConstant 추가

---
+++ ProjectManager/ProjectManager/ViewModel/ProjectConstant.swift
@@ -0,0 +1,26 @@
+//
+//  ProjectConstant.swift
+//  ProjectManager
+//
+//  Created by Gundy on 2023/01/12.
+//
+
+enum ProjectConstant {
+
+    enum Text {
+
+        static let navigationTitle: String = "Project Manager"
+        static let plusButton: String = "+"
+        static let doneButton: String = "Done"
+        static let editButton: String = "Edit"
+        static let cancelButton: String = "Cancel"
+        static let toDoTitle: String = "TODO"
+        static let doingTitle: String = "DOING"
+        static let doneTitle: String = "DONE"
+        static let deleteSwipeTitle: String = "Delete"
+        static let moveToToDo: String = "Move to TODO"
+        static let moveToDoing: String = "Move to DOING"
+        static let moveToDone: String = "Move to DONE"
+        static let titlePlaceHolder: String = "Title"
+    }
+}
