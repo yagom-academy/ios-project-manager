267838caf8cc13354f40608efa6548632c8cd1f6
woong <workplayhard1@naver.com>
2023-01-12 18:20:26 +0900

    feat: UI구성 (2) - 셀 Xib
    - 셀 파일 생성 및 CellXib 생성
    - IBOutlet 설정

---
+++ ProjectManager/ProjectManager/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  ProjectManager
+//
+//  Created by 서현웅 on 2023/01/12.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
