9e552d6cb072a11fd19f8d05871ab17564056f47
Kyo <hyosing92@gmail.com>
2023-01-12 20:48:47 +0900

    feat : Todo 데이터 타입 생성

---
+++ ProjectManager/ProjectManager/Model/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  ProjectManager
+//
+//  Created by Kyo on 2023/01/12.
+//
+
+import Foundation
+
+struct Todo {
+    var title: String
+    var content: String?
+    var deadLine: Date?
+}
