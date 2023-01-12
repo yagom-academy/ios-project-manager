42dba6af26d8d9e89f22ca39953273676e81ced9
Kyo <hyosing92@gmail.com>
2023-01-12 13:32:31 +0900

    feat : TableView 내부 Cell 구현

---
+++ ProjectManager/ProjectManager/View/ProcessTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  ProcessTableViewCell.swift
+//  ProjectManager
+//
+//  Created by Kyo on 2023/01/12.
+//
+
+import UIKit
+
+final class ProcessTableViewCell: UITableViewCell {
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title2)
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title3)
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel, dateLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fill
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        setupConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - UI Configuration
+extension ProcessTableViewCell {
+    private func setupView() {
+        contentView.addSubview(stackView)
+    }
+    
+    private func setupConstraint() {
+        let safeArea = contentView.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+}
