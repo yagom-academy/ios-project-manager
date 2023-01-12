d37bccac86e2a9082debfc76de2a0e29bd803bdb
DragonYG <kioswe92@gmail.com>
2023-01-12 23:16:01 +0900

    feat: MainProjectManagerView에 테이블뷰 3개 구현

--- ProjectManager/ProjectManager/Source/Model/Controller/MainViewController.swift
+++ ProjectManager/ProjectManager/Source/Model/Controller/MainViewController.swift
@@ -8,13 +8,44 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    // MARK: Properties
+    
+    private let projectManagerView: MainProjectManagerView = MainProjectManagerView()
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view = projectManagerView
+        
         configureUI()
+        projectManagerView.setUpTableView(with: self)
     }
+    
+    // MARK: Private Methods
     
     private func configureUI() {
         view.backgroundColor = .white
         navigationItem.title = "Project Manager"
+    }
+}
+
+// MARK: - TableViewDelegate
+
+extension MainViewController: UITableViewDelegate {
+}
+
+// MARK: - TableViewDataSource
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
     }
 }
---
+++ ProjectManager/ProjectManager/Source/Model/View/MainProjectManagerView.swift
@@ -0,0 +1,87 @@
+//
+//  MainProjectManagerView.swift
+//  ProjectManager
+//
+//  Created by Dragon on 2023/01/12.
+//
+
+import UIKit
+
+final class MainProjectManagerView: UIView {
+    
+    // MARK: Properties
+    
+    private let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        
+        return stackView
+    }()
+    private let leftTableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    private let centerTableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    private let rightTableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    // MARK: Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Internal Methods
+    
+    func setUpTableView(with mainViewController: MainViewController) {
+        leftTableView.delegate = mainViewController
+        centerTableView.delegate = mainViewController
+        rightTableView.delegate = mainViewController
+        
+        leftTableView.dataSource = mainViewController
+        centerTableView.dataSource = mainViewController
+        rightTableView.dataSource = mainViewController
+    }
+    
+    // MARK: Private Methods
+    
+    private func setUpStackView() {
+        mainStackView.addArrangedSubview(leftTableView)
+        mainStackView.addArrangedSubview(centerTableView)
+        mainStackView.addArrangedSubview(rightTableView)
+    }
+    
+    private func configureLayout() {
+        addSubview(mainStackView)
+        
+        NSLayoutConstraint.activate([
+            mainStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mainStackView.topAnchor.constraint(equalTo: topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
