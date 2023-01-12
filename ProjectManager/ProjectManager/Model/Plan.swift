7c784909a0c3ebb170dede3d26db3c57ba120c76
Gundy <korea1880@naver.com>
2023-01-12 18:15:09 +0900

    feat: 계획의 상태를 표현할 PlanState 추가 및 Plan, ToDo 수정

--- ProjectManager/ProjectManager/Model/Plan.swift
+++ ProjectManager/ProjectManager/Model/Plan.swift
@@ -8,13 +8,29 @@
 import Foundation
 
 protocol Plan {
-    var title: String? { get set }
-    var description: String? { get set }
-    var deadline: Date? { get set }
+    var title: String { get set }
+    var description: String { get set }
+    var deadline: Date { get set }
+    var state: PlanState { get set }
 }
 
 struct ToDo: Plan {
-    var title: String?
-    var description: String?
-    var deadline: Date?
+    var title: String
+    var description: String
+    var deadline: Date
+    var state: PlanState
+
+    init(title: String, description: String, deadline: Date, state: PlanState = .toDo) {
+        self.title = title
+        self.description = description
+        self.deadline = deadline
+        self.state = state
+    }
+}
+
+enum PlanState {
+
+    case toDo
+    case doing
+    case done
 }
