701695326b2abb6cb2c2cb7bf594afc6dd6fc2f9
junho <junhoL1046@gmail.com>
2023-01-13 22:34:56 +0900

    feat: 스토리보드 제거, ProjectListViewController 추가

---
+++ ProjectManager/ProjectManager/Controllers/ProjectListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProjectListViewController.swift
+//  ProjectManager
+//
+//  Created by junho lee on 2023/01/13.
+//
+
+import UIKit
+
+class ProjectListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
