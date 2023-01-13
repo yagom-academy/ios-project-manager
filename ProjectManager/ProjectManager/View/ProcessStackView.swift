0b87d699a25dbf2eda35f21f294aa79ec5897660
Kyo <hyosing92@gmail.com>
2023-01-13 12:14:00 +0900

    refactor : ProcessStackView 내부 불필요한 StackView 삭제

--- ProjectManager/ProjectManager/View/ProcessStackView.swift
+++ ProjectManager/ProjectManager/View/ProcessStackView.swift
@@ -16,15 +16,7 @@ final class ProcessStackView: UIStackView {
     
     let headerView: HeaderView
     let tableView = UITableView(frame: .zero, style: .grouped)
-    
-    private lazy var stackView = UIStackView(
-        views: [headerView, tableView],
-        axis: .vertical,
-        alignment: .fill,
-        distribution: .fill,
-        spacing: UIConstraint.stackViewSpacing
-    )
-    
+        
     init(process: Process) {
         headerView = HeaderView(process: process)
         super.init(frame: .zero)
@@ -41,9 +33,13 @@ final class ProcessStackView: UIStackView {
 // MARK: - UI Configuration
 extension ProcessStackView {
     private func setupView() {
+        [headerView, tableView].forEach(addArrangedSubview(_:))
+        axis = .vertical
+        alignment = .fill
+        distribution = .fill
+        spacing = UIConstraint.stackViewSpacing
+        
         headerView.translatesAutoresizingMaskIntoConstraints = false
-        translatesAutoresizingMaskIntoConstraints = false
-        addSubview(stackView)
     }
     
     private func setupTableView() {
@@ -65,11 +61,6 @@ extension ProcessStackView {
     private func setupCosntraint() {
         let safeArea = safeAreaLayoutGuide
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
-            
             headerView.widthAnchor.constraint(equalTo: widthAnchor),
             headerView.heightAnchor.constraint(equalToConstant: UIConstraint.headerViewHeight)
         ])
