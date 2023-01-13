2816fe7e8d84053eb709694e29bc66e2fdc56889
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-01-13 09:47:19 +0900

    feat: Model 타입 구현 - Project 구조체

---
+++ ProjectManager/ProjectManager/Project.swift
@@ -0,0 +1,15 @@
+//
+//  Project.swift
+//  ProjectManager
+//
+//  Created by 써니쿠키 on 2023/01/13.
+//
+
+import Foundation
+
+struct Project: Hashable {
+    
+    let title: String
+    let description: String
+    let date: Date
+}
