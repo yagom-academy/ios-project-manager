af0e6c53fefa9a8be7975c5f5166cdb4e291d45e
zhilly11 <zhzh1x4@gmail.com>
2023-01-13 20:47:41 +0900

    feat: ToDo 타입 생성

---
+++ ProjectManager/ProjectManager/Source/Model/ToDo.swift
@@ -0,0 +1,11 @@
+//  ProjectManager - ToDo.swift
+//  created by zhilly on 2023/01/13
+
+import Foundation
+
+struct ToDo {
+    var title: String
+    var body: String
+    var deadline: Date
+    var state: ToDoState
+}
