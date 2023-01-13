e967e75f0e4916c0a884ebadb1e7b7c2bed9c88d
Kyo <hyosing92@gmail.com>
2023-01-13 14:04:55 +0900

    feat : MainViewModel 내부 fetchData( )메서드 구현

--- ProjectManager/ProjectManager/ViewModel/MainViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel/MainViewModel.swift
@@ -11,4 +11,15 @@ final class MainViewModel {
     private var todoData: [Todo] = []
     private var doingData: [Todo] = []
     private var doneData: [Todo] = []
+    
+    func fetchData(process: Process) -> [Todo] {
+        switch process {
+        case .todo:
+            return todoData
+        case .doing:
+            return doingData
+        case .done:
+            return doneData
+        }
+    }
 }
