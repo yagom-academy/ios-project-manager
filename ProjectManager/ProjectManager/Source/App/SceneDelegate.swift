cfb3b3abbd108d9dc1ce10c8e5985d5ddf19a2aa
zhilly11 <zhzh1x4@gmail.com>
2023-01-13 22:10:33 +0900

    feat: MainViewController를 rootViewController로 설정

--- ProjectManager/ProjectManager/Source/App/SceneDelegate.swift
+++ ProjectManager/ProjectManager/Source/App/SceneDelegate.swift
@@ -9,5 +9,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let mainViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 }
