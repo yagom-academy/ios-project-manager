f0bac5e210dc7310989ae735a07abbd7770a094d
woong <workplayhard1@naver.com>
2023-01-13 17:00:50 +0900

    feat: 타입구현 (2) - TaskListViewModel, TaskViewmodel
    - TaskListViewModel이 Task를 배열로 가지도록 구현
    - TaskViewModel이 Task를 갖고 셀의 요소들을 반환하도록 구현

--- ProjectManager/ProjectManager/ViewModel/TaskViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel/TaskViewModel.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 struct TaskViewModel {
+    private let task: Task
     
+    init(_ task: Task) {
+        self.task = task
+    }
+}
+
+extension TaskViewModel {
+    var title: String? {
+        return self.task.title
+    }
+    var description: String? {
+        return self.task.description
+    }
+    var date: Date? {
+        return self.task.date
+    }
 }
