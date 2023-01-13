8465169c18b37a92d01214d064774fe373c40d67
Kyo <hyosing92@gmail.com>
2023-01-13 14:16:29 +0900

    feat : ModifyViewController 생성 및 View폴더 내부 화면 별 폴더화

---
+++ ProjectManager/ProjectManager/View/ModifyScenes/ModifyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ModifyViewController.swift
+//  ProjectManager
+//
+//  Created by Kyo on 2023/01/13.
+//
+
+import UIKit
+
+class ModifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
