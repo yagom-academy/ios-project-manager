fba719c28c8148f6f66a0512ffead1424571bc13
Kyo <hyosing92@gmail.com>
2023-01-13 14:02:33 +0900

    feat : MainViewModle 생성 및 상태별 Data 프로퍼티 정의

---
+++ ProjectManager/ProjectManager/ViewModel/MainViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewModel.swift
+//  ProjectManager
+//
+//  Created by Kyo on 2023/01/12.
+//
+
+import Foundation
+
+final class MainViewModel {
+    private var todoData: [Todo] = []
+    private var doingData: [Todo] = []
+    private var doneData: [Todo] = []
+}
