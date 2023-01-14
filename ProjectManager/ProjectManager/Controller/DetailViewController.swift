6ffd3df56e6012c2e43a3ecbf451bfb3d3e2dc5f
Mangdi <lkw4670@naver.com>
2023-01-15 01:37:36 +0900

    feat: TitleTextField 그림자효과 적용

--- ProjectManager/ProjectManager/Controller/DetailViewController.swift
+++ ProjectManager/ProjectManager/Controller/DetailViewController.swift
@@ -20,34 +20,18 @@ class DetailViewController: UIViewController {
         return navigationBar
     }()
 
-    private let titleTextView: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.font = UIFont.preferredFont(forTextStyle: .title3)
-        textView.layer.borderWidth = 1
-        textView.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.7).cgColor
-        textView.clipsToBounds = false
-        textView.layer.shadowOpacity = 0.3
-        textView.layer.shadowOffset = CGSize(width: 0, height: 3)
-        textView.textColor = UIColor.systemGray3
-        textView.textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        return textView
-    }()
-
     private let titleTextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.font = UIFont.preferredFont(forTextStyle: .title3)
         textField.layer.borderWidth = 1
         textField.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.7).cgColor
-//        textField.clipsToBounds = false
         textField.layer.shadowOpacity = 0.3
         textField.layer.shadowOffset = CGSize(width: 0, height: 3)
         textField.placeholder = "Title"
         textField.backgroundColor = .white
         textField.textColor = UIColor.systemGray3
-        // textview처럼 textContainerInset기능이 없음 따라서 아래와 같이 작성
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: textField.frame.height))
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: textField.frame.height))
         textField.leftView = paddingView
         textField.leftViewMode = .always
         return textField
@@ -58,7 +42,6 @@ class DetailViewController: UIViewController {
 
         view.backgroundColor = .white
         view.addSubview(navigationBar)
-        view.addSubview(titleTextView)
         view.addSubview(titleTextField)
     }
 
@@ -80,13 +63,7 @@ class DetailViewController: UIViewController {
             navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
 
-            titleTextView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 8),
-            titleTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            titleTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-//            titleTextView.bottomAnchor.constraint(equalTo: titleTextField.topAnchor),
-            titleTextView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1),
-
-            titleTextField.topAnchor.constraint(equalTo: titleTextView.bottomAnchor, constant: 40),
+            titleTextField.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 4),
             titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
             titleTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
             titleTextField.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1)
