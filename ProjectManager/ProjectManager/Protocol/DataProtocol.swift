2319f16ca3643723ca659f4691951514e20164cc
Kyo <hyosing92@gmail.com>
2023-01-14 22:30:33 +0900

    feat : Data 관련 DataUpdatable, DataFetchable 프로토콜 생성

---
+++ ProjectManager/ProjectManager/Protocol/DataProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  DataProtocol.swift
+//  ProjectManager
+//
+//  Created by Kyo on 2023/01/14.
+//
+
+import Foundation
+
+protocol DataUpdatable: AnyObject {
+    func updateData(
+        process: Process,
+        title: String,
+        content: String?,
+        date: Date?,
+        indexPath: IndexPath?
+    )
+}
+
+protocol DataFetchable: DataUpdatable {
+    func fetchSelectData(process: Process, indexPath: IndexPath, completion: (Todo) -> Void)
+}
