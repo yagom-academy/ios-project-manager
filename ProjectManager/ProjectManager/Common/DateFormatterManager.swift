527b5766d7b2048c3371d31140145ab5f53da94c
summercat <summerly812@gmail.com>
2023-01-14 23:22:36 +0900

    feat: create DateFormatterManager type and date formatting method

---
+++ ProjectManager/ProjectManager/Common/DateFormatterManager.swift
@@ -0,0 +1,15 @@
+//
+//  DateFormatterManager.swift
+//  ProjectManager
+//
+//  Created by summercat on 2023/01/14.
+//
+
+import Foundation
+
+struct DateFormatterManager {
+    func formatDate(_ date: Date?) -> String {
+        guard let date = date else { return "" }
+        return DateFormatter.localizedString(from: date, dateStyle: .long, timeStyle: .none)
+    }
+}
