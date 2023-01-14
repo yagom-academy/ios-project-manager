bbcb2b2bfa4b73114b2fa4faa8e05af8ae329476
Mangdi <lkw4670@naver.com>
2023-01-15 03:31:28 +0900

    feat: datePicker, bodyTextView 구현

--- ProjectManager/ProjectManager/Controller/DetailViewController.swift
+++ ProjectManager/ProjectManager/Controller/DetailViewController.swift
@@ -28,13 +28,37 @@ class DetailViewController: UIViewController {
         textField.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.7).cgColor
         textField.layer.shadowOpacity = 0.3
         textField.layer.shadowOffset = CGSize(width: 0, height: 3)
-        textField.placeholder = "Title"
         textField.backgroundColor = .white
-        textField.textColor = UIColor.systemGray3
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "Title",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray3]
+        )
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: textField.frame.height))
         textField.leftView = paddingView
         textField.leftViewMode = .always
         return textField
+    }()
+
+    private let datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        picker.datePickerMode = .date
+        picker.preferredDatePickerStyle = .wheels
+        return picker
+    }()
+
+    private let bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.preferredFont(forTextStyle: .title3)
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.7).cgColor
+        textView.clipsToBounds = false
+        textView.layer.shadowOpacity = 0.3
+        textView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        textView.textColor = UIColor.black
+        textView.textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        return textView
     }()
 
     override func viewDidLoad() {
@@ -43,6 +67,8 @@ class DetailViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(navigationBar)
         view.addSubview(titleTextField)
+        view.addSubview(datePicker)
+        view.addSubview(bodyTextView)
     }
 
     override func viewDidLayoutSubviews() {
@@ -62,11 +88,23 @@ class DetailViewController: UIViewController {
             navigationBar.topAnchor.constraint(equalTo: view.topAnchor),
             navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navigationBar.bottomAnchor.constraint(equalTo: titleTextField.topAnchor),
 
             titleTextField.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 4),
             titleTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
             titleTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-            titleTextField.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1)
+            titleTextField.bottomAnchor.constraint(equalTo: datePicker.topAnchor),
+            titleTextField.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1),
+
+            datePicker.topAnchor.constraint(equalTo: titleTextField.bottomAnchor),
+            datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            datePicker.bottomAnchor.constraint(equalTo: bodyTextView.topAnchor),
+
+            bodyTextView.topAnchor.constraint(equalTo: datePicker.bottomAnchor),
+            bodyTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            bodyTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            bodyTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16)
         ])
     }
 
