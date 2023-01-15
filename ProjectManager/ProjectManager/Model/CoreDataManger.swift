a8163737bb1a99bd74270fa8028892c128b257d8
Dylan-Yoon <d.dylany21@gmail.com>
2023-01-15 19:01:28 +0900

    feat: Coredata (fetch, save) 구현

---
+++ ProjectManager/ProjectManager/Model/CoreDataManger.swift
@@ -0,0 +1,57 @@
+//
+//  CoreDataManger.swift
+//  ProjectManager
+//
+//  Created by Baem on 2023/01/15.
+//
+
+import CoreData
+import UIKit
+
+final class CoreDataManager {
+    func fetch() ->  Result<[TodoModel], CoreDataError> {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let context = appDelegate?.persistentContainer.viewContext
+        
+        do {
+            if let contact = try context?.fetch(TodoModel.fetchRequest()) {
+                return .success(contact)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return .failure(.fetchError)
+    }
+    
+    func saveData(title: String, body: String, todoDate: Date) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let context = appDelegate?.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "TodoModel", in: context!)
+        
+        if let entity = entity {
+            let info = NSManagedObject(entity: entity, insertInto: context)
+            info.setValue(title, forKey: "title")
+            info.setValue(body, forKey: "body")
+            info.setValue(todoDate, forKey: "todoDate")
+            info.setValue(UUID(), forKey: "id")
+            
+            do {
+                try context?.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    // TODO: -Update
+    func updateData() {
+        
+    }
+    
+    // TODO: -Delete
+    func deleteDate() {
+        
+    }
+}
---
+++ ProjectManager/ProjectManager/Model/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  ProjectManager
+//
+//  Created by Baem on 2023/01/15.
+//
+
+enum CoreDataError: Error {
+    case fetchError
+    case saveError
+    case updateError
+    case deleteError
+}
