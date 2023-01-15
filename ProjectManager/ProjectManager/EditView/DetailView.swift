daca003367d08cc24e4468715525330d3d56005a
미니 <cow970814@naver.com>
2023-01-16 03:58:15 +0900

    :recycle: 디테일 뷰 바인딩 방법 변경

--- ProjectManager/ProjectManager/EditView/DetailView.swift
+++ ProjectManager/ProjectManager/EditView/DetailView.swift
@@ -12,19 +12,27 @@ struct ProjectDetailView: View {
   let store: StoreOf<DetailViewReducer>
   
   var body: some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
+    WithViewStore(store) { viewStore in
       NavigationView {
         VStack(spacing: 20) {
-          TextField("Title", text: viewStore.binding(\.$titleValue))
-            .textFieldStyle(.plain)
-            .padding(20)
-            .background(.white)
-            .cornerRadius(15)
-            .shadow(color: .secondary, radius: 5, y: 3)
-            .disabled(!viewStore.canEdit)
+          TextField(
+            "Title",
+            text: Binding(
+              get: { viewStore.titleValue },
+              set: { viewStore.send(.didChangeTitle($0)) })
+          )
+          .textFieldStyle(.plain)
+          .padding(20)
+          .background(.white)
+          .cornerRadius(15)
+          .shadow(color: .secondary, radius: 5, y: 3)
+          .disabled(!viewStore.canEdit)
           
           DatePicker(
-            selection: viewStore.binding(\.$selectedDate),
+            selection: Binding(
+              get: { viewStore.selectedDate },
+              set: { viewStore.send(.didChangeSelectedDate($0)) }
+            ),
             in: Date()...,
             displayedComponents: .date
           ) {
@@ -37,12 +45,17 @@ struct ProjectDetailView: View {
           .shadow(color: .secondary, radius: 5, y: 3)
           .disabled(!viewStore.canEdit)
           
-          TextEditor(text: viewStore.binding(\.$description))
-            .padding(20)
-            .background(.white)
-            .cornerRadius(15)
-            .shadow(color: .secondary, radius: 5, y: 3)
-            .disabled(!viewStore.canEdit)
+          TextEditor(
+            text: Binding(
+              get: { viewStore.description },
+              set: { viewStore.send(.didChangeDescription($0)) }
+            )
+          )
+          .padding(20)
+          .background(.white)
+          .cornerRadius(15)
+          .shadow(color: .secondary, radius: 5, y: 3)
+          .disabled(!viewStore.canEdit)
         }
         .padding(10)
         .navigationTitle("TODO")
@@ -68,17 +81,5 @@ struct ProjectDetailView: View {
       }
       .navigationViewStyle(.stack)
     }
-  }
-}
-
-struct ProjectEditView_Previews: PreviewProvider {
-  static let store = Store(
-    initialState: DetailViewReducer.State(canEdit: true),
-    reducer: DetailViewReducer()
-  )
-  static var previews: some View {
-    
-    ProjectDetailView(store: store)
-      .previewInterfaceOrientation(.landscapeLeft)
   }
 }
--- ProjectManager/ProjectManager/EditView/DetailViewReducer.swift
+++ ProjectManager/ProjectManager/EditView/DetailViewReducer.swift
@@ -10,28 +10,36 @@ import ComposableArchitecture
 struct DetailViewReducer: ReducerProtocol {
   struct State: Equatable {
     var canEdit: Bool
-    @BindableState var titleValue = ""
-    @BindableState var selectedDate = Date()
-    @BindableState var description = ""
+    var titleValue = ""
+    var selectedDate = Date()
+    var description = ""
   }
   
-  enum Action: BindableAction, Equatable {
-    case binding(BindingAction<State>)
+  enum Action: Equatable {
     case didTapEdit
+    case didChangeTitle(String)
+    case didChangeSelectedDate(Date)
+    case didChangeDescription(String)
   }
   
-  var body: some ReducerProtocol<State, Action> {
-    BindingReducer()
-    
-    Reduce { state, action in
-      switch action {
-      case .binding:
-        return .none
-        
-      case .didTapEdit:
-        state.canEdit.toggle()
-        return .none
-      }
+  func reduce(into state: inout State, action: Action) -> ComposableArchitecture.EffectTask<Action> {
+    switch action {
+    case .didTapEdit:
+      state.canEdit.toggle()
+      return .none
+      
+    case let .didChangeTitle(title):
+      state.titleValue = title
+      return .none
+      
+    case let .didChangeSelectedDate(date):
+      state.selectedDate = date
+      return .none
+      
+    case let .didChangeDescription(body):
+      state.description = body
+      return .none
     }
   }
+  
 }
