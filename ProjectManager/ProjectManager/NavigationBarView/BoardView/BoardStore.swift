4cfc4a632d9685f12ab08ff1e0f296234db5034e
미니 <cow970814@naver.com>
2023-01-17 01:35:02 +0900

    :recycle: 보드 뷰 구성 변경 및 스토어 구성 변경

--- ProjectManager/ProjectManager/NavigationBarView/BoardView/BoardStore.swift
+++ ProjectManager/ProjectManager/NavigationBarView/BoardView/BoardStore.swift
@@ -8,16 +8,41 @@ import ComposableArchitecture
 
 struct BoardStore: ReducerProtocol {
   struct State: Equatable {
-    
+    var todoState = BoardListStore.State(status: .todo, projects: [])
+    var doingState = BoardListStore.State(status: .doing, projects: [])
+    var doneState = BoardListStore.State(status: .done, projects: [])
   }
   
   enum Action: Equatable {
-    
+    case todo(BoardListStore.Action)
+    case doing(BoardListStore.Action)
+    case done(BoardListStore.Action)
   }
   
-  func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-    switch action {
-      
+  var body: some ReducerProtocol<State, Action> {
+    Reduce { state, action in
+      switch action {
+      case .todo:
+        return .none
+        
+      case .doing:
+        return .none
+        
+      case .done:
+        return .none
+      }
+    }
+    
+    Scope(state: \.todoState, action: /Action.todo) {
+      BoardListStore()
+    }
+    
+    Scope(state: \.doingState, action: /Action.doing) {
+      BoardListStore()
+    }
+    
+    Scope(state: \.doneState, action: /Action.done) {
+      BoardListStore()
     }
   }
 }
--- ProjectManager/ProjectManager/NavigationBarView/BoardView/BoardView.swift
+++ ProjectManager/ProjectManager/NavigationBarView/BoardView/BoardView.swift
@@ -8,8 +8,43 @@ import ComposableArchitecture
 import SwiftUI
 
 struct BoardView: View {
+  let store: StoreOf<BoardStore>
   
   var body: some View {
-    Text("Example")
+    HStack(spacing: 20) {
+      BoardListView(
+        boardListStore: store.scope(
+          state: \.todoState,
+          action: BoardStore.Action.todo
+        )
+      )
+      .padding(.leading, 20)
+      
+      BoardListView(
+        boardListStore: store.scope(
+          state: \.doingState,
+          action: BoardStore.Action.doing
+        )
+      )
+      
+      BoardListView(
+        boardListStore: store.scope(
+          state: \.doneState,
+          action: BoardStore.Action.done
+        )
+      )
+      .padding(.trailing, 20)
+    }
+  }
+}
+
+struct BoardView_Previews: PreviewProvider {
+  static let store = Store(
+    initialState: BoardStore.State(),
+    reducer: BoardStore()
+  )
+  static var previews: some View {
+    BoardView(store: store)
+      .previewInterfaceOrientation(.landscapeLeft)
   }
 }
