087b8fe8a7127dfcec1db878c8d536c2bf3c989c
summercat <summerly812@gmail.com>
2023-01-17 02:10:06 +0900

    feat: configure stackView layout

--- ProjectManager/ProjectManager/Controller/IssueViewController.swift
+++ ProjectManager/ProjectManager/Controller/IssueViewController.swift
@@ -14,6 +14,13 @@ final class IssueViewController: UIViewController {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.spacing = LayoutConstant.stackViewSpacing
+        stack.isLayoutMarginsRelativeArrangement = true
+        stack.directionalLayoutMargins = NSDirectionalEdgeInsets(top: LayoutConstant.margin,
+                                                                 leading: LayoutConstant.margin,
+                                                                 bottom: LayoutConstant.margin,
+                                                                 trailing: LayoutConstant.margin)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+
         
         return stack
     }()
@@ -53,6 +60,13 @@ final class IssueViewController: UIViewController {
     }
     
     private func configureStackView() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+
         stackView.addArrangedSubview(titleTextField)
         stackView.addArrangedSubview(datePicker)
         stackView.addArrangedSubview(bodyTextField)
@@ -60,5 +74,6 @@ final class IssueViewController: UIViewController {
     
     enum LayoutConstant {
         static let stackViewSpacing = CGFloat(8)
+        static let margin = CGFloat(16)
     }
 }
