90728d53a8c17597204a34d20340afb357c7d7fc
woong <workplayhard1@naver.com>
2023-01-16 17:53:22 +0900

    feat: Add액션구현 (2) - AddViewController UI
    - UI구성 및 IBOutlet 설정

--- ProjectManager/ProjectManager/Controller/AddViewController.swift
+++ ProjectManager/ProjectManager/Controller/AddViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class AddViewController: UIViewController {
-
+    @IBOutlet weak var titleTextFiled: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- 
     }
 }
+
