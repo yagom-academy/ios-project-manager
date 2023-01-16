eb16eb09b139a29b892b848238e53bc802ef5a57
미니 <cow970814@naver.com>
2023-01-16 23:25:06 +0900

    :recycle: 보드 리스트 리듀서 구성 및 뷰 구성

--- ProjectManager/ProjectManager/BoardView/BoardListView/BoardListStore.swift
+++ ProjectManager/ProjectManager/BoardView/BoardListView/BoardListStore.swift
@@ -3,6 +3,36 @@
 //  ProjectManager
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
+import ComposableArchitecture
 import Foundation
+
+struct BoardListStore: ReducerProtocol {
+  struct State: Equatable {
+    var projects: IdentifiedArrayOf<BoardListCellStore.State> = []
+    
+    init(projects: IdentifiedArrayOf<BoardListCellStore.State>) {
+      self.projects = projects
+    }
+  }
+  
+  enum Action: Equatable {
+    //MARK: 삭제 예정
+    case onAppear(BoardListCellStore.Action)
+    case projectItem(id: BoardListCellStore.State.ID, action: BoardListCellStore.Action)
+  }
+  
+  var body: some ReducerProtocol<State, Action> {
+    Reduce { state, action in
+      switch action {
+      case .onAppear:
+        return .none
+        
+      default:
+        return .none
+      }
+    }.forEach(\.projects, action: /Action.projectItem(id:action:)) {
+      BoardListCellStore()
+    }
+  }
+}
--- ProjectManager/ProjectManager/BoardView/BoardListView/BoardListView.swift
+++ ProjectManager/ProjectManager/BoardView/BoardListView/BoardListView.swift
@@ -3,6 +3,37 @@
 //  ProjectManager
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import SwiftUI
+import ComposableArchitecture
+
+struct BoardListView: View {
+  let boardListStore: StoreOf<BoardListStore>
+  
+  var body: some View {
+    WithViewStore(boardListStore, observe: { $0 }) { viewStore in
+      List {
+        ForEachStore(
+          boardListStore.scope(
+            state: \.projects,
+            action: BoardListStore.Action.projectItem(id:action:)
+          )) { viewStore in
+            BoardListCell(store: viewStore)
+          }
+          .listRowSeparator(.hidden)
+      }
+      .listStyle(.plain)
+    }
+  }
+}
+
+struct BoardListView_Previews: PreviewProvider {
+  static let boardListStore = Store(
+    initialState: BoardListStore.State(projects: Project.mock),
+    reducer: BoardListStore()
+  )
+  static var previews: some View {
+    BoardListView(boardListStore: boardListStore)
+      .previewInterfaceOrientation(.landscapeLeft)
+  }
+}
