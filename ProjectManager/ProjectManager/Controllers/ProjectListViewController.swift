6aae0a90f15003132fda76553e5b01ca829959f4
yuvinrho <yvrho@naver.com>
2023-01-17 19:04:00 +0900

    feat: ProjectListViewController 데이터소스 설정

--- ProjectManager/ProjectManager/Controllers/ProjectListViewController.swift
+++ ProjectManager/ProjectManager/Controllers/ProjectListViewController.swift
@@ -7,8 +7,11 @@
 import UIKit
 
 final class ProjectListViewController: UIViewController {
+    typealias DataSource = UITableViewDiffableDataSource<Int, Project>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Project>
+
     // MARK: Properties
-    private var projectList: [Project] = []
+    private var projectList: [Project] = DummyProjects.projects
     private var todoList: [Project] {
         return projectList.filter{ $0.status == .todo }
     }
@@ -28,12 +31,18 @@ final class ProjectListViewController: UIViewController {
         return view
     }()
 
+    private var todoDataSource: DataSource?
+    private var doingDataSource: DataSource?
+    private var doneDataSource: DataSource?
+
     // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationBar()
         configureView()
+        configureDataSource()
+        configureSnapshots()
     }
 
     // MARK: Configure NavigationBar
@@ -52,9 +61,50 @@ final class ProjectListViewController: UIViewController {
         navigationItem.rightBarButtonItem = makeRightNavigationBarButton()
     }
 
+    // MARK: Configure View
     private func configureView() {
         view.addSubview(projectListView)
-
     }
 
+    // MARK: Configure DataSource
+    private func makeDataSource(tableView: UITableView) -> DataSource {
+        let dataSource = DataSource(tableView: tableView) { tableView, indexPath, project in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ProjectTableViewCell.reuseIdentifier) as?
+                    ProjectTableViewCell else {
+                return UITableViewCell()
+            }
+
+            cell.configure(with: project)
+
+            return cell
+        }
+
+        return dataSource
+    }
+
+    private func configureDataSource() {
+        todoDataSource = makeDataSource(tableView: projectListView.todoTableView)
+        doingDataSource = makeDataSource(tableView: projectListView.doingTableView)
+        doneDataSource = makeDataSource(tableView: projectListView.doneTableView)
+    }
+
+    private func configureSnapshot(dataSource: DataSource, items: [Project]) {
+        var snapshot = Snapshot()
+
+        snapshot.appendSections([0])
+        snapshot.appendItems(items)
+        dataSource.apply(snapshot)
+    }
+
+    private func configureSnapshots() {
+        guard let todoDataSource,
+              let doingDataSource,
+              let doneDataSource else {
+            fatalError("데이터소스 없음")
+        }
+
+        configureSnapshot(dataSource: todoDataSource, items: todoList)
+        configureSnapshot(dataSource: doingDataSource, items: doingList)
+        configureSnapshot(dataSource: doneDataSource, items: doneList)
+    }
 }
