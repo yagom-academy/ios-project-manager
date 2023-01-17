8380698c8637cfff149453a7b61b627c95e3ef2f
lnhk98 <hozzang666@gmail.com>
2023-01-17 18:59:05 +0900

    feat: MVVM적용 - 셀의 뷰모델인 ListItemCellViewModel 클래스 구현

---
+++ ProjectManager/ProjectManager/Source/ViewModel/ListItemCellViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  ListItemCellViewModel.swift
+//  ProjectManager
+//  Created by inho on 2023/01/17.
+//
+
+import Foundation
+
+final class ListItemCellViewModel {
+    struct ListItemData {
+        var title: String
+        var body: String
+        var dueDate: String
+        var isOverDue: Bool = false
+    }
+    
+    private let listItem: ListItem
+    private var handler: ((ListItemData) -> Void)?
+    private var listItemData: ListItemData {
+        didSet {
+            self.handler?(listItemData)
+        }
+    }
+    
+    init(listItem: ListItem) {
+        self.listItem = listItem
+        self.listItemData = ListItemData(
+            title: listItem.title,
+            body: listItem.body,
+            dueDate: listItem.dueDate.convertedToString
+        )
+    }
+    
+    func bind(_ handler: @escaping (ListItemData) -> Void) {
+        self.handler = handler
+    }
+    
+    func updateItem(using updatedListItem: ListItem) {
+        let compareResult = updatedListItem.dueDate.compare(listItem.dueDate)
+        let updatedData = ListItemData(
+            title: updatedListItem.title,
+            body: updatedListItem.body,
+            dueDate: updatedListItem.dueDate.convertedToString,
+            isOverDue: compareResult == .orderedDescending ? true : false
+        )
+        
+        listItemData = updatedData
+    }
+}
