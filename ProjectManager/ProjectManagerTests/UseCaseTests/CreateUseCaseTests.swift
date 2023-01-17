acf8fdf3dac209bfefdfdf17a23347d1422a3aed
jpush <lj845854@gmail.com>
2023-01-17 16:38:09 +0900

    feat: CreateUseCaseTests 추가
    - 생성성공 단위테스트
    - 생성실패 단위테스트

--- ProjectManager/ProjectManagerTests/UseCaseTests/CreateUseCaseTests.swift
+++ ProjectManager/ProjectManagerTests/UseCaseTests/CreateUseCaseTests.swift
@@ -2,35 +2,64 @@
 //  CreateUseCaseTests.swift
 //  ProjectManager
 //
-//  Created by 이정민 on 2023/01/17.
+//  Created by ayaan, jpush on 2023/01/17.
 //
 
 import XCTest
 
-final class CreateUseCaseTests: XCTestCase {
-    var taskRepositoryMock = MockTaskRepository(taskEntities: TaskEntityDummy.dummys)
-    var usecase: CreateUseCase? = nil
-    
-    override func setUpWithError() throws {
-        self.taskRepositoryMock = MockTaskRepository(taskEntities: TaskEntityDummy.dummys)
-        self.usecase = CreateUseCase(
-            delegate: EndEditTask(),
-            repository: taskRepositoryMock
-        )
-    }
-    
-    func test_create_success() {
-        guard let usecase = usecase else {
-            return XCTFail()
-        }
-        
-        usecase.createResult
-            .subscribe(ObserverType)
-    }
-}
+import RxTest
+import RxSwift
 
-struct EndEditTask: DidEndEditTaskDelegate {
+private final class EndEditTask: DidEndEditTaskDelegate {
     func didEndEdit(task: Task) {
         return
     }
 }
+
+final class CreateUseCaseTests: XCTestCase {
+    var taskRepositoryMock: MockTaskRepository!
+    var usecase: CreateUseCase!
+    var dispose: DisposeBag!
+    
+    override func setUpWithError() throws {
+        taskRepositoryMock = MockTaskRepository(taskEntities: TaskEntityDummy.dummys)
+        usecase = CreateUseCase(
+            delegate: EndEditTask(),
+            repository: taskRepositoryMock
+        )
+        dispose = DisposeBag()
+    }
+    
+    func test_create_success() {
+        usecase.isCreatedSuccess
+            .subscribe(onNext: { isSuccess in
+                XCTAssertTrue(isSuccess)
+            }).disposed(by: dispose)
+        
+        usecase.addTask(Task(
+            id: UUID().uuidString,
+            title: "제목제목제목",
+            content: "내용내용내용",
+            deadLine: "Jan 20, 2023",
+            state: .toDo,
+            isExpired: true
+        ))
+    }
+    
+    func test_create_failure() {
+        usecase.isCreatedSuccess
+            .subscribe(onNext: { isSuccess in
+                XCTAssertFalse(isSuccess)
+            }).disposed(by: dispose)
+        
+        usecase.addTask(Task(
+            id: UUID().uuidString,
+            title: "제목제목제목",
+            content: "내용내용내용",
+            deadLine: "Jan 54, 2023",
+            state: .toDo,
+            isExpired: true
+        ))
+    }
+}
+
