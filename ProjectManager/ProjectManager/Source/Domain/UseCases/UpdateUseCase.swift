6bdfd30dd441c41b4bec54467bfc99401d61a518
Ayaan <onestar7984@gmail.com>
2023-01-17 21:17:11 +0900

    feat: UpdateUseCase Type 구현

---
+++ ProjectManager/ProjectManager/Source/Domain/UseCases/UpdateUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  UpdateUseCase.swift
+//  ProjectManager
+//
+//  Created by ayaan, jpush on 2023/01/17.
+//
+
+import RxSwift
+
+final class UpdateUseCase {
+    private weak var delegate: DidEndUpdatingDelegate?
+    private let repository: TaskRepository
+
+    private let translater = Translater()
+    private let disposeBag = DisposeBag()
+    let isUpdatedSuccess = PublishSubject<Bool>()
+
+    init(delegate: DidEndUpdatingDelegate, repository: TaskRepository) {
+        self.delegate = delegate
+        self.repository = repository
+    }
+
+    func deleteTask(_ task: Task) {
+        guard let entity = translater.toEntity(with: task) else {
+            return isUpdatedSuccess.onNext(false)
+        }
+
+        repository.update(entity)
+            .subscribe(onNext: { [weak self] isSuccess in
+                if isSuccess {
+                    self?.delegate?.didEndUpdating(task: task)
+                }
+
+                self?.isUpdatedSuccess.onNext(isSuccess)
+            })
+            .disposed(by: disposeBag)
+    }
+}
