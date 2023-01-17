fe8b5f655f65a681f3bff067f29c8c13c85a8238
lws2269 <lws2269@kakao.com>
2023-01-17 17:19:56 +0900

    feat: Category 상태 별로 변경가능한 상태에 대한 description 및 상태를 리턴하는 계산속성 추가

--- ProjectManager/ProjectManager/Model/Category.swift
+++ ProjectManager/ProjectManager/Model/Category.swift
@@ -22,4 +22,26 @@ enum Category {
             return "DONE"
         }
     }
+    
+    var other: (Category, Category) {
+        switch self {
+        case .todo:
+            return (.doing, .done)
+        case .doing:
+            return (.todo, .done)
+        case .done:
+            return (.todo, .doing)
+        }
+    }
+    
+    var otherDescription: (String, String) {
+        switch self {
+        case .todo:
+            return ("Move To Doing", "Move To Done")
+        case .doing:
+            return ("Move To Todo", "Move To Done")
+        case .done:
+            return ("Move To Todo", "Move To Doing")
+        }
+    }
 }
