e7c8341b9d403f0a064abcc96f1a2cdea2a850a8
DragonYG <kioswe92@gmail.com>
2023-01-17 17:12:25 +0900

    feat: MainCenterTableViewCell & MainRightTableView 파일 추가

---
+++ ProjectManager/ProjectManager/Source/Model/View/MainCenterTableViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  MainCenterTableViewCell.swift
+//  ProjectManager
+//
+//  Created by Dragon on 2023/01/17.
+//
+
+import UIKit
+
+class MainCenterTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    
+    static let identifier = "MainCenterTableViewCell"
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+    let bodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .gray
+        return label
+    }()
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 10)
+        return label
+    }()
+    
+    // MARK: Initializer
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: Internal Methods
+    func configureLabel(title: String, body: String, date: String) {
+        titleLabel.text = title
+        bodyLabel.text = body
+        dateLabel.text = date
+    }
+
+    // MARK: Private Methods
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            topAnchor.constraint(equalTo: contentView.topAnchor),
+            bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
