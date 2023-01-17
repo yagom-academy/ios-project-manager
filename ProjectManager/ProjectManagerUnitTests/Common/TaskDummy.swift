e0c9a3c1e7aef464d1e9cb059057398d204a23c9
jpush <lj845854@gmail.com>
2023-01-17 21:34:25 +0900

    refactor: Mock 용 데이터의 ID를 UUID에서 String으로 변경

--- ProjectManager/ProjectManagerUnitTests/Common/TaskDummy.swift
+++ ProjectManager/ProjectManagerUnitTests/Common/TaskDummy.swift
@@ -10,7 +10,16 @@ import Foundation
 struct TaskDummy {
     static var dummys: [Task] = [
         Task(
-            id: UUID().uuidString,
+            id: "1",
+            title: "RxSwift 추가",
+            content: "제곧내",
+            deadLine: "Jan 20, 2023", // 2023년
+            state: .toDo,
+            isExpired: true
+        ),
+        
+        Task(
+            id: "2",
             title: "제목제목제목",
             content: "내용내용내용",
             deadLine: "Jan 20, 2023",
@@ -19,7 +28,7 @@ struct TaskDummy {
         ),
         
         Task(
-            id: UUID().uuidString,
+            id: "3",
             title: "제목제목제목",
             content: "내용내용내용",
             deadLine: "Jan 10, 2023",
@@ -28,7 +37,7 @@ struct TaskDummy {
         ),
         
         Task(
-            id: UUID().uuidString,
+            id: "4",
             title: "제목제목제목",
             content: "내용내용내용",
             deadLine: "Sep 9, 2023",
@@ -37,7 +46,7 @@ struct TaskDummy {
         ),
         
         Task(
-            id: UUID().uuidString,
+            id: "5",
             title: "Mock Repository 만들기",
             content: "아얀이 열심히 만듦",
             deadLine: "Jan 20, 2023",
@@ -46,7 +55,7 @@ struct TaskDummy {
         ),
         
         Task(
-            id: UUID().uuidString,
+            id: "6",
             title: "step2 PR",
             content: "coordinator까지 만들어서 보내기",
             deadLine: "Jan 18, 2023",
@@ -55,7 +64,7 @@ struct TaskDummy {
         ),
         
         Task(
-            id: UUID().uuidString,
+            id: "7",
             title: "RxSwift 추가",
             content: "제곧내",
             deadLine: "Jan 10, 2023",
