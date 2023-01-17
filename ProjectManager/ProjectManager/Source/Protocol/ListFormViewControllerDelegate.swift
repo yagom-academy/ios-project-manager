1efe2b4a7487e8f5cb8a6864891cd07392d93b26
lnhk98 <hozzang666@gmail.com>
2023-01-17 22:45:29 +0900

    feat: 리스트 작성 및 편집 완료를 알리기 위한 ListFormViewControllerDelegate 프로토콜과 메서드 생성

---
+++ ProjectManager/ProjectManager/Source/Protocol/ListFormViewControllerDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  ListFormViewControllerDelegate.swift
+//  ProjectManager
+//  Created by inho on 2023/01/17.
+//
+
+import Foundation
+
+protocol ListFormViewControllerDelegate {
+    func addNewItem(_ listItem: ListItem)
+}
