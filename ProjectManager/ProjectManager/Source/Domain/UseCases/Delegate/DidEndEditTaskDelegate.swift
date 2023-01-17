945d86e9c47d90ef5c1ddc0888517d2af4f3a8f0
jpush <lj845854@gmail.com>
2023-01-17 13:11:51 +0900

    feat: DidEndEditTaskDelegate 추가
    - CUD 작업이 완료되고 fetch UseCase내의 배열 업데이트 용 프로토콜

---
+++ ProjectManager/ProjectManager/Source/Domain/UseCases/Delegate/DidEndEditTaskDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  DidEndEditTaskDelegate.swift
+//  ProjectManager
+//
+//  Created by ayaan, jpush on 2023/01/17.
+//
+
+import Foundation
+
+protocol DidEndEditTaskDelegate {
+    func didEndEdit(task: Task)
+}
