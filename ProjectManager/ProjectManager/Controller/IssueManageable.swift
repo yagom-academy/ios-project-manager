310423709a9a69d80eae30b85d15c4e4cc820e6b
summercat <summerly812@gmail.com>
2023-01-17 18:31:08 +0900

    feat: define IssueManageable protocol

---
+++ ProjectManager/ProjectManager/Controller/IssueManageable.swift
@@ -0,0 +1,11 @@
+//
+//  IssueManageable.swift
+//  ProjectManager
+//
+//  Created by summecat on 2023/01/17.
+//
+
+protocol IssueManageable {
+    func addIssue(issue: Issue?)
+    func updateIssue(issue: Issue?)
+}
