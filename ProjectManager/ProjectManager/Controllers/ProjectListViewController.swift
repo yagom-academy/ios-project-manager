0388c811518278bdaaba1b47f4d06398f2df914e
yuvinrho <yvrho@naver.com>
2023-01-17 17:16:23 +0900

    feat: ProjectListViewController 프로퍼티 생성

--- ProjectManager/ProjectManager/Controllers/ProjectListViewController.swift
+++ ProjectManager/ProjectManager/Controllers/ProjectListViewController.swift
@@ -6,7 +6,28 @@
 
 import UIKit
 
-class ProjectListViewController: UIViewController {
+final class ProjectListViewController: UIViewController {
+    // MARK: Properties
+    private var projectList: [Project] = []
+    private var todoList: [Project] {
+        return projectList.filter{ $0.status == .todo }
+    }
+
+    private var doingList: [Project] {
+        return projectList.filter{ $0.status == .doing }
+    }
+
+    private var doneList: [Project] {
+        return projectList.filter{ $0.status == .done }
+    }
+
+    private let projectManager = ProjectManager()
+    private lazy var projectListView: ProjectListView = {
+        let view = ProjectListView(frame: view.bounds)
+        view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
