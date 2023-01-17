9f35f3b7981741e96df788631595ee7913f74864
zhilly11 <zhzh1x4@gmail.com>
2023-01-17 13:39:35 +0900

    feat: MainViewController에 tableView 추가

--- ProjectManager/ProjectManager/Source/ViewController/MainViewController.swift
+++ ProjectManager/ProjectManager/Source/ViewController/MainViewController.swift
@@ -8,6 +8,35 @@ class MainViewController: UIViewController {
         static let title = "Project Manager"
     }
     
+    private let todoTableView: ToDoListView = {
+        let tableView = ToDoListView(status: .toDo)
+        
+        return tableView
+    }()
+    
+    private let doingTableView: ToDoListView = {
+        let tableView = ToDoListView(status: .doing)
+
+        return tableView
+    }()
+    
+    private let doneTableView: ToDoListView = {
+        let tableView = ToDoListView(status: .done)
+ 
+        return tableView
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
@@ -18,6 +47,7 @@ class MainViewController: UIViewController {
         title = Constant.title
         
         setupBarButtonItem()
+        setupView()
     }
     
     private func setupBarButtonItem() {
@@ -28,4 +58,21 @@ class MainViewController: UIViewController {
         
         navigationItem.setRightBarButton(rightBarButton, animated: true)
     }
+    
+    private func setupView() {
+        stackView.addArrangedSubview(todoTableView)
+        stackView.addArrangedSubview(doingTableView)
+        stackView.addArrangedSubview(doneTableView)
+        view.addSubview(stackView)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+}
 }
