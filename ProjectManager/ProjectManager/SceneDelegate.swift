1ea24502242c52243528539b9bf2d9d44d03b932
yjjem <marksim0606@gmail.com>
2023-01-18 11:20:11 +0900

    build(SceneDelegate): add manual window configuration

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -11,8 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let rootViewController = ViewController()
+        let mainViewController = UINavigationController(rootViewController: rootViewController)
         
-        guard let _ = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
     }
     
     func sceneDidDisconnect(_ scene: UIScene) { }
