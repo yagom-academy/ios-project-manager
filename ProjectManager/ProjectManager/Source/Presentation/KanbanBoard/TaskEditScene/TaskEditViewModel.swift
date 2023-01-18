8da48b540f7ac64f8557d0059346d4b25f859211
jpush <lj845854@gmail.com>
2023-01-18 19:17:03 +0900

    feat: TaskEditViewModel 생성
    - Input, Output 추가
    - transform 틀 추가

--- ProjectManager/ProjectManager/Source/Presentation/KanbanBoard/TaskEditScene/TaskEditViewModel.swift
+++ ProjectManager/ProjectManager/Source/Presentation/KanbanBoard/TaskEditScene/TaskEditViewModel.swift
@@ -5,4 +5,37 @@
 //  Created by ayaan, jpush on 2023/01/18.
 //
 
-import Foundation
+import RxRelay
+import RxSwift
+
+// task 받기
+final class TaskEditViewModel {
+    let updateTaskUseCase: UpdateTaskUseCase
+    
+    private var title: String = ""
+    private var content: String = ""
+    private var date: Double = 0
+    
+    init(updateTaskUseCase: UpdateTaskUseCase) {
+        self.updateTaskUseCase = updateTaskUseCase
+    }
+    
+    // MARK: - Output
+    struct Output {
+        let isSuccess = PublishRelay<Bool>()
+    }
+    
+    // MARK: - Input
+    struct Input {
+        let titleDidEditEvent: Observable<String>
+        let contentDidEditEvent: Observable<String>
+        let datePickerDidEditEvent: Observable<Double>
+        let doneButtonTapEvent: Observable<Void>
+    }
+    
+    func transform(from input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+
+        return output
+    }
+}
