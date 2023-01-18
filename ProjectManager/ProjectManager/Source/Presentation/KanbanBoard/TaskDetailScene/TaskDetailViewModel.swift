3c69b99e59f86134361635656d04b18e826f46e1
jpush <lj845854@gmail.com>
2023-01-18 22:02:45 +0900

    feat: TaskDetailViewModel 생성

--- ProjectManager/ProjectManager/Source/Presentation/KanbanBoard/TaskDetailScene/TaskDetailViewModel.swift
+++ ProjectManager/ProjectManager/Source/Presentation/KanbanBoard/TaskDetailScene/TaskDetailViewModel.swift
@@ -5,4 +5,34 @@
 //  Created by ayaan, jpush on 2023/01/18.
 //
 
-import Foundation
+import RxSwift
+
+final class TaskDetailViewModel {
+    // MARK: - Input
+    struct Input {
+        let editButtonTappedEvent: Observable<Void>
+        let doneButtonTappedEvent: Observable<Void>
+    }
+
+    // MARK: - Output
+    let task: Task
+    
+    init(task: Task) {
+        self.task = task
+    }
+    
+    func bind(with input: Input, disposeBag: DisposeBag) {
+        input.editButtonTappedEvent
+            .subscribe(onNext: {
+                // coordinator todo
+            })
+            .disposed(by: disposeBag)
+        
+        input.doneButtonTappedEvent
+            .subscribe(onNext: {
+                // coordinator todo
+            })
+            .disposed(by: disposeBag)
+    }
+    
+}
