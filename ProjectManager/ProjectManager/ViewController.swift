e70353258dc16eecf5cd3a8b9d746e1dcb84dcac
yjjem <marksim0606@gmail.com>
2023-01-18 11:45:03 +0900

    feat(ProjectManagerViewController): add view configuration

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -6,13 +6,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ProjectManagerViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureNavigationController()
+        configureView()
     }
-
-
+    
+    private func configureNavigationController() {
+        if let navigationController = self.navigationController {
+            let navigationBar = navigationController.navigationBar
+            navigationBar.backgroundColor = UIColor.systemGray
+            let rightAddButton = UIBarButtonItem(barButtonSystemItem: .add, target: self,
+                                                action: #selector(tapNavigationAddButton))
+            navigationController.title = "Project Manager"
+            navigationItem.rightBarButtonItem = rightAddButton
+        }
+    }
+    
+    private func configureView() {
+        self.view.backgroundColor = UIColor.systemGray3
+    }
+    
+    @objc
+    private func tapNavigationAddButton() {
+        // TODO: Add some action
+    }
 }
 
