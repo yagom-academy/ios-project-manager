f3338a8df1a516e9a62aa2f6b04d7e68a5871970
yjjem <marksim0606@gmail.com>
2023-01-18 13:12:08 +0900

    docs: separate files by clean architecture layers

---
+++ ProjectManager/ProjectManager/Domain/Entities/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  ProjectManager
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+struct Task {
+    let title: String
+    let description: String
+    let expireDate: Date
+}
---
+++ ProjectManager/ProjectManager/Presentation/ProjectManager/ViewModel/ProjectManagerViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ProjectManagerViewModel.swift
+//  ProjectManager
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+// TODO: Create ViewModel
