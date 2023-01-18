8d2a8283aaa4e420faf9bd4ea28ad284fbecf228
Kyo <hyosing92@gmail.com>
2023-01-18 22:12:10 +0900

    feat : Main, Detail, Cell ViewModel에 대한 UnitTest 파일 생성

---
+++ ProjectManager/ProjectManagerTests/DetailViewModelTest.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewModelTest.swift
+//  ProjectManagerTests
+//
+//  Created by Kyo on 2023/01/18.
+//
+
+import XCTest
+
+final class DetailViewModelTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
---
+++ ProjectManager/ProjectManagerTests/MainViewModelTest.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewModelTest.swift
+//  ProjectManagerTests
+//
+//  Created by Kyo on 2023/01/18.
+//
+
+import XCTest
+
+final class MainViewModelTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
