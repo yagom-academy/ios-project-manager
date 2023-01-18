7127c6ee3c968cb475bcee8a6d322bd75962d1a9
Kyo <hyosing92@gmail.com>
2023-01-19 01:42:20 +0900

    feat : DetailViewModel UnitTest 실행

--- ProjectManager/ProjectManagerTests/DetailViewModelTest.swift
+++ ProjectManager/ProjectManagerTests/DetailViewModelTest.swift
@@ -6,14 +6,36 @@
 //
 
 import XCTest
+@testable import ProjectManager
 
 final class DetailViewModelTest: XCTestCase {
+    var sut: DetailViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = DetailViewModel(data: nil)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
+    }
+
+    func test_data를_신규추가할때_Mode가_new인지_check() {
+        XCTAssertTrue(sut.isNewMode())
+    }
+
+    func test_data를_변경할때_Mode가_edit인지_check() {
+        sut = DetailViewModel(data: TestData.data1)
+        XCTAssertFalse(sut.isNewMode())
+    }
+
+    func test_data를_신규추가할때_Edit이_가능한지_check() {
+        XCTAssertTrue(sut.fetchEditable())
+    }
+
+    func test_data를_변경할때_Edit이_가능한지_check() {
+        sut = DetailViewModel(data: TestData.data1)
+        XCTAssertFalse(sut.fetchEditable())
     }
 }
