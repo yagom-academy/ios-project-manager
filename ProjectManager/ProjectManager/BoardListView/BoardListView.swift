2f58896657141aac7b324d1760834a6141de5f95
미니 <cow970814@naver.com>
2023-01-19 22:33:41 +0900

    :recycle: list View 구성

--- ProjectManager/ProjectManager/BoardListView/BoardListView.swift
+++ ProjectManager/ProjectManager/BoardListView/BoardListView.swift
@@ -13,10 +13,62 @@ struct BoardListView: View {
   var body: some View {
     WithViewStore(store) { viewStore in
       List {
-        ForEach(viewStore.projects, id: \.id) { project in
-          Text(project.title)
+        
+        Section(viewStore.projects.count.description) {
+          ForEach(viewStore.projects, id: \.id) { project in
+            BoardListCellView(project: project)
+              .listRowSeparator(.hidden)
+          }
         }
       }
+      .listSectionSeparator(.hidden)
+      .listStyle(.plain)
     }
+  }
+}
+
+struct BoardListCellView: View {
+  let project: Project
+  
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading, spacing: 10) {
+        Text(project.title)
+          .font(.title)
+          .bold()
+        
+        Text(project.description)
+          .font(.body)
+        
+        Text(project.date.convertedDateDescription)
+          .font(.footnote)
+      }
+      
+      Spacer()
+    }
+    .padding(10)
+    .background(Color.secondaryBackground)
+    .cornerRadius(10)
+    .padding()
+
+  }
+}
+
+struct BoardListView_Previews: PreviewProvider {
+  static let store = Store(
+    initialState: ProjectListState(),
+    reducer: projectListReducer,
+    environment: ProjectListEnvironment()
+  )
+  static var previews: some View {
+    BoardListView(store: store)
+    
+    BoardListCellView(
+      project: Project(
+        title: "Example",
+        date: 99999999,
+        description: "Example"
+      )
+    )
   }
 }
