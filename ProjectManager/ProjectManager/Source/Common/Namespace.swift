6c5c6c08957a84145aa2b5b5c0f2d99eb03d4ba3
jsa0224 <voidness45@gmail.com>
2023-01-19 18:38:49 +0900

    refactor: 네임스페이스 추가

---
+++ ProjectManager/ProjectManager/Source/Common/Namespace.swift
@@ -0,0 +1,14 @@
+//
+//  Namespace.swift
+//  ProjectManager
+//
+//  Created by som on 2023/01/19.
+//
+
+import Foundation
+
+enum PlanText {
+    static let title = "Title"
+    static let description = "Description"
+    static let emptyString = ""
+}
