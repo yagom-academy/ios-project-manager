35f6ff25f7ca98c46d1332dedabfb1a1c351e389
Ayaan <onestar7984@gmail.com>
2023-01-19 20:24:20 +0900

    feat: StateUpdateViewModel Test 추가

--- ProjectManager/ProjectManagerUnitTests/ViewModelTests/StateUpdateViewModelTests.swift
+++ ProjectManager/ProjectManagerUnitTests/ViewModelTests/StateUpdateViewModelTests.swift
@@ -2,34 +2,108 @@
 //  StateUpdateViewModelTests.swift
 //  ProjectManagerUnitTests
 //
-//  Created by 이정민 on 2023/01/18.
+//  Created by ayaan, jpush on 2023/01/18.
 //
 
 import XCTest
 
+import RxSwift
+import RxTest
+
 final class StateUpdateViewModelTests: XCTestCase {
-
+    private var viewModel: StateUpdateViewModel!
+    private var disposeBag: DisposeBag!
+    private var scheduler: TestScheduler!
+    private var input: StateUpdateViewModel.Input!
+    private var output: StateUpdateViewModel.Output!
+    private var updateTaskUseCase: UpdateTaskUseCaseMock!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        updateTaskUseCase = UpdateTaskUseCaseMock()
+        viewModel = StateUpdateViewModel(updateTaskUseCase: updateTaskUseCase,
+                                         task: TaskDummy.dummys[0])
+        disposeBag = DisposeBag()
+        scheduler = TestScheduler(initialClock: 0)
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
+        updateTaskUseCase = nil
+        disposeBag = nil
+        scheduler = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_tap_move_to_todo_button() {
+        //current State todo
+        let moveToTodoButtonDidTapTestable = scheduler.createHotObservable([
+            .next(0, ())
+        ])
+        
+        let isUpdatedObserver = scheduler.createObserver(Bool.self)
+        
+        input = StateUpdateViewModel.Input(
+            moveToTodoButtonTapEvent: moveToTodoButtonDidTapTestable.asObservable(),
+            moveToDoingButtonTapEvent: Observable.empty(),
+            moveToDoneButtonTapEvent: Observable.empty()
+        )
+        
+        let output = viewModel.transform(from: input)
+        output.isSuccess
+            .subscribe(isUpdatedObserver)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertTrue(isUpdatedObserver.events.isEmpty)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_tap_move_to_doing_button() {
+        //current State todo
+        let moveToDoingButtonDidTapTestable = scheduler.createHotObservable([
+            .next(0, ())
+        ])
+        
+        let isUpdatedObserver = scheduler.createObserver(Bool.self)
+        
+        input = StateUpdateViewModel.Input(
+            moveToTodoButtonTapEvent: Observable.empty(),
+            moveToDoingButtonTapEvent: moveToDoingButtonDidTapTestable.asObservable(),
+            moveToDoneButtonTapEvent: Observable.empty()
+        )
+        
+        let output = viewModel.transform(from: input)
+        output.isSuccess
+            .subscribe(isUpdatedObserver)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(isUpdatedObserver.events,
+                       [.next(0, true)])
     }
-
+    
+    func test_tap_move_to_done_button() {
+        //current State todo
+        let moveToDoneButtonDidTapTestable = scheduler.createHotObservable([
+            .next(0, ())
+        ])
+        
+        let isUpdatedObserver = scheduler.createObserver(Bool.self)
+        
+        input = StateUpdateViewModel.Input(
+            moveToTodoButtonTapEvent: Observable.empty(),
+            moveToDoingButtonTapEvent: Observable.empty(),
+            moveToDoneButtonTapEvent: moveToDoneButtonDidTapTestable.asObservable()
+        )
+        
+        let output = viewModel.transform(from: input)
+        output.isSuccess
+            .subscribe(isUpdatedObserver)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(isUpdatedObserver.events,
+                       [.next(0, true)])
+    }
 }
