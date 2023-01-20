6c32aab69bf931fb6ac7b8d0498b39cfdd39d26f
jpush <lj845854@gmail.com>
2023-01-20 16:48:10 +0900

    feat: TaskDetailViewController 구현

--- ProjectManager/ProjectManager/Source/Presentation/KanbanBoard/TaskDetailScene/View/TaskDetailViewController.swift
+++ ProjectManager/ProjectManager/Source/Presentation/KanbanBoard/TaskDetailScene/View/TaskDetailViewController.swift
@@ -7,23 +7,116 @@
 
 import UIKit
 
-class TaskDetailViewController: UIViewController {
+import RxCocoa
+import RxSwift
 
+final class TaskDetailViewController: UIViewController {
+    private let viewModel: TaskDetailViewModel
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.numberOfLines = 0
+        label.font = .boldSystemFont(ofSize: 24)
+        
+        return label
+    }()
+    private let datePickerView: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        return datePicker
+    }()
+    private let contentLabel: UITextView = {
+        let textView = UITextView()
+        
+        textView.isEditable = false
+        textView.font = .systemFont(ofSize: 18)
+        
+        return textView
+    }()
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        return stackView
+    }()
+    private let editBarButton: UIBarButtonItem = {
+        let barButton = UIBarButtonItem(systemItem: .edit)
+        
+        return barButton
+    }()
+    private let doneBarButton: UIBarButtonItem = {
+        let barButton = UIBarButtonItem(systemItem: .done)
+        
+        return barButton
+    }()
+    
+    init(viewModel: TaskDetailViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setNavigationBarButton()
+        setUI()
+        bind()
     }
+}
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+private extension TaskDetailViewController {
+    func setNavigationBarButton() {
+        navigationItem.leftBarButtonItem = editBarButton
+        navigationItem.rightBarButtonItem = doneBarButton
     }
-    */
-
+    
+    func setUI() {
+        let spacing: CGFloat = 8
+        let safeArea = view.safeAreaLayoutGuide
+        
+        [titleLabel, datePickerView, contentLabel].forEach { view in
+            stackView.addArrangedSubview(view)
+        }
+        view.backgroundColor = .systemBackground
+        view.addSubview(stackView)
+        
+        titleLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        contentLabel.setContentHuggingPriority(.defaultLow, for: .vertical)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: spacing),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: spacing),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -spacing),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -spacing),
+        ])
+    }
+    
+    func bind() {
+        let input = TaskDetailViewModel.Input(editButtonTappedEvent: editBarButton.rx.tap.asObservable(),
+                                              doneButtonTappedEvent: doneBarButton.rx.tap.asObservable())
+        let output = viewModel.transform(with: input)
+        
+        setContents(with: output.task)
+    }
+    
+    func setContents(with task: Task) {
+        let date = Date(timeIntervalSince1970: task.deadLine)
+        
+        titleLabel.text = task.title
+        contentLabel.text = task.content
+        datePickerView.minimumDate = date
+        datePickerView.maximumDate = date
+    }
 }
