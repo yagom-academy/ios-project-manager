dd282041bf490a2531f0379de4b3bae6151d2c49
yuvinrho <yvrho@naver.com>
2023-01-23 18:26:33 +0900

    refactor: ProjectManager 리팩토링

--- ProjectManager/ProjectManager/Managers/ProjectManager.swift
+++ ProjectManager/ProjectManager/Managers/ProjectManager.swift
@@ -2,45 +2,60 @@
 //  ProjectManager.swift
 //  ProjectManager
 //
-//  Created by 노유빈 on 2023/01/17.
+//  Created by 로빈 on 2023/01/17.
 //
 
 import Foundation
 
-struct ProjectManager {
-    func create(projectList: inout [Project], project: Project) {
-        projectList.append(project)
-    }
+class ProjectManager {
+    private var ProjectListDictionary: [ProjectStatus: [Project]] = [:]
 
-    func update(projectList: inout [Project], project: Project) {
-        let index = searchIndex(projectList: &projectList, project: project)
-
-        if projectList[index].status != project.status {
-            projectList[index].status = project.status
-        }
-
-        if projectList[index].title != project.title {
-            projectList[index].title = project.title
-        }
-
-        if projectList[index].description != project.description {
-            projectList[index].description = project.description
-        }
-
-        if projectList[index].dueDate != project.dueDate {
-            projectList[index].dueDate = project.dueDate
+    func create(project: Project) {
+        if ProjectListDictionary[project.status] == nil {
+            ProjectListDictionary[project.status] = [project]
+        } else {
+            ProjectListDictionary[project.status]?.append(project)
         }
     }
 
-    func delete(projectList: inout [Project], project: Project) {
-        let index = searchIndex(projectList: &projectList, project: project)
+    func read() -> [Project] {
+        return ProjectListDictionary.values.flatMap{ $0 }
+    }
+
+    func read(status: ProjectStatus) -> [Project] {
+        return ProjectListDictionary[status] ?? []
+    }
+
+    func update(project: Project) {
+        guard let index = searchIndex(project: project) else {
+            return create(project: project)
+        }
+
+        guard var projectList = ProjectListDictionary[project.status] else {
+            fatalError("해당 프로젝트 리스트가 없음")
+        }
+
+        if projectList[index] == project {
+            return
+        }
+
+        projectList[index] = project
+        ProjectListDictionary[project.status] = projectList
+    }
+
+    func delete(project: Project) {
+        guard let index = searchIndex(project: project),
+              var projectList = ProjectListDictionary[project.status] else {
+            fatalError("삭제하려는 데이터가 없음")
+        }
 
         projectList.remove(at: index)
+        ProjectListDictionary[project.status] = projectList
     }
 
-    private func searchIndex(projectList: inout [Project], project: Project) -> Int {
-        guard let index = projectList.firstIndex(where: {$0.id == project.id}) else {
-            fatalError()
+    private func searchIndex(project: Project) -> Int? {
+        guard let index = ProjectListDictionary[project.status]?.firstIndex(where: {$0.id == project.id}) else {
+            return nil
         }
 
         return index
