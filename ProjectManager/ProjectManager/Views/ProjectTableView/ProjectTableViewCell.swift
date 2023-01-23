c1b584ecbde3342858b19732b00f0dce02daff2c
yuvinrho <yvrho@naver.com>
2023-01-24 01:07:34 +0900

    refactor: ProjectTableViewCell 데이트포매터 추가

--- ProjectManager/ProjectManager/Views/ProjectTableView/ProjectTableViewCell.swift
+++ ProjectManager/ProjectManager/Views/ProjectTableView/ProjectTableViewCell.swift
@@ -40,17 +40,24 @@ final class ProjectTableViewCell: UITableViewCell {
         return stackView
     }()
 
-    // MARK: Initialization
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .current
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+        return dateFormatter
+    }()
 
+    // MARK: Initialization
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
         selectionStyle = .none
         configureView()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     // MARK: Private Method
@@ -76,10 +83,6 @@ final class ProjectTableViewCell: UITableViewCell {
         } else {
             dueDateLabel.textColor = .black
         }
-
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = .current
-        dateFormatter.dateFormat = "yyyy.MM.dd"
 
         titleLabel.text = project.title
         descriptionLabel.text = project.description
