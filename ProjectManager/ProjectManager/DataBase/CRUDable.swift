cf479d7c740396a277c439d3e239bbecfdcbdedb
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-01-23 20:00:45 +0900

    feat: CRUDable 프로토콜 생성, CoreDataManager가 CRUDable채택 및 필수 인터페     이스 구현

---
+++ ProjectManager/ProjectManager/DataBase/CRUDable.swift
@@ -0,0 +1,23 @@
+//
+//  CRUDable.swift
+//  ProjectManager
+//
+//  Created by 써니쿠키 on 2023/01/23.
+//
+
+import Foundation
+
+protocol CRUDable {
+    
+    associatedtype DataType
+    
+    func create(_ data: DataType)
+    
+    func read() -> [DataType]
+    
+    func update(_ data: DataType)
+    
+    func delete(_ data: DataType)
+    
+    func deleteAll()
+}
---
+++ ProjectManager/ProjectManager/DataBase/CoreDataManager + CRUDable.swift
@@ -0,0 +1,113 @@
+//
+//  ProjectCoreData.swift
+//  ProjectManager
+//
+//  Created by 써니쿠키 on 2023/01/23.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataManager {
+    
+    private let containerName = "CoreData"
+    private let entityName = "ProjectCoreModel"
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("unresolved Error \(error), \(error.userInfo)")
+            }
+        }
+        
+        return container
+    }()
+    
+    private var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    private func saveContext() {
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func fetchContext(request: NSFetchRequest<ProjectCoreModel>) -> [ProjectCoreModel] {
+        var projectCoreModels: [ProjectCoreModel] = []
+        
+        do {
+            projectCoreModels = try context.fetch(request)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return projectCoreModels
+    }
+}
+
+extension CoreDataManager: CRUDable {
+    
+    typealias DataType = ProjectViewModel
+    
+    func create(_ data: ProjectViewModel) {
+        let projectCoreModel = ProjectCoreModel(context: context)
+        projectCoreModel.title = data.project.title
+        projectCoreModel.detail = data.project.detail
+        projectCoreModel.date = data.project.date
+        projectCoreModel.uuid = data.project.uuid
+        projectCoreModel.stateRawValue = data.state.rawValue
+        
+        saveContext()
+    }
+    
+    func read() -> [ProjectViewModel] {
+        let request = NSFetchRequest<ProjectCoreModel>(entityName: entityName)
+        let projectCoreModels = fetchContext(request: request)
+        let projectViewModels = projectCoreModels.map { projectCoreModel in
+            ProjectViewModel(project: Project(title: projectCoreModel.title,
+                                              detail: projectCoreModel.detail,
+                                              date: projectCoreModel.date,
+                                              uuid: projectCoreModel.uuid),
+                             state: projectCoreModel.state)}
+        
+        return projectViewModels
+    }
+    
+    func update(_ data: ProjectViewModel) {
+        let request = NSFetchRequest<ProjectCoreModel>(entityName: entityName)
+        request.predicate = .init(format: "uuid = %@", data.project.uuid.uuidString)
+        
+        guard let projectCoreModel = fetchContext(request: request).first else { return }
+        projectCoreModel.title = data.project.title
+        projectCoreModel.detail = data.project.detail
+        projectCoreModel.date = data.project.date
+        projectCoreModel.stateRawValue = data.state.rawValue
+        
+        saveContext()
+    }
+    
+    func delete(_ data: ProjectViewModel) {
+        let request = NSFetchRequest<ProjectCoreModel>(entityName: entityName)
+        request.predicate = .init(format: "uuid = %@", data.project.uuid.uuidString)
+        
+        guard let projectCoreModel = fetchContext(request: request).first else { return }
+        context.delete(projectCoreModel)
+        
+        saveContext()
+    }
+    
+    func deleteAll() {
+        let request = NSFetchRequest<ProjectCoreModel>(entityName: entityName)
+        
+        let projectCoreModels = fetchContext(request: request)
+        projectCoreModels.forEach { projectCoreModel in
+            context.delete(projectCoreModel)
+        }
+        
+        saveContext()
+    }
+}
