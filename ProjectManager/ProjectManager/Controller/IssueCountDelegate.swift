40c53606048a0a062a261aa7eaa97301f6d6697a
summercat <summerly812@gmail.com>
2023-01-23 22:56:49 +0900

    feat: declare IssueCountDelegate

---
+++ ProjectManager/ProjectManager/Controller/IssueCountDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  IssueCountDelegate.swift
+//  ProjectManager
+//
+//  Created by summercat on 2023/01/23.
+//
+
+protocol IssueCountDelegate {
+    func updateCountLabel(with count: Int)
+}
