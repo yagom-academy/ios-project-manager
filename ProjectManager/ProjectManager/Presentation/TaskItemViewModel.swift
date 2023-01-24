889a209e0f6080b855c3cea5670b17e34b4366b3
yjjem <marksim0606@gmail.com>
2023-01-25 02:08:22 +0900

    feat: add TaskItemViewModel

---
+++ ProjectManager/ProjectManager/Presentation/TaskItemViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  TaskItemViewModel.swift
+//  ProjectManager
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+final class TaskItemViewModel {
+    let task: Task
+    let title: String
+    let description: String
+    let date: Date
+    let tag: Status
+
+    init(task: Task) {
+        self.task = task
+        self.title = task.title
+        self.description = task.description
+        self.date = task.expireDate
+        self.tag = task.tag
+    }
+}
