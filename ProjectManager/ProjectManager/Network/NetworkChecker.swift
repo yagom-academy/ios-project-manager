3eacef8c7f375aa2c5d6f8a554dd7b735f293fba
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-01-25 00:15:43 +0900

    feat: NetworkCheckerDelegate, NetworkChecker 타입생성 및 내부로직 구현
    - Network 라이브러리사용

---
+++ ProjectManager/ProjectManager/Network/NetworkChecker.swift
@@ -0,0 +1,47 @@
+//
+//  NetworkChecker.swift
+//  ProjectManager
+//
+//  Created by 써니쿠키 on 2023/01/24.
+//
+
+import Foundation
+import Network
+
+protocol NetworkCheckerDelegate: AnyObject {
+    
+    func updateRemoteDataBase()
+    func informUserOfNoNetwork()
+}
+
+final class NetworkChecker {
+    
+    var delegate: NetworkCheckerDelegate?
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitor")
+    private var isConnected: Bool = true {
+        didSet {
+            if isConnected {
+                delegate?.updateRemoteDataBase()
+            } else {
+                delegate?.informUserOfNoNetwork()
+            }
+        }
+    }
+   
+    init() {
+        monitorNetworkChanges()
+    }
+    
+    private func monitorNetworkChanges() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            if path.status == .satisfied {
+                self?.isConnected = true
+            } else {
+                self?.isConnected = false
+            }
+        }
+        
+        monitor.start(queue: queue)
+    }
+}
