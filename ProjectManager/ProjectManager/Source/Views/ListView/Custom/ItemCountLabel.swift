e8fef4e43368569edb6bb7ee9bfb9a52a1cde2d6
zhilly11 <zhzh1x4@gmail.com>
2023-01-24 17:44:02 +0900

    refactor: Drawing Cycle에 따른 코드수정

--- ProjectManager/ProjectManager/Source/Views/ListView/Custom/ItemCountLabel.swift
+++ ProjectManager/ProjectManager/Source/Views/ListView/Custom/ItemCountLabel.swift
@@ -29,8 +29,8 @@ final class ItemCountLabel: UILabel {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    override func layoutSubviews() {
+        super.layoutSubviews()
         layer.cornerRadius = bounds.size.height / 2
     }
     
