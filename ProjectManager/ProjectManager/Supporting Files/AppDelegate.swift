40cd95e1a1941428447f5f371eb757f636708018
yuvinrho <yvrho@naver.com>
2023-01-26 01:02:15 +0900

    chore: FirebaseDatabase 설치
    - .gitignore 수정

--- ProjectManager/ProjectManager/Supporting Files/AppDelegate.swift
+++ ProjectManager/ProjectManager/Supporting Files/AppDelegate.swift
@@ -5,12 +5,16 @@
 // 
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+
+    FirebaseApp.configure()
+    
+    return true
+}
 
     // MARK: UISceneSession Lifecycle
 
