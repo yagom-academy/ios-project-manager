7c43f238f1b233f0c965c35ff09dff67b046bc9b
yjjem <marksim0606@gmail.com>
2023-01-25 16:38:12 +0900

    refactor(TaskCell): remove self, move function to extension

--- ProjectManager/ProjectManager/Presentation/TaskCell.swift
+++ ProjectManager/ProjectManager/Presentation/TaskCell.swift
@@ -3,11 +3,13 @@
 //  ProjectManager
 //
 //  Copyright (c) 2023 Jeremy All rights reserved.
-    
+
 
 import UIKit
 
 final class TaskCell: UITableViewCell {
+    
+    // MARK: View
     
     private var taskTitleLabel: UILabel = {
         let label = UILabel()
@@ -35,18 +37,22 @@ final class TaskCell: UITableViewCell {
         return stack
     }()
     
+    // MARK: ViewModel
+    
     var viewModel: TaskItemViewModel?
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        layout()
+        configureViewsLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+// MARK: Function
 
 extension TaskCell {
     func setupUsingViewModel() {
@@ -61,27 +67,35 @@ extension TaskCell {
         
         taskExpirationLabel.text = viewModel.date.converted()
     }
+}
+
+// MARK: Layout
+
+extension TaskCell {
     
-    private func layout() {
-        self.layer.cornerRadius = 10
-        self.clipsToBounds = true
-        self.contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+    private func configureViewsLayout() {
+        let contentViewInset = UIEdgeInsets(top: 10, left: 10,
+                                            bottom: 10, right: 10)
+        contentView.frame.inset(by: contentViewInset)
         
-        self.wholeStackView.backgroundColor = .white
-        self.wholeStackView.addArrangedSubview(taskTitleLabel)
-        self.wholeStackView.addArrangedSubview(taskDescriptionLabel)
-        self.wholeStackView.addArrangedSubview(taskExpirationLabel)
-        self.contentView.addSubview(wholeStackView)
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        wholeStackView.backgroundColor = .white
+        wholeStackView.addArrangedSubview(taskTitleLabel)
+        wholeStackView.addArrangedSubview(taskDescriptionLabel)
+        wholeStackView.addArrangedSubview(taskExpirationLabel)
+        contentView.addSubview(wholeStackView)
         
         NSLayoutConstraint.activate([
-            self.wholeStackView.topAnchor.constraint(equalTo: contentView.topAnchor,
-                                                     constant: 8),
-            self.wholeStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor,
-                                                        constant: -8),
-            self.wholeStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
-                                                         constant: 8),
-            self.wholeStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
-                                                          constant: -8),
+            wholeStackView.topAnchor.constraint(equalTo: contentView.topAnchor,
+                                                constant: 8),
+            wholeStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor,
+                                                   constant: -8),
+            wholeStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
+                                                    constant: 8),
+            wholeStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
+                                                     constant: -8),
         ])
     }
     
