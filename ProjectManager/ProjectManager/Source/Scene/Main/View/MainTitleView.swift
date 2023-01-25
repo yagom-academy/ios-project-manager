f6a113e489e4a604eb66c3bd306cb92223048fc6
DragonYG <kioswe92@gmail.com>
2023-01-25 21:15:37 +0900

    refactor: View의 initializer를 draw->init으로 변경

--- ProjectManager/ProjectManager/Source/Scene/Main/View/MainTitleView.swift
+++ ProjectManager/ProjectManager/Source/Scene/Main/View/MainTitleView.swift
@@ -22,8 +22,13 @@ final class MainTitleView: UIView {
     
     // MARK: Initializer
     
-    override func draw(_ rect: CGRect) {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: Private Methods
