868dfab4bbb295f55c22b6be2f0827c7b24f1eef
zhilly11 <zhzh1x4@gmail.com>
2023-01-27 03:37:36 +0900

    feat: NotificationName+Extension 타입 구현

---
+++ ProjectManager/ProjectManager/Resource/Extension/NotificationName+Extension.swift
@@ -0,0 +1,8 @@
+//  ProjectManager - NotificationName+Extension.swift
+//  created by zhilly on 2023/01/27
+
+import Foundation
+
+extension Notification.Name {
+    static let deleted = Notification.Name("Deleted")
+}
