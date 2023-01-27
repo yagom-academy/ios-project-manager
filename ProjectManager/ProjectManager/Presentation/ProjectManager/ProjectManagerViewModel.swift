40be62685dce09c47a6f9d0214f6c37cc6af01f0
yjjem <marksim0606@gmail.com>
2023-01-28 02:38:13 +0900

    style(ProjectManagerViewController): change rx, return conventions

--- ProjectManager/ProjectManager/Presentation/ProjectManager/ProjectManagerViewModel.swift
+++ ProjectManager/ProjectManager/Presentation/ProjectManager/ProjectManagerViewModel.swift
@@ -3,40 +3,45 @@
 //  ProjectManager
 //
 //  Copyright (c) 2023 Jeremy All rights reserved.
-    
+
 
 import Foundation
 import RxSwift
 
 final class ProjectManagerViewModel: ViewModelType {
     private var useCase: TaskUseCaseType
-
+    
     init(useCase: TaskUseCaseType) {
         self.useCase = useCase
     }
 }
 
 extension ProjectManagerViewModel {
-
+    
     func transform(input: Input) -> Output {
-
+        
         let update = input.update.flatMapLatest {
-            return self.useCase.getTasks()
+            return self.useCase
+                .getTasks()
                 .map { $0.map { TaskItemViewModel(task: $0) } }
         }
         
-        let delete = input.delete.flatMapLatest { item in
-            return self.useCase.delete(task: item.task)
-        }
-
+        let delete = input.delete
+            .flatMapLatest { item in
+                return self.useCase
+                    .delete(task: item.task)
+            }
+        
         let todoItems = update.map { $0.filter { $0.status == .todo } }
         let doingItems = update.map { $0.filter { $0.status == .doing } }
         let doneItems = update.map { $0.filter { $0.status == .done } }
-
-        return Output(todoItems: todoItems,
-                      doingItems: doingItems,
-                      doneItems: doneItems,
-                      deletedItem: delete)
+        
+        return Output(
+            todoItems: todoItems,
+            doingItems: doingItems,
+            doneItems: doneItems,
+            deletedItem: delete
+        )
     }
 }
 
@@ -45,7 +50,6 @@ extension ProjectManagerViewModel {
         let update: Observable<Void>
         let delete: Observable<TaskItemViewModel>
     }
-
     struct Output {
         let todoItems: Observable<[TaskItemViewModel]>
         let doingItems: Observable<[TaskItemViewModel]>
