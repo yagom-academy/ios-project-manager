d1676bc4644d314ba3d801053a0a974c2933738a
junho <junhoL1046@gmail.com>
2023-01-28 17:34:48 +0900

    refactor: 컨벤션 수정

--- ProjectManager/ProjectManager/Models/UserNotificationManager.swift
+++ ProjectManager/ProjectManager/Models/UserNotificationManager.swift
@@ -10,6 +10,20 @@ import UserNotifications
 final class UserNotificationManager {
     private let userNotificationCenter = UNUserNotificationCenter.current()
 
+    private func makeNotificationContent(title: String, body: String) -> UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        return content
+    }
+
+    private func makeNotificationTrigger(date: Date) -> UNCalendarNotificationTrigger {
+        let calendar = Calendar.current
+        var dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+        dateComponents.hour = Constants.notificationHour
+        return UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+    }
+
     func registerNotification(identifier: UUID, title: String, dueDate: Date) {
         guard dueDate.isEarlierThanToday() == false else { return }
         let content = makeNotificationContent(title: title, body: dueDate.localizedDateString())
@@ -29,19 +43,5 @@ final class UserNotificationManager {
     func updateNotification(identifier: UUID, title: String, dueDate: Date) {
         removeNotification(with: identifier)
         registerNotification(identifier: identifier, title: title, dueDate: dueDate)
-    }
-
-    private func makeNotificationContent(title: String, body: String) -> UNMutableNotificationContent {
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.body = body
-        return content
-    }
-
-    private func makeNotificationTrigger(date: Date) -> UNCalendarNotificationTrigger {
-        let calendar = Calendar.current
-        var dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
-        dateComponents.hour = Constants.notificationHour
-        return UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
     }
 }
