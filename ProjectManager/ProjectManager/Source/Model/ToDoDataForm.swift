7e958672c86c46a1d8d979947c1594a08aeb0859
zhilly11 <zhzh1x4@gmail.com>
2023-01-30 09:35:04 +0900

    feat: ToDoDataForm 타입 생성

---
+++ ProjectManager/ProjectManager/Source/Model/ToDoDataForm.swift
@@ -0,0 +1,12 @@
+//  ProjectManager - ToDoDataForm.swift
+//  created by zhilly on 2023/01/27
+
+import Foundation
+
+struct ToDoDataForm {
+    
+    var title: String
+    var body: String
+    var deadline: Date
+    var state: ToDoState
+}
