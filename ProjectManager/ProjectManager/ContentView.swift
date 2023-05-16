49da4e505a6c509bf257e393918398a9f8120d7e
kaki <ycg02109@naver.com>
2023-05-16 18:59:24 +0900

    refactor: SwiftUI 프로젝트 생성 및 타켓 버전 iOS 16.0 설정

---
+++ ProjectManager/ProjectManager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ProjectManager
+//
+//  Created by kaki, 릴라 on 2023/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
