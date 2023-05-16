e628a6bb030d09cef09d0adf5271bcd4d9b9de2c
Jun <59365211+juun97@users.noreply.github.com>
2023-05-16 19:18:52 +0900

    feat : Firebase sdk 추가

---
+++ ProjectManager/ProjectManager/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ProjectManager
+//
+//  Created by kaki, 릴라 on 2023/05/16.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
