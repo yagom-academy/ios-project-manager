4edaaac438e07ad975b1a4e4b896cd285a13c5a8
minsu3007 <minsu3007@gmail.com>
2023-05-17 13:32:39 +0900

    feat: initalize TaskTableViewCell

---
+++ ProjectManager/ProjectManager/MainScene/View/TaskTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TaskTableViewCell.swift
+//  ProjectManager
+//
+//  Created by 강민수 on 2023/05/17.
+//
+
+import UIKit
+
+final class TaskTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
