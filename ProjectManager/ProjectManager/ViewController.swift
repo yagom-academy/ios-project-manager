b62955b29850e53566d126a6369aecab5d85ccf0
kimseongj <ksj8277@naver.com>
2023-05-17 14:05:18 +0900

    feat : StackView 구현

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -8,11 +8,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(frame: view.bounds)
+        stackView.backgroundColor = .brown
+        stackView.distribution = .fillEqually
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = stackView
+        configureNavigationBar()
     }
-
-
+    
+    private func configureNavigationBar() {
+        self.title = "ProjectManager"
+    }
 }
 
+class CollectionView: UICollectionView {
+    
+}
+
+struct collectionViewContent {
+    let title: String
+    let detail: String
+    let expirationDate: String
+}
