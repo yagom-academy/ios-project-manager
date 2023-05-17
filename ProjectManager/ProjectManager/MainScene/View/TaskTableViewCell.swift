3be09721c6a32e86b42cecdaaa27e1597e50a04e
minsu3007 <minsu3007@gmail.com>
2023-05-17 15:28:38 +0900

    feat: TableViewCell updateText method

--- ProjectManager/ProjectManager/MainScene/View/TaskTableViewCell.swift
+++ ProjectManager/ProjectManager/MainScene/View/TaskTableViewCell.swift
@@ -47,6 +47,12 @@ final class TaskTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func updateText(by task: Task) {
+        titleLabel.text = task.title
+        descriptionLabel.text = task.description
+        dateLabel.text = task.date.description
+    }
 }
 
 // MARK: UI
