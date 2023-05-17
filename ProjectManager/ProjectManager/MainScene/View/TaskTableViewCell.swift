dbf51f2f1575e33b36312ab8dd053733206d7d83
minsu3007 <minsu3007@gmail.com>
2023-05-17 13:39:36 +0900

    feat: TaskTableViewCell Properties

--- ProjectManager/ProjectManager/MainScene/View/TaskTableViewCell.swift
+++ ProjectManager/ProjectManager/MainScene/View/TaskTableViewCell.swift
@@ -8,7 +8,36 @@
 import UIKit
 
 final class TaskTableViewCell: UITableViewCell {
-
+    
+    private let titleLabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.numberOfLines = 1
+        
+        return label
+    }()
+    private let descriptionLabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 14, weight: .light)
+        label.numberOfLines = 3
+        
+        return label
+    }()
+    private let dateLabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 12)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
