40d65b3f3ec54d55d19154663b1238a5a11fcc20
minsu3007 <minsu3007@gmail.com>
2023-05-17 13:47:34 +0900

    feat: tableViewCell configureUI

--- ProjectManager/ProjectManager/MainScene/View/TaskTableViewCell.swift
+++ ProjectManager/ProjectManager/MainScene/View/TaskTableViewCell.swift
@@ -40,9 +40,32 @@ final class TaskTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: UI
+extension TaskTableViewCell {
+    private func configureUI() {
+        let mainStackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel, dateLabel])
+        
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.axis = .vertical
+        mainStackView.spacing = 2
+        mainStackView.setCustomSpacing(4, after: descriptionLabel)
+        
+        contentView.addSubview(mainStackView)
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            mainStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            mainStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
     }
 }
