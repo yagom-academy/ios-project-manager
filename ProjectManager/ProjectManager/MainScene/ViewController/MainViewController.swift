4284bc86227686d7dc1e4cba1e7f6c37aaf9bf59
minsu3007 <minsu3007@gmail.com>
2023-05-17 14:10:05 +0900

    feat: tableView layout

--- ProjectManager/ProjectManager/MainScene/ViewController/MainViewController.swift
+++ ProjectManager/ProjectManager/MainScene/ViewController/MainViewController.swift
@@ -7,22 +7,44 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-
+    
+    private let todoTableView = UITableView()
+    private let doingTableView = UITableView()
+    private let doneTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationView()
         configureView()
+        configureTableView()
     }
 }
 
 extension MainViewController {
     private func configureNavigationView() {
-        navigationController?.title = "ProjectManager"
+        navigationItem.title = "Project Manager"
         navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .add)
     }
     
     private func configureView() {
         view.backgroundColor = .systemBackground
+    }
+    
+    private func configureTableView() {
+        let tableStackView = UIStackView(arrangedSubviews: [todoTableView, doingTableView, doneTableView])
+        
+        tableStackView.translatesAutoresizingMaskIntoConstraints = false
+        tableStackView.spacing = 0
+        tableStackView.distribution = .fillEqually
+        
+        view.addSubview(tableStackView)
+        
+        NSLayoutConstraint.activate([
+            tableStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
