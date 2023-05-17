88bdd627676cbaef807999f3d1bf2f6e18197c5f
seunghyunCheon <csh7215@naver.com>
2023-05-17 15:34:27 +0900

    feat: Todo모델 생성

--- ProjectManager/ProjectManager/MainView/MainViewController.swift
+++ ProjectManager/ProjectManager/MainView/MainViewController.swift
@@ -1,16 +1,15 @@
 //
-//  ProjectManager - ViewController.swift
+//  ProjectManager - MainViewController.swift
 //  Created by yagom. 
 //  Copyright © yagom. All rights reserved.
 // 
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 }
-
---
+++ ProjectManager/ProjectManager/Model/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  ProjectManager
+//
+//  Created by Brody, Rowan on 2023/05/17.
+//
+import Foundation
+
+struct Todo {
+    let title: String
+    let date: Date
+    let body: String
+}
