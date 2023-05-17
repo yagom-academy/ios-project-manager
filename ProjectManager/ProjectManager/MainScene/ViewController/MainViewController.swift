051a2cfb57fc9bb7968c12af968a6ad5b7705c18
minsu3007 <minsu3007@gmail.com>
2023-05-17 21:08:23 +0900

    feat: configureDatasource method

--- ProjectManager/ProjectManager/MainScene/ViewController/MainViewController.swift
+++ ProjectManager/ProjectManager/MainScene/ViewController/MainViewController.swift
@@ -8,6 +8,10 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
+    private var todoCollectionView: UICollectionView?
+    
+    private var datasource: UICollectionViewDiffableDataSource<TaskState, Task>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,6 +23,15 @@ final class MainViewController: UIViewController {
 
 extension MainViewController {
     private func configureDatasource() {
+        guard let collectionView = todoCollectionView else { return }
+        
+        let cellRegistration = UICollectionView.CellRegistration<TaskListCell, Task> { cell, indexPath, itemIdentifier in
+            cell.updateText(by: itemIdentifier)
+        }
+        
+        datasource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
     }
 }
 
@@ -62,5 +75,7 @@ extension MainViewController {
             collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+        
+        todoCollectionView = collectionView
     }
 }
