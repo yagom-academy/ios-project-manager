407f19880b5817ff0ff5d6476fdddbde8671ea1d
ROWAN <kimkj93@naver.com>
2023-05-17 15:46:08 +0900

    feat: ToDoTableViewController, Cell 생성

---
+++ ProjectManager/ProjectManager/MainView/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  ProjectManager
+//
+//  Created by Brody, Rowan on 2023/05/17.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+    static let identifier = "ToDoCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ProjectManager/ProjectManager/MainView/ToDoTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ToDoTableViewController.swift
+//  ProjectManager
+//
+//  Created by Brody, Rowan on 2023/05/17.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    var toDoList: [Todo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ToDoTableViewCell.identifier, for: indexPath) as? ToDoTableViewCell
+        else { return UITableViewCell() }
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+}
