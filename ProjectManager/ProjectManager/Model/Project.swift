6daa9015d9d320b12be1fdd534f76c6553dc635f
Rhode-park <jinah.park.16@gmail.com>
2023-05-17 20:16:50 +0900

    feat: Project 구조체 구현

---
+++ ProjectManager/ProjectManager/Model/Project.swift
@@ -0,0 +1,11 @@
+//
+//  ProjectManager - Project.swift
+//  Created by Rhode.
+//  Copyright © yagom. All rights reserved.
+//
+
+struct Project {
+    var title: String
+    var body: String
+    var date: Date
+}
