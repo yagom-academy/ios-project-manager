381baf8ad1c49a7e350f4122ff096809979293cf
Rhode-park <jinah.park.16@gmail.com>
2023-05-17 17:08:51 +0900

    feat: contentStackView, titleTextField, datePicker, bodyTextview 구현 및 세부 설정

--- ProjectManager/ProjectManager/Controller/AddProjectViewController.swift
+++ ProjectManager/ProjectManager/Controller/AddProjectViewController.swift
@@ -7,14 +7,49 @@
 import UIKit
 
 class AddProjectViewController: UIViewController {
+    private let contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    private let titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Title"
+        textField.layer.borderWidth = 0.5
+        textField.layer.borderColor = CGColor(gray: 0.5, alpha: 0.5)
+        
+        return textField
+    }()
+    
+    private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        return datePicker
+    }()
+    
+    private let bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.layer.borderWidth = 0.5
+        textView.layer.borderColor = CGColor(gray: 0.5, alpha: 0.5)
+        
+        return textView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureContentStackView()
+        configureConstraint()
     }
     
     private func configureUI() {
-        view.backgroundColor = .systemGray6
+        view.backgroundColor = .white
+        navigationController?.navigationBar.backgroundColor = UIColor(displayP3Red: 200/255, green: 200/255, blue: 200/255, alpha: 0.5)
+
         title = "TODO"
         
         let editButton = UIBarButtonItem(barButtonSystemItem: .edit,
@@ -36,4 +71,25 @@ class AddProjectViewController: UIViewController {
     private func doneEditingProject() {
         print("doneEditingProject")
     }
+    
+    private func configureContentStackView() {
+        view.addSubview(contentStackView)
+        contentStackView.addArrangedSubview(titleTextField)
+        contentStackView.addArrangedSubview(datePicker)
+        contentStackView.addArrangedSubview(bodyTextView)
+    }
+    
+    private func configureConstraint() {
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            contentStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            contentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bodyTextView.topAnchor.constraint(equalTo: datePicker.bottomAnchor),
+            bodyTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bodyTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bodyTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+
 }
