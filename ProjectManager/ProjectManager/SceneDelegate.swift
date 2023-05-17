51b7f8b849aaf7bb0b60346b6f41b68fd2def8dd
yijiye <jiyee4013@naver.com>
2023-05-17 10:31:19 +0900

    feat: todoTableView, Navigation 기본 구현 및 PlusTodoViewController 파일 생성

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: TodoListViewController())
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
     }
---
+++ ProjectManager/ProjectManager/TodoListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ProjectManager - TodoListViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class TodoListViewController: UIViewController {
+    
+    private let todoTableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpTableView()
+        configureNavigationBar()
+    }
+    
+    private func setUpTableView() {
+        view.backgroundColor = .white
+        view.addSubview(todoTableView)
+        let safeArea = view.safeAreaLayoutGuide
+        
+        todoTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            todoTableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            todoTableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            todoTableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            todoTableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+    
+    private func configureNavigationBar() {
+        let title = "Project Manager"
+        let plusButton = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(plusButtonTapped))
+        
+        navigationItem.title = title
+        navigationItem.rightBarButtonItem = plusButton
+    }
+    
+    @objc func plusButtonTapped() {
+        let plusTodoViewController = PlusTodoViewController()
+        present(plusTodoViewController, animated: false)
+    }
+}
