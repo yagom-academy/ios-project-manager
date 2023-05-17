750d76f087eb62a7617f92da90e577b6c7fe9b9b
Rhode-park <jinah.park.16@gmail.com>
2023-05-17 14:34:26 +0900

    feat: storyboard 삭제 및 관련 셋팅

--- ProjectManager/ProjectManager/ProjectManagerViewController.swift
+++ ProjectManager/ProjectManager/ProjectManagerViewController.swift
@@ -1,16 +1,16 @@
 //
-//  ProjectManager - ViewController.swift
+//  ProjectManager - ProjectManagerViewController.swift
 //  Created by yagom. 
 //  Copyright © yagom. All rights reserved.
 // 
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProjectManagerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
