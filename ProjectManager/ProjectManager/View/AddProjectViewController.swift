9e34d1fa3d27ca294e6944eb8a96087d3919eef9
parkmuri <smxhmu@naver.com>
2023-05-18 19:46:21 +0900

    feat: MainVC의 navigationController 설정 및 addProjectButton 생성

---
+++ ProjectManager/ProjectManager/View/AddProjectViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddProjectViewController.swift
+//  ProjectManager
+//
+//  Created by 무리 on 2023/05/18.
+//
+
+import UIKit
+
+final class AddProjectViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ ProjectManager/ProjectManager/View/MainListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProjectManager - MainListViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    private lazy var collectionView = UICollectionView(frame: .zero)
+    // let viewModel = ViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigation()
+    }
+    
+    private func configureNavigation() {
+        view.backgroundColor = .white
+        title = NameSpace.projectName
+        
+        let addProjectButton = UIBarButtonItem(barButtonSystemItem: .add,
+                                               target: self,
+                                               action: #selector(addProject))
+        
+        navigationItem.rightBarButtonItem = addProjectButton
+    }
+    
+    @objc
+    private func addProject() {
+        let addProjectViewController = AddProjectViewController()
+        navigationController?.present(addProjectViewController, animated: true)
+    }
+}
+
+private enum NameSpace {
+    static let projectName = "Project Manager"
+}
