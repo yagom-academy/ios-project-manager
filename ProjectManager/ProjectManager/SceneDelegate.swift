acea91cfc6d43ee865ec209b94d0dd0b02dbe912
HarryHyeon <hharry0728@gmail.com>
2023-05-18 15:32:49 +0900

    feat: MainViewController에서 3개의 뷰컨트롤러 화면을 띄우는 기능 추가

--- ProjectManager/ProjectManager/SceneDelegate.swift
+++ ProjectManager/ProjectManager/SceneDelegate.swift
@@ -13,7 +13,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let rootViewController = TaskListViewController()
+        let rootViewController = MainViewController()
         let navigationController = UINavigationController(rootViewController: rootViewController)
         
         window = UIWindow(frame: UIScreen.main.bounds)
--- ProjectManager/ProjectManager/TaskList/View/MainViewController.swift
+++ ProjectManager/ProjectManager/TaskList/View/MainViewController.swift
@@ -8,8 +8,56 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    private let todoViewController = TaskListViewController()
+    private let doingViewController = TaskListViewController()
+    private let doneViewController = TaskListViewController()
+    
+    private let mainStackView = {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        view.addSubview(mainStackView)
+        view.backgroundColor = .systemBackground
+        
+        addChildViews()
+        addSubViews()
+        setupMainStackViewConstraints()
+    }
+    
+    private func addChildViews() {
+        self.addChild(todoViewController)
+        self.addChild(doingViewController)
+        self.addChild(doneViewController)
+    }
+    
+    private func addSubViews() {
+        mainStackView.addArrangedSubview(todoViewController.view)
+        mainStackView.addArrangedSubview(doingViewController.view)
+        mainStackView.addArrangedSubview(doneViewController.view)
+    }
+    
+    private func setupMainStackViewConstraints() {
+        let safe = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: safe.topAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: safe.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: safe.trailingAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: safe.bottomAnchor)
+        ])
     }
 }
