2a20e9875be68332025d7901be976ba329f398ea
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-05-18 15:37:09 +0900

    feat: cell Label 설정 및 오토레이아웃 구현

--- ProjectManager/ProjectManager/TodoTableViewCell.swift
+++ ProjectManager/ProjectManager/TodoTableViewCell.swift
@@ -11,19 +11,65 @@ class ToDoTableViewCell: UITableViewCell {
     
     static let identifier = "ToDoTableViewCell"
     
-   let titleLabel: UILabel = {
+    let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.textColor = .lightGray
+        label.numberOfLines = 3
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
         
         return label
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-
+        configureCellUI()
+        setUpLabel()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("")
+    }
+    
+    
+    func configureCellUI() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(titleLabel)
+        self.addSubview(descriptionLabel)
+        self.addSubview(dateLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -16),
+            
+            dateLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            dateLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            dateLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10)
+            ])
+    }
+    
+    func setUpLabel() {
+        titleLabel.text = "title"
+        descriptionLabel.text = "description description description description description description description description description description description description description description description description description description description description description description description description description description description "
+        dateLabel.text = "date"
     }
 }
