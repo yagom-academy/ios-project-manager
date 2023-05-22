a67c1f63f86102e63e72ea59a23942a24b06f502
seunghyunCheon <csh7215@naver.com>
2023-05-22 16:00:23 +0900

    feat: textView layer설정

--- ProjectManager/ProjectManager/DetailView/View/Controller/DetailViewController.swift
+++ ProjectManager/ProjectManager/DetailView/View/Controller/DetailViewController.swift
@@ -17,8 +17,8 @@ final class DetailViewController: UIViewController {
         textField.layer.borderColor = UIColor.systemGray6.cgColor
         textField.layer.masksToBounds = false
         textField.layer.shadowColor = UIColor.black.cgColor
-        textField.layer.shadowOffset = CGSize(width: 0, height: 2)
-        textField.layer.shadowOpacity = 0.5
+        textField.layer.shadowOffset = CGSize(width: 0, height: 4)
+        textField.layer.shadowOpacity = 0.3
         textField.layer.shadowRadius = 1.0
         
         return textField
@@ -27,12 +27,29 @@ final class DetailViewController: UIViewController {
     private let datePicker = {
         let datePicker = UIDatePicker()
         datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
         return datePicker
     }()
     
     private let bodyTextView = {
         let textView = UITextView()
+        textView.font = .preferredFont(forTextStyle: .title3)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
         return textView
+    }()
+    
+    private let shadowView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.borderWidth = 1.0
+        view.layer.borderColor = UIColor.systemGray6.cgColor
+        view.layer.masksToBounds = false
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 0, height: 4)
+        view.layer.shadowOpacity = 0.3
+        view.layer.shadowRadius = 1.0
+        return view
     }()
     
     private let stackView = {
@@ -45,6 +62,7 @@ final class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         configureNavigationBar()
+        configureShadowView()
         configureStackView()
         configureRootView()
     }
@@ -73,6 +91,17 @@ final class DetailViewController: UIViewController {
         view.addSubview(naviBar)
     }
     
+    private func configureShadowView() {
+        shadowView.addSubview(bodyTextView)
+        
+        NSLayoutConstraint.activate([
+            bodyTextView.leadingAnchor.constraint(equalTo: shadowView.leadingAnchor),
+            bodyTextView.trailingAnchor.constraint(equalTo: shadowView.trailingAnchor),
+            bodyTextView.topAnchor.constraint(equalTo: shadowView.topAnchor),
+            bodyTextView.bottomAnchor.constraint(equalTo: shadowView.bottomAnchor),
+        ])
+    }
+    
     @objc
     private func tapDoneButton() {
         
@@ -81,7 +110,7 @@ final class DetailViewController: UIViewController {
     private func configureStackView() {
         stackView.addArrangedSubview(titleTextfield)
         stackView.addArrangedSubview(datePicker)
-        stackView.addArrangedSubview(bodyTextView)
+        stackView.addArrangedSubview(shadowView)
     }
     
     private func configureRootView() {
@@ -94,7 +123,7 @@ final class DetailViewController: UIViewController {
             stackView.leadingAnchor.constraint(equalTo: safe.leadingAnchor, constant: 10),
             stackView.trailingAnchor.constraint(equalTo: safe.trailingAnchor, constant: -10),
             stackView.topAnchor.constraint(equalTo: safe.topAnchor, constant: 60),
-            stackView.bottomAnchor.constraint(equalTo: safe.bottomAnchor, constant: -20)
+            stackView.bottomAnchor.constraint(equalTo: safe.bottomAnchor, constant: -20),
         ])
     }
 }
