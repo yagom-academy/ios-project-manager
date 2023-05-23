92f47f3a97c1c6ce586615ddb9bde971dd816e81
seunghyunCheon <csh7215@naver.com>
2023-05-23 23:33:21 +0900

    feat: 셀 Delete UI 설정

--- ProjectManager/ProjectManager/MainView/ViewModel/MainCollectionViewModel.swift
+++ ProjectManager/ProjectManager/MainView/ViewModel/MainCollectionViewModel.swift
@@ -125,7 +125,18 @@ extension MainCollectionViewModel {
         let taskViewModel = TaskCellViewModel(task: task)
         
         cell.provide(taskViewModel)
+        cell.delegate = self
         
         return cell
+    }
+}
+
+extension MainCollectionViewModel: SwipeableCollectionViewCellDelegate {
+    func tapVisibleContainerView(inCell cell: UICollectionViewCell) {
+        print("visible Action")
+    }
+    
+    func tapHiddenContainerView(inCell cell: UICollectionViewCell) {
+        print("delete Action")
     }
 }
