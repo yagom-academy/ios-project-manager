fc09dfcb3fa2f9dc6fc55a2e73b0cf8c56070374
ROWAN <kimkj93@naver.com>
2023-05-23 10:28:57 +0900

    refactor: Combine UIControlPublisher UIControler Extension 내부로 위치 변경 및 내용 수정

--- ProjectManager/ProjectManager/Utility/UIContorl+Combine.swift
+++ ProjectManager/ProjectManager/Utility/UIContorl+Combine.swift
@@ -7,45 +7,68 @@
 
 import UIKit
 import Combine
+//
+//final class UIControlSubscription<SubscriberType: Subscriber, Control: UIControl>: Subscription where SubscriberType.Input == Control {
+//    private var subscriber: SubscriberType?
+//    private let control: Control
+//
+//    init(subscriber: SubscriberType, control: Control, event: UIControl.Event) {
+//        self.subscriber = subscriber
+//        self.control = control
+//        control.addTarget(self, action: #selector(eventHandler), for: event)
+//    }
+//
+//    func request(_ demand: Subscribers.Demand) { }
+//
+//    func cancel() {
+//        subscriber = nil
+//    }
+//
+//    @objc
+//    private func eventHandler() {
+//        _ = subscriber?.receive(control)
+//    }
+//}
+//
+//struct UIControlPublisher: Publisher {
+//
+//    typealias Output = UIControl
+//    typealias Failure = Never
+//
+//    let control: UIControl
+//    let controlEvents: UIControl.Event
+//
+//    init(control: UIControl, events: UIControl.Event) {
+//        self.control = control
+//        self.controlEvents = events
+//    }
+//
+//    func receive<S>(subscriber: S) where S : Subscriber, S.Failure == UIControlPublisher.Failure, S.Input == UIControlPublisher.Output {
+//        let subscription = UIControlSubscription(subscriber: subscriber, control: control, event: controlEvents)
+//        subscriber.receive(subscription: subscription)
+//    }
+//}
 
-final class UIControlSubscription<SubscriberType: Subscriber, Control: UIControl>: Subscription where SubscriberType.Input == Control {
-    private var subscriber: SubscriberType?
-    private let control: Control
-    
-    init(subscriber: SubscriberType, control: Control, event: UIControl.Event) {
-        self.subscriber = subscriber
-        self.control = control
-        control.addTarget(self, action: #selector(eventHandler), for: event)
-    }
-    
-    func request(_ demand: Subscribers.Demand) { }
-    
-    func cancel() {
-        subscriber = nil
-    }
-    
-    @objc
-    private func eventHandler() {
-        _ = subscriber?.receive(control)
-    }
-}
-
-struct UIControlPublisher: Publisher {
-
-    typealias Output = UIControl
-    typealias Failure = Never
-
-    let control: UIControl
-    let controlEvents: UIControl.Event
-
-    init(control: UIControl, events: UIControl.Event) {
-        self.control = control
-        self.controlEvents = events
-    }
-    
-    func receive<S>(subscriber: S) where S : Subscriber, S.Failure == UIControlPublisher.Failure, S.Input == UIControlPublisher.Output {
-        let subscription = UIControlSubscription(subscriber: subscriber, control: control, event: controlEvents)
-        subscriber.receive(subscription: subscription)
+extension UIControl {
+    struct EventPublisher: Publisher {
+        typealias Output = Void
+        typealias Failure = Never
+        
+        private var control: UIControl
+        private var event: Event
+        
+        func receive<S: Subscriber>(subscriber: S) where S.Input == Output, S.Failure == Failure {
+            let subscription = EventSubscription<S>()
+            subscription.target = subscriber
+            
+            subscriber.receive(subscription: subscription)
+            
+            control.addTarget(
+                subscription,
+                action: #selector(subscription.trigger),
+                for: event
+            )
+        }
     }
 }
 
