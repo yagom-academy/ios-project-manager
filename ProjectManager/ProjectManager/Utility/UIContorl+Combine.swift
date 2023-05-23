d05ec0e2fd5a628ca2e9253513508ed37c6b4e02
seunghyunCheon <csh7215@naver.com>
2023-05-23 10:34:36 +0900

    feat: UIControl EventSubscription, EventPublisher 반환함수 정의

--- ProjectManager/ProjectManager/Utility/UIContorl+Combine.swift
+++ ProjectManager/ProjectManager/Utility/UIContorl+Combine.swift
@@ -54,8 +54,8 @@ extension UIControl {
         typealias Output = Void
         typealias Failure = Never
         
-        private var control: UIControl
-        private var event: Event
+        fileprivate var control: UIControl
+        fileprivate var event: Event
         
         func receive<S: Subscriber>(subscriber: S) where S.Input == Output, S.Failure == Failure {
             let subscription = EventSubscription<S>()
@@ -72,10 +72,37 @@ extension UIControl {
     }
 }
 
-protocol CombineCompatible { }
-extension UIControl: CombineCompatible { }
-extension CombineCompatible where Self: UIControl {
-    func publisher(for events: UIControl.Event) -> UIControlPublisher {
-        return UIControlPublisher(control: self, events: events)
+private extension UIControl {
+    class EventSubscription<Target: Subscriber>: Subscription where Target.Input == Void {
+        
+        var target: Target?
+        
+        func request(_ demand: Subscribers.Demand) { }
+        
+        func cancel() {
+            target = nil
+        }
+        
+        @objc
+        func trigger() {
+            target?.receive(())
+        }
     }
 }
+
+extension UIControl {
+    func publisher(for event: Event) -> EventPublisher {
+        EventPublisher(
+            control: self,
+            event: event
+        )
+    }
+}
+
+//protocol CombineCompatible { }
+//extension UIControl: CombineCompatible { }
+//extension CombineCompatible where Self: UIControl {
+//    func publisher(for events: UIControl.Event) -> UIControlPublisher {
+//        return UIControlPublisher(control: self, events: events)
+//    }
+//}
