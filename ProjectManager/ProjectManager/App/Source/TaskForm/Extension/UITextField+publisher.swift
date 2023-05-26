f632a0ef26abc257349a03af1634cd3214101f20
HarryHyeon <hharry0728@gmail.com>
2023-05-26 14:14:26 +0900

    style: 코드 개행 추가

--- ProjectManager/ProjectManager/App/Source/TaskForm/Extension/UITextField+publisher.swift
+++ ProjectManager/ProjectManager/App/Source/TaskForm/Extension/UITextField+publisher.swift
@@ -10,7 +10,8 @@ import Combine
 
 extension UITextField {
     var textPublisher: AnyPublisher<String, Never> {
-        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification,
+                                             object: self)
             .compactMap { $0.object as? UITextField }
             .map { $0.text ?? "" }
             .eraseToAnyPublisher()
--- ProjectManager/ProjectManager/App/Source/TaskForm/Extension/UITextView+publisher.swift
+++ ProjectManager/ProjectManager/App/Source/TaskForm/Extension/UITextView+publisher.swift
@@ -10,7 +10,8 @@ import Combine
 
 extension UITextView {
     var textPublisher: AnyPublisher<String, Never> {
-        NotificationCenter.default.publisher(for: UITextView.textDidChangeNotification, object: self)
+        NotificationCenter.default.publisher(for: UITextView.textDidChangeNotification,
+                                             object: self)
             .compactMap { $0.object as? UITextView }
             .map { $0.text ?? "" }
             .eraseToAnyPublisher()
