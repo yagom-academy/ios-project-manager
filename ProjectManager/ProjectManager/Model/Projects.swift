096832a79c4de031199a960604a054c95521bb93
Rhode-park <jinah.park.16@gmail.com>
2023-05-28 16:42:31 +0900

    refactor: 가짜데이터 삭제

--- ProjectManager/ProjectManager/Model/Projects.swift
+++ ProjectManager/ProjectManager/Model/Projects.swift
@@ -11,24 +11,5 @@ final class Projects {
     
     private init() { }
     
-    var list: [Project] = {
-        var list = [Project]()
-        
-        for i in 1...1 {
-            let project = Project(title: "프로젝트 \(i)", body: "프로젝트 \(i)에 대한 설명", date: Date()-TimeInterval(i*10000), status: .todo)
-            list.append(project)
-        }
-        
-        for i in 1...11 {
-            let project = Project(title: "프로젝트 \(i)", body: "프로젝트 \(i)에 대한 설명", date: Date()-TimeInterval(i*10000), status: .doing)
-            list.append(project)
-        }
-        
-        for i in 1...111 {
-            let project = Project(title: "프로젝트 \(i)", body: "프로젝트 \(i)에 대한 설명", date: Date()-TimeInterval(i*10000), status: .done)
-            list.append(project)
-        }
-        
-        return list
-    }()
+    var list = [Project]()
 }
