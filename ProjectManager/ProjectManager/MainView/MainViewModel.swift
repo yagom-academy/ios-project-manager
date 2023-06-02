616dd3c0e3c35cb7a13a61157dc9bf7e2d9bba9d
seunghyunCheon <csh7215@naver.com>
2023-06-02 16:27:59 +0900

    refactor: MainViewModel 3개의 [Plan] 프로퍼티에서 하나의 프로퍼티로 변경

--- ProjectManager/ProjectManager/MainView/MainViewModel.swift
+++ ProjectManager/ProjectManager/MainView/MainViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 final class MainViewModel {
-    private var planListDictionary: [WorkState: [Plan]] = [:]
+    private var planList: [Plan] = []
     private var viewModelDictionary: [WorkState: PlanListViewModel] = [:]
     private let service: PlanStorageService
     
@@ -25,13 +25,7 @@ final class MainViewModel {
     }
     
     func fetchPlanList() {
-        let todoList = service.fetchPlanList().filter { $0.workState == .todo }
-        let doingList = service.fetchPlanList().filter { $0.workState == .doing }
-        let doneList = service.fetchPlanList().filter { $0.workState == .done }
-        
-        planListDictionary[.todo] = todoList
-        planListDictionary[.doing] = doingList
-        planListDictionary[.done] = doneList
+        planList = service.fetchPlanList()
     }
     
     func distributePlan() {
@@ -45,45 +39,43 @@ final class MainViewModel {
     }
     
     private func providePlanList(workState: WorkState) {
-        guard let planList = planListDictionary[workState] else {
-            return
-        }
-        
-        viewModelDictionary[workState]?.planList = planList
+        viewModelDictionary[workState]?.planList = planList.filter { $0.workState == workState }
     }
 }
 
 extension MainViewModel: PlanListViewModelDelegate {
     func create(plan: Plan) {
+        planList.append(plan)
         service.create(plan: plan)
     }
     
     func update(plan: Plan) {
+        guard let targetIndex = planList.firstIndex(of: plan) else { return }
+        
+        planList[targetIndex] = plan
         service.update(plan: plan)
     }
     
     func deletePlan(id: UUID) {
+        guard let targetIndex = planList.firstIndex(where: { $0.id == id }) else { return }
+        
+        planList.remove(at: targetIndex)
         service.deletePlan(id: id)
     }
 }
 
 extension MainViewModel: ChangeWorkStateViewModelDelegate {
     func changeWorkState(of plan: Plan, to workState: WorkState) {
-        guard var planList = planListDictionary[plan.workState],
-              var targetList = planListDictionary[workState],
-              let targetIndex = planList.firstIndex(of: plan) else { return }
-        
-        let target = planList.remove(at: targetIndex)
-        targetList.append(target)
-        
-        planListDictionary[plan.workState] = planList
-        planListDictionary[workState] = targetList
-        
+        guard let targetIndex = planList.firstIndex(of: plan) else { return }
+        planList[targetIndex].workState = workState
         providePlanList(workState: plan.workState)
         providePlanList(workState: workState)
         
-        viewModelDictionary[plan.workState]?.planDeleted.send((planList.count, plan.id))
-        viewModelDictionary[workState]?.planCreated.send((targetList.count))
+        let deletedPlanListCount = planList.filter { $0.workState == plan.workState }.count
+        let targetPlanListCount = planList.filter { $0.workState == workState }.count
+        
+        viewModelDictionary[plan.workState]?.planDeleted.send((deletedPlanListCount, plan.id))
+        viewModelDictionary[workState]?.planCreated.send((targetPlanListCount))
         service.changeWorkState(planID: plan.id, with: workState)
     }
 }
