d172f8901aef25f86c91a41bf38b1469284e1547
Whales <Whales.w.Jin@gmail.com>
2023-09-20 17:03:20 +0900

    ✨ Feat: TodoView 구현

--- ProjectManager/ProjectManager/Presentation/View/TodoView.swift
+++ ProjectManager/ProjectManager/Presentation/View/TodoView.swift
@@ -7,20 +7,44 @@
 
 import SwiftUI
 
-
 struct TodoView: View {
     @ObservedObject var vm = ViewModel()
     
+    init(vm: ViewModel = ViewModel()) {
+        self.vm = vm
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.systemBackground
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
     var body: some View {
         NavigationView {
-            
+            HStack {
+                ListView(tasks: vm.todos, title: "TODO")
+                ListView(tasks: vm.doings, title: "DOING")
+                ListView(tasks: vm.dones, title: "DONE")
+            }
+            .navigationTitle("Project Manager")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        vm.createTask()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
+        .navigationViewStyle(.stack)
     }
 }
 
 struct TodoView_Previews: PreviewProvider {
     static var previews: some View {
-        TodoView()
+        TodoView(vm: TodoView.ViewModel(todos: TodoView.ViewModel.dummyTodos, doings: TodoView.ViewModel.dummyDoings, dones: TodoView.ViewModel.dummyDones))
     }
 }
-
--- ProjectManager/ProjectManager/Presentation/ViewModel/TodoViewModel.swift
+++ ProjectManager/ProjectManager/Presentation/ViewModel/TodoViewModel.swift
@@ -4,15 +4,42 @@
 //
 //  Created by Minsup & Whales on 2023/09/20.
 //
-
-import Foundation
+import SwiftUI
 
 extension TodoView {
     final class ViewModel: ObservableObject {
-        @Published var tasks: [Task] = []
+        @Published var todos: [Task]
+        @Published var doings: [Task]
+        @Published var dones: [Task]
         
         func createTask() {
-            tasks.append(Task(title: "test", content: "sdfsdf", date: .now))
-        }        
+            todos.append(Task(title: "TEST", content: "test", date: .now))
+        }
+        
+        static let dummyTodos: [Task] = [
+            Task(title: "제목1", content: "내용", date: .now),
+            Task(title: "제목2", content: "내용", date: .now),
+            Task(title: "제목3", content: "내용", date: .now),
+            Task(title: "제목4", content: "내용", date: .now),
+            Task(title: "제목5", content: "내용", date: .now),
+        ]
+        
+        static let dummyDoings = [
+            Task(title: "제목1", content: "내용", date: .now),
+            Task(title: "제목2", content: "내용", date: .now),
+        ]
+        
+        static let dummyDones = [
+            Task(title: "제목1", content: "내용", date: .now),
+            Task(title: "제목2", content: "내용", date: .now),
+            Task(title: "제목3", content: "내용", date: .now),
+        ]
+        
+        init(todos: [Task] = [], doings: [Task] = [], dones: [Task] = []) {
+            self.todos = todos
+            self.doings = doings
+            self.dones = dones
+        }
+        
     }
 }
