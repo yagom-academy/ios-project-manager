9ee0370aba6a60d40ea0518c591cbb3fbbf947d8
DasanKim <tempzipp@gmail.com>
2023-09-20 16:41:19 +0900

    ♻️Refactor: AppDelegate 파일 분리

--- ProjectManager/ProjectManager/ProjectManagerApp.swift
+++ ProjectManager/ProjectManager/ProjectManagerApp.swift
@@ -6,16 +6,6 @@
 //
 
 import SwiftUI
-import FirebaseCore
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
 
 @main
 struct ProjectManagerApp: App {
