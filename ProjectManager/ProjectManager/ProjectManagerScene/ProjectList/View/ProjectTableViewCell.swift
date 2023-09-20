a33c41ec98f49defab286efc5013171aed89f896
h-suo <phstoo1021@naver.com>
2023-09-20 17:12:55 +0900

    ✨Feat: ProjectTableViewCell 생성 및 UI 구현

---
+++ ProjectManager/ProjectManager/ProjectManagerScene/ProjectList/View/ProjectTableViewCell.swift
@@ -0,0 +1,104 @@
+//
+//  ProjectTableViewCell.swift
+//  ProjectManager
+//
+//  Created by Erick on 2023/09/20.
+//
+
+import UIKit
+
+final class ProjectTableViewCell: UITableViewCell {
+
+    // MARK: - Static Property
+    static let identifier = String(describing: ProjectTableViewCell.self)
+    
+    // MARK: - Private Property
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        
+        return stackView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        
+        return label
+    }()
+    
+    private let bodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = UIColor.systemGray
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private let deadlineLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        
+        return label
+    }()
+    
+    // MARK: - Life Cycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - layout SubViews
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(
+            by: UIEdgeInsets(top: 0, left: 0, bottom: 8, right: 0)
+        )
+    }
+}
+
+// MARK: - Configure UI
+extension ProjectTableViewCell {
+    private func configureUI() {
+        configureView()
+        configureStackView()
+        configureLayout()
+    }
+    
+    private func configureView() {
+        self.backgroundColor = UIColor.clear
+        self.selectionStyle = .none
+        
+        contentView.addSubview(stackView)
+        contentView.backgroundColor = UIColor.systemBackground
+    }
+    
+    private func configureStackView() {
+        [titleLabel, bodyLabel, deadlineLabel].forEach {
+            stackView.addArrangedSubview($0)
+        }
+    }
+    
+    private func configureLayout() {
+        let safe = contentView.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: safe.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: safe.trailingAnchor, constant: -8),
+            stackView.topAnchor.constraint(equalTo: safe.topAnchor, constant: 8),
+            stackView.bottomAnchor.constraint(equalTo: safe.bottomAnchor, constant: -8)
+        ])
+    }
+}
