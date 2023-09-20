60345d3327baf168871505a54e2c7488dc507c94
idinaloq <idinaloq@icloud.com>
2023-09-20 20:34:18 +0900

    feat: 코드베이스 사용을 위한 설정 추가, 파일 수정

--- ProjectManager/DBManager/DBManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DBManager.swift
-//  ProjectManager
-//
-//  Created by JSB on 2023/09/20.
-//
-
-import Foundation
---
+++ ProjectManager/ProjectManager/DBManager/SQLiteManager.swift
@@ -0,0 +1,12 @@
+//
+//  SQLiteManager.swift
+//  ProjectManager
+//
+//  Created by JSB on 2023/09/20.
+//
+
+import Foundation
+
+class SQLiteManager {
+    static let sqlite: SQLiteManager = SQLiteManager()
+}
