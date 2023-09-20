dcf31e96bb6841121cb61c757a381942a6599c2f
agilestarskim <agilestars@kakao.com>
2023-09-20 15:37:33 +0900

    :sparkles: Feat: 클린아키텍처 + MVVM 구조생성

---
+++ ProjectManager/ProjectManager/Domain/UseCases/TaskUseCases.swift
@@ -0,0 +1,10 @@
+//
+//  TaskUseCases.swift
+//  ProjectManager
+//
+//  Created by Minsup & Whales on 2023/09/20.
+//
+
+import Foundation
+
+final class TaskUseCases { }
---
+++ ProjectManager/ProjectManager/Presentation/View/TodoView.swift
@@ -0,0 +1,26 @@
+//
+//  TodoView.swift
+//  ProjectManager
+//
+//  Created by Minsup & Whales on 2023/09/20.
+//
+
+import SwiftUI
+
+
+struct TodoView: View {
+    @ObservedObject var vm = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+}
+
+struct TodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoView()
+    }
+}
+
