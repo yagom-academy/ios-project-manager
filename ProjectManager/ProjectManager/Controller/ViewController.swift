28b5da62004ddaf48efcd0e79c03aace2f012366
idinaloq <idinaloq@icloud.com>
2023-09-22 18:46:52 +0900

    feat: TODO, DOING, DONE 타이틀뷰 추가

--- ProjectManager/ProjectManager/Controller/ViewController.swift
+++ ProjectManager/ProjectManager/Controller/ViewController.swift
@@ -7,12 +7,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let TODOTitleView: TitleView = {
+        let titleView: TitleView = TitleView()
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        titleView.backgroundColor = .red
+        
+        return titleView
+    }()
+    
+    private let DOINGTitleView: TitleView = {
+        let titleView: TitleView = TitleView()
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        titleView.backgroundColor = .red
+        
+        return titleView
+    }()
+    
+    private let DONETitleView: TitleView = {
+        let titleView: TitleView = TitleView()
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        titleView.backgroundColor = .red
+        
+        return titleView
+    }()
+    
     private let leftTableView: UITableView = {
         let tableView: UITableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(TableViewCell.self, forCellReuseIdentifier: "leftTableView")
         tableView.tag = 1
+        
+        
+        tableView.backgroundColor = .systemGray5
         
         return tableView
     }()
@@ -22,6 +49,7 @@ class ViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(TableViewCell.self, forCellReuseIdentifier: "centerTableView")
         tableView.tag = 2
+        tableView.backgroundColor = .systemGray5
         
         return tableView
     }()
@@ -31,6 +59,7 @@ class ViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(TableViewCell.self, forCellReuseIdentifier: "rightTableView")
         tableView.tag = 3
+        tableView.backgroundColor = .systemGray5
         
         return tableView
     }()
@@ -44,13 +73,16 @@ class ViewController: UIViewController {
     }
     
     private func configureView() {
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemGray6
         leftTableView.delegate = self
         leftTableView.dataSource = self
         centerTableView.delegate = self
         centerTableView.dataSource = self
         rightTableView.delegate = self
         rightTableView.dataSource = self
+        view.addSubview(TODOTitleView)
+        view.addSubview(DOINGTitleView)
+        view.addSubview(DONETitleView)
         view.addSubview(leftTableView)
         view.addSubview(centerTableView)
         view.addSubview(rightTableView)
@@ -61,6 +93,7 @@ class ViewController: UIViewController {
         let plusBotton: UIBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .done, target: self, action: #selector(tappedPlusButton))
         navigationItem.rightBarButtonItem = plusBotton
         navigationItem.title = "Project Manager"
+        
     }
     
 
@@ -72,23 +105,39 @@ class ViewController: UIViewController {
         let viewWidth = view.safeAreaLayoutGuide.layoutFrame.width / 3.0
         
         NSLayoutConstraint.activate([
-            leftTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            leftTableView.topAnchor.constraint(equalTo: TODOTitleView.bottomAnchor, constant: 8),
             leftTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
 //            leftTableView.trailingAnchor.constraint(equalTo: centerTableView.leadingAnchor),
             leftTableView.widthAnchor.constraint(equalToConstant: viewWidth),
             leftTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
 
             
-            centerTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            centerTableView.topAnchor.constraint(equalTo: DOINGTitleView.bottomAnchor, constant: 8),
             centerTableView.leadingAnchor.constraint(equalTo: leftTableView.trailingAnchor),
 //            centerTableView.trailingAnchor.constraint(equalTo: rightTableView.leadingAnchor),
             centerTableView.widthAnchor.constraint(equalToConstant: viewWidth),
             centerTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             
-            rightTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            rightTableView.topAnchor.constraint(equalTo: DONETitleView.bottomAnchor, constant: 8),
             rightTableView.leadingAnchor.constraint(equalTo: centerTableView.trailingAnchor),
             rightTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            rightTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            rightTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            TODOTitleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            TODOTitleView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            TODOTitleView.widthAnchor.constraint(equalToConstant: viewWidth),
+            TODOTitleView.bottomAnchor.constraint(equalTo: leftTableView.topAnchor),
+            
+            DOINGTitleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            DOINGTitleView.leadingAnchor.constraint(equalTo: leftTableView.trailingAnchor),
+            DOINGTitleView.widthAnchor.constraint(equalToConstant: viewWidth),
+            DOINGTitleView.bottomAnchor.constraint(equalTo: centerTableView.topAnchor),
+            
+            DONETitleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            DONETitleView.leadingAnchor.constraint(equalTo: centerTableView.trailingAnchor),
+            DONETitleView.widthAnchor.constraint(equalToConstant: viewWidth),
+            DONETitleView.bottomAnchor.constraint(equalTo: rightTableView.topAnchor)
+            
         ])
     }
 }
@@ -107,12 +156,14 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         return TableViewCell()
     }
     
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         
     }
+
     
 //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 //        return 10
---
+++ ProjectManager/ProjectManager/View/TitleView.swift
@@ -0,0 +1,52 @@
+//
+//  TitleView.swift
+//  ProjectManager
+//
+//  Created by idinaloq on 2023/09/22.
+//
+
+import UIKit
+
+class TitleView: UIView {
+    private let titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "타이틀"
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let titleStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        titleStackView.addArrangedSubview(titleLabel)
+        addSubview(titleStackView)
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            titleStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            titleStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            titleStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            titleStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            titleStackView.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+}
