d1af0ff6ae30da0bd498583af555b09fbbf1e370
Moon <1tjen@naver.com>
2023-09-25 07:58:47 +0900

    refactor: ViewController에서 ListViewController로 네이밍 수정

--- ProjectManager/ProjectManager/View/ViewController.swift
+++ ProjectManager/ProjectManager/View/ViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
