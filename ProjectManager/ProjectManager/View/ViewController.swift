4749fcca1c63f6abe94c799a0e905d3cb6c73f84
Moon <1tjen@naver.com>
2023-09-25 07:59:30 +0900

    feat: ListViewController에 tableView 추가

--- ProjectManager/ProjectManager/View/ViewController.swift
+++ ProjectManager/ProjectManager/View/ViewController.swift
@@ -7,9 +7,63 @@
 import UIKit
 
 final class ListViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .orange
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .yellow
+        
+        configureUI()
     }
+}
+
+// MARK: - Configure UI
+extension ListViewController {
+    private func configureUI() {
+        addSubviews()
+        setUpTableViewConstraints()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setUpTableViewConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor
+                .constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor
+                .constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.topAnchor
+                .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor
+                .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - Table View Data Source
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+// MARK: - Table View Delegate
+extension ListViewController: UITableViewDelegate {
+    
 }
