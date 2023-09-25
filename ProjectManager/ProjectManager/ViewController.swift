acf49b1970d7b230938e0974255a1f80d50aa829
YaRkyungmin <kkgm94@naver.com>
2023-09-25 19:08:04 +0900

    🩹chore: created 수정 및 컨벤션 수정

--- ProjectManager/ProjectManager/ViewController.swift
+++ ProjectManager/ProjectManager/ViewController.swift
@@ -1,18 +1,14 @@
 //
 //  ProjectManager - ViewController.swift
-//  Created by yagom. 
+//  Created by kyungmin
 //  Copyright © yagom. All rights reserved.
 // 
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
