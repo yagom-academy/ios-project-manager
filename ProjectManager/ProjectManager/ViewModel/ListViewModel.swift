5b4b6ec3999d3dc00f9e3cb01166a35efd8f0047
Moon <1tjen@naver.com>
2023-09-25 20:40:41 +0900

    refactor: ListHeader와 바인딩할 수 있도록 수정
    - todoList가 변경되면 ListHeader와 바인딩할 count가 바뀔 수 있도록 구현
    - ListHeader와 바인딩할 count 변수 생성

--- ProjectManager/ProjectManager/ViewModel/ListViewModel.swift
+++ ProjectManager/ProjectManager/ViewModel/ListViewModel.swift
@@ -8,16 +8,46 @@
 import Foundation
 
 final class ListViewModel {
-    private let todoList = [
-        Todo(title: "1", description: "111", deadline: Date()),
-        Todo(title: "2", description: "222", deadline: Date()),
-        Todo(title: "3", description: "333", deadline: Date()),
-        Todo(title: "4", description: "444", deadline: Date()),
-        Todo(title: "5", description: "555", deadline: Date()),
-        Todo(title: "6", description: "666", deadline: Date())
-    ]
+    // ListViewController에서 ListCell의 ListCellViewModel로 전달하게될 모델의 배열
+    private var todoList: [Todo] {
+        didSet {
+            count = String(todoList.count)
+        }
+    }
     
-    func getTodoList() -> [Todo] {
-        return todoList
+    // ListHeader의 contentAmountLabel와 바인딩할 변수
+    var count: String {
+        didSet {
+            bindCount?(self)
+        }
+    }
+    // ListHeader의 contentAmountLabel와 바인딩하기 위한 클로저
+    private var bindCount: ((ListViewModel) -> Void)?
+    
+    init() {
+        todoList = [
+            Todo(title: "1", description: "111", deadline: Date()),
+            Todo(title: "2", description: "222", deadline: Date()),
+            Todo(title: "3", description: "333", deadline: Date()),
+            Todo(title: "4", description: "444", deadline: Date()),
+            Todo(title: "5", description: "555", deadline: Date()),
+            Todo(title: "6", description: "666", deadline: Date())
+        ]
+        
+        count = String(todoList.count)
+    }
+    
+    // ListViewController에서 ListCell의 ListCellViewModel로 전달하기 위해 인덱스로 찾아 리턴
+    func getTodo(index: Int) -> Todo {
+        return todoList[index]
+    }
+    
+    func countTodo() -> Int {
+        return todoList.count
+    }
+    
+    func bindCount(_ handler: @escaping (ListViewModel) -> Void) {
+        handler(self)
+        bindCount = handler
     }
 }
