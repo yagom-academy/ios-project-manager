77ca031f037af683e767d7e77ddf98ea360fe7e4
agilestarskim <agilestars@kakao.com>
2023-09-27 01:05:08 +0900

    :bug: Fix: 키보드로 인해 TextEditor가 가리는 문제 해결

--- ProjectManager/ProjectManager/Presentation/Kanban/TaskForm/TaskFormView.swift
+++ ProjectManager/ProjectManager/Presentation/Kanban/TaskForm/TaskFormView.swift
@@ -9,80 +9,98 @@ import SwiftUI
 
 struct TaskFormView: View {    
     @EnvironmentObject private var kanbanViewModel: KanbanViewModel
-    @ObservedObject private var taskFormViewModel: TaskFormViewModel
+    @EnvironmentObject private var keyboard: KeyboardManager
+    
+    @ObservedObject private var taskFormViewModel: TaskFormViewModel    
     
     @FocusState private var textEditorIsFocused: Bool
-    @Namespace var textEditor
+    @Namespace private var textEditor
+    
     init(title: String, size: CGSize) {
         self.taskFormViewModel = TaskFormViewModel(formTitle: title, formSize: size)
     }
     
     var body: some View {
-        ScrollViewReader { proxy in
-            NavigationStack {
+        NavigationStack {
+            ScrollViewReader { proxy in
                 ScrollView {
                     VStack {
-                        TextField("제목을 입력하세요(필수)", text: $taskFormViewModel.title)
-                            .shadowBackground()
-                        DatePicker(
-                            "날짜를 입력하세요",
-                            selection: $taskFormViewModel.date,
-                            displayedComponents: .date
-                        )
-                        .datePickerStyle(.wheel)
-                        .labelsHidden()
-                        
-                                                
-                        TextEditor(text: $taskFormViewModel.content)
-                            .focused($textEditorIsFocused)
-                            .shadowBackground()
-                            .frame(height: taskFormViewModel.formHeight * 0.5)
-                            .id(textEditor)
-                            .onChange(of: textEditorIsFocused) { isFocused in
-                                if isFocused {
-                                    withAnimation {
-                                        proxy.scrollTo(textEditor, anchor: .top)
-                                    }
-                                } else {
-                                    withAnimation {
-                                        proxy.scrollTo(textEditor, anchor: .bottom)
-                                    }
-                                }
-                            }
-                        
-                        if textEditorIsFocused {
-                            VStack {}
-                                .frame(height: 400)
-                        }
-                        
+                        titleTextField
+                        datePicker
+                        contentTextEditor(proxy)
+                        EmptySpaceForKeyboard
                     }
                     .padding()
                 }
-                .ignoresSafeArea(.keyboard, edges: .bottom)
-                .toolbarBackground(.visible, for: .navigationBar)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button("Cancel") {
-                            kanbanViewModel.setFormVisible(false)
-                        }
-                    }
-                    
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("Done") {
-                            let task = taskFormViewModel.task
-                            
-                            kanbanViewModel.create(task)
-                            kanbanViewModel.setFormVisible(false)
-                        }
-                        .disabled(taskFormViewModel.title.isEmpty)
-                    }
-                }
-                .navigationTitle(taskFormViewModel.formTitle)
-                .navigationBarTitleDisplayMode(.inline)
             }
+            .ignoresSafeArea(.keyboard, edges: .bottom)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbar { toolbarItems }
+            .navigationTitle(taskFormViewModel.formTitle)
+            .navigationBarTitleDisplayMode(.inline)
         }
         .cornerRadius(10)
         .frame(width: taskFormViewModel.formWidth, height: taskFormViewModel.formHeight)
+    }
+    
+    var titleTextField: some View {
+        TextField("제목을 입력하세요(필수)", text: $taskFormViewModel.title)
+            .shadowBackground()
+    }
+    
+    var datePicker: some View {
+        DatePicker(
+            "날짜를 입력하세요",
+            selection: $taskFormViewModel.date,
+            displayedComponents: .date
+        )
+        .datePickerStyle(.wheel)
+        .labelsHidden()
+    }
+    
+    func contentTextEditor(_ proxy: ScrollViewProxy) -> some View {
+        TextEditor(text: $taskFormViewModel.content)
+            .focused($textEditorIsFocused)
+            .shadowBackground()
+            .frame(height: taskFormViewModel.formHeight * 0.5)
+            .id(textEditor)
+            .onChange(of: textEditorIsFocused) { IsFocused in
+                if IsFocused {
+                    withAnimation {
+                        proxy.scrollTo(textEditor, anchor: UnitPoint(x: 0, y: 0.05))
+                    }
+                } else {
+                    withAnimation {
+                        proxy.scrollTo(textEditor, anchor: .bottom)
+                    }
+                }
+            }
+    }
+    
+    @ViewBuilder
+    var EmptySpaceForKeyboard: some View {
+        if keyboard.isVisible {
+            Spacer(minLength: keyboard.height)
+        }
+    }
+    
+    @ToolbarContentBuilder
+    var toolbarItems: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            Button("Cancel") {
+                kanbanViewModel.setFormVisible(false)
+            }
+        }
+        
+        ToolbarItem(placement: .navigationBarTrailing) {
+            Button("Done") {
+                let task = taskFormViewModel.task
+                
+                kanbanViewModel.create(task)
+                kanbanViewModel.setFormVisible(false)
+            }
+            .disabled(taskFormViewModel.title.isEmpty)
+        }
     }
 }
 
