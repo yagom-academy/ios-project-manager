df03ccf32d7c0eeffbcf9bed8a45d57e06bc9327
h-suo <phstoo1021@naver.com>
2023-09-28 20:14:04 +0900

    ✨Feat: MainViewModel 생성

---
+++ ProjectManager/ProjectManager/Presentation/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewModel.swift
+//  ProjectManager
+//
+//  Created by Erick on 2023/09/22.
+//
+
+import UIKit
+
+struct MainViewModelActions {
+    let showAddProject: () -> Void
+}
+
+protocol MainViewModelInput {
+    func viewDidLoad()
+    func tapAddButton()
+}
+
+protocol MainViewModelOutput {
+    var navigationTitle: String { get }
+}
+
+typealias MainViewModel = MainViewModelInput & MainViewModelOutput
+
+final class DefaultMainViewModel: MainViewModel {
+    
+    // MARK: - Private Property
+    private let projectUseCase: ProjectUseCase
+    private let actions: MainViewModelActions
+    
+    // MARK: - Life Cycle
+    init(projectUseCase: ProjectUseCase,
+         actions: MainViewModelActions
+    ) {
+        self.projectUseCase = projectUseCase
+        self.actions = actions
+    }
+    
+    // MARK: - OUTPUT
+    var navigationTitle: String { "ProjectManager" }
+}
+
+// MARK: - INPUT View event methods
+extension DefaultMainViewModel {
+    func viewDidLoad() { }
+    
+    func tapAddButton() {
+        actions.showAddProject()
+    }
+}
