eb6c675b60422ba8d87535fd01c1488c96ce6839
karen <karenrodanthe@icloud.com>
2023-10-03 16:25:14 +0900

    feat: MainViewController 레이아웃 구현

--- ProjectManager/ProjectManager/App/SceneDelegate.swift
+++ ProjectManager/ProjectManager/App/SceneDelegate.swift
@@ -12,9 +12,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let mainViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = mainViewController
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
--- ProjectManager/ProjectManager/Controller/MainViewController.swift
+++ ProjectManager/ProjectManager/Controller/MainViewController.swift
@@ -7,6 +7,22 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    private let todoListViewController = ListViewController(listKind: .todo)
+    private let doingListViewController = ListViewController(listKind: .doing)
+    private let doneListViewController = ListViewController(listKind: .done)
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.backgroundColor = .systemBackground
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,14 +32,39 @@ final class MainViewController: UIViewController {
     }
     
     private func configureUI() {
+        [todoListViewController, doingListViewController, doneListViewController].forEach {
+            stackView.addArrangedSubview($0.view)
+        }
         
+        view.addSubview(stackView)
     }
     
     private func setUpConstraints() {
-        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
     private func setUpViewController() {
         view.backgroundColor = .systemBackground
+        navigationItem.title = "Project Manager"
+        
+        let rightAddButtonAction: UIAction = .init {
+            action in self.didTappedRightAddButton()
+        }
+        
+        navigationItem.rightBarButtonItem = .init(systemItem: .add, primaryAction: rightAddButtonAction)
+    }
+}
+
+extension MainViewController {
+    private func didTappedRightAddButton() {
+        let taskViewController = TaskViewController()
+        let navigationController = UINavigationController(rootViewController: taskViewController)
+        
+        present(navigationController, animated: true)
     }
 }
