5d609895f608fded1efb6eb46085297243755f47
DasanKim <tempzipp@gmail.com>
2023-10-07 22:27:38 +0900

    ♻️Refactor: MemoDetail, NewMemo를 SheetView으로 통합

---
+++ ProjectManager/ProjectManager/Views/Sheet/SheetView.swift
@@ -0,0 +1,59 @@
+//
+//  SheetView.swift
+//  ProjectManager
+//
+//  Created by Mary & Dasan on 2023/10/04.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject var modelData: ModelData
+    @ObservedObject var viewModel: SheetViewModel
+        
+    var body: some View {
+        NavigationView {
+            VStack {
+                TitleTextField(content: $viewModel.memo.title)
+                DeadlinePicker(date: $viewModel.memo.deadline)
+                BodyTextField(content: $viewModel.memo.body)
+            }
+            .sheetBackground()
+            .disabled(!viewModel.canEditable)
+            .navigationTitle(viewModel.memo.category.description)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(
+                leading: viewModel.canEditable ? AnyView(cancelButton) : AnyView(editButton),
+                trailing: doneButton
+            )
+        }
+        .navigationViewStyle(.stack)
+    }
+    
+    var editButton: some View {
+        Button("Edit") {
+            viewModel.canEditable.toggle()
+        }
+    }
+    
+    var cancelButton: some View {
+        Button("Cancel") {
+            dismiss()
+            viewModel.canEditable = true
+        }
+    }
+    
+    var doneButton: some View {
+        Button("Done") {
+            modelData.saveMemo(viewModel.memo)
+            dismiss()
+        }
+    }
+}
+
+struct SheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetView(viewModel: SheetViewModel(memo: ModelData().newMemo, canEditable: true))
+    }
+}
