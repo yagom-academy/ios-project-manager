245c6943350f23d0fbe77c8fb329e6a687ba44fc
DasanKim <tempzipp@gmail.com>
2023-10-07 22:29:46 +0900

    ✨Feat: isOverdue에 따른 deadline 글자색 변경

--- ProjectManager/ProjectManager/Views/Memo/MemoRow.swift
+++ ProjectManager/ProjectManager/Views/Memo/MemoRow.swift
@@ -14,6 +14,13 @@ struct MemoRow: View {
         self.memo = memo
     }
     
+    var isOverdue: Bool {
+        if (memo.category == .toDo || memo.category == .doing) && (Calendar.current.compare(memo.deadline, to: .now, toGranularity: .day) == .orderedAscending) {
+            return true
+        }
+        return false
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(memo.title)
@@ -25,6 +32,7 @@ struct MemoRow: View {
                 .lineLimit(3)
             
             Text(memo.deadline.formatted(date: .numeric, time: .omitted))
+                .foregroundColor(isOverdue ? .red : .primary)
         }
     }
 }
