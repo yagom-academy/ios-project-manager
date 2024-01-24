2ba240dee45de4ae6a82b9b976918b70a947a1d1
Nova <woohyeon3382@naver.com>
2024-01-24 21:03:09 +0900

    Refactor: MainViewController 로 네이밍 변경

---
+++ ProjectManager/ProjectManager/View/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ProjectManager - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    private let todoTableView = ListViewController(scheduleType: .todo)
+    private let doingTableView = ListViewController(scheduleType: .doing)
+    private let doneTableView = ListViewController(scheduleType: .done)
+    
+    private let stackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 10
+        stackView.backgroundColor = .lightGray
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupStackView()
+        setupStackViewConstraint()
+    }
+    
+    private func setupStackView() {
+        stackView.addArrangedSubview(todoTableView.view)
+        stackView.addArrangedSubview(doingTableView.view)
+        stackView.addArrangedSubview(doneTableView.view)
+        view.addSubview(stackView)
+    }
+    
+    private func setupStackViewConstraint() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+        ])
+    }
+    
+}
