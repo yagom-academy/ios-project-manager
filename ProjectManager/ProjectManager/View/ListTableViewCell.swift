45e01ef9c507ff2db4f658dafe4f4f39f55dc11b
Nova <woohyeon3382@naver.com>
2024-01-25 18:05:08 +0900

    Feat: ListTableViewCell 파일 추가

---
+++ ProjectManager/ProjectManager/View/ListTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListTableViewCell.swift
+//  ProjectManager
+//
+//  Created by Toy on 1/25/24.
+//
+
+import UIKit
+
+final class ListTableViewCell: UITableViewCell {
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
