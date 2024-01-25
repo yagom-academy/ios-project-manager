c7b136cb50c5910e25ef36eefdc2e7c01ef42e79
Nova <woohyeon3382@naver.com>
2024-01-25 18:25:34 +0900

    Feat: 생성자 구현 및 identifier 구현

--- ProjectManager/ProjectManager/View/ListTableViewCell.swift
+++ ProjectManager/ProjectManager/View/ListTableViewCell.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 final class ListTableViewCell: UITableViewCell {
+    // MARK: - Property
+    static let identifier = String(describing: ListViewController.self)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
     }
-
+    
+    // MARK: - Helper
+    
 }
