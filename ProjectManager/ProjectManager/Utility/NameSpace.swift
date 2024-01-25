0ccb92a300c81ddc79af3c015ba6f3cb80414219
Nova <woohyeon3382@naver.com>
2024-01-25 17:58:17 +0900

    Feat: NameSpace 에 색상 추가

--- ProjectManager/ProjectManager/Utility/NameSpace.swift
+++ ProjectManager/ProjectManager/Utility/NameSpace.swift
@@ -5,9 +5,12 @@
 //  Created by Toy on 1/24/24.
 //
 
+import UIKit
+
 enum NameSpace {
     static let todo = "TODO"
     static let doing = "DOING"
     static let done = "DONE"
     static let title = "Project Manager"
+    static let headerAndFooterColor = UIColor(red: 0xE5 / 255.0, green: 0xE5 / 255.0, blue: 0xE5 / 255.0, alpha: 1.0)
 }
