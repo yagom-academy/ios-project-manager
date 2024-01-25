45f69b9b84c594cb60616b7e2f6e29a2d608959e
Nova <woohyeon3382@naver.com>
2024-01-25 16:42:23 +0900

    Feat: HeaderView 오토레이아웃 구현

--- ProjectManager/ProjectManager/View/HeaderView.swift
+++ ProjectManager/ProjectManager/View/HeaderView.swift
@@ -11,17 +11,42 @@ final class HeaderView: UIView {
     // MARK: - Property
     private let titleLabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 30)
         return label
+    }()
+    
+    private let stackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
     
     init(frame: CGRect, schedule: Schedule) {
         self.titleLabel.text = schedule.discription
         super.init(frame: frame)
+        setupStackView()
+        setupStackViewConstraint()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper
+    private func setupStackView() {
+        stackView.addArrangedSubview(titleLabel)
+        self.addSubview(stackView)
+    }
+    
+    private func setupStackViewConstraint() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15)
+        ])
     }
     
 }
